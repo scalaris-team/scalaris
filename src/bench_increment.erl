1222076927f87b61ab72706e9aafd6b81fe9618a
schuett <schuett@zib.de>
2008-10-13 10:47:42 +0000

    - added missing file (runs concurrent i++ transactions)

---
+++ src/bench_increment.erl
@@ -0,0 +1,132 @@
+%  Copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+%%%-------------------------------------------------------------------
+%%% File    : bench_increment.erl
+%%% Author  : Thorsten Schuett <schuett@zib.de>
+%%% Description : i++ benchmark
+%%%
+%%% Created :  25 Aug 2008 by Thorsten Schuett <schuett@zib.de>
+%%%-------------------------------------------------------------------
+-module(bench_increment).
+
+-author('schuett@zib.de').
+-vsn('$Id$ ').
+
+-import(transstore.transaction_api, [read2/2, write2/3]).
+
+-export([start/0, bench/0, bench_raw/0, process/3]).
+
+init() ->
+    Pid = spawn(fun () ->
+			process_dictionary:start_link_for_unittest(), 
+			boot_sup:start_link(), 
+			timer:sleep(11000),
+			admin:add_nodes(3),
+			timer:sleep(6010000) 
+		end),
+    timer:sleep(22000),
+    io:format("init done~n", []),
+    Pid.
+
+tini(Pid) ->
+    exit(Pid, kill).
+
+start() ->
+    Pid = init(),
+    increment_test(),
+    tini(Pid),
+    init:stop().
+
+make_tfun(Key) ->
+    fun (TransLog)->
+	    {Counter, TransLog1} = read2(TransLog, Key),
+	    TransLog2 = write2(TransLog1, Key, Counter + 1),
+	    {{ok, ok}, TransLog2}
+    end.
+
+process(Parent, Key, Count) ->
+    SuccessFun = fun(X) ->
+                         {success, X}
+                 end,
+    FailureFun = fun(Reason)->
+                         {failure, Reason}
+                 end,
+    process_iter(Parent, make_tfun(Key), Count, SuccessFun, FailureFun, 0).
+
+process_iter(Parent, _Key, 0, _SuccessFun, _FailureFun, AbortCount) ->
+    Parent ! {done, AbortCount};
+process_iter(Parent, TFun, Count, SuccessFun, FailureFun, AbortCount) ->
+    case transstore.transaction_api:do_transaction(TFun, SuccessFun, FailureFun) of
+	{success, {commit, _Y}} ->
+	    process_iter(Parent, TFun, Count - 1, SuccessFun, FailureFun, AbortCount);
+	{failure, abort} ->
+	    process_iter(Parent, TFun, Count, SuccessFun, FailureFun, AbortCount + 1);
+	X ->
+	    ct:pal("~p~n", [X])
+    end.
+
+bench() ->
+    bench_raw().
+
+bench_raw() ->    
+    Self = self(),
+    Count = 1000,
+    Key = "i",
+    spawn(fun () -> process(Self, Key, Count) end),
+    spawn(fun () -> process(Self, Key, Count) end),
+    spawn(fun () -> process(Self, Key, Count) end),
+    spawn(fun () -> process(Self, Key, Count) end),
+    spawn(fun () -> process(Self, Key, Count) end),
+    spawn(fun () -> process(Self, Key, Count) end),
+    wait_for_done(6),
+    Count.
+
+bench_cprof() ->    
+    Self = self(),
+    Count = 300,
+    Key = "i",
+    cprof:start(), 
+    spawn(fun () -> process(Self, Key, Count) end),
+    wait_for_done(1),
+    cprof:pause(), 
+    io:format("~p~n", [cprof:analyse()]),
+    Count.
+
+bench_fprof() ->
+    Count = fprof:apply(bench_increment, bench_raw, [], [{procs, process_dictionary:get_all_pids()}]),
+    fprof:profile(),
+    %fprof:analyse(),
+    fprof:analyse([{cols, 140}, details, callers, totals, {dest, []}]), % , totals, no_details, no_callers, {sort, acc}, 
+    Count.
+
+
+
+increment_test() ->
+    % init: i = 0
+    Key = "i",
+    commit = transstore.transaction_api:single_write(Key, 0),
+
+    {Time, Value} = timer:tc(bench_increment, bench, []),
+    io:format("executed ~p transactions in ~p us: ~p~n", [Value, Time, Value / Time * 1000000]),
+    %error_logger:tty(false),
+    ok.
+
+wait_for_done(0) ->
+    [];
+wait_for_done(Count) ->
+    receive
+	{done, Aborts} ->
+	    io:format("aborts: ~p~n", [Aborts]),
+	    [Aborts |wait_for_done(Count - 1)]
+    end.
