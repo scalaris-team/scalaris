4f3995a572e2d1fbb4ff423d54e14229e02940e8
kruber@informatik.hu-berlin.de <kruber@informatik.hu-berlin.de>
2009-01-08 18:30:34 +0000

    - ant: removed building of (deprecated) chordsharp4j.jar files by default - updated to OtpErlang-1.4.2 -> added a fix rewriting "...@localhost" node names to "...@<hostname>" - removed temporary outputs

--- java-api/src/de/zib/scalaris/Main.java
+++ java-api/src/de/zib/scalaris/Main.java
@@ -20,7 +20,6 @@ import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
-import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
