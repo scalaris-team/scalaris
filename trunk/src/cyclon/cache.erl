380534b0e26f25624394c63ab2229e449806fd9b
caraboides <caraboides@71766da9-9752-0410-ab17-bfafa751c56a>
2009-01-09 14:08:33 +0000

    merge cyclon [102]:[126] into trunk

---
+++ trunk/src/cyclon/cache.erl
@@ -0,0 +1,236 @@
+%%%-------------------------------------------------------------------
+%%% File    : cyclon.erl
+%%% Author  : Christian Hennig <hennig@zib.de>
+%%% Description : Cache implementation, intern it is a gb_set, extern a list
+%%%
+%%% Created :  1 Dec 2008 by Christian Hennig <hennig@zib.de>
+%%%-------------------------------------------------------------------
+%% @author Christian Hennig <hennig@zib.de>
+%% @copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%% @version $Id $
+-module(cyclon.cache).
+-author('hennig@zib.de').
+-vsn('$Id $ ').
+
+-import(lists).
+-import(cs_send).
+-import(config).
+-import(random).
+%-import(node).
+-import(io).
+-import(crypto).
+-import(util).
+%% API
+
+-export([add_element/2, get_cache/1, add_list/2, size/1, new/0, get_random_element/1, get_random_subset/2, is_element/2, delete/2 ,update/1, minus/2, merge/3, trim/1, get_list_of_cyclons/1 ,inc_age/1, get_oldest/1]).
+
+% list of {pid of cyclon process, pid of cs_node process, age}
+-type(cache() :: list({{node:node_type(), node:node_type()}, pos_integer()})).
+
+%% @doc Firstly try to insert QT in Cache on the empty slots, and secondly replacing entries among the ones original sent
+-spec(merge/3 :: (cache(), cache(), cache()) -> cache()).
+merge(Cache,[],_Send) ->
+    Cache;
+merge([],QsSub,_SendSub) ->
+    QsSub;
+merge(Cache,[_QH|_QT],[]) ->
+    Cache;
+%     Cache  QsSUB    SendSub 	
+merge(Cache,[QH|QT],[SH|ST] = S) ->
+    %io:format("~p ~p-~p-~p ~n",[self(),length(Cache),length(Q),length(S)]),
+    case (cache:size(Cache) < config:read(cyclon_cache_size)) of
+	true ->
+	    merge(add_element(QH,Cache),QT,S);
+	false ->
+	    merge(add_element(QH,trim(delete(SH,Cache))),QT,ST)
+    end.
+
+%% @doc minus(M,N) : { x | x in M and x notin N} 
+minus([],_N) ->
+    [];
+minus([H|T],N) ->
+    case is_element(H,N) of
+ 	true -> 
+	    minus(T,N);
+	false ->
+	    [H]++minus(T,N)
+    end.
+
+
+%% @doc 
+get_list_of_cyclons([]) ->
+	[];
+get_list_of_cyclons([{{A,_},_}|T]) ->
+	[A]++get_list_of_cyclons(T).
+	
+%CacheSize = config:read(cyclon_cache_size).
+send_req([]) ->
+    nil;
+send_req([{{Cyclon,Node},_}|T]) ->
+    case Cyclon of
+	nil ->
+						%io:format("Send to: ~p~n",[Node]),
+	    cs_send:send(Node,{get_cyclon_pid, cs_send:this(), Node}),
+	    send_req(T);
+	_ ->
+	    send_req(T)
+    end.
+
+receive_req(Cache) ->
+    case cache:size(Cache) ==length(Cache) of % Replace this with a Test of nil !!!!!!!!!!!
+	true ->
+	    Cache;
+	false ->
+	    receive 
+		{cyclon_pid,Node,Cyclon} ->
+		    %io:format("+ ~p | ~p ~n",[Node,Cyclon]),
+		    receive_req(add_element({{Cyclon,Node},0},Cache))				
+	    after 300 ->
+		    
+		    Cache
+	    end
+    end.
+
+%% @doc make the cache valid, by ataching CyclonPids to they NodePids
+update(Cache) ->
+    send_req(Cache),
+    receive_req(Cache).
+
+%% @doc ensure that one place is empty in the stack, by delete a random entrie if no space left
+trim(Cache) ->
+	case cache:size(Cache) <  config:read(cyclon_cache_size) of
+		true ->
+			Cache;
+		false ->
+			delete(get_random_element(Cache),Cache)
+	end.
+
+get_cache(Foo) ->
+	Foo.
+
+add_list([],Foo) ->
+	Foo;
+add_list([NodePid|T],Foo) ->
+	add_list(T,add_element({{nil,NodePid},0},Foo)).
+
+
+get_random_element(State) ->
+    L=cache:size(State),
+    P=crypto:rand_uniform(0, L)+1,
+    % picks nth element of state
+    lists:nth(P,State).
+
+worker(_,Target,[]) -> Target;
+worker(N,Target,Cache) ->
+    case N==length(Target) of
+	true ->
+	    Target;
+	false ->
+	    Q = get_random_element(Cache),
+	    worker(N,add_element(Q,Target),delete(Q,Cache))
+    end.
+	
+
+get_random_subset(0,_Cache) -> 
+		new();
+get_random_subset(N,Cache) -> 
+		worker(N,[],Cache).
+
+%% @doc find oldest element (randomize if multiple oldest elements)
+get_oldest(Cache) ->
+    HighestAge = lists:foldl(fun ({_, Age}, MaxAge) ->
+				    util:max(Age, MaxAge)
+			    end,
+			    0,
+			    Cache),
+    OldElements = lists:filter(fun ({_, Age}) ->
+				       Age == HighestAge
+			       end,
+			       Cache),
+    get_random_element(OldElements).
+				    
+
+inc_age(Cache) ->
+    lists:map(fun({{A,B},C}) ->
+		      {{A,B}, C + 1}
+	      end,
+	      Cache).
+
+older({_,A},{_,B}) ->
+	A > B.
+
+lt({{_,A},_},{{_,B},_}) ->
+	A < B . 
+
+eq({{_,A},_},{{_,B},_}) ->
+	A==B . 
+
+
+new() ->
+	[].
+%% @doc Amount of valid Cache entries 
+size([]) ->
+0;
+size([H|T]) ->
+{{Cyclon,_},_}=H,
+case Cyclon of
+	nil	-> cache:size(T);
+	_	-> 1+cache:size(T)
+end.
+
+
+
+
+
+
+
+
+
+%% @doc returns true if Element is in Cache
+is_element(Element, Cache) ->
+    lists:any(fun(SomeElement) -> eq(Element, SomeElement) end, Cache).
+
+%% @doc removes Element from Cache
+delete(Element, Cache) ->
+    lists:filter(fun(SomeElement) -> not eq(SomeElement, Element) end, Cache).
+
+%% @doc adds Element to Cache or updates Element in Cache
+add_element(Element, Cache) ->
+    case is_element(Element, Cache) of
+	true ->
+	    add_element(Element, delete(Element, Cache));
+	false ->
+	    [Element | Cache]
+    end.
+
+
+%% @doc delete an element in the Cache    
+delete2(_Foo,[]) ->
+	[];
+delete2(Foo,[H|T] = L) ->
+    case lt(Foo,H) of
+	true ->
+	    L;
+	false ->
+	    case eq(Foo,H) of
+		true ->
+		    T;
+		false ->
+		    [H]++delete2(Foo,T)
+	    end
+    end.
+
+add_element2(Foo,[]) ->
+	[Foo];
+add_element2(Foo,[H|T]) ->
+	case lt(Foo,H) of 
+		true -> 
+			[Foo]++[H|T];
+		false -> 
+			case eq(Foo,H) of
+				true ->
+					[Foo|T];
+				false ->
+					[H]++add_element2(Foo,T)
+			end
+	end.
---
+++ trunk/src/cyclon/cyclon.erl
@@ -0,0 +1,191 @@
+%%%-------------------------------------------------------------------
+%%% File    : cyclon.erl
+%%% Author  : Christian Hennig <hennig@zib.de>
+%%% Description : cyclon main file
+%%%
+%%% Created :  1 Dec 2008 by Christian Hennig <hennig@zib.de>
+%%%-------------------------------------------------------------------
+%% @author Christian Hennig <hennig@zib.de>
+%% @copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%% @version $Id $
+%% @reference S. Voulgaris, D. Gavidia, M. van Steen. CYCLON: 
+%% Inexpensive Membership Management for Unstructured P2P Overlays. 
+%% Journal of Network and Systems Management, Vol. 13, No. 2, June 2005.
+
+-module(cyclon.cyclon).
+-author('hennig@zib.de').
+-vsn('$Id $ ').
+
+-import(boot_server).
+-import(config).
+-import(cs_send).
+-import(io).
+-import(io_lib).
+-import(lists).
+-import(process_dictionary).
+-import(erlang).
+-import(node).
+%% API
+-export([start_link/1, start/1]).
+
+-export([]).
+
+%%====================================================================
+%% API
+%%====================================================================
+%%--------------------------------------------------------------------
+%% Function: start_link() -> {ok,Pid} | ignore | {error,Error}
+%% Description: Starts the server
+%%--------------------------------------------------------------------
+start_link(InstanceId) ->
+	case config:read(cyclon_enable) of
+		true -> 
+			 {ok, spawn_link(?MODULE, start, [InstanceId])};
+		_ ->
+			ignore
+	end.
+   
+start(InstanceId) ->
+    boot_server:connect(),
+    process_dictionary:register_process(InstanceId, cyclon, self()),
+    %process_dictionary:register_process(InstanceId, cyclonNode, cs_send:this()),
+    %{observer,'me@csr-pc38.zib.de' } ! {cyclon, self()},
+    get_pid() ! {get_pred_succ, cs_send:this()},
+    erlang:send_after(config:read(cyclon_interval), self(), {shuffle}),
+    io:format("Cyclon spawn: ~p~n", [cs_send:this()]),
+    loop(cache:new(),cs_send:get(get_pid(), cs_send:this()),0).
+
+
+%% Cache
+%% Node: the scalaris node of this cyclon-task
+%% State: {preshuffle|shuffle}
+%% Cycles: the amount of shuffle-cycles
+
+loop(Cache,Node,Cycles) ->
+    receive
+	{getcache,Pid} ->
+		Pid ! {cache,cs_send:this(),Cycles,cache:get_list_of_cyclons(Cache)},
+		loop(Cache,Node,Cycles);
+	{flush_cache} ->
+		get_pid() ! {get_pred_succ, cs_send:this()},
+		loop(cache:new(),Node,0);	
+	{start_shuffling} ->
+		erlang:send_after(config:read(cyclon_interval), self(), {shuffle}),
+		loop(Cache,Node,Cycles);
+			
+	{get_pred_succ_response, Pred, Succ} ->
+	    %Me = cs_send:get(get_pid(), cs_send:this()),
+	   	%io:format("Pred ~p~n Succ~p~n ~n", [node:pidX(Pred),pidX(Succ)]),
+		case node:pidX(Pred) /= Node of
+ 	 		true ->
+				NewCache =  cache:add_list([node:pidX(Pred),node:pidX(Succ)], Cache),
+				loop(cache:update(NewCache),Node,Cycles);
+			false ->
+				loop(Cache,Node,Cycles)
+		end;
+		
+	{'$gen_cast', {debug_info, Requestor}}  ->
+	    %io:format("gen_cast~n", []),
+		% io:format("~p~n", [lists:flatten(io_lib:format("~p", [State]))]),
+	    Requestor ! {debug_info_response, [
+					       	{"cs_node", lists:flatten(io_lib:format("~p", [get_pid()]))},
+						   	{"cache-items", lists:flatten(io_lib:format("~p", [cache:size(Cache)]))},
+							{"cache", lists:flatten(io_lib:format("~p", [Cache])) }
+					      ]},
+	    loop(Cache,Node,Cycles);
+	{shuffle} 	->
+	    %io:format("shuffle~n", []),
+		case Node of	
+		    nil ->
+			loop(Cache,Node,Cycles);
+		    _	->
+			NewCache = case cache:size(Cache) of
+				       0 -> 
+					   Cache;	
+				       _  ->
+					   eshuffle(Cache,Node)
+				   end,
+			erlang:send_after(config:read(cyclon_interval), self(), {shuffle}),
+			loop(NewCache,Node,Cycles+1)
+		end;
+	{subset,P,Subset} ->
+		%io:format("subset~n", []),
+		ForSend=cache:get_random_subset(get_L(Cache),Cache),
+		io:format("<",[]),
+		cs_send:send(P,{subset_response,ForSend,Subset}),
+		N2=cache:minus(Subset,Cache),
+		NewCache = cache:merge(Cache,N2,ForSend),
+		loop(NewCache,Node,Cycles);
+	{subset_response,Subset,OldSubset} ->
+		%io:format("subset_response~n", []),
+		N1=cache:delete({{nil,Node},nil},Subset),
+		N2=cache:minus(N1,Cache),
+		NewCache=cache:merge(Cache,N2,OldSubset),
+		loop(NewCache,Node,Cycles);
+	{random_element,Pid} ->
+		Pid ! {random_element_response,cache:get_random_element(Cache)},
+		loop(Cache,Node,Cycles);
+	X ->
+		io:format("%% Unhandle Message: ~p~n", [X]),
+	    loop(Cache,Node,Cycles)
+    end.
+
+eshuffle(Cache, Node) ->
+    Cache_1= cache:inc_age(Cache),
+    Q=cache:get_oldest(Cache_1),
+    Subset=cache:get_random_subset(get_L(Cache_1),Cache_1),
+    {{QCyclon,_},_} = Q,
+    case (QCyclon /= cs_send:this()) of
+	true ->
+	    NSubset_pre=cache:delete(Q,Subset),
+	    NSubset = case cache:size(NSubset_pre) == config:read(cyclon_shuffle_length) of
+			  true ->
+			      cache:delete(cache:get_random_element(NSubset_pre),NSubset_pre);
+			  false ->
+			      NSubset_pre
+		      end,
+	    ForSend=cache:add_element({{cs_send:this(),Node},0},NSubset),
+	    io:format(">",[]),
+	    cs_send:send(QCyclon,{subset,cs_send:this(),ForSend}),
+	    cache:delete(Q,Cache);	
+	false -> 
+	    error
+    end.
+
+simple_shuffle(Cache, Node) ->
+	Subset=cache:get_random_subset(get_L(Cache),Cache),
+	Q=cache:get_random_element(Subset),
+	{{QCyclon,_},_} = Q,
+	case (QCyclon /= cs_send:this()) of
+	true ->
+		NSubset=cache:delete(Q,Subset),
+		ForSend=cache:add_element({{cs_send:this(),Node},0},NSubset),
+		io:format("~p",[length(ForSend)]),
+		cs_send:send(QCyclon,{subset,cs_send:this(),ForSend}),
+		cache:delete(Q,Cache);	
+	false -> 
+		Cache
+	end.
+
+
+get_L(Cache) ->
+	Cl= config:read(cyclon_shuffle_length),
+	Size =  cache:size(Cache),
+	if
+		Cl < Size ->
+			L=Cl;
+		true ->
+			L=cache:size(Cache)
+	end,
+	L.
+	
+get_pid() ->
+    InstanceId = erlang:get(instance_id),
+    if
+	InstanceId == undefined ->
+	    io:format("~p~n", [util:get_stacktrace()]);
+	true ->
+	    ok
+    end,
+    process_dictionary:lookup_process(InstanceId, cs_node).
+
