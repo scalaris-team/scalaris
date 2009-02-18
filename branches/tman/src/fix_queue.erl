052c7ba02e6ecf8a9d084ee5eb5b29595ef69a67
caraboides <caraboides@71766da9-9752-0410-ab17-bfafa751c56a>
2009-02-18 16:32:33 +0000

    - Bug fixed in fix_queue

--- branches/tman/src/fix_queue.erl
+++ branches/tman/src/fix_queue.erl
@@ -1,6 +1,6 @@
 %% Author: christian
 %% Created: Feb 16, 2009
-%% Description: TODO: Add description to ch_queue
+%% Description: TODO: Add description to fix_queue
 -module(fix_queue).
 
 %%
@@ -21,21 +21,12 @@ filter(Fun,{_,_,Q}) ->
     
 new(MaxLength) ->
     {MaxLength,0,queue:new()}.
-
 add(Elem,{Max,Len,Q}) ->
     {_,NewQ} = 
-        case Len = Max of
+        case Len == Max of
         	true ->  	NewLen=Len,
                 		queue:out(queue:in(Elem, Q));
 			false -> 	NewLen=Len+1,
-                		{foo,queue:in(Elem, Q)}
+           		{foo,queue:in(Elem, Q)}
         end,
-    {Max,NewLen,NewQ}
-	;
-add(_,_) ->
-    badarg.
-	
-%%
-%% Local Functions
-%%
-
+    {Max,NewLen,NewQ}.
