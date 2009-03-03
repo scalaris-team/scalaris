80f0f14c98f0d1b197c76caec20cb9446e345a25
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2009-03-03 09:16:18 +0000

    - more specs for gen_component

--- trunk/src/rse_chord.erl
+++ trunk/src/rse_chord.erl
@@ -22,7 +22,16 @@
 -export([start_link/1]).
 -export([init/1, on/2]).
 
+% state of a rse_chord process
 -type(state() :: {float()}).
+
+% accepted messages of rse_chord processes
+-type(message() :: {init_shuffle} 
+      | {get_pred_succ_response, Pred::node:node_type(), Succ::node:node_type()}
+      | {shuffle_trigger}
+      | {cache, Cache::[node:node_type()]}
+      | {get_rse_chord_response, RemoteEstimate::float()}
+      | {get_rse_chord, Pid::cs_send:mypid()}).
 
 shuffle_interval() ->
     25000.
@@ -31,6 +40,7 @@ shuffle_reset_interval() ->
     180000.
 
 %% @doc start_link for supervisor
+-spec(start_link/1 :: (any()) -> {ok, pid()}).
 start_link(InstanceId) ->
     gen_component:start_link(?MODULE, [InstanceId], [profile]).
 
@@ -47,7 +57,7 @@ init([InstanceId]) ->
 % ring size estimator
 %================================================================================
 %% @doc message handler
--spec(on/2 :: (any(), state()) -> state()).
+-spec(on/2 :: (message(), state()) -> state()).
 
 %% trigger new estimation round
 on({init_shuffle}, State) ->
@@ -108,6 +118,7 @@ get_cyclon_pid() ->
 
 %% @doc Calculate initial estimate for ring size based on distance to 
 %%      predecessor and successor
+-spec(get_initial_estimate/3 :: (rt_simple:key(), rt_simple:key(), rt_simple:key()) -> float()).
 get_initial_estimate(Pred, Id, Succ) ->
     if
 	Pred == Id andalso Id == Succ ->
@@ -119,6 +130,7 @@ get_initial_estimate(Pred, Id, Succ) ->
     end.
 
 %% @doc normalize Chord identifier
+-spec(normalize/1 :: (rt_simple:key()) -> rt_simple:key()).
 normalize(Key) ->
     if
 	Key < 0 ->
@@ -128,3 +140,5 @@ normalize(Key) ->
 	true ->
 	    Key
     end.
+
+% -*-  indent-tabs-mode:nil;  -*-
