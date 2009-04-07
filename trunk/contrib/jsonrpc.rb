597890f50fefae33aef99b92d77e983104947d73
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2009-04-07 14:34:28 +0000

    - workaround for OTP-7682

--- trunk/contrib/jsonrpc.rb
+++ trunk/contrib/jsonrpc.rb
@@ -3,10 +3,11 @@
 require 'rubygems'
 require 'json'
 require 'net/http'
+require 'benchmark'
 
 def test_and_set(url, key, oldvalue, newvalue)
   uri = URI.parse url
-  req = Net::HTTP::Post.new(url)
+  req = Net::HTTP::Post.new(uri.path)
   req.add_field 'Content-Type', 'application/json'
   req.body = {:version => '1.1', :method => 'test_and_set', :params => [key, oldvalue, newvalue], :id => 0}.to_json
   res = Net::HTTP.start(uri.host, uri.port){|http|
@@ -17,7 +18,7 @@ end
 
 def read(url, key)
   uri = URI.parse url
-  req = Net::HTTP::Post.new(url)
+  req = Net::HTTP::Post.new(uri.path)
   req.add_field 'Content-Type', 'application/json'
   req.body = {:version => '1.1', :method => 'read', :params => [key], :id => 0}.to_json
   res = Net::HTTP.start(uri.host, uri.port){|http|
@@ -28,7 +29,7 @@ end
 
 def write(url, key, value)
   uri = URI.parse url
-  req = Net::HTTP::Post.new(url)
+  req = Net::HTTP::Post.new(uri.path)
   req.add_field 'Content-Type', 'application/json'
   req.body = {:version => '1.1', :method => 'write', :params => [key, value], :id => 0}.to_json
   res = Net::HTTP.start(uri.host, uri.port){|http|
@@ -37,11 +38,55 @@ def write(url, key, value)
   JSON.parse(res.body)['result']
 end
 
-puts "read('http://localhost:8000/jsonrpc.yaws', 'Bar') \n    == #{read('http://localhost:8000/jsonrpc.yaws', 'Bar').to_json}"
-puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
-puts "read('http://localhost:8000/jsonrpc.yaws', 'Bar') \n    == #{read('http://localhost:8000/jsonrpc.yaws', 'Bar').to_json}"
-puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
-puts "write('http://localhost:8000/jsonrpc.yaws', 'Bar', '') \n    == #{write("http://localhost:8000/jsonrpc.yaws", "Bar", "").to_json}"
-puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
+def nop(url, value)
+  uri = URI.parse url
+  req = Net::HTTP::Post.new(uri.path)
+  req.add_field 'Content-Type', 'application/json'
+  req.body = {:version => '1.1', :method => 'nop', :params => [value], :id => 0}.to_json
+  res = Net::HTTP.start(uri.host, uri.port){|http|
+    http.request(req)
+  }
+  JSON.parse(res.body)['result']
+end
+
+#puts "read('http://localhost:8000/jsonrpc.yaws', 'Bar') \n    == #{read('http://localhost:8000/jsonrpc.yaws', 'Bar').to_json}"
+#puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
+#puts "read('http://localhost:8000/jsonrpc.yaws', 'Bar') \n    == #{read('http://localhost:8000/jsonrpc.yaws', 'Bar').to_json}"
+#puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
+#puts "write('http://localhost:8000/jsonrpc.yaws', 'Bar', '') \n    == #{write("http://localhost:8000/jsonrpc.yaws", "Bar", "").to_json}"
+#puts "test_and_set('http://localhost:8000/jsonrpc.yaws', 'Bar', '', 'Foo') \n    == #{test_and_set("http://localhost:8000/jsonrpc.yaws", "Bar", "", "Foo").to_json}"
 
 
+
+n = 100
+url = 'http://localhost:8000/jsonrpc.yaws'
+
+nop = Benchmark.realtime {
+  n.times do
+    nop(url, "key")
+  end
+}
+
+read = Benchmark.realtime {
+  n.times do
+    read(url, "key")
+  end
+}
+
+write = Benchmark.realtime {
+  n.times do
+    write(url, "key", "value")
+  end
+}
+
+test_and_set = Benchmark.realtime {
+  n.times do
+    test_and_set(url, "key", "value", "value")
+  end
+}
+
+printf("              time[s]\t1/s\n")
+printf("nop          : %0.02f     %0.02f\n", nop, n/nop)
+printf("read         : %0.02f     %0.02f\n", read, n/read)
+printf("write        : %0.02f     %0.02f\n", write, n/write)
+printf("test_and_set : %0.02f     %0.02f\n", test_and_set, n/test_and_set)
