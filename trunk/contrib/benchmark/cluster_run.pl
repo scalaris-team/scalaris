aa97052b5443c708111830f3ad0d7fb02eeeea84
caraboides <caraboides@71766da9-9752-0410-ab17-bfafa751c56a>
2009-07-27 16:31:54 +0000

    - Bugfixes for cluster benchmarks

--- trunk/contrib/benchmark/cluster_run.pl
+++ trunk/contrib/benchmark/cluster_run.pl
@@ -8,24 +8,25 @@ $cl = $ARGV[1];
 $name = $ARGV[2]."-".$cl."-".$rev."-".`date +%m%d%y%H%M%S`; 
 chomp($name);
 use Template;
-my @Servers = (1,2,5,10,15);
+my @Servers = (1,2,5);
 my $resdir = `pwd`;
 chomp($resdir);
 $resdir.="/$name";
 system "mkdir $resdir";
 system "svn checkout -r $rev http://scalaris.googlecode.com/svn/trunk/ $resdir/scalaris-read-only";
 #build scalairs
-if($cl=="de") {
-system "patch -p1  $resdir/scalaris-read-only/src/cs_send.erl <  $resdir/scalaris-read-only/contrib/using_distributed_erlang.patch";
+if($cl eq 'de') {
+	system "patch -p1  $resdir/scalaris-read-only/src/cs_send.erl <  $resdir/scalaris-read-only/contrib/using_distributed_erlang.patch";
 }
 system "cd  $resdir/scalaris-read-only/ ; ./configure";
 system "cd  $resdir/scalaris-read-only/ ; make";
+system "cd  $resdir/scalaris-read-only/bin ; chmod u+x bench_master.sh ; chmod u+x bench_slave.sh " ;
 my $runfile= $resdir."/qsub.sh";
 open(RUNFILE,">$runfile");
 $resdir =~ s/NFS3/NFS4/g ;
 foreach  my $s (@Servers) {
     my $tt = Template->new;
-    $tt->process('bench_tt',  { server => $s , resdir => $resdir, cl => $cl , scalaris => $resdir."/scalaris-read-only/" },  $name."/bench_".$s."_run")
+    $tt->process('bench_tt',  { server => $s , resdir => $resdir, cl => $cl , scalaris => $resdir."/scalaris-read-only/" , name => $ARGV[2]."-".$cl."-".$rev },  $name."/bench_".$s."_run")
     		|| die $tt->error;
    print(RUNFILE "qsub bench_".$s."_run\n");
 }
