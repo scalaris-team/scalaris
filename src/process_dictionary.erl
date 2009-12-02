78201c8caf4e6ca27ffce19a40c54b5d3a918e9a
schuett <schuett@zib.de>
2009-12-02 13:48:06 +0000

    - process_dictionary now maintains a link to the registered processes

--- src/process_dictionary.erl
+++ src/process_dictionary.erl
@@ -29,12 +29,12 @@
 %     nodes in one erlang vm. But for the processes forming a scalaris
 %     node being able to talk to each other, they have to now their
 %     names (cs_node, config, etc.). This module allows the processes
-%     to keep their names. 
+%     to keep their names.
 %
 %     When a new process group is created, a unique "instance_id" is
 %     created, which has to be shared by all nodes in this
-%     group. 
-%     
+%     group.
+%
 %     {@link register_process/3} registers the name of a process in
 %     his group and stores the instance_id in the calling processes'
 %     environment using {@link erlang:put/2}.
@@ -54,22 +54,21 @@
 
 %% gen_server callbacks
 -export([init/1,on/2,
-    
-	 register_process/3, 
-	 lookup_process/2,
-	 lookup_process/1,
-	 find_cs_node/0, 
-	 find_all_cs_nodes/0, 
-	 find_all_processes/1, 
-	 find_group/1,
+         register_process/3,
+         lookup_process/2,
+         lookup_process/1,
+         find_cs_node/0,
+         find_all_cs_nodes/0,
+         find_all_processes/1,
+         find_group/1,
          find_all_groups/1,
 
-	 get_groups/0,
-	 get_processes_in_group/1, 
-	 %get_info/2,
+         get_groups/0,
+         get_processes_in_group/1,
+         %get_info/2,
 
-	 %for fprof
-	 get_all_pids/0]).
+         %for fprof
+         get_all_pids/0]).
 
 %% for unit testing
 -export([start_link_for_unittest/0]).
@@ -81,7 +80,6 @@
 %% @doc register a process with InstanceId and Name
 %% @spec register_process(term(), term(), pid()) -> ok
 register_process(InstanceId, Name, Pid) ->
-    
     erlang:put(instance_id, InstanceId),
     erlang:put(instance_name, Name),
     cs_send:send_local(get_pid() , {register_process, InstanceId, Name, Pid}),
@@ -103,10 +101,10 @@ lookup_process(InstanceId, Name) ->
 -spec(lookup_process/1 :: (pid()) -> {any(), any()} | failed).
 lookup_process(Pid) ->
     case ets:match(?MODULE, {'$1',Pid}) of
-	[[{Group, Name}]] ->
-	    {Group, Name};
-	[] ->
-	    failed
+        [[{Group, Name}]] ->
+            {Group, Name};
+        [] ->
+            failed
     end.
 
 %% @doc tries to find a cs_node process
@@ -149,19 +147,17 @@ get_groups() ->
     AllGroups = find_all_groups(ets:tab2list(?MODULE), gb_sets:new()),
     GroupsAsJson = {array, lists:foldl(fun(El, Rest) -> [{struct, [{id, El}, {text, El}, {leaf, false}]} | Rest] end, [], gb_sets:to_list(AllGroups))},
     GroupsAsJson.
- 
+
 %% @doc find processes in a group (for web interface)
 %% @spec get_processes_in_group(term()) -> term()
 get_processes_in_group(Group) ->
     AllProcesses = find_processes_in_group(ets:tab2list(?MODULE), gb_sets:new(), Group),
     ProcessesAsJson = {array, lists:foldl(fun(El, Rest) -> [{struct, [{id, toString(El)}, {text, toString(El)}, {leaf, true}]} | Rest] end, [], gb_sets:to_list(AllProcesses))},
     ProcessesAsJson.
-    
 
-
-get_all_pids() ->   
+get_all_pids() ->
     [X || [X]<- ets:match(?MODULE, {'_','$1'})].
-    
+
 %%====================================================================
 %% API
 %%====================================================================
@@ -173,25 +169,23 @@ get_all_pids() ->
 start_link() ->
     io:format("Start PD~n"),
     gen_component:start_link(?MODULE, [], [{register_native, process_dictionary}]).
-  
 
 %@doc Starts the server for unit testing
 start_link_for_unittest() ->
     case whereis(process_dictionary) of
-	undefined ->
-	    gen_component:start_link(?MODULE, [], [{register_native, process_dictionary}]);
-	_ ->
-	    cs_send:send_local(get_pid() , {drop_state}),
-	    already_running
+        undefined ->
+            gen_component:start_link(?MODULE, [], [{register_native, process_dictionary}]);
+        _ ->
+            cs_send:send_local(get_pid() , {drop_state}),
+            already_running
     end.
-    
+
 %%--------------------------------------------------------------------
 %% Function: start() -> {ok,Pid} | ignore | {error,Error}
 %% Description: Starts the server; for use with the test framework
 %%--------------------------------------------------------------------
 %@doc Starts the server; for use with the test framework
 start() ->
-    
     gen_component:start_link(?MODULE, [], [{register_native, process_dictionary}]).
 
 
@@ -210,6 +204,7 @@ start() ->
 %@private
 init(_Args) ->
     ets:new(?MODULE, [set, protected, named_table]),
+    process_flag(trap_exit, true),
     State = null,
     State.
 
@@ -225,15 +220,29 @@ init(_Args) ->
 
 %@private
 
+-spec(on/2 :: (any(), null) -> null).
 on({register_process, InstanceId, Name, Pid}, State) ->
-    %io:format("insert: ~p ~n",[{{InstanceId, Name}, Pid}]),
-    ets:insert(?MODULE, {{InstanceId, Name}, Pid}),
+    case ets:insert_new(?MODULE, {{InstanceId, Name}, Pid}) of
+        true ->
+            link(Pid);
+        false ->
+            OldPid = ets:lookup_element(?MODULE, {InstanceId, Name}, 2),
+            unlink(OldPid),
+            link(Pid),
+            ets:insert_new(?MODULE, {{InstanceId, Name}, Pid})
+    end,
     cs_send:send_local(Pid , {ok}),
-    {State};
+    State;
 on({drop_state}, State) ->
+    % only for unit tests
+    Links = ets:match(?MODULE, {'_', '$1'}),
+    [unlink(Pid) || [Pid] <- Links],
     ets:delete_all_objects(?MODULE),
-    {State};
-
+    State;
+on({'EXIT', FromPid, _Reason}, State) ->
+    Processes = ets:match(?MODULE, {'$1', FromPid}),
+    [ets:delete(?MODULE, {InstanceId, Name}) || [{InstanceId, Name}] <- Processes],
+    State;
 on(_, _State) ->
     unknown_event.
 
@@ -254,10 +263,6 @@ find_process(Name) ->
              failed
          end.
 
-
-
-
-
 find_all_groups([], Set) ->
     Set;
 find_all_groups([{{InstanceId, _}, _} | Rest], Set) ->
@@ -267,21 +272,16 @@ find_processes_in_group([], Set, _Group) ->
     Set;
 find_processes_in_group([{{InstanceId, TheName}, _} | Rest], Set, Group) ->
     if
-	InstanceId == Group ->
-	    find_processes_in_group(Rest, gb_sets:add_element(TheName, Set), Group);
-	true ->
-	    find_processes_in_group(Rest, Set, Group)
+        InstanceId == Group ->
+            find_processes_in_group(Rest, gb_sets:add_element(TheName, Set), Group);
+        true ->
+            find_processes_in_group(Rest, Set, Group)
     end.
 
 toString(X) when is_atom(X) ->
     atom_to_list(X);
 toString(X) ->
     X.
-    
-
-
-
-
 
 %%--------------------------------------------------------------------
 %%% Internal functions
