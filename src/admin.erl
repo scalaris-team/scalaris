cf3591770bd24e1b74b0745c55161c81dd34f72f
schintke <schintke@zib.de>
2009-12-03 16:46:50 +0000

    - admin:add_nodes() starts supervisors

--- src/admin.erl
+++ src/admin.erl
@@ -1,4 +1,4 @@
-%  Copyright 2008 Konrad-Zuse-Zentrum fr Informationstechnik Berlin
+%  Copyright 2008, 2009 Konrad-Zuse-Zentrum fr Informationstechnik Berlin
 %
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -22,12 +22,12 @@
 %% @copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
 %% @version $Id$
 -module(admin).
-
 -author('schuett@zib.de').
 -vsn('$Id$ ').
 
--export([add_nodes/1, add_nodes/2, check_ring/0, nodes/0, start_link/0, start/0, 
-	 get_dump/0, get_dump_bw/0, diff_dump/3, print_ages/0, check_routing_tables/1,
+-export([add_nodes/1, add_nodes/2, check_ring/0, nodes/0, start_link/0, start/0,
+         get_dump/0, get_dump_bw/0, diff_dump/3, print_ages/0,
+         check_routing_tables/1,
          dd_check_ring/1,dd_check_ring/0, number_of_nodes/0 ]).
 
 %%====================================================================
@@ -52,12 +52,9 @@ add_nodes(Count, Delay) ->
 add_nodes_loop(0, _) ->
     ok;
 add_nodes_loop(Count, Delay) ->
-    supervisor:start_child(main_sup, {randoms:getRandomId(),
-				      {cs_sup_or, start_link, []},
-				      permanent,
-				      brutal_kill,
-				      worker,
-				      []}),
+    Desc = util:sup_supervisor_desc(randoms:getRandomId(),
+                                    cs_sup_or, start_link),
+    supervisor:start_child(main_sup, Desc),
     %timer:sleep(Delay),
     add_nodes_loop(Count - 1, Delay).
 %% userdevguide-end admin:add_nodes
@@ -70,16 +67,13 @@ add_nodes_loop(Count, Delay) ->
 -spec(check_ring/0 :: () -> {error, string()} | ok).
 check_ring() ->
     erlang:put(instance_id, process_dictionary:find_group(cs_node)),
-    
     Nodes = statistics:get_ring_details(),
-    
     case lists:foldl(fun check_ring_foldl/2, first, Nodes) of
-	{error, Reason} ->
-		{error, Reason};
-	_X ->
-		ok
+        {error, Reason} ->
+            {error, Reason};
+        _X ->
+            ok
     end.
-
 
 check_ring_foldl({ok, Node}, first) ->
     get_id(hd(node_details:succlist(Node)));
@@ -90,21 +84,17 @@ check_ring_foldl(_, {error, Message}) ->
 check_ring_foldl({ok, Node}, PredsSucc) ->
     MyId = get_id(node_details:me(Node)),
     if
-	MyId == PredsSucc ->
-	    get_id(hd(node_details:succlist(Node)));
-	true ->
-	    {error, lists:flatten(io_lib:format("~p didn't match ~p", [MyId, PredsSucc]))}
+        MyId == PredsSucc ->
+            get_id(hd(node_details:succlist(Node)));
+        true ->
+            {error, lists:flatten(io_lib:format("~p didn't match ~p", [MyId, PredsSucc]))}
     end.
-
-
 
 get_id(Node) ->
     case node:is_null(Node) of
-		true ->
-			"null";
-		false ->
-	    	node:id(Node)
-    end.    
+        true -> "null";
+        false -> node:id(Node)
+    end.
 
 number_of_nodes() ->
     boot_server:number_of_nodes(),
@@ -120,18 +110,18 @@ number_of_nodes() ->
 %% comm_logger functions
 %%===============================================================================
 get_dump() ->
-    [cs_send:send(cs_send:get(admin_server, Server), {get_comm_layer_dump, cs_send:this()}) 
+    [cs_send:send(cs_send:get(admin_server, Server), {get_comm_layer_dump, cs_send:this()})
      || Server <- util:get_nodes()],
-    % list({Map, StartTime})
-    Dumps = [receive 
-	 {get_comm_layer_dump_response, Dump} ->
-	     Dump
-     end || _ <- util:get_nodes()],
+    %% list({Map, StartTime})
+    Dumps = [receive
+                 {get_comm_layer_dump_response, Dump} ->
+                     Dump
+             end || _ <- util:get_nodes()],
     StartTime = lists:min([Start || {_, Start} <- Dumps]),
     Keys = lists:usort(lists:flatten([gb_trees:keys(Map) || {Map, _} <- Dumps])),
-    {lists:foldl(fun (Tag, Map) -> 
-			 gb_trees:enter(Tag, get_aggregate(Tag, Dumps), Map)
-		 end, gb_trees:empty(), Keys), StartTime}.
+    {lists:foldl(fun (Tag, Map) ->
+                         gb_trees:enter(Tag, get_aggregate(Tag, Dumps), Map)
+                 end, gb_trees:empty(), Keys), StartTime}.
 
 get_dump_bw() ->
     {Map, StartTime} = get_dump(),
@@ -142,32 +132,32 @@ get_aggregate(_Tag, []) ->
     {0, 0};
 get_aggregate(Tag, [{Dump, _} | Rest]) ->
     case gb_trees:lookup(Tag, Dump) of
-	none ->
-	    get_aggregate(Tag, Rest);
-	{value, {Size, Count}} ->
-	    {AggSize, AggCount} = get_aggregate(Tag, Rest),
-	    {AggSize + Size, AggCount + Count}
+        none ->
+            get_aggregate(Tag, Rest);
+        {value, {Size, Count}} ->
+            {AggSize, AggCount} = get_aggregate(Tag, Rest),
+            {AggSize + Size, AggCount + Count}
     end.
-	    
-diff_dump(BeforeDump, AfterDump, _RunTime) ->    
-    Tags = lists:usort(lists:flatten([gb_trees:keys(BeforeDump), 
-					       gb_trees:keys(AfterDump)])),
+
+diff_dump(BeforeDump, AfterDump, _RunTime) ->
+    Tags = lists:usort(lists:flatten([gb_trees:keys(BeforeDump),
+                                      gb_trees:keys(AfterDump)])),
     diff(Tags, BeforeDump, AfterDump).
-    
+
 diff([], _Before, _After) ->
     [];
 diff([Tag | Rest], Before, After) ->
     {NewSize, NewCount} = gb_trees:get(Tag, After),
     case gb_trees:lookup(Tag, Before) of
-	none ->
-	    [{Tag, NewSize, NewCount} | diff(Rest, Before, After)];
-	{value, {Size, Count}} ->
-	    [{Tag, NewSize - Size, NewCount - Count} | diff(Rest, Before, After)]
+        none ->
+            [{Tag, NewSize, NewCount} | diff(Rest, Before, After)];
+        {value, {Size, Count}} ->
+            [{Tag, NewSize - Size, NewCount - Count} | diff(Rest, Before, After)]
     end.
 
-%%===============================================================================
+%%=============================================================================
 %% admin server functions
-%%===============================================================================
+%%=============================================================================
 start_link() ->
     {ok, spawn_link(?MODULE, start, [])}.
 
@@ -177,12 +167,12 @@ start() ->
 
 loop() ->
     receive
-    {halt,N} ->
-        halt(N);
-	{get_comm_layer_dump, Sender} ->
-	    cs_send:send(Sender, {get_comm_layer_dump_response, 
-				  comm_logger:dump()}),
-	    loop()
+        {halt,N} ->
+            halt(N);
+        {get_comm_layer_dump, Sender} ->
+            cs_send:send(Sender, {get_comm_layer_dump_response,
+                                  comm_logger:dump()}),
+            loop()
     end.
 
 %%--------------------------------------------------------------------
@@ -200,9 +190,9 @@ nodes() ->
             end,
     Nodes.
 
-%%===============================================================================
+%%=============================================================================
 %% Debug functions
-%%===============================================================================
+%%=============================================================================
 
 -spec(print_ages/0 :: () -> ok).
 print_ages() ->
