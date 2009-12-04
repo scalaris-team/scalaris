8a23fbd9db9fc1988134e0f0baf56bee121f1f7e
schintke <schintke@zib.de>
2009-12-04 09:24:38 +0000

    - added admin:del_nodes() to stop virtual nodes started with admin:add_nodes()

--- test/unittest_helper.erl
+++ test/unittest_helper.erl
@@ -1,4 +1,4 @@
-%  Copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%  Copyright 2008, 2009 Konrad-Zuse-Zentrum für Informationstechnik Berlin
 %
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -14,50 +14,42 @@
 %%%-------------------------------------------------------------------
 %%% File    : unittest_helper.erl
 %%% Author  : Thorsten Schuett <schuett@zib.de>
-%%% Description : Helper functions for Unit tests 
+%%% Description : Helper functions for Unit tests
 %%%
 %%% Created :  27 Aug 2008 by Thorsten Schuett <schuett@zib.de>
 %%%-------------------------------------------------------------------
 -module(unittest_helper).
-
 -author('schuett@zib.de').
 -vsn('$Id$ ').
 
 -export([make_ring/1, stop_ring/1]).
 
 make_ring(Size) ->
-
     error_logger:tty(true),
     Owner = self(),
     Pid = spawn(fun () ->
-			%timer:sleep(1000),
+                        %timer:sleep(1000),
                         ct:pal("Try to build Scalaris~n"),
                         randoms:start(),
-
-			process_dictionary:start_link_for_unittest(),
-
+                        process_dictionary:start_link_for_unittest(),
                         %timer:sleep(1000),
-
-			boot_sup:start_link(),
-              
-			%timer:sleep(1000),
-			boot_server:connect(),
-              
-			admin:add_nodes(Size - 1, 1000),
-			Owner ! {continue},
-			receive
+                        boot_sup:start_link(),
+                        %timer:sleep(1000),
+                        boot_server:connect(),
+                        admin:add_nodes(Size - 1),
+                        Owner ! {continue},
+                        receive
                             {done} ->
                                 ok
                         end
-		end),
+                end),
     %erlang:monitor(process, Pid),
     receive
-	{'DOWN', _Ref, process, _Pid2, Reason} ->
-	    ct:pal("process died: ~p ~n", [Reason]);
-	{continue} ->
+        {'DOWN', _Ref, process, _Pid2, Reason} ->
+            ct:pal("process died: ~p ~n", [Reason]);
+        {continue} ->
             ok
     end,
-    
     timer:sleep(1000),
     check_ring_size(Size),
     wait_for_stable_ring(),
@@ -68,17 +60,16 @@ make_ring(Size) ->
 
 stop_ring(Pid) ->
     error_logger:tty(false),
-
     exit(Pid, kill).
 
 wait_for_stable_ring() ->
     R = admin:check_ring(),
     ct:pal("CheckRing: ~p~n",[R]),
     case R of
-	ok ->
-	    ok;
-	_ ->
-	    timer:sleep(1000),
+        ok ->
+            ok;
+        _ ->
+            timer:sleep(1000),
             wait_for_stable_ring()
     end.
 
@@ -91,9 +82,9 @@ check_ring_size(Size) ->
     end,
     ct:pal("Size: ~p~n",[RSize]),
     case (RSize == Size) of
-	true ->
-	    ok;
-	_ ->
-	    timer:sleep(1000),
-	    check_ring_size(Size)
+        true ->
+            ok;
+        _ ->
+            timer:sleep(1000),
+            check_ring_size(Size)
     end.
