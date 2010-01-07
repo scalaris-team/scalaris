1c7a7a5b171f7d12ba6b082445c7b7ddfd522f78
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2010-01-07 09:26:42 +0000

    - simple profiler

--- trunk/src/tracer.erl
+++ trunk/src/tracer.erl
@@ -1,11 +1,11 @@
 %%%------------------------------------------------------------------------------
 %%% File    : tracer.erl
 %%% Author  : Thorsten Schuett <schuett@zib.de>
-%%% Description : 
+%%% Description : Simple Profiler for Scalaris
 %%%
 %%% Created : 8 Jan 2009 by Thorsten Schuett <schuett@zib.de>
 %%%------------------------------------------------------------------------------
-%% @doc 
+%% @doc Simple Profiler for Scalaris
 %% @author Thorsten Schuett <schuett@zib.de>
 %% @copyright 2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
 %% @version $Id$
@@ -14,48 +14,52 @@
 -author('schuett@zib.de').
 -vsn('$Id$ ').
 
--export([tracer/1, start_link/0]).
+-export([tracer/1, start/0, dump/0]).
 
-start_link() ->
-    Link = spawn_link(?MODULE, tracer, [self()]),
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% 1. put tracer:start() into boot.erl before application:start(boot_cs)
+% 2. run benchmark
+% 3. call tracer:dump()
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+start() ->
+    spawn(?MODULE, tracer, [self()]),
     receive
 	done ->
 	    ok
     end,
-    {ok, Link}.
-
-
+    ok.
 
 tracer(Pid) ->
-    erlang:trace(all, true, [send, procs]),
+    erlang:trace(all, true, [running, timestamp]),
     cs_send:send_local(Pid , done),
+    ets:new(?MODULE, [set, public, named_table]),
     loop([]).
 
 loop(Ps) ->
     receive
-	{trace, Pid, send_to_non_existing_process, Msg, To} ->
-        
-        log:log(error,"send_to_non_existing_process: ~p -> ~p (~p)", [Pid, To, Msg]),
-        
-	    loop(Ps);
-	{trace, Pid, exit, Reason} ->
-        case Reason  of
-			normal ->
-					loop(Ps);
-            {ok, _Stack,_Num} -> 
-					loop(Ps);				
-			_ ->
-               
-        			%io:format(" EXIT: ~p | ~p~n", [Pid,Reason]),
-                    %io:format("~p~n",Ps),
-        			log:log(warn,"EXIT: ~p | ~p", [Pid,Reason]),
-	    			loop(Ps)        	
-            
-        end;
-   {trace, Pid, spawn, Pid2, {M, F, Args}} ->
-        %io:format(" SPAWN: ~p -> ~p in  ~p~n", [Pid,Pid2,{M, F, Args}]),
-        %log:log2file("TRACER",lists:flatten(io_lib:format(" SPAWN: ~p -> ~p in  ~p~n", [Pid,Pid2,{M, F, Args}]))),
-    	loop([{Pid,Pid2,{M, F, Args}}|Ps]);
-	_X ->
-	    loop(Ps)
+        {trace_ts, Pid, in, _, TS} ->
+            case ets:lookup(?MODULE, Pid) of
+                [] ->
+                    ets:insert(?MODULE, {Pid, TS, 0});
+                [{Pid, _, Sum}] ->
+                    ets:insert(?MODULE, {Pid, TS, Sum})
+            end,
+            loop(Ps);
+        {trace_ts, Pid, out, _, TS} ->
+            case ets:lookup(?MODULE, Pid) of
+                [] ->
+                    ets:insert(?MODULE, {Pid, ok, 0});
+                [{Pid, In, Sum}] ->
+                    ets:insert(?MODULE, {Pid, ok, timer:now_diff(TS, In) + Sum})
+            end,
+            loop(Ps);
+        _X ->
+            io:format("unknown message: ~p~n", [_X]),
+            loop(Ps)
     end.
+
+dump() ->
+    lists:reverse(lists:keysort(3, ets:tab2list(?MODULE))).
