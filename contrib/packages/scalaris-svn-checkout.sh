ad38dfa576a5bc8964aebfd050f7922cc349fa5d
kruber@informatik.hu-berlin.de <kruber@informatik.hu-berlin.de>
2010-01-12 17:10:59 +0000

    svn packages: do not Obsolete/Replace the ordinare scalaris packages, instead just Conflict with them (svn packages had problems obsoleting themselves and should also not be treated as updates!)

--- contrib/packages/scalaris-svn-checkout.sh
+++ contrib/packages/scalaris-svn-checkout.sh
@@ -40,10 +40,10 @@ if [ ${result} -eq 0 ]; then
   echo "extracting .spec file ..."
   sourcefolder=${folder}/contrib/packages
   sed -e "s/%define pkg_version .*/%define pkg_version ${revision}/g" \
-      -e "s/Name:           scalaris/Name:           scalaris-svn\nObsoletes:      scalaris\nProvides:       scalaris = svn%{pkg_version}/g" \
-      -e "s/%package java/%package java\nObsoletes:  scalaris-java\nProvides:   scalaris-java = svn%{pkg_version}/g" \
-      -e "s/%package doc/%package doc\nObsoletes:  scalaris-doc\nProvides:   scalaris-doc = svn%{pkg_version}/g" \
-      -e "s/%package client/%package client\nObsoletes:  scalaris-client\nProvides:   scalaris-client = svn%{pkg_version}/g" \
+      -e "s/Name:           scalaris/Name:           scalaris-svn\nConflicts:      scalaris/g" \
+      -e "s/%package java/%package java\nConflicts:  scalaris-java/g" \
+      -e "s/%package doc/%package doc\nConflicts:  scalaris-doc/g" \
+      -e "s/%package client/%package client\nConflicts:  scalaris-client/g" \
       < ${sourcefolder}/scalaris.spec > ./scalaris-svn.spec
   result=$?
 fi
@@ -58,10 +58,10 @@ if [ ${result} -eq 0 ]; then
       -e "s/stable/unstable/g" \
       < ${sourcefolder}/debian.changelog > ./debian.changelog && \
   sed -e "s/Source: scalaris/Source: scalaris-svn/g" \
-      -e "s/Package: scalaris\$/Package: scalaris-svn\nReplaces: scalaris\nConflicts: scalaris/g" \
-      -e "s/Package: scalaris-client\$/Package: scalaris-svn-client\nReplaces: scalaris-client\nConflicts: scalaris-client/g" \
-      -e "s/Package: scalaris-java\$/Package: scalaris-svn-java\nReplaces: scalaris-java\nConflicts: scalaris-java/g" \
-      -e "s/Package: scalaris-doc\$/Package: scalaris-svn-doc\nReplaces: scalaris-doc\nConflicts: scalaris-doc/g" \
+      -e "s/Package: scalaris\$/Package: scalaris-svn\nConflicts: scalaris/g" \
+      -e "s/Package: scalaris-client\$/Package: scalaris-svn-client\nConflicts: scalaris-client/g" \
+      -e "s/Package: scalaris-java\$/Package: scalaris-svn-java\nConflicts: scalaris-java/g" \
+      -e "s/Package: scalaris-doc\$/Package: scalaris-svn-doc\nConflicts: scalaris-doc/g" \
       -e 's/scalaris (= ${binary:Version})/scalaris-svn (= ${binary:Version})/g' \
       -e 's/scalaris-java (= ${binary:Version})/scalaris-svn-java (= ${binary:Version})/g' \
       < ${sourcefolder}/debian.control > ./debian.control && \
