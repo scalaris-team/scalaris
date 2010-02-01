7113e40e27f6e560e168d62bc695253c63d56b6c
Marie Hoffmann <ozymandiaz147@googlemail.com>
2010-02-01 15:26:42 +0000

    - added agglomerative clustering algorithm

--- src/mathlib.erl
+++ src/mathlib.erl
@@ -1,4 +1,4 @@
-%  Copyright 2007-2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%  Copyright 2007-2008 Konrad-Zuse-Zentrum fÃ¼r Informationstechnik Berlin
 %
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -12,26 +12,36 @@
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
 %%%-------------------------------------------------------------------
-%%% File    : vivaldi.erl
+%%% File    : mathlib.erl
 %%% Author  : Marie Hoffmann <ozymandiaz147@googlemail.com>
 %%% Description : math utility functions
 %%%
 %%% Created :  28 July 2009 by Marie Hoffmann <ozymandiaz147@googlemail.com>
 %%%-------------------------------------------------------------------
 %% @author Marie Hoffmann <ozymandiaz147@googlemail.com>
-%% @copyright 2009 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+%% @copyright 2009 Konrad-Zuse-Zentrum fÃ¼r Informationstechnik Berlin
+
 -module(mathlib).
 
--export([max/2, vecAdd/2, vecSub/2, vecMult/2, euclideanDist/1, euclideanDist/2, u/1, u/2, closestPointsHelper/1, vecWeightedAvg/4, median/1]).
+-export([closestPoints/1, euclideanDistance/1, euclideanDistance/2, max/2, u/1, vecAdd/2, vecSub/2, vecMult/2, vecWeightedAvg/4, zeros/1, median/1]).
 
 % get maximum of two values
 max(V1, V2) ->
     T1 = V1, T2 = V2, if T1 > T2 -> T1 ; true -> T2 end.
 
-% add two vectors X,Y element by element
+% median of an unsorted list
+median(L) ->
+     L1 = lists:sort(L),
+     N = length(L1),
+     case N rem 2 of
+ 	1 -> lists:nth(round(N/2), L1);
+ 	0 -> (lists:nth(trunc(N/2), L1) + lists:nth(trunc(N/2)+1, L1))/2
+     end.
+
+
+% add two vectors X,Y
 vecAdd(L1, L2) ->
     lists:zipwith(fun(X, Y) -> X+Y end, L1, L2).
-
 
 % substract two vectors X,Y
 vecSub(L1, L2) ->
@@ -42,46 +52,57 @@ vecMult(V, S) ->
     lists:map(fun(X) -> S*X end, V).
 
 vecWeightedAvg(L1, L2, W1, W2) ->
-    [vecMult(vecAdd(vecMult(L1, W1), vecMult(L2, W2)), 1/(W1+W2))].
+    vecMult(vecAdd(vecMult(L1, W1), vecMult(L2, W2)), 1/(W1+W2)).
 
 % euclidean distance between origin and V
-euclideanDist(V) ->
-    math:sqrt(lists:foldl(fun(X, Acc) -> Acc + math:pow(X, 2) end, 0, V)).
+euclideanDistance(V) ->
+    euclidDistToZero(V, 0).
 
-% euclidean distance between two Vectors V, W
-euclideanDist(V, W) ->
-    euclideanDist(vecSub(V,W)).
+euclidDistToZero([HV | V], Acc) ->
+    euclidDistToZero(V, Acc + math:pow(HV,2));
+
+euclidDistToZero([], Acc) ->
+    math:sqrt(Acc).
+
+% euclidean distance between two vectors
+euclideanDistance(V, W) ->
+    euclidDist(V, W, 0).
+
+euclidDist([HV | V], [HW | W], Acc) ->
+    euclidDist(V, W, Acc + math:pow(HV-HW, 2));
+
+euclidDist([], [], Acc) ->
+    math:sqrt(Acc).
+
 
 % unit vector u(v) = v/||v||
 u(V) -> 
-    vecMult(V, 1/euclideanDist(V)).
-
-% unit vector u(v-w)
-u(V, W) ->
-    u(vecSub(V,W)).
+    vecMult(V, 1/euclideanDistance(V)).
 
 % find indices of closest centroids
-closestPointsHelper([C1, C2| Rest]) ->
-    closestPoints([C1, C2| Rest], euclideanDist(C1, C2), 1, 2, 1, 2);
+closestPoints([C1, C2| Rest]) ->
+    closestPointsForI([C1, C2| Rest], 1, 2, euclideanDistance(C1, C2), 1, 2);
 
-closestPointsHelper(_) ->
+closestPoints(_) ->
     {-1, -1, -1}.
 
-closestPoints(Centroids, Min, Idx1, Idx2, I, J) when I < J-1 ->
-    I2 = I + 1, 
-    Dist = euclideanDist(lists:nth(I2, Centroids), lists:nth(J, Centroids)),
-    {Min1, Idx3, Idx4} = condExchange(Min, Idx1, Idx2, Dist, I2, J),
-    closestPoints(Centroids, Min1, Idx3, Idx4, I2, J);
+closestPointsForI([First | Rest], I, J, Min, MinI, MinJ) ->
+  {Min1, MinI1, MinJ1} = closestPointsForJ(First, Rest, I, J, Min, MinI, MinJ),
+    I1 = I + 1, 
+    J1 = J + 1,
+  closestPointsForI(Rest, I1, J1, Min1, MinI1, MinJ1);
 
-closestPoints(Centroids, Min, Idx1, Idx2, _, J) when J < length(Centroids) ->
-    I2 = 1,
-    J2 = J + 1,
-    Dist = euclideanDist(lists:nth(I2, Centroids), lists:nth(J2, Centroids)),
-    {Min1, Idx3, Idx4} = condExchange(Min, Idx1, Idx2, Dist, I2, J2),
-    closestPoints(Centroids, Min1, Idx3, Idx4, I2, J2);
+closestPointsForI([], _, _, Min, I, J) ->
+    {Min, I, J}.
 
-closestPoints(_, Min, Idx1, Idx2, _, _) ->
-    {Min, Idx1, Idx2}.
+closestPointsForJ(First, [Centroid | Rest], I, J, Min, MinI, MinJ) ->
+    Dist = euclideanDistance(First, Centroid),
+    {Min1, MinI1, MinJ1} = condExchange(Min, MinI, MinJ, Dist, I, J),
+    J1 = J + 1,
+    closestPointsForJ(First, Rest, I, J1, Min1, MinI1, MinJ1);
+
+closestPointsForJ(_, [], _, _, Min, MinI, MinJ) ->
+  {Min, MinI, MinJ}.
 
 % update smallest distance and its indices
 condExchange(Min, I, J, Dist, _, _) when Min =< Dist ->
@@ -90,11 +111,29 @@ condExchange(Min, I, J, Dist, _, _) when Min =< Dist ->
 condExchange(_, _, _, Dist, I, J) ->
     {Dist, I, J}.
 
-% median of an unsorted list
-median(L) ->
-    L1 = lists:sort(L),
-    N = length(L1),
-    case N rem 2 of
-	1 -> lists:nth(round(N/2), L1);
-	0 -> (lists:nth(trunc(N/2), L1) + lists:nth(trunc(N/2)+1, L1))/2
-    end.
+% initialize list with zeros
+zeros(N) ->
+    zerosRec(N, []).
+
+zerosRec(N, L) when N > 0->
+    zerosRec(N-1, [0 | L]);
+
+zerosRec(_, L) ->
+    L.
+
+% get closest centroids and melt them if their distance is within radius()
+aggloClustering(Centroids, Sizes, Radius) ->
+    {Min, I, J} = mathlib:closestPoints(Centroids),
+    aggloClusteringHelper(Centroids, Sizes, Radius, Min, I, J).
+
+aggloClusteringHelper(Centroids, Sizes, Radius, Min, I, J) when Min =< Radius, length(Centroids) > 1 ->
+    C1 = lists:nth(I, Centroids), 
+    C2 = lists:nth(J, Centroids), 
+    S1 = lists:nth(I, Sizes),
+    S2 = lists:nth(J, Sizes),
+    Centroids1 = [mathlib:vecWeightedAvg(C1, C2, S1, S2)| tools:rmvTwo(Centroids, I, J)],
+    {Min1, I1, J1} = mathlib:closestPoints(Centroids1),
+    aggloClusteringHelper(Centroids1, [S1+S2 | tools:rmvTwo(Sizes, I, J)], Radius, Min1, I1, J1);
+
+aggloClusteringHelper(Centroids, Sizes, _, _, _, _) ->
+    {Centroids, Sizes}.
