557456ac02c628e4939b6e8edf236156a2912083
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2010-02-05 09:33:10 +0000

    - fixed issue 28 (java-api compilation with JRE 1.5) - added some svn:ignores

--- trunk/java-api/src/de/zib/scalaris/ConnectionFactory.java
+++ trunk/java-api/src/de/zib/scalaris/ConnectionFactory.java
@@ -1,5 +1,17 @@
 /**
- * 
+ *  Copyright 2007-2008 Konrad-Zuse-Zentrum für Informationstechnik Berlin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
  */
 package de.zib.scalaris;
 
@@ -119,7 +131,7 @@ public class ConnectionFactory {
 	public ConnectionFactory() {
 		Properties properties = new Properties();
 		String configFile = System.getProperty("scalaris.java.config");
-		if (configFile == null || configFile.isEmpty()) {
+		if (configFile == null || configFile.length() == 0) {
 			configFile = defaultConfigFile;
 		}
 //		System.out.println("loading config file: " + configFile);
