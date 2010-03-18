88492dc9c5f419885669c65ba93e8c09f52b5e82
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2010-03-18 10:52:47 +0000

    - fixed build for erlang < R13

--- trunk/src/merkerl.erl
+++ trunk/src/merkerl.erl
@@ -41,10 +41,10 @@
 %% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %% See the License for the specific language governing permissions and
 %% limitations under the License.
+-module(merkerl).
 
 -include("../include/scalaris.hrl").
 
--module(merkerl).
 -export([insert/2,delete/2,build_tree/1,diff/2,test_merkle/0,allkeys/1, contains/2]).
 
 % NOTE: userdata is the user-exposed key, 'key' is internal-only
