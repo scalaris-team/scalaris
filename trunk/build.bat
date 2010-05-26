aefe46c9b292e39d269013be8b80f96eade74fc6
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2010-05-26 17:23:46 +0000

    - fixed and enhanced windows build and boot script (also use windows line endings!) - added client startup scripts for windows - set erlang paths in windows scripts to the default paths of erlang R13B04

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,5 +1,33 @@
 @echo off
-REM set path to erlang installation
-set ERLANG="c:\program files\erl5.7.1\bin"
+:: set path to erlang installation
+set ERLANG="c:\program files\erl5.7.5\bin"
 
+::replace @EMAKEFILEDEFINES@ from Emakefile.in and write Emakefile
+::(this is what autoconf on *nix would do)
+:: depending on your config, you might need to add one of the following options:
+::  {d, HAVE_TCERL},
+::  {d, types_not_builtin},
+::  {d, term_not_builtin},
+::  {d, node_not_builtin},
+::  {d, module_not_builtin},
+::  {d, boolean_not_builtin},
+::  {d, recursive_types_are_not_allowed,
+::  {d, type_forward_declarations_are_not_allowed},
+::  {d, forward_or_recursive_types_are_not_allowed}
+:: refer to configure.ac for the appropriate checks for necessity
+:: Note: Search & Replace functionality from http://www.dostips.com
+if not exist Emakefile (
+	@echo Creating Emakefile...
+	for /f "tokens=1,* delims=&&&" %%a in (Emakefile.in) do (
+		set "line=%%a"
+		if defined line (
+			call set "line=echo.%%line:  @EMAKEFILEDEFINES@=%%"
+			for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X >> Emakefile
+		) ELSE echo.
+	)
+	@echo ...done!
+)
+
+@echo on
 %ERLANG%\erl -pa contrib\yaws -pa ebin -noinput +B -eval "case make:all() of up_to_date -> halt(0); error -> halt(1) end."
+@echo off
