ae35bb88c641957b44de2054215aa7754b17bb1c
schintke <schintke@71766da9-9752-0410-ab17-bfafa751c56a>
2010-06-16 15:46:21 +0000

    - user-dev-guide adapted to new filenames - added description on gen_component to the user-dev-guide.

--- trunk/src/boot_server.erl
+++ trunk/src/boot_server.erl
@@ -1,5 +1,5 @@
-%  Copyright 2007-2009 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin
-%
+% @copyright 2007-2010 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin
+
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
 %   You may obtain a copy of the License at
@@ -11,37 +11,28 @@
 %   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 %   See the License for the specific language governing permissions and
 %   limitations under the License.
-%%%-------------------------------------------------------------------
-%%% File    : boot_server.erl
-%%% Author  : Thorsten Schuett <schuett@zib.de>
-%%% Description : maintains a list of chord# nodes for bootstrapping
-%%%
-%%% Created :  3 May 2007 by Thorsten Schuett <schuett@zib.de>
-%%%-------------------------------------------------------------------
+
 %% @author Thorsten Schuett <schuett@zib.de>
-%% @copyright 2007-2008 Konrad-Zuse-Zentrum fuer Informationstechnik Berlin
-%% @version $Id$
 %% @doc The boot server maintains a list of scalaris nodes and checks the 
 %%  availability using a failure_detector. It also exports a webpage 
 %%  on port 8000 containing some statistics. Its main purpose is to 
 %%  give new chord# nodes a list of nodes already in the system.
-
+%% @end
 -module(boot_server).
-
 -author('schuett@zib.de').
 -vsn('$Id$').
 
--export([start_link/0, number_of_nodes/0, node_list/0, connect/0, be_the_first/0]).
+-export([start_link/0,
+         number_of_nodes/0,
+         node_list/0,
+         connect/0,
+         be_the_first/0]).
 
 -behaviour(gen_component).
-
 -include("scalaris.hrl").
 
 -export([init/1, on/2]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Public Interface
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% @doc trigger a message with  the number of nodes known to the boot server
 -spec number_of_nodes() -> ok.
 number_of_nodes() ->
@@ -66,11 +57,10 @@ node_list() ->
 % Implementation
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-
 on({crash, PID},{Nodes,First,Subscriber}) ->
     NewNodes = gb_sets:delete_any(PID, Nodes),
     {NewNodes,First,Subscriber};
+
 on({get_list, Ping_PID},{Nodes,First,Subscriber}) ->
     case gb_sets:is_empty(Nodes) of
         true ->
@@ -78,6 +68,7 @@ on({get_list, Ping_PID},{Nodes,First,Subscriber}) ->
         _ -> comm:send(Ping_PID, {get_list_response, gb_sets:to_list(Nodes)}),
              {Nodes,First,Subscriber}
     end;
+
 on({be_the_first,Ping_PID},{Nodes,First,Subscriber}) ->
     comm:send(Ping_PID, {be_the_first_response,First}),
     {Nodes,false,Subscriber};
@@ -85,19 +76,21 @@ on({be_the_first,Ping_PID},{Nodes,First,Subscriber}) ->
 on({get_list_length,Ping_PID},{Nodes,First,Subscriber}) ->
     comm:send(Ping_PID, {get_list_length_response, length(gb_sets:to_list(Nodes))}),
     {Nodes,First,Subscriber};
+
 on({register, Ping_PID},{Nodes,First,Subscriber}) ->
     fd:subscribe(Ping_PID),
     NewNodes = gb_sets:add(Ping_PID, Nodes),
     case Subscriber of
-        [] ->
-            ok;
+        [] -> ok;
         _ ->
             [comm:send(Node,{get_list_response,gb_sets:to_list(NewNodes)} )|| Node <- Subscriber]
     end,
     {NewNodes,First,[]};
+
 on({connect},State) ->
     % ugly work around for finding the local ip by setting up a socket first
     State;
+
 on(_, _State) ->
     unknown_event.
 
@@ -108,6 +101,7 @@ init(_Arg) ->
 
 %% @doc starts the server; called by the boot supervisor
 %% @see sup_scalaris
+
 -spec start_link() -> {ok, pid()}.
 start_link() ->
      gen_component:start_link(?MODULE, [], [{register_native, boot}]).
