18367741c18603010097808f538836d3bcfc278d
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2010-07-27 16:16:46 +0000

    - added trigger:stop/1 - allowed trigger:first to be called on uninitialized trigger_* states - do not use comm:send_local_after(0,...) in trigger_*:first - made trigger states opaque

--- trunk/src/trigger_periodic.erl
+++ trunk/src/trigger_periodic.erl
@@ -31,39 +31,44 @@
 
 -include("scalaris.hrl").
 
--export([init/4, first/1, next/2]).
+-export([init/4, first/1, next/2, stop/1]).
 
--type interval() :: trigger:interval().
--type interval_fun() :: trigger:interval_fun().
--type message_tag() :: comm:message_tag().
--type state() :: {interval_fun(), message_tag(), reference() | ok}.
+-opaque state() :: {trigger:interval_fun(), comm:message_tag(), reference() | ok}.
 
 %% @doc Initializes the trigger with the given interval functions and the given
 %%      message tag used for the trigger message.
--spec init(BaseIntervalFun::interval_fun(), MinIntervalFun::interval_fun(), MaxIntervalFun::interval_fun(), message_tag()) -> {interval_fun(), message_tag(), ok}.
+-spec init(BaseIntervalFun::trigger:interval_fun(), MinIntervalFun::trigger:interval_fun(), MaxIntervalFun::trigger:interval_fun(), comm:message_tag()) -> state().
 init(BaseIntervalFun, _MinIntervalFun, _MaxIntervalFun, MsgTag) when is_function(BaseIntervalFun, 0) ->
     {BaseIntervalFun, MsgTag, ok}.
 
 %% @doc Sets the trigger to send its message immediately, for example after
 %%      its initialization.
--spec first(state()) -> {interval_fun(), message_tag(), reference()}.
-first({BaseIntervalFun, MsgTag, ok}) ->
-    TimerRef = comm:send_local_after(0, self(), {MsgTag}),
+-spec first(state()) -> state().
+first({BaseIntervalFun, MsgTag, TimerRef}) ->
+    comm:send_local(self(), {MsgTag}),
     {BaseIntervalFun, MsgTag, TimerRef}.
 
 %% @doc Sets the trigger to send its message after BaseIntervalFun()
 %%      milliseconds.
--spec next(state(), _IntervalTag::interval()) -> {interval_fun(), message_tag(), reference()}.
+-spec next(state(), IntervalTag::trigger:interval()) -> state().
 next({BaseIntervalFun, MsgTag, ok}, _IntervalTag) ->
     NewTimerRef = comm:send_local_after(BaseIntervalFun(), self(), {MsgTag}),
     {BaseIntervalFun, MsgTag, NewTimerRef};
 
-next({BaseIntervalFun, MsgTag, TimerRef}, _U) ->
+next({BaseIntervalFun, MsgTag, TimerRef} = State, _U) ->
     % timer still running?
     case erlang:read_timer(TimerRef) of
         false ->
-            NewTimerRef = comm:send_local_after(BaseIntervalFun(), self(), {MsgTag});
+            {BaseIntervalFun, MsgTag,
+             comm:send_local_after(BaseIntervalFun(), self(), {MsgTag})};
         _T ->
-            NewTimerRef = TimerRef
-    end,
-    {BaseIntervalFun, MsgTag, NewTimerRef}.
+            State
+    end.
+
+-spec stop(state()) -> state().
+stop({_BaseIntervalFun, _MsgTag, ok} = State) ->
+    State;
+stop({BaseIntervalFun, MsgTag, TimerRef}) ->
+    % timer still running?
+    erlang:cancel_timer(TimerRef),
+    {BaseIntervalFun, MsgTag, ok}.
