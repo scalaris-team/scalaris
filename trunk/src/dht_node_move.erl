346960e6cf57f1093c0fadf22cc6f78faff1d6ec
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2010-10-26 13:53:16 +0000

    - rt_loop:  * fixed message() type  * allow the process to get deactivated, too  * use active/inactive instead of init/uninit  * renamed trigger tag  * do not queue any messages in inactive state

--- trunk/src/dht_node_move.erl
+++ trunk/src/dht_node_move.erl
@@ -631,6 +631,7 @@ change_my_id(State, NewSlideOp, TargetId) ->
 %%             vivaldi:deactivate(),
 %%             dc_clustering:deactivate(),
             dht_node_reregister:deactivate(),
+            rt_loop:deactivate(),
             dht_node_state:set_slide(
               State, succ, slide_op:set_phase(NewSlideOp, wait_for_node_update));
         _ ->
