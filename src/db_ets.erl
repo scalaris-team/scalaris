a533b9f4ceb8ab9d99a32a1c02dac0507d697788
schuett <schuett@zib.de>
2010-10-27 15:45:35 +0000

    - proposal for db_ets:get_chunk

--- src/db_ets.erl
+++ src/db_ets.erl
@@ -26,6 +26,8 @@
 -behaviour(db_beh).
 -opaque(db() :: {Table::tid() | atom(), RecordChangesInterval::intervals:interval(), ChangedKeysTable::tid() | atom()}).
 
+-type(chunk() :: list(db_entry:entry())).
+
 % Note: must include db_beh.hrl AFTER the type definitions for erlang < R13B04
 % to work.
 -include("db_beh.hrl").
@@ -68,27 +70,104 @@ get_name(_State) ->
 get_data({DB, _CKInt, _CKDB}) ->
     ets:tab2list(DB).
 
-%{DB, _CKInt, _CKDB}
--spec get_chunk(db(), ?RT:key() | '$end_of_table', pos_integer()) -> {?RT:key() | '$end_of_table', list()}.
-get_chunk(_DB, '$end_of_table', _ChunkSize) ->
-    {'$end_of_table', []};
-get_chunk({ETSDB, _CKInt, _CKDB} = DB, Start, 1) ->
-    case get_entry2(DB, Start) of
-        {true, Entry} ->
-            Next = ets:next(ETSDB, Start),
-            {Next, [Entry]};
-        {false, _} ->
+% @doc tries to return all key-value pairs of the given DB which are in the
+% given interval but at most ChunkSize elements
+% assumes the ets-table is an ordered_set
+% may return data from "both ends" of the DB-range if Begin(interval) > End(Interval)
+% returns the chunk and the remaining interval
+-spec get_chunk(DB::db(), Interval::intervals:interval(), ChunkSize::pos_integer()) ->
+    {intervals:interval(), chunk()}.
+get_chunk(DB, Interval, ChunkSize) ->
+    % assert ChunkSize > 0, see ChunkSize type
+    case get_load(DB) of
+        0 -> {intervals:empty(), []};
+        _ ->
+            %ct:pal("0: ~p ~p", [intervals:get_bounds(Interval), Interval]),
+            {'[', Begin, End, ')'} = intervals:get_bounds(Interval),
+            {Next, Chunk} = get_chunk_inner_start(DB, Begin, Interval, ChunkSize),
+            case Next of
+                '$end_of_table' ->
+                    {intervals:empty(), Chunk};
+                _ ->
+                    {intervals:new({'[', Begin, End, ')'}), Chunk}
+            end
+    end.
+
+% @doc find first real item in range
+-spec get_chunk_inner_start(DB::db(),
+                            Next::?RT:key() | '$end_of_table',
+                            Interval::intervals:interval(),
+                            ChunkSize::pos_integer()) ->
+    {?RT:key() | '$end_of_table', chunk()}.
+get_chunk_inner_start({ETSDB, _CKInt, _CKDB} = DB, '$end_of_table', Interval, ChunkSize) ->
+    %ct:pal("start: 0: ~p", ['$end_of_table']),
+    get_chunk_inner_start(DB, ets:first(ETSDB), Interval, ChunkSize);
+get_chunk_inner_start({ETSDB, _CKInt, _CKDB} = DB, Current, Interval, ChunkSize) ->
+    %ct:pal("start: 1: ~p", [Current]),
+    case intervals:in(Current, Interval) of
+        true ->
+            case get_entry2(DB, Current) of
+                {true, Entry} ->
+                    % Current is the first real entry and it is in the given range
+                    Next = ets:next(ETSDB, Current),
+                    case intervals:in(Next, Interval) of
+                        true ->
+                            get_chunk_inner(DB, Next, Current, Interval,
+                                            ChunkSize - 1, [Entry]);
+                        false ->
+                            {'$end_of_table', [Entry]}
+                    end;
+                {false, _} ->
+                    Next = ets:next(ETSDB, Current),
+                    get_chunk_inner_start(DB, Next, Interval, ChunkSize)
+            end;
+        false ->
+            % found nothing in range
             {'$end_of_table', []}
+    end.
+
+
+% @doc inner loop for get_chunk
+-spec get_chunk_inner(DB::db(),
+                      Next::?RT:key() | '$end_of_table',
+                      RealStart::?RT:key(),
+                      Interval::intervals:interval(),
+                      ChunkSize::pos_integer(),
+                      Chunk::chunk()) ->
+    {?RT:key() | '$end_of_table', chunk()}.
+get_chunk_inner(_DB, Next, _RealStart, _Interval, 0, Chunk) ->
+    %ct:pal("inner: 0: ~p", [Next]),
+    % we hit the chunk size limit
+    {Next, Chunk};
+get_chunk_inner(_DB, RealStart, RealStart, _Interval, _ChunkSize, Chunk) ->
+    %ct:pal("inner: 1: ~p", [RealStart]),
+    % we hit the start element
+    {RealStart, Chunk};
+get_chunk_inner({ETSDB, _CKInt, _CKDB} = DB, '$end_of_table', RealStart, Interval, ChunkSize, Chunk) ->
+    %ct:pal("inner: 2: ~p", ['$end_of_table']),
+    First = ets:first(ETSDB),
+    case intervals:in(First, Interval) of
+        true ->
+            get_chunk_inner(DB, First, RealStart, Interval, ChunkSize, Chunk); %continue
+        false ->
+            {'$end_of_table', Chunk} %done
     end;
-get_chunk({ETSDB, _CKInt, _CKDB} = DB, Start, ChunkSize) ->
-    case get_entry2(DB, Start) of
+get_chunk_inner({ETSDB, _CKInt, _CKDB} = DB, Current, RealStart, Interval, ChunkSize, Chunk) ->
+    %ct:pal("inner: 3: ~p", [Current]),
+    % pre: Current is in Interval
+    case get_entry2(DB, Current) of
         {true, Entry} ->
-            {Next, Chunk} = get_chunk(DB, ets:next(ETSDB, Start), ChunkSize - 1),
-            {Next, [Entry | Chunk]};
+            Next = ets:next(ETSDB, Current),
+            case intervals:in(Next, Interval) of
+                true ->
+                    get_chunk_inner(DB, Next, RealStart, Interval, ChunkSize - 1, [Entry | Chunk]);
+                false ->
+                    {'$end_of_table', [Entry | Chunk]}
+            end;
         {false, _} ->
-            get_chunk(DB, ets:next(ETSDB, Start), ChunkSize - 1)
+            Next = ets:next(ETSDB, Current),
+            get_chunk_inner(DB, Next, RealStart, Interval, ChunkSize, Chunk)
     end.
 
 -define(ETS, ets).
 -include("db_generic_ets.hrl").
-
--- test/db_ets_SUITE.erl
+++ test/db_ets_SUITE.erl
@@ -27,10 +27,70 @@
 
 -include("db_SUITE.hrl").
 
-all() -> tests_avail().
+%all() -> tests_avail() ++ [get_chunk].
+all() -> [get_chunk].
 
 %% @doc Specify how often a read/write suite can be executed in order not to
 %%      hit a timeout (depending on the speed of the DB implementation).
 -spec max_rw_tests_per_suite() -> pos_integer().
 max_rw_tests_per_suite() ->
     10000.
+
+
+get_chunk(Config) ->
+    tester_get_chunk(Config).
+
+-spec prop_get_chunk(Keys::list(?RT:key()), Begin::?RT:key(), End::?RT:key()) -> true.
+prop_get_chunk(Keys2, Begin, End) ->
+    case Begin /= End of
+        true ->
+            Keys = lists:usort(Keys2),
+            DB = db_ets:new(),
+            DB2 = fill_db(DB, Keys),
+            ChunkSize = 5,
+            Interval = intervals:new('[', Begin, End, ')'),
+            {Next, Chunk} = db_ets:get_chunk(DB2, Interval, ChunkSize),
+            db_ets:close(DB2),
+            case length(Chunk) /= length(lists:usort(Chunk)) of
+                true ->
+                    ct:pal("chunk contains duplicates ~p ~p ~p ~p", [Chunk, Keys, Begin, End]),
+                    false;
+                false ->
+                    ExpectedChunkSize = util:min(count_keys_in_range(Keys, Interval),
+                                            ChunkSize),
+                    case ExpectedChunkSize /= length(Chunk) of
+                        true ->
+                            ct:pal("chunk has wrong size ~p ~p ~p ~p", [Chunk, Keys, Begin, End]),
+                            false;
+                        false ->
+                            case check_chunk_entries_are_in_interval(Chunk, Interval) of
+                                false ->
+                                    ct:pal("check_chunk_entries_are_in_interval failed ~p ~p ~p ~p",
+                                           [Chunk, Keys, Begin, End]),
+                                    false;
+                                true ->
+                                    true
+                            end
+                    end
+            end;
+        false ->
+            true
+    end.
+
+tester_get_chunk(_Config) ->
+    tester:test(?MODULE, prop_get_chunk, 3, rw_suite_runs(10)).
+
+fill_db(DB, []) ->
+    DB;
+fill_db(DB, [Key | Rest]) ->
+    fill_db(db_ets:write(DB, Key, "Value", 1), Rest).
+
+check_chunk_entries_are_in_interval(Entries, Interval) ->
+    lists:all(fun (Entry) ->
+                      intervals:in(db_entry:get_key(Entry), Interval)
+              end, Entries).
+
+count_keys_in_range(Keys, Interval) ->
+    length(lists:filter(fun(Key) ->
+                                intervals:in(Key, Interval)
+                        end, Keys)).
