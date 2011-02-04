c2b6d830b2e911969420595173ba7302777efaee
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-02-04 15:29:38 +0000

    - moved unit test from cs_api_v2_write_timeout_SUITE to cs_api_v2_SUITE

--- trunk/test/cs_api_v2_SUITE.erl
+++ trunk/test/cs_api_v2_SUITE.erl
@@ -28,7 +28,7 @@
 -include("unittest.hrl").
 
 all() ->
-    [read, write, test_and_set].
+    [read, write, test_and_set, write_test_race_mult_rings].
 
 suite() ->
     [
@@ -36,14 +36,27 @@ suite() ->
     ].
 
 init_per_suite(Config) ->
-    Config2 = unittest_helper:init_per_suite(Config),
-    {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config2),
-    unittest_helper:make_ring(4, [{config, [{log_path, PrivDir}]}]),
-    Config2.
+    unittest_helper:init_per_suite(Config).
 
 end_per_suite(Config) ->
     unittest_helper:end_per_suite(Config),
     ok.
+
+init_per_testcase(TestCase, Config) ->
+    case TestCase of
+        write_test_race_mult_rings ->
+            Config;
+        _ ->
+            % stop ring from previous test case (it may have run into a timeout)
+            unittest_helper:stop_ring(),
+            {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
+            unittest_helper:make_ring(4, [{config, [{log_path, PrivDir}]}]),
+            Config
+    end.
+
+end_per_testcase(_TestCase, Config) ->
+    unittest_helper:stop_ring(),
+    Config.
 
 read(_Config) ->
     ?equals(cs_api_v2:read("ReadKey"), {fail, not_found}),
@@ -63,3 +76,52 @@ test_and_set(_Config) ->
     ?equals(cs_api_v2:test_and_set("TestAndSetKey", "", "IsSet"), {fail, {key_changed, "IsSet"}}),
     ?equals(cs_api_v2:test_and_set("TestAndSetKey", "IsSet", "IsSet2"), ok),
     ok.
+
+
+%% @doc Test for cs_api_v2:write taking at least 2s after stopping a ring and
+%%      starting a new one.
+write_test_race_mult_rings(_Config) ->
+    % first ring:
+    write_test(),
+%%     timer:sleep(10000),
+    % second ring and more:
+    write_test(),
+    write_test(),
+    write_test(),
+    write_test(),
+    write_test(),
+    write_test(),
+    write_test().
+
+-spec write_test() -> ok.
+write_test() -> 
+    OldRegistered = erlang:registered(),
+    OldProcesses = unittest_helper:get_processes(),
+    unittest_helper:make_ring(1),
+    Self = self(),
+    BenchPid1 = erlang:spawn(fun() ->
+                                     {Time, _} = util:tc(cs_api_v2, write, [1, 1]),
+                                     comm:send_local(Self, {time, Time}),
+                                     ct:pal("~.0pus~n", [Time])
+                             end),
+    receive {time, FirstWriteTime} -> ok
+    end,
+    unittest_helper:wait_for_process_to_die(BenchPid1),
+    BenchPid2 = erlang:spawn(fun() ->
+                                     {Time, _} = util:tc(cs_api_v2, write, [2, 2]),
+                                     comm:send_local(Self, {time, Time}),
+                                     ct:pal("~.0pus~n", [Time])
+                             end),
+    receive {time, SecondWriteTime} -> ok
+    end,
+    unittest_helper:wait_for_process_to_die(BenchPid2),
+    dht_node_move_SUITE:check_size2_v2(4  * 2),
+    unittest_helper:stop_ring(),
+%%     randoms:stop(), %doesn't matter
+    inets:stop(),
+    unittest_helper:kill_new_processes(OldProcesses),
+    {_, _, OnlyNewReg} =
+        util:split_unique(OldRegistered, erlang:registered()),
+    ct:pal("NewReg: ~.0p~n", [OnlyNewReg]),
+    ?equals_pattern(FirstWriteTime, X when X =< 1000000),
+    ?equals_pattern(SecondWriteTime, X when X =< 1000000).
