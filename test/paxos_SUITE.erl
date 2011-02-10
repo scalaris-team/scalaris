8cc86a160f4b98538a8dcb9e001bc3081a8e1c3e
kruber@zib.de <kruber@zib.de>
2011-02-10 09:45:36 +0000

    - fixed paxos SUITE:  + fixed wrong call of msg_delay:start_link/1  + fixed several wrong types in calls to other methods, especially thos with pid_groups:groupname()  + correctly extract the local pid from a comm:mypid() with comm:make_local/1  + added some type specs

--- test/paxos_SUITE.erl
+++ test/paxos_SUITE.erl
@@ -46,24 +46,33 @@ end_per_suite(Config) ->
     unittest_helper:end_per_suite(Config),
     ok.
 
+-spec make_groupname(Prefix::string(), Number::integer()) -> pid_groups:groupname().
+make_groupname(Prefix, Number) ->
+    lists:flatten(io_lib:format("~s.~B", [Prefix, Number])).
+
 %% make proposers, acceptors, and learners
+-spec make(P::pos_integer(), A::pos_integer(), L::pos_integer(), Prefix::string())
+        -> {Ps::[comm:mypid(),...], As::[comm:mypid(),...], Ls::[comm:mypid(),...]}.
 make(P, A, L, Prefix) ->
     NumMDs = lists:max([P,A,L]),
-    [ msg_delay:start_link({Prefix, X}) || X <- lists:seq(1,NumMDs)],
-    Ps = [ comm:make_global(element(2, proposer:start_link({Prefix, X})))
-           || X <- lists:seq(1,P)],
-    As = [ comm:make_global(element(2, acceptor:start_link({Prefix, X})))
-           || X <- lists:seq(1,A)],
-    Ls = [ comm:make_global(element(2, learner:start_link({Prefix, X})))
-           || X <- lists:seq(1,L)],
+    _  = [ msg_delay:start_link(make_groupname(Prefix, X))
+             || X <- lists:seq(1, NumMDs)],
+    Ps = [ comm:make_global(element(2, proposer:start_link(make_groupname(Prefix, X))))
+             || X <- lists:seq(1, P)],
+    As = [ comm:make_global(element(2, acceptor:start_link(make_groupname(Prefix, X))))
+             || X <- lists:seq(1, A)],
+    Ls = [ comm:make_global(element(2, learner:start_link(make_groupname(Prefix, X))))
+             || X <- lists:seq(1,L)],
     {Ps, As, Ls}.
 
+-spec collector(Count::pos_integer(), Owner::pid()) -> pid().
 collector(Count, Owner) ->
     spawn(fun() ->
                   collect(Count),
                   Owner ! done
           end).
 
+-spec collect(non_neg_integer()) -> ok.
 collect(0) ->
     ok;
 collect(Count) ->
@@ -74,6 +83,7 @@ collect(Count) ->
 %%             collect(Count - 1)
     end.
 
+-spec tester_fast_paxos(CountAcceptors::pos_integer(), Count::pos_integer(), Prefix::string()) -> ok.
 tester_fast_paxos(CountAcceptors, Count, Prefix) ->
     %% Count = 10,
     CountProposers = 1,
@@ -84,19 +94,17 @@ tester_fast_paxos(CountAcceptors, Count, Prefix) ->
 
     Collector = comm:make_global(collector(Count, self())),
 
-    [learner:start_paxosid(hd(Learners), Id, Majority, Collector, chocolate_chip_cookie)
-     || Id <- lists:seq(1, Count)],
-    [acceptor:start_paxosid(X, Id, Learners)
-     || X <- Acceptors,  Id <- lists:seq(1, Count)],
-    [proposer:start_paxosid(hd(Proposers), Id, Acceptors, prepared,
-                            Majority, CountProposers, 0)
-     || Id <- lists:seq(1, Count)],
-    receive
-        done ->
-            ok
-    end,
-    ok.
+    _ = [ learner:start_paxosid(hd(Learners), Id, Majority, Collector, chocolate_chip_cookie)
+            || Id <- lists:seq(1, Count)],
+    _ = [ acceptor:start_paxosid(X, Id, Learners)
+            || X <- Acceptors,  Id <- lists:seq(1, Count)],
+    _ = [ proposer:start_paxosid(hd(Proposers), Id, Acceptors, prepared,
+                                 Majority, CountProposers, 0)
+            || Id <- lists:seq(1, Count)],
+    receive done -> ok
+    end.
 
+-spec tester_paxos(CountAcceptors::pos_integer(), Count::pos_integer(), Prefix::string()) -> ok.
 tester_paxos(CountAcceptors, Count, Prefix) ->
     CountProposers = 1,
     Majority = CountAcceptors div 2 + 1,
@@ -105,24 +113,21 @@ tester_paxos(CountAcceptors, Count, Prefix) ->
 
     Collector = comm:make_global(collector(Count, self())),
 
-    spawn(fun() ->
-                  [learner:start_paxosid(hd(Learners), Id, Majority, Collector, chocolate_chip_cookie)
-                   || Id <- lists:seq(1, Count)]
-          end),
-    spawn(fun() ->
-                  [acceptor:start_paxosid(X, Id, Learners)
-                   || X <- Acceptors,  Id <- lists:seq(1, Count)]
+    _ = spawn(fun() ->
+                      [ learner:start_paxosid(hd(Learners), Id, Majority, Collector, chocolate_chip_cookie)
+                          || Id <- lists:seq(1, Count)]
+              end),
+    _ = spawn(fun() ->
+                      [ acceptor:start_paxosid(X, Id, Learners)
+                          || X <- Acceptors,  Id <- lists:seq(1, Count)]
            end),
-    spawn( fun() ->
-                   [proposer:start_paxosid(hd(Proposers), Id, Acceptors,
-                                           prepared, Majority, CountProposers)
-                    || Id <- lists:seq(1, Count)]
-           end),
-    receive
-        done ->
-            ok
-    end,
-    ok.
+    _ = spawn(fun() ->
+                      [ proposer:start_paxosid(hd(Proposers), Id, Acceptors,
+                                               prepared, Majority, CountProposers)
+                          || Id <- lists:seq(1, Count)]
+              end),
+    receive done -> ok
+    end.
 
 test_fast_acceptors_4(_Config) ->
     Count = 10000,
@@ -162,15 +167,15 @@ test_two_proposers(_Config) ->
     CountAcceptors = 4,
     Majority = CountAcceptors div 2 + 1,
     {Proposers, Acceptors, Learners} =
-        make(CountProposers, CountAcceptors, 1, two_proposers),
+        make(CountProposers, CountAcceptors, 1, "two_proposers"),
 
     %% start paxosids in the components
     learner:start_paxosid(hd(Learners), paxid123, Majority, comm:this(), cpaxid123),
-    [ acceptor:start_paxosid(X, paxid123, Learners) || X <- Acceptors ],
+    _ = [ acceptor:start_paxosid(X, paxid123, Learners) || X <- Acceptors ],
     [ Proposer1, Proposer2 ] = Proposers,
 
     %% set some breakpoints
-    gen_component:bp_set(element(3, Proposer1), acceptor_ack, bp1),
+    gen_component:bp_set(comm:make_local(Proposer1), acceptor_ack, bp1),
     %% initiate full paxos
     proposer:start_paxosid(Proposer1, paxid123, Acceptors,
                            prepared, Majority, length(Proposers), 3),
@@ -180,9 +185,9 @@ test_two_proposers(_Config) ->
     %% should receive an abort
     receive {learner_decide, cpaxid123, _, Res1} = Any -> io:format("Expected abort Received ~p~n", [Any]) end,
 
-    gen_component:bp_barrier(element(3, Proposer1)),
-    gen_component:bp_del(element(3, Proposer1), bp1),
-    gen_component:bp_cont(element(3, Proposer1)),
+    gen_component:bp_barrier(comm:make_local(Proposer1)),
+    gen_component:bp_del(comm:make_local(Proposer1), bp1),
+    gen_component:bp_cont(comm:make_local(Proposer1)),
 
     %% should receive also an abort as proposer1 was hold
     receive {learner_decide, cpaxid123, _, Res2} = Any2 ->
@@ -194,10 +199,10 @@ test_two_proposers(_Config) ->
 
     %% start paxosids in the components
     learner:start_paxosid(hd(Learners), paxid124, Majority, comm:this(), cpaxid124),
-    [ acceptor:start_paxosid(X, paxid124, Learners) || X <- Acceptors ],
+    _ = [ acceptor:start_paxosid(X, paxid124, Learners) || X <- Acceptors ],
     [ Proposer1, Proposer2 ] = Proposers,
     %% set some breakpoints
-    gen_component:bp_set(element(3, Proposer2), acceptor_ack, bp2),
+    gen_component:bp_set(comm:make_local(Proposer2), acceptor_ack, bp2),
     %% initiate full paxos
     proposer:start_paxosid(Proposer1, paxid124, Acceptors,
                            prepared, Majority, length(Proposers), 1),
@@ -207,9 +212,9 @@ test_two_proposers(_Config) ->
     %% should receive a prepared as proposer2 was hold
     receive {learner_decide, cpaxid124, _, Res3} = Any3 -> io:format("Expected prepared Received ~p~n", [Any3]) end,
 
-    gen_component:bp_barrier(element(3, Proposer2)),
-    gen_component:bp_del(element(3, Proposer2), bp2),
-    gen_component:bp_cont(element(3, Proposer2)),
+    gen_component:bp_barrier(comm:make_local(Proposer2)),
+    gen_component:bp_del(comm:make_local(Proposer2), bp2),
+    gen_component:bp_cont(comm:make_local(Proposer2)),
 
     %% should receive also an abort
     receive
@@ -222,42 +227,42 @@ test_two_proposers(_Config) ->
 
 %% userdevguide-begin paxos_SUITE:random_interleaving_test
 -spec prop_rnd_interleave(1..4, 4..16, {pos_integer(), pos_integer(), pos_integer()})
-        -> boolean().
+        -> true.
 prop_rnd_interleave(NumProposers, NumAcceptors, Seed) ->
     ct:pal("Called with: paxos_SUITE:prop_rnd_interleave(~p, ~p, ~p).~n",
            [NumProposers, NumAcceptors, Seed]),
     Majority = NumAcceptors div 2 + 1,
     {Proposers, Acceptors, Learners} =
-        make(NumProposers, NumAcceptors, 1, rnd_interleave),
+        make(NumProposers, NumAcceptors, 1, "rnd_interleave"),
     %% set bp on all processes
-    [ gen_component:bp_set(element(3, X), proposer_initialize, bp)
-      ||  X <- Proposers],
-    [ gen_component:bp_set(element(3, X), acceptor_initialize, bp)
-      ||  X <- Acceptors ],
-    [ gen_component:bp_set(element(3, X), learner_initialize, bp)
-      || X <- Learners],
+    _ = [ gen_component:bp_set(comm:make_local(X), proposer_initialize, bp)
+            ||  X <- Proposers],
+    _ = [ gen_component:bp_set(comm:make_local(X), acceptor_initialize, bp)
+            ||  X <- Acceptors ],
+    _ = [ gen_component:bp_set(comm:make_local(X), learner_initialize, bp)
+            || X <- Learners],
     %% start paxos instances
-    [ proposer:start_paxosid(X, paxidrndinterl, Acceptors,
-                             proposal, Majority, NumProposers, Y)
-      || {X,Y} <- lists:zip(Proposers, lists:seq(1, NumProposers)) ],
-    [ acceptor:start_paxosid(X, paxidrndinterl, Learners)
-      || X <- Acceptors ],
-    [ learner:start_paxosid(X, paxidrndinterl, Majority,
-                            comm:this(), cpaxidrndinterl)
-      || X <- Learners],
+    _ = [ proposer:start_paxosid(X, paxidrndinterl, Acceptors,
+                                 proposal, Majority, NumProposers, Y)
+            || {X,Y} <- lists:zip(Proposers, lists:seq(1, NumProposers)) ],
+    _ = [ acceptor:start_paxosid(X, paxidrndinterl, Learners)
+            || X <- Acceptors ],
+    _ = [ learner:start_paxosid(X, paxidrndinterl, Majority,
+                                comm:this(), cpaxidrndinterl)
+            || X <- Learners],
     %% randomly step through protocol
     OldSeed = random:seed(Seed),
     Steps = step_until_decide(Proposers ++ Acceptors ++ Learners, cpaxidrndinterl, 0),
     ct:pal("Needed ~p steps~n", [Steps]),
-    case OldSeed of
-        undefined -> ok;
-        _ -> random:seed(OldSeed)
-    end,
+    _ = case OldSeed of
+            undefined -> ok;
+            _ -> random:seed(OldSeed)
+        end,
     true.
 
 step_until_decide(Processes, PaxId, SumSteps) ->
     %% io:format("Step ~p~n", [SumSteps]),
-    Runnable = [ X || X <- Processes, gen_component:runnable(element(3,X)) ],
+    Runnable = [ X || X <- Processes, gen_component:runnable(comm:make_local(X)) ],
     case Runnable of
         [] ->
             ct:pal("No runnable processes of ~p~n", [length(Processes)]),
@@ -265,7 +270,7 @@ step_until_decide(Processes, PaxId, SumSteps) ->
         _ -> ok
     end,
     Num = random:uniform(length(Runnable)),
-    gen_component:bp_step(element(3,lists:nth(Num, Runnable))),
+    _ = gen_component:bp_step(comm:make_local(lists:nth(Num, Runnable))),
     receive
         {learner_decide, cpaxidrndinterl, _, _Res} = _Any ->
             %% io:format("Received ~p~n", [_Any]),
