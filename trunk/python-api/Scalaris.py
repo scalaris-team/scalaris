fc3f234f3113889cabb876cda63e1f3f88d23c22
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-03-17 17:21:34 +0000

    - matured Python-JSON-API which now behaves similar to Java (including exceptions) - completed the Python benchmark implementation

--- trunk/python-api/Scalaris.py
+++ trunk/python-api/Scalaris.py
@@ -13,12 +13,12 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import json, httplib, urlparse
+import json, httplib, urlparse,  socket
 import sys
 import base64
-#import urllib
 
 default_url = 'http://localhost:8000/jsonrpc.yaws'
+default_timeout = 5 # socket timeout in seconds
 
 def _json_call(conn,  uri,  function, params):
     params = {'version': '1.1',
@@ -31,12 +31,16 @@ def _json_call(conn,  uri,  function, params):
     #print params_json
     # no need to quote - we already encode to json:
     headers = {"Content-type": "application/json"}
-    #conn.request("POST", uri.path, urllib.quote(params_json), headers)
-    conn.request("POST", uri.path, params_json, headers)
-    response = conn.getresponse()
-    # TODO: handle connection error
-    #print response.status, response.reason
-    data = response.read()
+    try:
+        #conn.request("POST", uri.path, urllib.quote(params_json), headers)
+        conn.request("POST", uri.path, params_json, headers)
+        response = conn.getresponse()
+        #print response.status, response.reason
+        if (response.status < 200 or response.status >= 300):
+            raise ConnectionException(response)
+        data = response.read()
+    except socket.timeout,  (instance):
+        raise ConnectionException(instance)
     #print data
     response_json = json.loads(data)
     #print response_json
@@ -49,115 +53,247 @@ def _json_encode_value(value):
         return {'type': 'as_is',  'value': value}
 
 def _json_decode_value(value):
+    if ('type' not in value) or ('value' not in value):
+        raise UnknownException(value)
     if value['type'] == 'as_bin':
         return bytearray(base64.b64decode(value['value']),  'base64')
     else:
         return value['value']
 
-def _json_replace_value_in_result(result):
-    if 'value' in result:
-        result['value'] = _json_decode_value(result['value'])
-    return result
+def getConnection(url,  timeout = default_timeout):
+    uri = urlparse.urlparse(url)
+    return httplib.HTTPConnection(uri.hostname,  uri.port, timeout = timeout)
+
+# Exception that is thrown if a the commit of a write operation on a
+# scalaris ring fails.
+class AbortException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
+
+# Exception that is thrown if an operation on a scalaris ring fails because
+# a connection does not exist or has been disconnected.
+class ConnectionException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
+
+# Exception that is thrown if a test_and_set operation on a scalaris ring
+# fails because the old value did not match the expected value.
+class KeyChangedException(Exception):
+   def __init__(self, raw_result,  old_value):
+       self.raw_result = raw_result
+       self.old_value = old_value
+   def __str__(self):
+       return repr(self.raw_result) + ', old value: ' + repr(self.old_value)
+
+# Exception that is thrown if a delete operation on a scalaris ring fails
+# because no scalaris node was found.
+class NodeNotFoundException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
+
+# Exception that is thrown if a read operation on a scalaris ring fails
+# because the key did not exist before.
+class NotFoundException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
+
+# Exception that is thrown if a read or write operation on a scalaris ring
+#fails due to a timeout.
+class TimeoutException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
+
+# Generic exception that is thrown during operations on a scalaris ring, e.g.
+# if an unknown result has been returned.
+class UnknownException(Exception):
+   def __init__(self, raw_result):
+       self.raw_result = raw_result
+   def __str__(self):
+       return repr(self.raw_result)
 
 class TransactionSingleOp(object):
-    def __init__(self,  **parameters):
-        if ('url' in parameters):
-            self.uri = urlparse.urlparse(parameters['url'])
+    def __init__(self, url = None,  conn = None,  timeout = default_timeout):
+        if (url == None):
+            self._uri = urlparse.urlparse(default_url)
         else:
-            self.uri = urlparse.urlparse(default_url )
+            self._uri = urlparse.urlparse(url)
         
-        if ('conn' in parameters):
-            self.conn = parameters['conn']
+        if (conn == None):
+            try:
+                self._conn = httplib.HTTPConnection(self._uri.hostname,  self._uri.port,  timeout = timeout)
+            except Exception,  (instance):
+                raise ConnectionException(instance)
         else:
-            self.conn = httplib.HTTPConnection(self.uri.hostname,  self.uri.port)
+            self._conn = conn
+
+    def read(self,  key):
+        result = _json_call(self._conn,  self._uri, 'read', [key])
+        # results: {'status': 'ok', 'value', xxx} or
+        #          {'status': 'fail', 'reason': 'timeout' or 'not_found'}
+        if isinstance(result,  dict) and 'status' in result:
+            if result['status'] == 'ok' and 'value' in result:
+                return _json_decode_value(result['value'])
+            elif result['status'] == 'fail' and 'reason' in result:
+                if result['reason'] == 'timeout':
+                    raise TimeoutException(result)
+                elif result['reason'] == 'not_found':
+                    raise NotFoundException(result)
+                else:
+                    raise UnknownException(result)
+            else:
+                raise UnknownException(result)
+        else:
+            raise UnknownException(result)
+
+    def write(self,  key, value):
+        value = _json_encode_value(value)
+        result = _json_call(self._conn,  self._uri, 'write', [key, value])
+        # results: {'status': 'ok'} or
+        #          {'status': 'fail', 'reason': 'timeout' or 'abort'}
+        if isinstance(result,  dict) and 'status' in result:
+            if result['status'] == 'ok':
+                return None
+            elif result['status'] == 'fail' and 'reason' in result:
+                if result['reason'] == 'timeout':
+                    raise TimeoutException(result)
+                elif result['reason'] == 'abort':
+                    raise AbortException(result)
+                else:
+                    raise UnknownException(result)
+            else:
+                raise UnknownException(result)
+        else:
+            raise UnknownException(result)
     
-    # results: {'status': 'ok'} or
-    #              {'status': 'fail', 'reason': 'timeout' or 'abort' or 'not_found'} or
-    #              {'status': 'fail', 'reason': 'key_changed', 'value': xxx}
     def testAndSet(self,  key, oldvalue, newvalue):
         oldvalue = _json_encode_value(oldvalue)
         newvalue = _json_encode_value(newvalue)
-        result = _json_call(self.conn,  self.uri, 'test_and_set', [key, oldvalue, newvalue])
-        return _json_replace_value_in_result(result)
+        result = _json_call(self._conn,  self._uri, 'test_and_set', [key, oldvalue, newvalue])
+        # results: {'status': 'ok'} or
+        #          {'status': 'fail', 'reason': 'timeout' or 'abort' or 'not_found'} or
+        #          {'status': 'fail', 'reason': 'key_changed', 'value': xxx}
+        if isinstance(result,  dict) and 'status' in result:
+            if result['status'] == 'ok':
+                return None
+            elif result['status'] == 'fail' and 'reason' in result:
+                if result['reason'] == 'timeout':
+                    raise TimeoutException(result)
+                elif result['reason'] == 'abort':
+                    raise AbortException(result)
+                elif result['reason'] == 'not_found':
+                    raise NotFoundException(result)
+                elif result['reason'] == 'key_changed' and 'value' in result:
+                    raise KeyChangedException(result,  _json_decode_value(result['value']))
+                else:
+                    raise UnknownException(result)
+            else:
+                raise UnknownException(result)
+        else:
+            raise UnknownException(result)
 
-    # results: {'status': 'ok', 'value', xxx} or
-    #              {'status': 'fail', 'reason': 'timeout' or 'not_found'}
-    def read(self,  key):
-        result = _json_call(self.conn,  self.uri, 'read', [key])
-        return _json_replace_value_in_result(result)
-
-    # results: {'status': 'ok'} or
-    #              {'status': 'fail', 'reason': 'timeout' or 'abort'}
-    def write(self,  key, value):
-        value = _json_encode_value(value)
-        return _json_call(self.conn,  self.uri, 'write', [key, value])
-
-    # results: 'ok'
     def nop(self,  value):
         value = _json_encode_value(value)
-        return _json_call(self.conn,  self.uri, 'nop', [value])
+        _json_call(self._conn,  self._uri, 'nop', [value])
+        # results: 'ok'
     
     def closeConnection(self):
-        self.conn.close()
-    
-    def __del__ (self):
-        self.conn.close()
+        self._conn.close()
 
 class Transaction(object):
-    def __init__(self,  **parameters):
-        if ('url' in parameters):
-            self.uri = urlparse.urlparse(parameters['url'])
+    def __init__(self, url = None,  conn = None,  timeout = default_timeout):
+        if (url == None):
+            self._uri = urlparse.urlparse(default_url )
         else:
-            self.uri = urlparse.urlparse(default_url )
+            self._uri = urlparse.urlparse(url)
         
-        if ('conn' in parameters):
-            self.conn = parameters['conn']
+        if (conn == None):
+            try:
+                self._conn = httplib.HTTPConnection(self._uri.hostname,  self._uri.port,  timeout = timeout)
+            except Exception,  (instance):
+                raise ConnectionException(instance)
         else:
-            self.conn = httplib.HTTPConnection(self.uri.hostname,  self.uri.port)
+            self._conn = conn
         
-        self.tlog = None
+        self._tlog = None
     
-    # results: [{'status': 'ok'} or {'status': 'ok', 'value': xxx} or
-    #              {'status': 'fail', 'reason': 'timeout' or 'abort' or 'not_found}]
     def req_list(self,  reqlist):
-        if self.tlog == None:
-            result = _json_call(self.conn,  self.uri, 'req_list', [reqlist.getJSONRequests()])
+        if self._tlog == None:
+            result = _json_call(self._conn,  self._uri, 'req_list', [reqlist.getJSONRequests()])
         else:
-            result = _json_call(self.conn,  self.uri, 'req_list', [self.tlog, reqlist.getJSONRequests()])
-        self.tlog = result['tlog']
-        resultlist = [_json_replace_value_in_result(res) for res in result['results']]
-        return resultlist
+            result = _json_call(self._conn,  self._uri, 'req_list', [self._tlog, reqlist.getJSONRequests()])
+        # results: {'tlog': xxx,
+        #           'results': [{'status': 'ok'} or {'status': 'ok', 'value': xxx} or
+        #                       {'status': 'fail', 'reason': 'timeout' or 'abort' or 'not_found}]}
+        if 'tlog' not in result or 'results' not in result or not isinstance(result['results'],  list) or len(result['results']) < 1:
+            raise UnknownException(result)
+        self._tlog = result['tlog']
+        return result['results']
+    
+    # result: {'status': 'ok'} or {'status': 'ok', 'value': xxx} or
+    #         {'status': 'fail', 'reason': 'timeout' or 'abort' or 'not_found}
+    # op: 'read' or 'write' or 'commit' or 'any'
+    def process_result(self,  result,  op):
+        if isinstance(result,  dict) and 'status' in result:
+            if result['status'] == 'ok':
+                if 'value' in result:
+                    return _json_decode_value(result['value'])
+                elif op == 'read':
+                    raise UnknownException((result,  op))
+            elif result['status'] == 'fail' and 'reason' in result:
+                if result['reason'] == 'timeout':
+                    raise TimeoutException(result)
+                elif result['reason'] == 'not_found' and op not in ['commit',  'write']:
+                    raise NotFoundException(result)
+                elif result['reason'] == 'abort' and op not in ['read',  'write']:
+                    raise AbortException(result)
+                else:
+                    raise UnknownException(result)
+            else:
+                raise UnknownException(result)
+        else:
+            raise UnknownException(result)
     
     # results: {'status': 'ok'} or
-    #              {'status': 'fail', 'reason': 'timeout' or 'abort'}
+    #          {'status': 'fail', 'reason': 'timeout' or 'abort'}
     def commit(self):
         result = self.req_list(ReqList().addCommit())[0]
+        self.process_result(result,  'commit')
         # reset tlog (minor optimization which is not done in req_list):
-        self.tlog = None
-        return result
+        self._tlog = None
     
     # results: None
     def abort(self):
-        self.tlog = None
+        self._tlog = None
     
     # results: {'status': 'ok', 'value', xxx} or
     #              {'status': 'fail', 'reason': 'timeout' or 'not_found'}
     def read(self,  key):
-        return self.req_list(ReqList().addRead(key))[0]
+        result = self.req_list(ReqList().addRead(key))[0]
+        return self.process_result(result,  'commit')
     
     # results: {'status': 'ok'} or
     #              {'status': 'fail', 'reason': 'timeout' or 'abort'}
     def write(self,  key,  value):
-        return self.req_list(ReqList().addWrite(key,  value))[0]
+        result = self.req_list(ReqList().addWrite(key,  value))[0]
+        self.process_result(result,  'commit')
 
-    # results: 'ok'
     def nop(self,  value):
         value = _json_encode_value(value)
-        return _json_call(self.conn,  self.uri, 'nop', [value])
+        _json_call(self._conn,  self._uri, 'nop', [value])
+        # results: 'ok'
     
-    def __del__ (self):
-        self.tlog = None
-        self.conn.close()
+    def closeConnection(self):
+        self._conn.close()
 
 class ReqList(object):
     def __init__(self):
@@ -192,16 +328,36 @@ if __name__ == "__main__":
     sc = TransactionSingleOp()
     if (sys.argv[1] == "read"):
         key = sys.argv[2]
-        res = sc.read(key)
-        if (res['status'] == 'ok'):
-            print 'read(' + key + ') = ' + res['value']
-        elif (res['status'] == 'fail'):
-            print 'read(' + key + ') failed with ' + res['reason']
+        try:
+            value = sc.read(key)
+            print 'read(' + key + ') = ' + rep(value)
+        except ConnectionException,  (instance):
+            print 'read(' + key + ') failed with connection error'
+            sys.exit(1)
+        except TimeoutException,  (instance):
+            print 'read(' + key + ') failed with timeout'
+            sys.exit(1)
+        except NotFoundException,  (instance):
+            print 'read(' + key + ') failed with not_found'
+            sys.exit(1)
+        except UnknownException,  (instance):
+            print 'read(' + key + ') failed with unknown: ' + str(instance)
+            sys.exit(1)
     elif (sys.argv[1] == "write"):
         key = sys.argv[2]
         value = sys.argv[3]
-        res = sc.write(key,  value)
-        if (res['status'] == 'ok'):
-            print 'write(' + key + ', ' + value + ') = ' + res['status']
-        elif (res['status'] == 'fail'):
-            print 'write(' + key + ', ' + value + ') failed with ' + res['reason']
+        try:
+            sc.write(key,  value)
+            print 'write(' + key + ', ' + value + '): ok'
+        except ConnectionException,  (instance):
+            print 'write(' + key + ', ' + value + ') failed with connection error'
+            sys.exit(1)
+        except TimeoutException,  (instance):
+            print 'write(' + key + ', ' + value + ') failed with timeout'
+            sys.exit(1)
+        except AbortException,  (instance):
+            print 'write(' + key + ', ' + value + ') failed with abort'
+            sys.exit(1)
+        except UnknownException,  (instance):
+            print 'write(' + key + ', ' + value + ') failed with unknown: ' + str(instance)
+            sys.exit(1)
