5ccf397c706a1dbc195ba93618b9d92a9bbc37f4
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2011-04-13 08:51:21 +0000

    - split jsonrpc.rb into a module, a client, and an example file

--- trunk/contrib/jsonrpc.rb
+++ trunk/contrib/jsonrpc.rb
@@ -17,68 +17,9 @@ require 'rubygems'
 require 'json'
 require 'net/http'
 require 'benchmark'
+require 'scalaris'
 
 $url = 'http://localhost:8000/jsonrpc.yaws'
-
-def json_call(function, params)
-  uri = URI.parse $url
-  req = Net::HTTP::Post.new(uri.path)
-  req.add_field 'Content-Type', 'application/json'
-  req.body =
-    {
-    :version => '1.1',
-    :method => function,
-    :params => params,
-    :id => 0}.to_json
-  res = Net::HTTP.start(uri.host, uri.port){|http|http.request(req)}
-  JSON.parse(res.body)['result']
-end
-
-def _json_encode_value(value)
-  # TODO: encode to base64 if binary
-  return { :type => 'as_is',
-           :value => value }
-end
-
-def _json_decode_value(value)
-  if value['type'] == 'as_bin':
-    # TODO: decode from base64 if binary
-    return value['value']
-  else
-    return value['value']
-  end
-end
-
-def _json_replace_value_in_result(result)
-  if result.has_key?('value')
-    result['value'] = _json_decode_value(result['value'])
-  end
-  return result
-end
-
-def _json_replace_value_in_results(results)
-  results.each do |result|
-    _json_replace_value_in_result(result)
-  end
-end
-
-def test_and_set(key, oldvalue, newvalue)
-  result = json_call('test_and_set', [key, _json_encode_value(oldvalue), _json_encode_value(newvalue)])
-  _json_replace_value_in_result(result)
-end
-
-def read(key)
-  result = json_call('read', [key])
-  _json_replace_value_in_result(result)
-end
-
-def write(key, value)
-  json_call('write', [key, _json_encode_value(value)])
-end
-
-def nop(value)
-  json_call('nop', [_json_encode_value(value)])
-end
 
 def req_list()
   puts "Start sample request list run..."
@@ -86,35 +27,35 @@ def req_list()
   # first transaction sets two keys and commits
   puts "  Write keyA, KeyB and commit."
   rlist = []
-  rlist[0] = { :write => {'keyA' => _json_encode_value('valueA')} }
-  rlist[1] = { :write => {'keyB' => _json_encode_value('valueB')} }
+  rlist[0] = { :write => {'keyA' => Scalaris::_json_encode_value('valueA')} }
+  rlist[1] = { :write => {'keyB' => Scalaris::_json_encode_value('valueB')} }
   rlist[2] = { :commit => 'commit' }
 
-  result = json_call('req_list', [rlist])
-  values = _json_replace_value_in_results(result['results'])
+  result = Scalaris::json_call('req_list', [rlist])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
   puts "  Commit was: #{values[2]['value']}."
 
   # second transaction reads two keys and then modifies one of them
   rlist2 = []
   rlist2[0] = { :read => 'keyA' }
   rlist2[1] = { :read => 'keyB' }
-  result = json_call('req_list', [rlist2])
+  result = Scalaris::json_call('req_list', [rlist2])
 
   translog = result['tlog']
-  values = _json_replace_value_in_results(result['results'])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
 
   puts "  Read key #{values[0]['key']} as: #{values[0]['value']}"
   puts "  Read key #{values[1]['key']} as: #{values[1]['value']}"
 
   puts "  Modify keyA and commit."
   rlist3 = []
-  rlist3[0] = { :write => {'keyA' => _json_encode_value('valueA2')} }
+  rlist3[0] = { :write => {'keyA' => Scalaris::_json_encode_value('valueA2')} }
   rlist3[1] = { :commit => 'commit' }
-  result = json_call('req_list', [translog, rlist3])
-  values = _json_replace_value_in_results(result['results'])
+  result = Scalaris::json_call('req_list', [translog, rlist3])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
 
   puts "  Commit was: #{values[1]['value']}#{values[1]['fail']}, write request was #{values[0]['value']}."
-   result = json_call('delete', ["keyA"])
+   result = Scalaris::json_call('delete', ["keyA"])
    puts "  Delete keyA: #{result['ok']} replicas deleted. Report: #{result['results'].to_json}."
   puts
 end
@@ -122,57 +63,41 @@ end
 def req_list2()
   # first transaction sets two keys and commits
   rlist = []
-  rlist[0] = { :write => {'keyA' => _json_encode_value('valueA')} }
-  rlist[1] = { :write => {'keyB' => _json_encode_value('valueB')} }
+  rlist[0] = { :write => {'keyA' => Scalaris::_json_encode_value('valueA')} }
+  rlist[1] = { :write => {'keyB' => Scalaris::_json_encode_value('valueB')} }
   rlist[2] = { :commit => 'commit' }
 
-  result = json_call('req_list', [rlist])
-  values = _json_replace_value_in_results(result['results'])
+  result = Scalaris::json_call('req_list', [rlist])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
 
   # second transaction reads two keys and then modifies one of them
   rlist2 = []
   rlist2[0] = { :read => 'keyA' }
   rlist2[1] = { :read => 'keyB' }
-  result = json_call('req_list', [rlist2])
+  result = Scalaris::json_call('req_list', [rlist2])
 
   translog = result['tlog']
-  values = _json_replace_value_in_results(result['results'])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
 
   rlist3 = []
-  rlist3[0] = { :write => {'keyA' => _json_encode_value('valueA2')} }
+  rlist3[0] = { :write => {'keyA' => Scalaris::_json_encode_value('valueA2')} }
   rlist3[1] = { :commit => 'commit' }
-  result = json_call('req_list', [translog, rlist3])
-  values = _json_replace_value_in_results(result['results'])
+  result = Scalaris::json_call('req_list', [translog, rlist3])
+  values = Scalaris::_json_replace_value_in_results(result['results'])
 
-  result = json_call('delete', ["keyA"])
+  result = Scalaris::json_call('delete', ["keyA"])
 end
 
 def range_query()
-  result = json_call('range_read',
+  result = Scalaris::json_call('range_read',
                      [0, 0x40000000000000000000000000000000])
   puts result.to_json
-  write("keyA", "valueA")
-  write("keyB", "valueB")
-  write("keyC", "valueC")
-  result = json_call('range_read',
+  Scalaris::write("keyA", "valueA")
+  Scalaris::write("keyB", "valueB")
+  Scalaris::write("keyC", "valueC")
+  result = Scalaris::json_call('range_read',
                      [0, 0x40000000000000000000000000000000])
   puts result.to_json
-end
-
-def publish(topic, content)
-  json_call('publish', [topic,content])
-end
-
-def subscribe(topic, url)
-  json_call('subscribe', [topic, url])
-end
-
-def unsubscribe(topic,url)
-  json_call('unsubscribe', [topic,url])
-end
-
-def get_subscribers(topic)
-  json_call('get_subscribers', [topic])
 end
 
 n = 100
@@ -188,28 +113,28 @@ puts "benchmarking ..."
 puts " nops ..."
 nop = Benchmark.realtime {
   n.times do
-    nop("key")
+    Scalaris::nop("key")
   end
 }
 
 puts " read ..."
 read = Benchmark.realtime {
   n.times do
-    read("key")
+    Scalaris::read("key")
   end
 }
 
 puts " write ..."
 write = Benchmark.realtime {
   n.times do
-    write("key", "value")
+    Scalaris::write("key", "value")
   end
 }
 
 puts " test and set ..."
 test_and_set = Benchmark.realtime {
   n.times do
-    test_and_set("key", "value", "value")
+    Scalaris::test_and_set("key", "value", "value")
   end
 }
 
@@ -222,17 +147,17 @@ reql = Benchmark.realtime {
 
 puts "testing pub sub once more ..."
 def pubsub()
-  subs = get_subscribers("Topic")
+  subs = Scalaris::get_subscribers("Topic")
   printf("subscribers: %s\n", subs.to_json)
   # register scalaris boot server itself as subscriber
   # (prints a message on the console then)
-  subscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
-  subscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
-  subs = get_subscribers("Topic")
+  Scalaris::subscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
+  Scalaris::subscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
+  subs = Scalaris::get_subscribers("Topic")
   printf("subscribers: %s\n", subs.to_json)
-  publish("Topic", "Value")
-  unsubscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
-  subs = get_subscribers("Topic")
+  Scalaris::publish("Topic", "Value")
+  Scalaris::unsubscribe("Topic", "http://localhost:8000/jsonrpc.yaws")
+  subs = Scalaris::get_subscribers("Topic")
   printf("subscribers: %s\n", subs.to_json)
 end
 
