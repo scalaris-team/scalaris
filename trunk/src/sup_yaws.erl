4e0ec75f8e303a3f313fff634fed404d014172b9
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-04-27 15:47:50 +0000

    - properly start yaws in embedded mode with its own supervisor

---
+++ trunk/src/sup_yaws.erl
@@ -0,0 +1,66 @@
+%% @copyright 2011 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Nico Kruber <kruber@zib.de>
+%% @version $Id$
+-module(sup_yaws).
+-author('kruber@zib.de').
+-vsn('$Id$').
+
+-behaviour(supervisor).
+
+-export([start_link/0, init/1]).
+
+-spec start_link() -> {ok, Pid::pid()} | ignore |
+                      {error, Error::{already_started, Pid::pid()} | shutdown | term()}.
+start_link() ->
+    Id = io_lib:format("yaws@~p", [node()]),
+    Docroot = config:read(docroot),
+    GconfList = [{max_connections, 800},
+                 {logdir, config:read(log_path)},
+                 {id, Id}
+                ],
+    SconfList = [{docroot, Docroot},
+                 {port, config:read(yaws_port)},
+                 {listen, {0,0,0,0}},
+                 {allowed_scripts, [yaws]},
+                 {flags, [{access_log, false}]} % {deflate, true}
+                ],
+    
+    % load yaws application (required by yaws_api:embedded_start_conf)
+    _ = application:load(
+          {application, yaws,
+           [{description, "yaws WWW server"},
+            {vsn, "%VSN%"},
+            {modules, []},
+            {registered, []},
+            {mod, {yaws_app, []}},
+            {env, []},
+            {applications, [kernel, stdlib]}]}),
+    
+    {ok, SCList, GC, ChildSpecs} =
+        yaws_api:embedded_start_conf(Docroot, SconfList, GconfList, Id),
+    
+    X = supervisor:start_link(?MODULE, ChildSpecs),
+    
+    %% now configure Yaws
+    yaws_api:setconf(GC, SCList),
+    
+    X.
+
+-spec init(ChildSpecs) -> {ok, {{one_for_all, MaxRetries::pos_integer(),
+                                      PeriodInSeconds::pos_integer()},
+                         ChildSpecs}}.
+init(ChildSpecs) ->
+    {ok, {{one_for_all, 10, 1}, ChildSpecs}}.
