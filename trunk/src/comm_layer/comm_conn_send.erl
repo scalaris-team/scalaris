b6a29de91c139510e74383afd743bfc6eaac0db8
schintke <schintke@71766da9-9752-0410-ab17-bfafa751c56a>
2011-05-04 11:19:17 +0000

    - TCP: allow larger message bundles on high send load to improve throughput

--- trunk/src/comm_layer/comm_conn_send.erl
+++ trunk/src/comm_layer/comm_conn_send.erl
@@ -94,9 +94,10 @@ on({send, DestPid, Message}, State) ->
                     if MQL > 5 ->
                             %% start message bundle for sending
                             %% io:format("MQL ~p~n", [MQL]),
+                            MaxBundle = util:max(200, MQL div 100),
                             T1 = set_msg_queue(State, [{DestPid, Message}]),
                             T2 = set_msg_queue_len(T1, 1),
-                            set_desired_bundle_size(T2, util:min(MQL,200));
+                            set_desired_bundle_size(T2, util:min(MQL,MaxBundle));
                        true ->
                             NewSocket =
                                 send({dest_ip(State), dest_port(State), Socket},
