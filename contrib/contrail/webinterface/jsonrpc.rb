2d0ba8776e1e931971bf2b6fcf6042019f3c9d22
schuett <schuett@zib.de>
2011-05-30 12:30:54 +0000

    - added jsonrpc interface for contrail

---
+++ contrib/contrail/webinterface/jsonrpc.rb
@@ -0,0 +1,48 @@
+module JSONRPC
+  def self.get_scalaris_info(params)
+    id = params[0].to_i
+    info = ScalarisHelper.get_instance_info(id)
+    info
+  end
+
+  def self.create_scalaris(params)
+    user = params[0]
+    res = ScalarisHelper.create(user)
+    if res[0] == true
+      {"scalaris_id" => res[1]}
+    else
+      raise "create failed with #{res[1]}"
+    end
+  end
+
+  def self.add_vm_scalaris(params)
+    id = params[0].to_i
+    res = ScalarisHelper.add(id)
+    if res[0] == true
+      {"vm_id" => res[1]}
+    else
+      raise "create failed with #{res[1]}"
+    end
+  end
+
+  def self.destroy_scalaris(params)
+    id = params[0].to_i
+    res = ScalarisHelper.destroy(id)
+    "success"
+  end
+
+  def self.call(jsonreq)
+    method = jsonreq["method"]
+
+    begin
+      res = self.send method, jsonreq["params"]
+      "{ \"result\": #{res.to_json}, \"error\": null, \"id\": #{jsonreq['id']}}"
+    rescue NoMethodError
+      puts $!
+      "{ \"error\": \"undefined method #{method}\", \"id\": #{jsonreq['id']}}"
+    rescue
+      puts $!
+      "{ \"error\": #{$!.to_json}, \"id\": #{jsonreq['id']}}"
+    end
+  end
+end
