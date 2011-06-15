ac9f9205f05168236e295cbe2f51fd78d338fb78
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2011-06-15 15:00:45 +0000

    - fixed timestamp handling in rrd

--- trunk/test/rrd_SUITE.erl
+++ trunk/test/rrd_SUITE.erl
@@ -25,7 +25,8 @@
 all()   -> [simple_create,
             fill_test,
             create_gauge,
-            create_counter
+            create_counter,
+            timestamp
            ].
 suite() -> [ {timetrap, {seconds, 40}} ].
 
@@ -37,30 +38,35 @@ end_per_suite(_Config) ->
 
 simple_create(_Config) ->
     Adds = [{20, 5}, {25, 6}],
-    DB0 = rrd:create(10, 10, gauge, 0),
+    DB0 = rrd:create(10, 10, gauge, {0,0,0}),
     DB1 = lists:foldl(fun apply/2, DB0, Adds),
-    ?equals(rrd:dump(DB1), [{20, 6}]),
+    ?equals(rrd:dump(DB1), [{{0,0,20}, 6}]),
     ok.
 
 fill_test(_Config) ->
     Adds = [{20, 1}, {30, 2}, {40, 3}, {60, 5}],
-    DB0 = rrd:create(10, 3, gauge, 0),
+    DB0 = rrd:create(10, 3, gauge, {0,0,0}),
     DB1 = lists:foldl(fun apply/2, DB0, Adds),
-    ?equals(rrd:dump(DB1), [{60, 5}, {40, 3}]),
+    ?equals(rrd:dump(DB1), [{{0,0,60}, 5}, {{0,0,40}, 3}]),
     ok.
 
 create_gauge(_Config) ->
     Adds = [{20, 5}, {25, 6}, {30, 1}, {42, 2}],
-    DB0 = rrd:create(10, 10, gauge, 0),
+    DB0 = rrd:create(10, 10, gauge, {0,0,0}),
     DB1 = lists:foldl(fun apply/2, DB0, Adds),
-    ?equals(rrd:dump(DB1), [{40, 2}, {30, 1}, {20, 6}]),
+    ?equals(rrd:dump(DB1), [{{0,0,40}, 2}, {{0,0,30}, 1}, {{0,0,20}, 6}]),
     ok.
 
 create_counter(_Config) ->
     Adds = [{20, 5}, {25, 6}, {30, 1}, {42, 2}],
-    DB0 = rrd:create(10, 10, counter, 0),
+    DB0 = rrd:create(10, 10, counter, {0,0,0}),
     DB1 = lists:foldl(fun apply/2, DB0, Adds),
-    ?equals(rrd:dump(DB1), [{40, 2}, {30, 1}, {20, 11}]),
+    ?equals(rrd:dump(DB1), [{{0,0,40}, 2}, {{0,0,30}, 1}, {{0,0,20}, 11}]),
+    ok.
+
+timestamp(_Config) ->
+    TS = erlang:now(),
+    ?equals(TS, rrd:us2timestamp(rrd:timestamp2us(TS))),
     ok.
 
 apply({Time, Value}, DB) ->
