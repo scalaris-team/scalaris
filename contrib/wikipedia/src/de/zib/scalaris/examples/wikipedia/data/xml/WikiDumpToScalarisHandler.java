9f1fd44c0b0f175c9c180cc0e8a05b5e4a5a570b
kruber@zib.de <kruber@zib.de>
2011-06-21 19:01:38 +0000

    - Wikipedia on Scalaris example:  * distinguish articles from pages (articles are pages in the main namespace)  * show random articles, not pages on "Show any entry"  * save page and article lists counter separately for faster access  * let MyFullurl and MyLocalurl work in wiki models that do not inherit from MyWikiModel  * remove support for interwiki links (they may interfere with internal links to namespaces)

--- contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpToScalarisHandler.java
+++ contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpToScalarisHandler.java
@@ -38,6 +38,7 @@ import de.zib.scalaris.Transaction.ResultList;
 import de.zib.scalaris.TransactionSingleOp;
 import de.zib.scalaris.UnknownException;
 import de.zib.scalaris.examples.wikipedia.ScalarisDataHandler;
+import de.zib.scalaris.examples.wikipedia.bliki.MyWikiModel;
 import de.zib.scalaris.examples.wikipedia.data.Page;
 import de.zib.scalaris.examples.wikipedia.data.Revision;
 import de.zib.scalaris.examples.wikipedia.data.ShortRevision;
@@ -58,6 +59,7 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
     private ArrayBlockingQueue<TransactionSingleOp> scalaris_single = new ArrayBlockingQueue<TransactionSingleOp>(MAX_SCALARIS_CONNECTIONS);
     private ArrayBlockingQueue<Transaction> scalaris_tx = new ArrayBlockingQueue<Transaction>(MAX_SCALARIS_CONNECTIONS);
     private List<String> newPages = new LinkedList<String>();
+    private List<String> newArticles = new LinkedList<String>();
     private HashMap<String, List<String>> newCategories = new HashMap<String, List<String>>(NEW_CATS_HASH_DEF_SIZE);
     private HashMap<String, List<String>> newTemplates = new HashMap<String, List<String>>(NEW_TPLS_HASH_DEF_SIZE);
     private HashMap<String, List<String>> newBackLinks = new HashMap<String, List<String>>(NEW_BLNKS_HASH_DEF_SIZE);
@@ -335,6 +337,10 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
         executor.execute(worker);
         newPages.add(page.getTitle());
         ++pageCount;
+        // simple content filter: only show pages in main namespace:
+        if (MyWikiModel.getNamespace(page.getTitle()).isEmpty()) {
+            newArticles.add(page.getTitle());
+        }
         // only export page list every UPDATE_PAGELIST_EVERY pages:
         if ((newPages.size() % UPDATE_PAGELIST_EVERY) == 0) {
             updatePageLists();
@@ -352,18 +358,25 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
     }
     
     private void updatePageLists() {
-        String scalaris_key = ScalarisDataHandler.getPageListKey();
+        String scalaris_key;
         Runnable worker;
         
         // list of pages:
-        worker = new MyScalarisAddToPageListRunnable(scalaris_key, newPages, scalaris_single);
+        scalaris_key = ScalarisDataHandler.getPageListKey();
+        worker = new MyScalarisAddToPageListRunnable(scalaris_key, newPages, scalaris_single, scalaris_tx, ScalarisDataHandler.getPageCountKey());
         executor.execute(worker);
         newPages = new LinkedList<String>();
+        
+        // list of articles:
+        scalaris_key = ScalarisDataHandler.getArticleListKey();
+        worker = new MyScalarisAddToPageListRunnable(scalaris_key, newArticles, scalaris_single, scalaris_tx, ScalarisDataHandler.getArticleCountKey());
+        executor.execute(worker);
+        newArticles = new LinkedList<String>();
         
         // list of pages in each category:
         for (Entry<String, List<String>> category: newCategories.entrySet()) {
             scalaris_key = ScalarisDataHandler.getCatPageListKey(category.getKey());
-            worker = new MyScalarisAddToPageListRunnable(scalaris_key, category.getValue(), scalaris_single);
+            worker = new MyScalarisAddToPageListRunnable(scalaris_key, category.getValue(), scalaris_single, scalaris_tx);
             executor.execute(worker);
         }
         newCategories = new HashMap<String, List<String>>(NEW_CATS_HASH_DEF_SIZE);
@@ -371,7 +384,7 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
         // list of pages a template is used in:
         for (Entry<String, List<String>> template: newTemplates.entrySet()) {
             scalaris_key = ScalarisDataHandler.getTplPageListKey(template.getKey());
-            worker = new MyScalarisAddToPageListRunnable(scalaris_key, template.getValue(), scalaris_single);
+            worker = new MyScalarisAddToPageListRunnable(scalaris_key, template.getValue(), scalaris_single, scalaris_tx);
             executor.execute(worker);
         }
         newTemplates = new HashMap<String, List<String>>(NEW_TPLS_HASH_DEF_SIZE);
@@ -379,7 +392,7 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
         // list of pages linking to other pages:
         for (Entry<String, List<String>> backlinks: newBackLinks.entrySet()) {
             scalaris_key = ScalarisDataHandler.getBackLinksPageListKey(backlinks.getKey());
-            worker = new MyScalarisAddToPageListRunnable(scalaris_key, backlinks.getValue(), scalaris_single);
+            worker = new MyScalarisAddToPageListRunnable(scalaris_key, backlinks.getValue(), scalaris_single, scalaris_tx);
             executor.execute(worker);
         }
         newBackLinks = new HashMap<String, List<String>>(NEW_BLNKS_HASH_DEF_SIZE);
@@ -394,15 +407,33 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
         private String scalaris_key;
         private List<String> newEntries;
         private ArrayBlockingQueue<TransactionSingleOp> scalaris_single;
+        private ArrayBlockingQueue<Transaction> scalaris_tx;
+        private String scalaris_pageCount_key = "";
         
-        public MyScalarisAddToPageListRunnable(String scalaris_key, List<String> newEntries, ArrayBlockingQueue<TransactionSingleOp> scalaris_single) {
+        public MyScalarisAddToPageListRunnable(String scalaris_key, List<String> newEntries, ArrayBlockingQueue<TransactionSingleOp> scalaris_single, ArrayBlockingQueue<Transaction> scalaris_tx) {
             this.scalaris_key = scalaris_key;
             this.newEntries = newEntries;
             this.scalaris_single = scalaris_single;
+            this.scalaris_tx = scalaris_tx;
+        }
+        
+        public MyScalarisAddToPageListRunnable(String scalaris_key, List<String> newEntries, ArrayBlockingQueue<TransactionSingleOp> scalaris_single, ArrayBlockingQueue<Transaction> scalaris_tx, String scalaris_pageCount_key) {
+            this.scalaris_key = scalaris_key;
+            this.newEntries = newEntries;
+            this.scalaris_single = scalaris_single;
+            this.scalaris_tx = scalaris_tx;
+            this.scalaris_pageCount_key = scalaris_pageCount_key;
         }
         
         @Override
         public void run() {
+            updatePageList();
+            if (!scalaris_pageCount_key.isEmpty()) {
+                updatePageCount();
+            }
+        }
+        
+        private void updatePageList() {
             List<String> entries;
             TransactionSingleOp scalaris_single;
             try {
@@ -446,9 +477,65 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
                 System.err.println("write of page list \"" + scalaris_key + "\" failed with unknown: " + e.getMessage());
                 return;
             }
+            
             if (scalaris_single != null) {
                 try {
                     this.scalaris_single.put(scalaris_single);
+                } catch (InterruptedException e) {
+                    System.err.println("Interrupted while putting back a connection to Scalaris");
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+        
+        private void updatePageCount() {
+            int count = 0;
+            Transaction scalaris_tx;
+            try {
+                scalaris_tx = this.scalaris_tx.take();
+            } catch (InterruptedException e) {
+                System.err.println("update of " + scalaris_pageCount_key + " interrupted while getting connection to Scalaris");
+                throw new RuntimeException(e);
+            }
+            try {
+                count = scalaris_tx.read(scalaris_pageCount_key).intValue();
+            } catch (NotFoundException e) {
+                // if the key does not exist, previous count is "0"
+            } catch (ConnectionException e) {
+                System.err.println("read of page count \"" + scalaris_pageCount_key + "\" failed with connection error");
+                return;
+            } catch (TimeoutException e) {
+                System.err.println("read of page count \"" + scalaris_pageCount_key + "\" failed with timeout");
+                return;
+            } catch (UnknownException e) {
+                System.err.println("read of page count \"" + scalaris_pageCount_key + "\" failed with unknown: " + e.getMessage());
+                return;
+            } catch (ClassCastException e) {
+                System.err.println("read of page count \"" + scalaris_pageCount_key + "\" failed with unexpected type: " + e.getMessage());
+                return;
+            }
+            
+            count += newEntries.size();
+
+            try {
+                scalaris_tx.req_list(new RequestList().addWrite(scalaris_pageCount_key, count).addCommit());
+            } catch (ConnectionException e) {
+                System.err.println("write of page count \"" + scalaris_pageCount_key + "\" failed with connection error");
+                return;
+            } catch (TimeoutException e) {
+                System.err.println("write of page count \"" + scalaris_pageCount_key + "\" failed with timeout");
+                return;
+            } catch (AbortException e) {
+                System.err.println("write of page count \"" + scalaris_pageCount_key + "\" failed with abort");
+                return;
+            } catch (UnknownException e) {
+                System.err.println("write of page count \"" + scalaris_pageCount_key + "\" failed with unknown: " + e.getMessage());
+                return;
+            }
+            
+            if (scalaris_tx != null) {
+                try {
+                    this.scalaris_tx.put(scalaris_tx);
                 } catch (InterruptedException e) {
                     System.err.println("Interrupted while putting back a connection to Scalaris");
                     throw new RuntimeException(e);
