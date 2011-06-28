4b12a79b7889b82b9d40dbe2348c29bda8ab5758
kruber@zib.de <kruber@zib.de>
2011-06-28 12:48:01 +0000

    - prepare package management for 0.3.0 release

--- contrib/packages/scalaris-checkout.sh
+++ contrib/packages/scalaris-checkout.sh
@@ -2,18 +2,22 @@
 
 date=`date +"%Y%m%d"`
 name="scalaris" # folder base name (without version)
-tagversion=${1:-"0.2.3"}
-url="http://scalaris.googlecode.com/svn/tags/${tagversion}"
+tagversion=${1:-"0.3"}
+url="http://scalaris.googlecode.com/svn/branches/${tagversion}"
 deletefolder=0 # set to 1 to delete the folder the repository is checked out to
 
 #####
 
-result=1
+result=0
 folder="./${name}-${tagversion}"
 
 if [ ! -d ${folder} ]; then
   echo "checkout ${url} -> ${folder} ..."
   svn checkout ${url} ${folder}
+  result=$?
+else
+  echo "update ${url} -> ${folder} ..."
+  svn update ${folder}
   result=$?
 fi
 
@@ -25,13 +29,28 @@ if [ ${result} -eq 0 ]; then
 fi
 
 if [ ${result} -eq 0 ]; then
-  echo "changing scalaris.spec and scalaris.dsc files ..."
-  cp scalaris.spec scalaris.spec.old && \
-  sed -e "s/%define pkg_version .*/%define pkg_version ${tagversion}/g" < scalaris.spec.old > scalaris.spec && \
-  rm scalaris.spec.old && \
-  cp scalaris.dsc scalaris.dsc.old && \
-  sed -e "s/0.2.3/${tagversion}/g" < scalaris.dsc.old > scalaris.dsc && \
-  rm scalaris.dsc.old
+  echo "extracting .spec file ..."
+  sourcefolder=${folder}/contrib/packages
+  sed -e "s/%define pkg_version .*/%define pkg_version ${tagversion}/g" \
+      < ${sourcefolder}/scalaris.spec > ./scalaris.spec
+  result=$?
+fi
+
+if [ ${result} -eq 0 ]; then
+  echo "extracting Debian package files ..."
+  sourcefolder=${folder}/contrib/packages
+  sed -e "s/0.3.0/${tagversion}/g" \
+      < ${sourcefolder}/scalaris.dsc          > ./scalaris.dsc && \
+  sed -e "s/(0.3.0-/(${tagversion}-/g" \
+      < ${sourcefolder}/debian.changelog      > ./debian.changelog && \
+  cp  ${sourcefolder}/debian.control            ./debian.control && \
+  cp  ${sourcefolder}/debian.rules              ./debian.rules && \
+  cp  ${sourcefolder}/debian.scalaris-doc.files ./debian.scalaris-doc.files && \
+  cp  ${sourcefolder}/debian.scalaris.conffiles ./debian.scalaris.conffiles && \
+  cp  ${sourcefolder}/debian.scalaris.files     ./debian.scalaris.files && \
+  cp  ${sourcefolder}/debian.scalaris.postinst  ./debian.scalaris.postinst && \
+  cp  ${sourcefolder}/debian.scalaris.postrm    ./debian.scalaris.postrm
+
   result=$?
 fi
 
