1d489b482f210d32392bad35ac91ea6db4be889c
schuett <schuett@zib.de>
2011-07-12 14:03:32 +0000

    - webinterface v2 for opennebula

---
+++ contrib/opennebula/webinterface-v2/cli.rb
@@ -0,0 +1,97 @@
+#!/usr/bin/ruby
+# Copyright 2008-2011 Zuse Institute Berlin
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+require 'rubygems'
+require 'json'
+require 'net/http'
+require 'optparse'
+require 'pp'
+
+$url = 'http://localhost:4567/jsonrpc'
+#$uri = URI.parse $url
+
+def json_call(url, function, params)
+  req = Net::HTTP::Post.new(url.path)
+  req.add_field 'Content-Type', 'application/json'
+  req.body =
+    {
+    :version => '1.1',
+    :method => function,
+    :params => params,
+    :id => 0}.to_json
+  res = Net::HTTP.start(url.host, url.port){|http|http.request(req)}
+  jsonres = JSON.parse(res.body)
+  if jsonres['error'] == nil
+    JSON.parse(res.body)["result"]
+  else
+    JSON.parse(res.body)['error']
+  end
+end
+
+def get_scalaris_info(url)
+  json_call(URI.parse url, "get_scalaris_info", []).to_json
+end
+
+def create_scalaris(url)
+  json_call(URI.parse(url), "create_scalaris", []).to_json
+end
+
+def add_vm_scalaris(url, count)
+  json_call(URI.parse url, "add_vm_scalaris", [count]).to_json
+end
+
+def destroy_scalaris(fe_url, mgmt_url)
+  json_call(URI.parse fe_url, "destroy_scalaris", [mgmt_url]).to_json
+end
+
+options = {}
+
+optparse = OptionParser.new do |opts|
+  options[:url] = $url
+  opts.on('--url URL', String, 'Service URL' ) do |url|
+    options[:url] = url
+  end
+
+  options[:scalaris_create] = nil
+  opts.on('--create-scalaris', 'Create new scalaris cluster' ) do |key|
+    options[:scalaris_create] = true
+  end
+
+  options[:scalaris_add_vm] = nil
+  opts.on('--scalaris-add-vm Count', Integer, 'Add more VMs to this scalaris cluster' ) do |count|
+    options[:scalaris_add_vm] = count
+  end
+
+  options[:scalaris_destroy] = nil
+  opts.on('--destroy-scalaris URL', String, 'Destroy scalaris cluster URL' ) do |url|
+    options[:scalaris_destroy] = url
+  end
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end
+
+begin
+  optparse.parse!
+rescue OptionParser::ParseError
+  $stderr.print "Error: " + $! + "\n"
+  exit
+end
+
+puts create_scalaris(options[:url]) unless options[:scalaris_create] == nil
+puts add_vm_scalaris(options[:url], options[:scalaris_add_vm]) unless options[:scalaris_add_vm] == nil
+puts destroy_scalaris(options[:url], options[:scalaris_destroy]) unless options[:scalaris_destroy] == nil
---
+++ contrib/opennebula/webinterface-v2/hadoophelper.rb
@@ -0,0 +1,114 @@
+require 'erb'
+
+require 'one.rb'
+
+class HadoopHelper
+  def self.create()
+    description = get_description(HADOOPIMAGE, "true", "$NIC[IP]")
+    puts description
+
+    begin
+      response = OpenNebulaHelper.create_vm(description)
+      if OpenNebula.is_error?(response)
+        [false, response.to_str]
+      else
+        vm_id = response.to_i
+        url = "http://#{get_ip(vm_id)}:4567/"
+        [true, url]
+      end
+    rescue Exception => e
+      [false, e.message]
+    end
+  end
+
+  def self.add(id)
+    ips = get_ips(id)
+    head_node = get_ip(Hadoop[id].master_node)
+    description = get_description(HADOOPIMAGE, "false", head_node)
+    puts description
+
+    begin
+      response = OpenNebulaHelper.create_vm(description)
+
+      instance = Hadoop[id]
+      new_vm_id = response.to_i
+      instance.add_hadoopvm(:one_vm_id => new_vm_id)
+
+      [true, response.to_i]
+    rescue Exception => e
+      [false, e.message]
+    end
+  end
+
+  def self.destroy(url)
+    get_vms(id).each do |vm_id|
+      OpenNebulaHelper.delete_vm vm_id
+    end
+    Hadoop[id].remove_all_hadoopvms
+    Hadoop[id].destroy
+  end
+
+  def self.get_instance_info(id)
+    info = {}
+    info["ips"] = get_ips(id)
+    info["id"] = id
+    info
+  end
+
+  private
+
+  def self.get_description(image, first, master_ip)
+    @image = image
+    @hadoopfirst = first
+    @hadoopmaster = master_ip
+    erb = ERB.new(File.read("hadoop.one.vm.erb"))
+    erb.result binding
+  end
+
+  def self.ip_to_erlang(ip)
+  end
+
+  def self.get_ip(one_id)
+    client = Client.new(CREDENTIALS, ENDPOINT)
+    pool = VirtualMachinePool.new(client, -1)
+    pool.info
+    vm = pool.find {|i| i.id.to_i == one_id.to_i}
+    Nokogiri::XML(vm.to_xml).xpath("/VM/TEMPLATE/NIC/IP/text()").text
+  end
+
+  def self.get_vms(id)
+    instance = Hadoop[id]
+    client = Client.new(CREDENTIALS, ENDPOINT)
+    pool = VirtualMachinePool.new(client, -1)
+    pool.info
+    vms = []
+    if instance == nil
+      raise "unknown hadoop instance"
+    end
+    instance.hadoopvms_dataset.each do |vm|
+      vm = pool.find {|i| i.id == vm.one_vm_id.to_i}
+      if vm != nil
+        vms.push vm.id
+      end
+    end
+    vms
+  end
+
+  def self.get_ips(id)
+    instance = Hadoop[id]
+    client = Client.new(CREDENTIALS, ENDPOINT)
+    pool = VirtualMachinePool.new(client, -1)
+    pool.info
+    ips = []
+    if instance == nil
+      raise "unknown hadoop instance"
+    end
+    instance.hadoopvms_dataset.each do |vm|
+      vm = pool.find {|i| i.id == vm.one_vm_id.to_i}
+      if vm != nil
+        ips.push Nokogiri::XML(vm.to_xml).xpath("/VM/TEMPLATE/NIC/IP/text()").text
+      end
+    end
+    ips
+  end
+end
