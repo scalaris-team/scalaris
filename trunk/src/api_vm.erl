6361d9e12eb4ba8631f6bed603ab87ca3a9c075b
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-08-18 17:10:12 +0000

    - changed semantics of api_vm:shutdown_node* and api_vm:kill_node*:  * return names of successfully removed nodes and those which could not be removed  * allow a number to be specified (instead of a list of names)

--- trunk/src/api_vm.erl
+++ trunk/src/api_vm.erl
@@ -44,41 +44,78 @@ get_nodes() ->
     pid_groups:groups_with(dht_node).
 
 %% @doc Adds Number Scalaris nodes to this VM.
--spec add_nodes(pos_integer()) -> {[pid_groups:groupname()], [{error, term()}]}.
-add_nodes(Number) ->
-    admin:add_nodes(Number).
+-spec add_nodes(non_neg_integer()) -> {[pid_groups:groupname()], [{error, term()}]}.
+add_nodes(0) -> {[], []};
+add_nodes(Number) -> admin:add_nodes(Number).
 
 %% @doc Sends a graceful leave request to a given node.
--spec shutdown_node(pid_groups:groupname()) -> ok | {error, not_found}.
+-spec shutdown_node(pid_groups:groupname()) -> ok | not_found.
 shutdown_node(Name) ->
-    hd(shutdown_nodes([Name])).
+    case element(1, shutdown_nodes([Name])) of
+        [] -> not_found;
+        _  -> ok
+    end.
 %% @doc Sends a graceful leave request to multiple nodes.
--spec shutdown_nodes([pid_groups:groupname()]) -> [ok | {error, not_found}].
+-spec shutdown_nodes
+        ([pid_groups:groupname()]) -> {Ok::[pid_groups:groupname()], NotFound::[pid_groups:groupname()]};
+        (non_neg_integer())        -> {Ok::[pid_groups:groupname()], NotFound::[]}.
 shutdown_nodes(Names) ->
     del_nodes(Names, true).
 
 %% @doc Kills a given node.
--spec kill_node(pid_groups:groupname()) -> ok | {error, not_found}.
+-spec kill_node(pid_groups:groupname()) -> ok | not_found.
 kill_node(Name) ->
-    hd(kill_nodes([Name])).
+    case element(1, kill_nodes([Name])) of
+        [] -> not_found;
+        _  -> ok
+    end.
 %% @doc Kills multiple nodes.
--spec kill_nodes([pid_groups:groupname()]) -> [ok | {error, not_found}].
+-spec kill_nodes
+        ([pid_groups:groupname()]) -> {Ok::[pid_groups:groupname()], NotFound::[pid_groups:groupname()]};
+        (non_neg_integer())        -> {Ok::[pid_groups:groupname()], NotFound::[]}.
 kill_nodes(Names) ->
     del_nodes(Names, false).
 
 %% @doc Gets supervisor specs for all nodes with the given names and kills them
 %%      (internal function).
--spec del_nodes([pid_groups:groupname()], Graceful::boolean()) -> [ok | {error, not_found}].
+-spec del_nodes([pid_groups:groupname()], Graceful::boolean())
+        -> {Ok::[pid_groups:groupname()], NotFound::[pid_groups:groupname()]};
+               (non_neg_integer(), Graceful::boolean())
+        -> {Ok::[pid_groups:groupname()], NotFound::[]}.
+del_nodes(0, _Graceful) -> {[], []};
+del_nodes(1, Graceful) ->
+    % delete one after another for a better chance of success during concurrent executions
+    AllSpecs = [{Spec, pid_groups:group_of(Pid)}
+               || {_Id, Pid, _Type, _} = Spec <- supervisor:which_children(main_sup),
+                  is_pid(Pid)],
+    case util:random_subset(1, AllSpecs) of
+        [] -> {[], []};
+        [{Spec, Name} | _] ->
+            case admin:del_node(Spec, Graceful) of
+                ok -> {[Name], []};
+                {error, not_found} -> del_nodes(1, Graceful)
+            end
+    end;
+del_nodes(Number, Graceful) when is_integer(Number) ->
+    {RestOk, []} = del_nodes(Number - 1, Graceful),
+    {MyOk, []} = del_nodes(1, Graceful),
+    {lists:append(MyOk, RestOk), []};
 del_nodes(Names, Graceful) ->
-    [begin
-         Specs = [Spec || {_Id, Pid, _Type, _} = Spec <- supervisor:which_children(main_sup),
-                          is_pid(Pid),
-                          pid_groups:group_of(Pid) =:= Name],
-         case Specs of
-             [Spec] -> admin:del_node(Spec, Graceful);
-             _      -> {error, not_found}
-         end
-     end || Name <- Names].
+    Results =
+        [begin
+             Specs = [Spec || {_Id, Pid, _Type, _} = Spec <- supervisor:which_children(main_sup),
+                              is_pid(Pid),
+                              pid_groups:group_of(Pid) =:= Name],
+             case Specs of
+                 [Spec] -> case admin:del_node(Spec, Graceful) of
+                               ok -> Name;
+                               {error, not_found} -> {not_found, Name}
+                           end;
+                 _      -> {not_found, Name}
+             end
+         end || Name <- Names],
+    {[Name || Name <- Results, not is_tuple(Name)],
+     [Name || {not_found, Name} <- Results]}.
 
 %% @doc Graceful shutdown of this VM.
 -spec shutdown_vm() -> no_return().
