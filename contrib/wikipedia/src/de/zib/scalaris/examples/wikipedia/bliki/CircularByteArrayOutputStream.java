fbb91b6c33b33bb2040af07da7bf1ee21a560b9c
kruber@zib.de <kruber@zib.de>
2011-09-09 08:57:51 +0000

    - Wiki on Scalaris: added missing CircularByteArrayOutputStream class

---
+++ contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/CircularByteArrayOutputStream.java
@@ -0,0 +1,121 @@
+/**
+ *  Copyright 2011 Zuse Institute Berlin
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package de.zib.scalaris.examples.wikipedia.bliki;
+
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+/**
+ * Circular {@link OutputStream} that limits the number of bytes.
+ * 
+ * @author Nico Kruber, kruber@zib.de
+ */
+public class CircularByteArrayOutputStream extends OutputStream {
+    /** 
+     * The buffer where data is stored. 
+     */
+    protected byte buf[];
+    
+    protected int pos = 0;
+    boolean filled = false;
+
+    /**
+     * Creates a new byte array output stream, with a buffer capacity of 
+     * the specified size, in bytes. 
+     *
+     * @param   size   the initial size.
+     * @exception  IllegalArgumentException if size is negative.
+     */
+    public CircularByteArrayOutputStream(int size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Negative initial size: "
+                    + size);
+        }
+        buf = new byte[size];
+    }
+
+    /* (non-Javadoc)
+     * @see java.io.ByteArrayOutputStream#write(int)
+     */
+    @Override
+    public synchronized void write(int b) {
+        if (pos == buf.length) {
+            filled = true;
+            pos = 0;
+        }
+        buf[pos++] = (byte) b;
+    }
+
+    /**
+     * Creates a newly allocated byte array. Its size is the current 
+     * size of this output stream and the valid contents of the buffer 
+     * have been copied into it. 
+     *
+     * @return  the current contents of this output stream, as a byte array.
+     * @see     java.io.ByteArrayOutputStream#size()
+     */
+    public synchronized byte toByteArray()[] {
+        if (!filled) {
+            return Arrays.copyOf(buf, pos);
+        }
+        byte[] ret = new byte[buf.length];
+        System.arraycopy(buf, pos, ret, 0, buf.length - pos);
+        System.arraycopy(buf, 0, ret, buf.length - pos, pos);
+        return ret;
+    }
+
+
+    /**
+     * Converts the buffer's contents into a string decoding bytes using the
+     * platform's default character set. The length of the new <tt>String</tt>
+     * is a function of the character set, and hence may not be equal to the 
+     * size of the buffer.
+     *
+     * <p> This method always replaces malformed-input and unmappable-character
+     * sequences with the default replacement string for the platform's
+     * default character set. The {@linkplain java.nio.charset.CharsetDecoder}
+     * class should be used when more control over the decoding process is
+     * required.
+     *
+     * @return String decoded from the buffer's contents.
+     */
+    public synchronized String toString() {
+        return new String(toByteArray());
+    }
+
+    /**
+     * Converts the buffer's contents into a string by decoding the bytes using
+     * the specified {@link java.nio.charset.Charset charsetName}. The length of
+     * the new <tt>String</tt> is a function of the charset, and hence may not be
+     * equal to the length of the byte array.
+     *
+     * <p> This method always replaces malformed-input and unmappable-character
+     * sequences with this charset's default replacement string. The {@link
+     * java.nio.charset.CharsetDecoder} class should be used when more control
+     * over the decoding process is required.
+     *
+     * @param  charsetName  the name of a supported
+     *          {@linkplain java.nio.charset.Charset </code>charset<code>}
+     * @return String decoded from the buffer's contents.
+     * @exception  UnsupportedEncodingException
+     *             If the named charset is not supported
+     */
+    public synchronized String toString(String charsetName)
+            throws UnsupportedEncodingException {
+        return new String(toByteArray(), charsetName);
+    }
+}
