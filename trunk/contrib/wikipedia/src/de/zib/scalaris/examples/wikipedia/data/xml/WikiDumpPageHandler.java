54144675a917ee279c4434ea76b2d79ab9c473a9
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-09-27 18:18:07 +0000

    - Wiki on Scalaris: some re-factoring in the XML dump classes

---
+++ trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpPageHandler.java
@@ -0,0 +1,176 @@
+package de.zib.scalaris.examples.wikipedia.data.xml;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import de.zib.scalaris.examples.wikipedia.data.Page;
+import de.zib.scalaris.examples.wikipedia.data.Revision;
+import de.zib.scalaris.examples.wikipedia.data.ShortRevision;
+import de.zib.scalaris.examples.wikipedia.data.SiteInfo;
+
+/**
+ * Intermediate class between XML processing of a Wiki dump and custom
+ * exporting of read {@link SiteInfo} and {@link Page} objects, including all
+ * revisions and the list of short revisions.
+ * 
+ * @author Nico Kruber, kruber@zib.de
+ */
+public abstract class WikiDumpPageHandler extends WikiDumpHandler {
+    protected static final int UPDATE_PAGELIST_EVERY = 400;
+    protected static final int NEW_CATS_HASH_DEF_SIZE = 100;
+    protected static final int NEW_TPLS_HASH_DEF_SIZE = 100;
+    protected static final int NEW_BLNKS_HASH_DEF_SIZE = 100;
+    
+    protected List<String> newPages = new LinkedList<String>();
+    protected List<String> newArticles = new LinkedList<String>();
+    protected HashMap<String, List<String>> newCategories = new HashMap<String, List<String>>(NEW_CATS_HASH_DEF_SIZE);
+    protected HashMap<String, List<String>> newTemplates = new HashMap<String, List<String>>(NEW_TPLS_HASH_DEF_SIZE);
+    protected HashMap<String, List<String>> newBackLinks = new HashMap<String, List<String>>(NEW_BLNKS_HASH_DEF_SIZE);
+
+    /**
+     * Sets up a SAX XmlHandler exporting all parsed pages except the ones in a
+     * blacklist.
+     * 
+     * @param blacklist
+     *            a number of page titles to ignore
+     * @param whitelist
+     *            only import these pages
+     * @param maxRevisions
+     *            maximum number of revisions per page (starting with the most
+     *            recent) - <tt>-1/tt> imports all revisions
+     *            (useful to speed up the import / reduce the DB size)
+     * @param maxTime
+     *            maximum time a revision should have (newer revisions are
+     *            omitted) - <tt>null/tt> imports all revisions
+     *            (useful to create dumps of a wiki at a specific point in time)
+     */
+    public WikiDumpPageHandler(Set<String> blacklist, Set<String> whitelist,
+            int maxRevisions, Calendar maxTime) {
+        super(blacklist, whitelist, maxRevisions, maxTime);
+    }
+
+    /**
+     * Exports the given siteinfo to Scalaris
+     * 
+     * @param revisions
+     *            the siteinfo to export
+     */
+    @Override
+    protected void export(XmlSiteInfo siteinfo_xml) {
+        doExport(siteinfo_xml.getSiteInfo());
+    }
+
+    /**
+     * Exports the given page (including all revisions) to Scalaris
+     * 
+     * @param page_xml
+     *            the page object extracted from XML
+     */
+    @Override
+    protected void export(XmlPage page_xml) {
+        Page page = page_xml.getPage();
+        ++pageCount;
+        
+        if (page.getCurRev() != null) {
+            List<Revision> revisions = page_xml.getRevisions();
+            List<ShortRevision> revisions_short = ShortRevision.fromRevisions(revisions);
+            Collections.sort(revisions, Collections.reverseOrder(new byRevId()));
+            Collections.sort(revisions_short, Collections.reverseOrder(new byShortRevId()));
+    
+            if (!revisions.isEmpty() && wikiModel != null) {
+                wikiModel.render(null, revisions.get(0).getText());
+                for (String cat_raw: wikiModel.getCategories().keySet()) {
+                    String category = wikiModel.getCategoryNamespace() + ":" + cat_raw;
+                    List<String> catPages = newCategories.get(category);
+                    if (catPages == null) {
+                        catPages = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
+                    }
+                    catPages.add(page.getTitle());
+                    newCategories.put(category, catPages);
+                }
+                for (String tpl_raw: wikiModel.getTemplates()) {
+                    String template = wikiModel.getTemplateNamespace() + ":" + tpl_raw;
+                    List<String> templatePages = newTemplates.get(template);
+                    if (templatePages == null) {
+                        templatePages = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
+                    }
+                    templatePages.add(page.getTitle());
+                    newTemplates.put(template, templatePages);
+                }
+                for (String link: wikiModel.getLinks()) {
+                    List<String> backLinks = newBackLinks.get(link);
+                    if (backLinks == null) {
+                        backLinks = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
+                    }
+                    backLinks.add(page.getTitle());
+                    newBackLinks.put(link, backLinks);
+                }
+            }
+    
+            doExport(page, revisions, revisions_short);
+        }
+        if ((pageCount % UPDATE_PAGELIST_EVERY) == 0) {
+            msgOut.println("processed pages: " + pageCount);
+        }
+    }
+
+    abstract protected void doExport(SiteInfo siteInfo);
+
+    abstract protected void doExport(Page page, List<Revision> revisions,
+            List<ShortRevision> revisions_short);
+
+    /**
+     * Provides a comparator for sorting {@link Revision} objects by their IDs.
+     * 
+     * @author Nico Kruber, kruber@zib.de
+     */
+    protected static class byRevId implements java.util.Comparator<Revision> {
+        /**
+         * Compares its two arguments for order. Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.
+         * 
+         * @param rev1
+         *            the first revision to be compared.
+         * @param rev2
+         *            the second revision to be compared.
+         * 
+         * @return a negative integer, zero, or a positive integer as the first
+         *         argument is less than, equal to, or greater than the second.
+         */
+        @Override
+        public int compare(Revision rev1, Revision rev2) {
+            return (rev1.getId() - rev2.getId());
+        }
+    }
+
+    /**
+     * Provides a comparator for sorting {@link Revision} objects by their IDs.
+     * 
+     * @author Nico Kruber, kruber@zib.de
+     */
+    protected static class byShortRevId implements java.util.Comparator<ShortRevision> {
+        /**
+         * Compares its two arguments for order. Returns a negative integer,
+         * zero, or a positive integer as the first argument is less than, equal
+         * to, or greater than the second.
+         * 
+         * @param rev1
+         *            the first revision to be compared.
+         * @param rev2
+         *            the second revision to be compared.
+         * 
+         * @return a negative integer, zero, or a positive integer as the first
+         *         argument is less than, equal to, or greater than the second.
+         */
+        @Override
+        public int compare(ShortRevision rev1, ShortRevision rev2) {
+            return (rev1.getId() - rev2.getId());
+        }
+    }
+}
--- trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpToScalarisHandler.java
+++ trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpToScalarisHandler.java
@@ -15,9 +15,7 @@
  */
 package de.zib.scalaris.examples.wikipedia.data.xml;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -42,28 +40,18 @@ import de.zib.scalaris.examples.wikipedia.bliki.MyWikiModel;
 import de.zib.scalaris.examples.wikipedia.data.Page;
 import de.zib.scalaris.examples.wikipedia.data.Revision;
 import de.zib.scalaris.examples.wikipedia.data.ShortRevision;
+import de.zib.scalaris.examples.wikipedia.data.SiteInfo;
 
 /**
  * Provides abilities to read an xml wiki dump file and write its contents to
- * the standard output.
+ * the Scalaris.
  * 
  * @author Nico Kruber, kruber@zib.de
  */
-public class WikiDumpToScalarisHandler extends WikiDumpHandler {
-    private static final int UPDATE_PAGELIST_EVERY = 400;
+public class WikiDumpToScalarisHandler extends WikiDumpPageHandler {
     private static final int MAX_SCALARIS_CONNECTIONS = 4;
-    private static final int NEW_CATS_HASH_DEF_SIZE = 100;
-    private static final int NEW_TPLS_HASH_DEF_SIZE = 100;
-    private static final int NEW_BLNKS_HASH_DEF_SIZE = 100;
-    
     private ArrayBlockingQueue<TransactionSingleOp> scalaris_single = new ArrayBlockingQueue<TransactionSingleOp>(MAX_SCALARIS_CONNECTIONS);
     private ArrayBlockingQueue<Transaction> scalaris_tx = new ArrayBlockingQueue<Transaction>(MAX_SCALARIS_CONNECTIONS);
-    private List<String> newPages = new LinkedList<String>();
-    private List<String> newArticles = new LinkedList<String>();
-    private HashMap<String, List<String>> newCategories = new HashMap<String, List<String>>(NEW_CATS_HASH_DEF_SIZE);
-    private HashMap<String, List<String>> newTemplates = new HashMap<String, List<String>>(NEW_TPLS_HASH_DEF_SIZE);
-    private HashMap<String, List<String>> newBackLinks = new HashMap<String, List<String>>(NEW_BLNKS_HASH_DEF_SIZE);
-    
     private ExecutorService executor = Executors.newFixedThreadPool(MAX_SCALARIS_CONNECTIONS);
 
     /**
@@ -89,7 +77,7 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
     public WikiDumpToScalarisHandler(Set<String> blacklist, Set<String> whitelist, int maxRevisions,
             Calendar maxTime) throws RuntimeException {
         super(blacklist, whitelist, maxRevisions, maxTime);
-        init(blacklist, maxRevisions, ConnectionFactory.getInstance());
+        init(ConnectionFactory.getInstance());
     }
 
     /**
@@ -118,26 +106,19 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
             Calendar maxTime, ConnectionFactory cFactory)
             throws RuntimeException {
         super(blacklist, whitelist, maxRevisions, maxTime);
-        init(blacklist, maxRevisions, cFactory);
+        init(cFactory);
     }
 
     /**
-     * Sets up a SAX XmlHandler exporting all parsed pages except the ones in a
-     * blacklist to stdout.
+     * Sets up connections to Scalaris.
      * 
-     * @param blacklist
-     *            a number of page titles to ignore
-     * @param maxRevisions
-     *            maximum number of revisions per page (starting with the most
-     *            recent) - <tt>-1/tt> imports all revisions
-     *            (useful to speed up the import / reduce the DB size)
      * @param cFactory
      *            the connection factory to use for creating new connections
      * 
      * @throws RuntimeException
      *             if the connection to Scalaris fails
      */
-    public void init(Set<String> blacklist, int maxRevisions, ConnectionFactory cFactory) throws RuntimeException {
+    private void init(ConnectionFactory cFactory) throws RuntimeException {
         try {
             for (int i = 0; i < MAX_SCALARIS_CONNECTIONS; ++i) {
                 Connection connection = cFactory.createConnection(
@@ -157,13 +138,11 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
     }
 
     /**
-     * Exports the given siteinfo to Scalaris
-     * 
-     * @param revisions
-     *            the siteinfo to export
+     * @param siteinfo
+     * @throws RuntimeException
      */
     @Override
-    protected void export(XmlSiteInfo siteinfo_xml) {
+    protected void doExport(SiteInfo siteinfo) throws RuntimeException {
         String key = ScalarisDataHandler.getSiteInfoKey();
         TransactionSingleOp scalaris_single;
         try {
@@ -173,7 +152,7 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
             throw new RuntimeException(e);
         }
         try {
-            scalaris_single.write(key, siteinfo_xml.getSiteInfo());
+            scalaris_single.write(key, siteinfo);
         } catch (ConnectionException e) {
             System.err.println("write of " + key + " failed with connection error");
         } catch (TimeoutException e) {
@@ -192,170 +171,36 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
             }
         }
     }
-
-    /**
-     * Provides a comparator for sorting {@link Revision} objects by their IDs.
-     * 
-     * @author Nico Kruber, kruber@zib.de
-     */
-    private static class byRevId implements java.util.Comparator<Revision> {
-        /**
-         * Compares its two arguments for order. Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.
-         * 
-         * @param rev1
-         *            the first revision to be compared.
-         * @param rev2
-         *            the second revision to be compared.
-         * 
-         * @return a negative integer, zero, or a positive integer as the first
-         *         argument is less than, equal to, or greater than the second.
-         */
-        @Override
-        public int compare(Revision rev1, Revision rev2) {
-            return (rev1.getId() - rev2.getId());
-        }
-    }
-
-    /**
-     * Provides a comparator for sorting {@link Revision} objects by their IDs.
-     * 
-     * @author Nico Kruber, kruber@zib.de
-     */
-    private static class byShortRevId implements java.util.Comparator<ShortRevision> {
-        /**
-         * Compares its two arguments for order. Returns a negative integer,
-         * zero, or a positive integer as the first argument is less than, equal
-         * to, or greater than the second.
-         * 
-         * @param rev1
-         *            the first revision to be compared.
-         * @param rev2
-         *            the second revision to be compared.
-         * 
-         * @return a negative integer, zero, or a positive integer as the first
-         *         argument is less than, equal to, or greater than the second.
-         */
-        @Override
-        public int compare(ShortRevision rev1, ShortRevision rev2) {
-            return (rev1.getId() - rev2.getId());
-        }
-    }
-    
-    private static class MyScalarisSingleRunnable implements Runnable {
-        private TransactionSingleOp.RequestList requests;
-        private ArrayBlockingQueue<TransactionSingleOp> scalaris_single;
-        private String note;
-        
-        public MyScalarisSingleRunnable(TransactionSingleOp.RequestList requests, ArrayBlockingQueue<TransactionSingleOp> scalaris_single, String note) {
-            this.requests = requests;
-            this.scalaris_single = scalaris_single;
-            this.note = note;
-        }
-        
-        @Override
-        public void run() {
-            TransactionSingleOp scalaris_single;
-            try {
-                scalaris_single = this.scalaris_single.take();
-            } catch (InterruptedException e) {
-                System.err.println("write of " + note + " interrupted while getting connection to Scalaris");
-                throw new RuntimeException(e);
-            }
-            try {
-                TransactionSingleOp.ResultList results = scalaris_single.req_list(requests);
-                for (int i = 0; i < results.size(); ++i) {
-                    results.processWriteAt(i);
-                }
-            } catch (ConnectionException e) {
-                System.err.println("write of " + note + " failed with connection error");
-            } catch (TimeoutException e) {
-                System.err.println("write of " + note + " failed with timeout");
-            } catch (AbortException e) {
-                System.err.println("write of " + note + " failed with abort");
-            }
-            if (scalaris_single != null) {
-                try {
-                    this.scalaris_single.put(scalaris_single);
-                } catch (InterruptedException e) {
-                    System.err.println("Interrupted while putting back a connection to Scalaris");
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-    }
     
     /**
-     * Exports the given page (including all revisions) to Scalaris
-     * 
-     * @param page_xml
-     *            the page object extracted from XML
+     * @param page
+     * @param revisions
+     * @param revisions_short
+     * @throws UnsupportedOperationException
      */
     @Override
-    protected void export(XmlPage page_xml) {
-        Page page = page_xml.getPage();
-        ++pageCount;
-        
-        if (page.getCurRev() != null) {
-            List<Revision> revisions = page_xml.getRevisions();
-            List<ShortRevision> revisions_short = ShortRevision.fromRevisions(revisions);
-            Collections.sort(revisions, Collections.reverseOrder(new byRevId()));
-            Collections.sort(revisions_short, Collections.reverseOrder(new byShortRevId()));
-
-            if (!revisions.isEmpty() && wikiModel != null) {
-                wikiModel.render(null, revisions.get(0).getText());
-                for (String cat_raw: wikiModel.getCategories().keySet()) {
-                    String category = wikiModel.getCategoryNamespace() + ":" + cat_raw;
-                    List<String> catPages = newCategories.get(category);
-                    if (catPages == null) {
-                        catPages = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
-                    }
-                    catPages.add(page.getTitle());
-                    newCategories.put(category, catPages);
-                }
-                for (String tpl_raw: wikiModel.getTemplates()) {
-                    String template = wikiModel.getTemplateNamespace() + ":" + tpl_raw;
-                    List<String> templatePages = newTemplates.get(template);
-                    if (templatePages == null) {
-                        templatePages = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
-                    }
-                    templatePages.add(page.getTitle());
-                    newTemplates.put(template, templatePages);
-                }
-                for (String link: wikiModel.getLinks()) {
-                    List<String> backLinks = newBackLinks.get(link);
-                    if (backLinks == null) {
-                        backLinks = new ArrayList<String>(UPDATE_PAGELIST_EVERY / 4);
-                    }
-                    backLinks.add(page.getTitle());
-                    newBackLinks.put(link, backLinks);
-                }
-            }
-
-            // do not make the translog too full -> write revisions beforehand,
-            // ignore the (rest of the) page if a failure occured
-            TransactionSingleOp.RequestList requests = new TransactionSingleOp.RequestList();
-            for (Revision rev : revisions) {
-                String key = ScalarisDataHandler.getRevKey(page.getTitle(), rev.getId());
-                requests.addWrite(key, rev);
-            }
-            requests.addWrite(ScalarisDataHandler.getRevListKey(page.getTitle()), revisions_short);
-            requests.addWrite(ScalarisDataHandler.getPageKey(page.getTitle()), page);
-            Runnable worker = new MyScalarisSingleRunnable(requests, scalaris_single, "revisions and page of " + page.getTitle());
-            executor.execute(worker);
-            newPages.add(page.getTitle());
-            // simple article filter: only pages in main namespace:
-            if (MyWikiModel.getNamespace(page.getTitle()).isEmpty()) {
-                newArticles.add(page.getTitle());
-            }
-            // only export page list every UPDATE_PAGELIST_EVERY pages:
-            if ((newPages.size() % UPDATE_PAGELIST_EVERY) == 0) {
-                updatePageLists();
-            }
+    protected void doExport(Page page, List<Revision> revisions,
+            List<ShortRevision> revisions_short)
+            throws UnsupportedOperationException {
+        // do not make the translog too full -> write revisions beforehand,
+        // ignore the (rest of the) page if a failure occured
+        TransactionSingleOp.RequestList requests = new TransactionSingleOp.RequestList();
+        for (Revision rev : revisions) {
+            String key = ScalarisDataHandler.getRevKey(page.getTitle(), rev.getId());
+            requests.addWrite(key, rev);
         }
-        if ((pageCount % UPDATE_PAGELIST_EVERY) == 0) {
-            msgOut.println("processed pages: " + pageCount);
+        requests.addWrite(ScalarisDataHandler.getRevListKey(page.getTitle()), revisions_short);
+        requests.addWrite(ScalarisDataHandler.getPageKey(page.getTitle()), page);
+        Runnable worker = new MyScalarisSingleRunnable(requests, scalaris_single, "revisions and page of " + page.getTitle());
+        executor.execute(worker);
+        newPages.add(page.getTitle());
+        // simple article filter: only pages in main namespace:
+        if (MyWikiModel.getNamespace(page.getTitle()).isEmpty()) {
+            newArticles.add(page.getTitle());
+        }
+        // only export page list every UPDATE_PAGELIST_EVERY pages:
+        if ((newPages.size() % UPDATE_PAGELIST_EVERY) == 0) {
+            updatePageLists();
         }
     }
 
@@ -418,7 +263,62 @@ public class WikiDumpToScalarisHandler extends WikiDumpHandler {
         }
         executor = Executors.newFixedThreadPool(MAX_SCALARIS_CONNECTIONS);
     }
-    
+
+    /**
+     * Processes write requests to Scalaris in a separate thread. Takes one of
+     * the available {@link #scalaris_single} connections.
+     * 
+     * @author Nico Kruber, kruber@zib.de
+     */
+    private static class MyScalarisSingleRunnable implements Runnable {
+        private TransactionSingleOp.RequestList requests;
+        private ArrayBlockingQueue<TransactionSingleOp> scalaris_single;
+        private String note;
+        
+        public MyScalarisSingleRunnable(TransactionSingleOp.RequestList requests, ArrayBlockingQueue<TransactionSingleOp> scalaris_single, String note) {
+            this.requests = requests;
+            this.scalaris_single = scalaris_single;
+            this.note = note;
+        }
+        
+        @Override
+        public void run() {
+            TransactionSingleOp scalaris_single;
+            try {
+                scalaris_single = this.scalaris_single.take();
+            } catch (InterruptedException e) {
+                System.err.println("write of " + note + " interrupted while getting connection to Scalaris");
+                throw new RuntimeException(e);
+            }
+            try {
+                TransactionSingleOp.ResultList results = scalaris_single.req_list(requests);
+                for (int i = 0; i < results.size(); ++i) {
+                    results.processWriteAt(i);
+                }
+            } catch (ConnectionException e) {
+                System.err.println("write of " + note + " failed with connection error");
+            } catch (TimeoutException e) {
+                System.err.println("write of " + note + " failed with timeout");
+            } catch (AbortException e) {
+                System.err.println("write of " + note + " failed with abort");
+            }
+            if (scalaris_single != null) {
+                try {
+                    this.scalaris_single.put(scalaris_single);
+                } catch (InterruptedException e) {
+                    System.err.println("Interrupted while putting back a connection to Scalaris");
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    /**
+     * Processes page list update requests to Scalaris in a separate thread.
+     * Takes one of the available {@link #scalaris_tx} connections.
+     * 
+     * @author Nico Kruber, kruber@zib.de
+     */
     private static class MyScalarisAddToPageListRunnable implements Runnable {
         private String scalaris_pageList_key;
         private List<String> newEntries;
