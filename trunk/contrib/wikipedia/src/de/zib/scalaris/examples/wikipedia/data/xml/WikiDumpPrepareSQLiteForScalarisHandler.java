7b042eb2e2d8057874e9c58ffc7ade9f009a343c
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2011-10-18 18:17:33 +0000

    - Wiki on Scalaris: merged WikiDumpPrepareForScalarisHandler into WikiDumpPrepareSQLiteForScalarisHandler

--- trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpPrepareSQLiteForScalarisHandler.java
+++ trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/WikiDumpPrepareSQLiteForScalarisHandler.java
@@ -23,6 +23,10 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -31,16 +35,24 @@ import com.almworks.sqlite4java.SQLiteConnection;
 import com.almworks.sqlite4java.SQLiteException;
 import com.almworks.sqlite4java.SQLiteStatement;
 
+import de.zib.scalaris.examples.wikipedia.ScalarisDataHandler;
+import de.zib.scalaris.examples.wikipedia.bliki.MyScalarisWikiModel;
+import de.zib.scalaris.examples.wikipedia.data.Page;
+import de.zib.scalaris.examples.wikipedia.data.Revision;
+import de.zib.scalaris.examples.wikipedia.data.ShortRevision;
+import de.zib.scalaris.examples.wikipedia.data.SiteInfo;
+
 /**
  * Provides abilities to read an xml wiki dump file and prepare its contents
  * for Scalaris by creating key/value pairs in a local SQLite db.
  * 
  * @author Nico Kruber, kruber@zib.de
  */
-public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForScalarisHandler {
+public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPageHandler {
     protected SQLiteConnection db;
     protected SQLiteStatement stRead;
     protected SQLiteStatement stWrite;
+    protected String dbFileName;
     
     /**
      * Sets up a SAX XmlHandler exporting all parsed pages except the ones in a
@@ -67,7 +79,8 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
     public WikiDumpPrepareSQLiteForScalarisHandler(Set<String> blacklist,
             Set<String> whitelist, int maxRevisions, Calendar maxTime,
             String dbFileName) throws RuntimeException {
-        super(blacklist, whitelist, maxRevisions, maxTime, dbFileName);
+        super(blacklist, whitelist, maxRevisions, maxTime);
+        this.dbFileName = dbFileName;
     }
 
     /**
@@ -121,7 +134,6 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
      * @throws IOException
      * @throws FileNotFoundException
      */
-    @Override
     protected <T> void writeObject(String key, T value)
             throws RuntimeException {
         writeObject(stWrite, key, value);
@@ -135,7 +147,6 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
      * @throws IOException
      * @throws FileNotFoundException
      */
-    @Override
     protected <T> T readObject(String key)
             throws RuntimeException, FileNotFoundException {
         // Note: need to qualify static function call due to
@@ -217,6 +228,7 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
     @Override
     public void setUp() {
         super.setUp();
+        msgOut.println("Pre-processing pages to key/value pairs...");
         
         try {
             db = openDB(this.dbFileName, false);
@@ -234,6 +246,7 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
     @Override
     public void tearDown() {
         super.tearDown();
+        updatePageLists();
         stRead.dispose();
         stWrite.dispose();
         db.dispose();
@@ -246,5 +259,113 @@ public class WikiDumpPrepareSQLiteForScalarisHandler extends WikiDumpPrepareForS
     protected void finalize() throws Throwable {
         super.finalize();
         db.dispose();
+    }
+
+    /**
+     * @param siteinfo
+     * @throws RuntimeException
+     */
+    @Override
+    protected void doExport(SiteInfo siteinfo) throws RuntimeException {
+        String key = ScalarisDataHandler.getSiteInfoKey();
+        writeObject(key, siteinfo);
+    }
+
+    /**
+     * @param page
+     * @param revisions
+     * @param revisions_short
+     * @throws UnsupportedOperationException
+     */
+    @Override
+    protected void doExport(Page page, List<Revision> revisions, List<ShortRevision> revisions_short)
+            throws UnsupportedOperationException {
+                for (Revision rev : revisions) {
+                    writeObject(ScalarisDataHandler.getRevKey(page.getTitle(), rev.getId()), rev);
+                }
+                writeObject(ScalarisDataHandler.getRevListKey(page.getTitle()), revisions_short);
+                writeObject(ScalarisDataHandler.getPageKey(page.getTitle()), page);
+                
+                newPages.add(page.getTitle());
+                // simple article filter: only pages in main namespace:
+                if (MyScalarisWikiModel.getNamespace(page.getTitle()).isEmpty()) {
+                    newArticles.add(page.getTitle());
+                }
+                // only export page list every UPDATE_PAGELIST_EVERY pages:
+                if ((newPages.size() % UPDATE_PAGELIST_EVERY) == 0) {
+                    updatePageLists();
+                }
+            }
+
+    protected void updatePageLists() {
+        String scalaris_key;
+        
+        // list of pages:
+        scalaris_key = ScalarisDataHandler.getPageListKey();
+        List<String> pageList;
+        try {
+            pageList = readObject(scalaris_key);
+        } catch (FileNotFoundException e) {
+            pageList = new LinkedList<String>();
+        }
+        pageList.addAll(newPages);
+        writeObject(scalaris_key, pageList);
+        writeObject(ScalarisDataHandler.getPageCountKey(), pageList.size());
+        newPages = new LinkedList<String>();
+        
+        // list of articles:
+        scalaris_key = ScalarisDataHandler.getArticleListKey();
+        List<String> articleList;
+        try {
+            articleList = readObject(scalaris_key);
+        } catch (FileNotFoundException e) {
+            articleList = new LinkedList<String>();
+        }
+        articleList.addAll(newArticles);
+        writeObject(scalaris_key, articleList);
+        writeObject(ScalarisDataHandler.getArticleCountKey(), articleList.size());
+        newArticles = new LinkedList<String>();
+        
+        // list of pages in each category:
+        for (Entry<String, List<String>> category: newCategories.entrySet()) {
+            scalaris_key = ScalarisDataHandler.getCatPageListKey(category.getKey());
+            List<String> catPageList;
+            try {
+                catPageList = readObject(scalaris_key);
+            } catch (FileNotFoundException e) {
+                catPageList = new LinkedList<String>();
+            }
+            catPageList.addAll(category.getValue());
+            writeObject(scalaris_key, catPageList);
+        }
+        newCategories = new HashMap<String, List<String>>(NEW_CATS_HASH_DEF_SIZE);
+    
+        // list of pages a template is used in:
+        for (Entry<String, List<String>> template: newTemplates.entrySet()) {
+            scalaris_key = ScalarisDataHandler.getTplPageListKey(template.getKey());
+            List<String> tplPageList;
+            try {
+                tplPageList = readObject(scalaris_key);
+            } catch (FileNotFoundException e) {
+                tplPageList = new LinkedList<String>();
+            }
+            tplPageList.addAll(template.getValue());
+            writeObject(scalaris_key, tplPageList);
+        }
+        newTemplates = new HashMap<String, List<String>>(NEW_TPLS_HASH_DEF_SIZE);
+        
+        // list of pages linking to other pages:
+        for (Entry<String, List<String>> backlinks: newBackLinks.entrySet()) {
+            scalaris_key = ScalarisDataHandler.getBackLinksPageListKey(backlinks.getKey());
+            List<String> backLinksPageList;
+            try {
+                backLinksPageList = readObject(scalaris_key);
+            } catch (FileNotFoundException e) {
+                backLinksPageList = new LinkedList<String>();
+            }
+            backLinksPageList.addAll(backlinks.getValue());
+            writeObject(scalaris_key, backLinksPageList);
+        }
+        newBackLinks = new HashMap<String, List<String>>(NEW_BLNKS_HASH_DEF_SIZE);
     }
 }
