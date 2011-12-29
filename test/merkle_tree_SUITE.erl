9c5fa0f40f70edf2a8c0b55af5f93ab02ff24195
lakedaimon300@gmail.com <lakedaimon300@gmail.com>
2011-12-29 10:00:21 +0000

    - changed eprof:analyze/2 call to analyze/0 for R13x compatibility

--- test/merkle_tree_SUITE.erl
+++ test/merkle_tree_SUITE.erl
@@ -71,7 +71,7 @@ eprof(_) ->
     Fun = fun() -> merkle_tree:bulk_build(I, [], Keys) end,
     eprof:profile([], Fun),
     eprof:stop_profiling(),
-    eprof:analyze(procs, [{sort, time}]),
+    eprof:analyze(),
     
     ok.
 
