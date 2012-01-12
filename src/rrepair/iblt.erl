ebf1bbf285e004e71edfd32e88702070995127c1
lakedaimon300@gmail.com <lakedaimon300@gmail.com>
2012-01-12 13:41:53 +0000

    - iblt: 	* implemented list_entries function 	* fixed delete bug through adding column number to table columns (this also simplifies the code) - db_generator: 	* added output options to generate key lists or key-value lists - iblt_SUITE: 	* added tester_get and tester_list_entries tests

--- src/rrepair/iblt.erl
+++ src/rrepair/iblt.erl
@@ -26,31 +26,30 @@
 -include("record_helpers.hrl").
 -include("scalaris.hrl").
 
--export([new/2, insert/3, delete/3, get/2]). %list_entries/1
+-export([new/2, insert/3, delete/3, get/2, list_entries/1]).
 -export([get_fpr/1, get_item_count/1]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Types
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--type key()   :: integer().
--type value() :: integer().
--type cell() :: {Count  :: non_neg_integer(),
-                 KeySum :: key(),
-                 ValSum :: value()}.
+-type key()     :: integer().
+-type value()   :: integer().
+-type cell()    :: {Count   :: non_neg_integer(),
+                    KeySum  :: key(),
+                    ValSum  :: value()}.
 
--type table() :: [[cell()]] | [].
+-type table() :: [] | [{ColNr :: pos_integer(), Cells :: [cell()]}].
 
 -record(iblt, {
-               hfs         = ?required(iblt, hfs) :: ?REP_HFS:hfs(),    %HashFunctionSet
-               table       = []                   :: table(),
-               cell_count  = 0                    :: non_neg_integer(), 
-               col_size    = 0                    :: non_neg_integer(), %cells per column
-               items_count = 0                    :: non_neg_integer()  %number of inserted items
+               hfs        = ?required(iblt, hfs) :: ?REP_HFS:hfs(),    %HashFunctionSet
+               table      = []                   :: table(),
+               cell_count = 0                    :: non_neg_integer(), 
+               col_size   = 0                    :: non_neg_integer(), %cells per column
+               item_count = 0                    :: non_neg_integer()  %number of inserted items
                }).
 
--type iblt() :: #iblt{}.
-%-opaque iblt() :: #iblt{}. 
+-opaque iblt() :: #iblt{}. 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% API
@@ -62,48 +61,48 @@ new(Hfs, CellCount) ->
     CCount = resize(CellCount, K), 
     ColSize = erlang:round(CCount / K),
     SubTable = [{0, 0 ,0} || _ <- lists:seq(1, ColSize)],
-    Table = [ SubTable || _ <- lists:seq(1, K)],
+    Table = [ {I, SubTable} || I <- lists:seq(1, K)],
     #iblt{
           hfs = Hfs, 
           table = Table, 
           cell_count = CCount, 
           col_size = ColSize,
-          items_count = 0
+          item_count = 0
           }.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec insert(iblt(), key(), value()) -> iblt().
 insert(IBLT, Key, Value) ->
-    change_iblt(IBLT, add, Key, Value).
+    change_table(IBLT, add, Key, Value).
 
 -spec delete(iblt(), key(), value()) -> iblt().
 delete(IBLT, Key, Value) ->
-    change_iblt(IBLT, remove, Key, Value).
+    change_table(IBLT, remove, Key, Value).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--spec change_iblt(iblt(), add | remove, key(), value()) -> iblt().
-change_iblt(#iblt{ hfs = Hfs, table = T, items_count = ItemCount, col_size = ColSize } = IBLT, 
-            Operation, Key, Value) ->
-     %TODO calculate each column in a separate process
-    {NT, _} = lists:foldl(
-                fun(Col, {NewT, K}) ->
-                        NCol = change_cell(Col, 
-                                           ?REP_HFS:apply_val(Hfs, K, Key) rem ColSize, 
-                                           Key, Value, Operation),
-                        {[NCol | NewT], K - 1}
-                end, {[], ?REP_HFS:size(Hfs)}, T),
-    IBLT#iblt{ table = NT, items_count = ItemCount + case Operation of
-                                                         add -> 1;
-                                                         remove -> -1
-                                                     end}.   
+-spec change_table(iblt(), add | remove, key(), value()) -> iblt().
+change_table(#iblt{ hfs = Hfs, table = T, item_count = ItemCount, col_size = ColSize } = IBLT, 
+             Operation, Key, Value) ->
+    %TODO calculate each column in a separate process
+    NT = lists:foldl(
+           fun({ColNr, Col}, NewT) ->
+                   NCol = change_cell(Col, 
+                                      ?REP_HFS:apply_val(Hfs, ColNr, Key) rem ColSize, 
+                                      Key, Value, Operation),
+                   [{ColNr, NCol} | NewT]
+           end, [], T),
+    IBLT#iblt{ table = NT, item_count = ItemCount + case Operation of
+                                                        add -> 1;
+                                                        remove -> -1
+                                                    end}.   
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec change_cell([cell()], pos_integer(), key(), value(), add | remove) -> [cell()].
-change_cell(CellList, CellNr, Key, Value, Operation) ->    
-    {HeadL, [Cell | TailL]} = lists:split(CellNr, CellList),
+change_cell(Column, CellNr, Key, Value, Operation) ->    
+    {HeadL, [Cell | TailL]} = lists:split(CellNr, Column),
     {Count, KeySum, ValSum} = Cell,
     case Operation of
         add -> lists:flatten([HeadL, {Count + 1, KeySum + Key, ValSum + Value}, TailL]);
@@ -114,48 +113,63 @@ change_cell(CellList, CellNr, Key, Value, Operation) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec get(iblt(), key()) -> value() | not_found.
-get(#iblt{ table = T, hfs = Hfs } = IBLT, Key) ->
-    p_get(T, IBLT, 1, ?REP_HFS:size(Hfs), Key).
+get(#iblt{ table = T } = IBLT, Key) ->
+    p_get(T, IBLT, Key).
 
--spec p_get(Table, IBLT, ActColumn, MaxColumn, Key) -> Result when
+-spec p_get(Table, IBLT, Key) -> Result when
     is_subtype(Table,     table()),
     is_subtype(IBLT,      iblt()),
-    is_subtype(ActColumn, pos_integer()),
-    is_subtype(MaxColumn, pos_integer()),
     is_subtype(Key,       key()),
     is_subtype(Result,    value() | not_found).
-p_get([], _, _, _, _) -> not_found;
-p_get(_, _, K, KMax, _) when K > KMax -> not_found;
-p_get([Col | T], #iblt{ hfs = Hfs, col_size = ColSize} = IBLT, K, KMax, Key) when K =< KMax->
+p_get([], _, _) -> not_found;
+p_get([{ColNr, Col} | T], #iblt{ hfs = Hfs, col_size = ColSize} = IBLT, Key) ->
     {Count, KeySum, ValSum} = 
-        lists:nth((?REP_HFS:apply_val(Hfs, K, Key) rem ColSize) + 1, Col),
+        lists:nth((?REP_HFS:apply_val(Hfs, ColNr, Key) rem ColSize) + 1, Col),
     if
-        Count =:= 0 -> p_get(T, IBLT, K + 1, KMax, Key);
+        Count =:= 0 -> p_get(T, IBLT, Key);
         Count =:= 1 andalso KeySum =:= Key -> ValSum;
-        true -> p_get(T, IBLT, K + 1, KMax, Key)
+        true -> p_get(T, IBLT, Key)
     end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% -spec list_entries(iblt()) -> [{key(), value()}].
-%% list_entries(IBLT) ->
-%%     p_list_entries(IBLT, []).
-%% 
-%% p_list_entries(#iblt{ hfs = _Hfs, table = _T }, Acc) ->
-%%     %TODO to implement
-%%     [Acc].
+-spec list_entries(iblt()) -> [{key(), value()}].
+list_entries(IBLT) ->
+    p_list_entries(IBLT, []).
+
+-spec p_list_entries(iblt(), [{key(), value()}]) -> [{key(), value()}].
+p_list_entries(#iblt{ table = T } = IBLT, Acc) ->
+    case get_any_entry(T, []) of
+        [] -> Acc;
+        L ->
+            NewIBLT = lists:foldl(fun({Key, Val}, NT) -> 
+                                          delete(NT, Key, Val) 
+                                  end, IBLT, L),
+            p_list_entries(NewIBLT, lists:append([L, Acc]))
+    end.
+
+-spec get_any_entry(table(), [{key(), value()}]) -> [{key() | value()}].
+get_any_entry([], Acc) -> 
+    Acc;
+get_any_entry([{_, Col} | T], Acc) ->
+    Result = [{Key, Val} || {Count, Key, Val} <- Col, Count =:= 1],
+    if 
+        Result =:= [] -> get_any_entry(T, lists:append([Result, Acc]));
+        true -> Result
+    end.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% @doc calculates actual false positive rate depending on saturation degree
 -spec get_fpr(iblt()) -> float().
-get_fpr(#iblt{  hfs = Hfs, cell_count = M, items_count = N }) ->
+get_fpr(#iblt{  hfs = Hfs, cell_count = M, item_count = N }) ->
     K = ?REP_HFS:size(Hfs),
     math:pow(1 - math:pow(math:exp(1), (-K*N)/M), K).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 -spec get_item_count(iblt()) -> non_neg_integer().
-get_item_count(#iblt{ items_count = C }) -> 
+get_item_count(#iblt{ item_count = C }) -> 
     C.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
