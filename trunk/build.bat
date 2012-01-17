7dd6231cc1b6fc4ca1a64e7c7a35556b9453d42f
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-01-17 13:21:06 +0000

    - adapted Windows (build and run) scripts to Erlang R14B04 and our newer config options

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,20 +1,24 @@
 @echo off
 :: set path to erlang installation
-set ERLANG="C:\Program Files\erl5.8.4\bin"
+set ERLANG="C:\Program Files\erl5.8.5\bin"
 
 ::replace @EMAKEFILEDEFINES@ from Emakefile.in and write Emakefile
 ::(this is what autoconf on *nix would do)
-:: depending on your config, you might need to add one of the following options:
-::  {d, have_toke},
-::  {d, types_not_builtin},
-::  {d, term_not_builtin},
-::  {d, node_not_builtin},
-::  {d, module_not_builtin},
-::  {d, boolean_not_builtin},
-::  {d, recursive_types_are_not_allowed,
-::  {d, type_forward_declarations_are_not_allowed},
-::  {d, forward_or_recursive_types_are_not_allowed},
+:: depending on your config, you might need to add one or more of the following options:
+::  {d, have_toke}
+::  {d, types_not_builtin}
+::  {d, term_not_builtin}
+::  {d, node_not_builtin}
+::  {d, module_not_builtin}
+::  {d, boolean_not_builtin}
 ::  {d, tid_not_builtin}
+::  {d, recursive_types_are_not_allowed}
+::  {d, type_forward_declarations_are_not_allowed}
+::  {d, forward_or_recursive_types_are_not_allowed}
+::  {d, with_export_type_support}
+::  {d, have_ctline_support}
+::  {d, have_cthooks_support}
+::  {d, have_callback_support}
 :: For older erlang versions that do not have the httpc module but use the (older)
 :: http module, the following line needs to be added to Emakefile:
 :: ["{\"contrib/compat/httpc.erl\",[debug_info, nowarn_unused_function, nowarn_obsolete_guard, nowarn_unused_vars,{outdir, \"ebin\"}]}."]
@@ -25,7 +29,7 @@ if not exist Emakefile (
 	for /f "tokens=1,* delims=&&&" %%a in (Emakefile.in) do (
 		set "line=%%a"
 		if defined line (
-			call set "line=echo.%%line:  @EMAKEFILEDEFINES@=, {d, tid_not_builtin}%%"
+			call set "line=echo.%%line:  @EMAKEFILEDEFINES@=, {d, tid_not_builtin}, {d, have_ctline_support}, {d, have_cthooks_support}, {d, with_export_type_support}%%"
 			call set "line=%%line:@EMAKEFILECOMPILECOMPAT@=%%"
 			for /f "delims=" %%X in ('"echo."%%line%%""') do %%~X >> Emakefile
 		) ELSE echo.
