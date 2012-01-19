027f4b8b8f3ac755a66dd9968c64f2cdb8d8e0aa
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-01-19 16:33:22 +0000

    [branches/0.4] - increased Python-API version

--- branches/0.4/python-api/setup.py
+++ branches/0.4/python-api/setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='scalaris',
-      version='0.3.0svn',
+      version='0.4.0',
       description='Scalaris python bindings',
       author='Nico Kruber',
       author_email='kruber@zib.de',
