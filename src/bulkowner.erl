de986e7e786e5ad56df39457d034655fee469772
schintke <schintke@zib.de>
2012-01-24 15:01:27 +0000

    - moved bulkower impl (on handler) from dht_node to bulkowner

--- src/bulkowner.erl
+++ src/bulkowner.erl
@@ -1,4 +1,4 @@
-%  @copyright 2007-2011 Zuse Institute Berlin
+%  @copyright 2007-2012 Zuse Institute Berlin
 
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -28,27 +28,49 @@
          send_reply/5, send_reply_failed/6]).
 
 % only use inside the dht_node process:
--export([bulk_owner/5]).
+-export([on/2]).
+
+-ifdef(with_export_type_support).
+-export_type([bulkowner_msg/0]).
+-endif.
+
+-type bulkowner_req() ::
+      {bulkowner, start,
+       Id::util:global_uid(), I::intervals:interval(), Msg::comm:message()}
+      | {bulkowner, Id::util:global_uid(), I::intervals:interval(),
+         Msg::comm:message(), Parents::[comm:mypid(),...]}
+      | {bulkowner, deliver, Id::util:global_uid(), Range::intervals:interval(),
+         Msg::comm:message(), Parents::[comm:mypid(),...]}
+      | {bulkowner, reply, Id::util:global_uid(), Target::comm:mypid(),
+         Msg::comm:message(), Parents::[comm:mypid()]}
+      | {bulkowner, reply_process_all}
+      | {bulkowner, gather, Id::util:global_uid(), Target::comm:mypid(),
+         [comm:message()], Parents::[comm:mypid()]}.
+
+-type bulkowner_send_error() ::
+        {send_error, comm:mypid(), bulkowner_msg(), atom() }.
+
+-type bulkowner_msg() :: bulkowner_req() | bulkowner_send_error().
 
 %% @doc Start a bulk owner operation to send the message to all nodes in the
 %%      given interval.
 -spec issue_bulk_owner(Id::util:global_uid(), I::intervals:interval(), Msg::comm:message()) -> ok.
 issue_bulk_owner(Id, I, Msg) ->
     DHTNode = pid_groups:find_a(dht_node),
-    comm:send_local(DHTNode, {start_bulk_owner, Id, I, Msg}).
+    comm:send_local(DHTNode, {bulkowner, start, Id, I, Msg}).
 
 -spec issue_send_reply(Id::util:global_uid(), Target::comm:mypid(), Msg::comm:message(), Parents::[comm:mypid()]) -> ok.
 issue_send_reply(Id, Target, Msg, Parents) ->
     DHTNode = pid_groups:find_a(dht_node),
-    comm:send_local(DHTNode, {bulkowner_reply, Id, Target, Msg, Parents}).
+    comm:send_local(DHTNode, {bulkowner, reply, Id, Target, Msg, Parents}).
 
 -spec send_reply(Id::util:global_uid(), Target::comm:mypid(), Msg::comm:message(), Parents::[comm:mypid()], Shepherd::comm:erl_local_pid()) -> ok.
 send_reply(Id, Target, {send_to_group_member, Proc, Msg}, [], Shepherd) ->
-    comm:send(Target, {bulkowner_reply, Id, Msg}, [{shepherd, Shepherd}, {group_member, Proc}]);
+    comm:send(Target, {bulkowner, reply, Id, Msg}, [{shepherd, Shepherd}, {group_member, Proc}]);
 send_reply(Id, Target, Msg, [], Shepherd) ->
-    comm:send(Target, {bulkowner_reply, Id, Msg}, [{shepherd, Shepherd}]);
+    comm:send(Target, {bulkowner, reply, Id, Msg}, [{shepherd, Shepherd}]);
 send_reply(Id, Target, Msg, [Parent | Rest], Shepherd) ->
-    comm:send(Parent, {bulkowner_reply, Id, Target, Msg, Rest}, [{shepherd, Shepherd}]).
+    comm:send(Parent, {bulkowner, reply, Id, Target, Msg, Rest}, [{shepherd, Shepherd}]).
 
 -spec send_reply_failed(Id::util:global_uid(), Target::comm:mypid(), Msg::comm:message(), Parents::[comm:mypid()], Shepherd::comm:erl_local_pid(), FailedPid::comm:mypid()) -> ok.
 send_reply_failed(_Id, Target, Msg, [], _Shepherd, Target) ->
@@ -67,7 +89,7 @@ bulk_owner(State, Id, I, Msg, Parents) ->
         true  -> ok;
         false ->
             comm:send(node:pidX(nodelist:succ(Neighbors)),
-                      {bulkowner_deliver, Id, SuccIntI, Msg, Parents})
+                      {bulkowner, deliver, Id, SuccIntI, Msg, Parents})
     end,
     case I =:= SuccIntI of
         true  -> ok;
@@ -101,8 +123,90 @@ bulk_owner_iter([Head | Tail], Id, I, Msg, Limit, Parents) ->
 %%     ct:pal("send_bulk_owner_if: ~p ~p ~n", [I, Range]),
     NewLimit =
         case intervals:is_empty(Range) of
-            false -> comm:send(node:pidX(Head), {bulk_owner, Id, Range, Msg, Parents}),
+            false -> comm:send(node:pidX(Head), {bulkowner, Id, Range, Msg, Parents}),
                      node:id(Head);
             true  -> Limit
         end,
     bulk_owner_iter(Tail, Id, I, Msg, NewLimit, Parents).
+
+%% protocol implementation / called inside the dht_node on handler
+-spec on(bulkowner_msg(), dht_node_state:state()) -> dht_node_state:state().
+on({bulkowner, start, Id, I, Msg}, State) ->
+    bulk_owner(State, Id, I, Msg, []),
+    State;
+
+on({bulkowner, Id, I, Msg, Parents}, State) ->
+    bulk_owner(State, Id, I, Msg, Parents),
+    State;
+
+on({bulkowner, deliver, Id, Range, Msg, Parents}, State) ->
+    MsgFwd = dht_node_state:get(State, msg_fwd),
+
+    F = fun({FwdInt, FwdPid}, AccI) ->
+                case intervals:is_subset(FwdInt, AccI) of
+                    true ->
+                        FwdRange = intervals:intersection(AccI, FwdInt),
+                        comm:send(FwdPid, {bulkowner, deliver, Id, FwdRange, Msg, Parents}),
+                        intervals:minus(AccI, FwdRange);
+                    _    -> AccI
+                end
+        end,
+    MyRange = lists:foldl(F, Range, MsgFwd),
+    case intervals:is_empty(MyRange) of
+        true -> ok;
+        _ ->
+            case Msg of
+                {bulk_read_entry, Issuer} ->
+                    Data = ?DB:get_entries(dht_node_state:get(State, db), MyRange),
+                    ReplyMsg = {bulk_read_entry_response, MyRange, Data},
+                    % for aggregation using a tree, activate this instead:
+                    % issue_send_reply(Id, Issuer, ReplyMsg, Parents);
+                    comm:send(Issuer, {bulkowner, reply, Id, ReplyMsg});
+                {send_to_group_member, Proc, Msg1} when Proc =/= dht_node ->
+                    comm:send_local(pid_groups:get_my(Proc),
+                                    {bulkowner, deliver, Id, Range, Msg1, Parents})
+            end
+    end,
+    State;
+
+on({bulkowner, reply, Id, Target, Msg, Parents}, State) ->
+    State1 =
+        case dht_node_state:get_bulkowner_reply_timer(State) =:= null of
+            true ->
+                Timer = comm:send_local_after(100, self(), {bulkowner, reply_process_all}),
+                dht_node_state:set_bulkowner_reply_timer(State, Timer);
+            false ->
+                State
+        end,
+    dht_node_state:add_bulkowner_reply_msg(State1, Id, Target, Msg, Parents);
+
+on({bulkowner, reply_process_all}, State) ->
+    {State1, Replies} = dht_node_state:take_bulkowner_reply_msgs(State),
+    _ = [begin
+             case Msgs of
+                 [] -> ok;
+                 [{bulk_read_entry_response, _HRange, _HData} | _] ->
+                     comm:send_local(self(),
+                                     {bulkowner, gather, Id, Target, Msgs, Parents});
+                 [{send_to_group_member, Proc, _Msg} | _] ->
+                     Msgs1 = [Msg1 || {send_to_group_member, _Proc, Msg1} <- Msgs],
+                     comm:send_local(pid_groups:get_my(Proc),
+                                     {bulkowner, gather, Id, Target, Msgs1, Parents})
+             end
+         end || {Id, Target, Msgs, Parents} <- Replies],
+    State1;
+
+on({bulkowner, gather, Id, Target, [H = {bulk_read_entry_response, _HRange, _HData} | T], Parents}, State) ->
+    Msg = lists:foldl(
+            fun({bulk_read_entry_response, Range, Data},
+                {bulk_read_entry_response, AccRange, AccData}) ->
+                    {bulk_read_entry_response,
+                     intervals:union(Range, AccRange),
+                     lists:append(Data, AccData)}
+            end, H, T),
+    send_reply(Id, Target, Msg, Parents, self()),
+    State;
+
+on({send_error, FailedTarget, {bulkowner, reply, Id, Target, Msg, Parents}, _Reason}, State) ->
+    bulkowner:send_reply_failed(Id, Target, Msg, Parents, self(), FailedTarget),
+    State.
