c78f6f812f8fdc610dd3535a84cc135cd843334b
maier@zib.de <maier@zib.de>
2012-02-07 10:22:13 +0000

    Hadoop image for contrail:  - service will stay in INIT state until /tmp/mrmetrics.log is created.

--- contrib/opennebula/webinterface-v2/hadoophelper.rb
+++ contrib/opennebula/webinterface-v2/hadoophelper.rb
@@ -19,8 +19,44 @@ class HadoopHelper < OpenNebulaHelper
     [false, "Not yet implemented"]
   end
 
+  def list(instance)
+    nodes = {}
+    nodes[:masters] = [ENV["VMID"]] 
+    nodes[:workers] = get_vms(instance)
+    nodes
+  end
+
   def get_node_info(instance, vmid)
     info = {}
+    dn_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_datanode'")
+    tt_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_tasktracker'")
+    metrics_avail = system("test -f /tmp/mrmetrics.log");
+
+    errorstring = (dn_running ? "" : "DataNode ") + (tt_running ? "" : "TaskTracker ")
+
+    myip = get_ip(ENV['VMID'])
+    if myip == ENV['HADOOP_MASTER']
+      nn_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_namenode'")
+      sn_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_secondarynamenode'")
+      jt_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_jobtracker'")
+      errorstring += (nn_running ? "" : "NameNode ") + (sn_running ? "" : "SecondaryNameNode ")
+      errorstring += (jt_running ? "" : "JobTracker")
+    else
+      nn_running = true
+      sn_running = true
+      jt_running = true
+    end
+    if dn_running && tt_running && nn_running && sn_running && jt_running
+      if ! metrics_avail
+        info[:state] = "INIT"
+      else
+        info[:state] = "RUNNING"
+      end
+    else
+      info[:state] = "ERROR"
+      info[:error] = "Missing processes: " + errorstring
+    end
+
     found = {}
     File.readlines("/tmp/mrmetrics.log").reverse_each do |line|
       if line.start_with?("mapred.tasktracker") || line.start_with?("mapred.shuffleOutput")
@@ -52,17 +88,33 @@ class HadoopHelper < OpenNebulaHelper
   def get_service_info(instance)
     myip = get_ip(ENV['VMID'])
     if myip == ENV['HADOOP_MASTER']
+      metrics_avail = system("test -f /tmp/mrmetrics.log");
       info = {}
-      File.readlines("/tmp/mrmetrics.log").reverse_each do |line|
-        if line.start_with?("mapred.jobtracker") 
-          line.split(' ').each do |field|
-            if field.match("=") 
-              data = field.split('=')
-              info[data[0]] = data[1].sub(/,/, '') 
+      if metrics_avail
+        File.readlines("/tmp/mrmetrics.log").reverse_each do |line|
+          if line.start_with?("mapred.jobtracker") 
+            line.split(' ').each do |field|
+              if field.match("=") 
+                data = field.split('=')
+                info[data[0]] = data[1].sub(/,/, '') 
+              end
             end
+            break
           end
-          break
         end
+        nn_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_namenode'")
+        sn_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_secondarynamenode'")
+        jt_running = system("ps ax | grep -v grep | grep -q 'java -Dproc_jobtracker'")
+        errorstring = (nn_running ? "" : "NameNode ") + (sn_running ? "" : "SecondaryNameNode ")
+        errorstring += (jt_running ? "" : "JobTracker")
+        if nn_running && sn_running && jt_running
+          info[:state] = "RUNNING"
+        else
+          info[:state] = "ERROR"
+          info[:error] = "Missing processes: " + errorstring
+        end
+      else
+        info[:state] = "INIT"
       end
       info
     else
