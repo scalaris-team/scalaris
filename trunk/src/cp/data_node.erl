6205811f19b0cdb03988f42f7eb13e3876ca7934
schintke <schintke@71766da9-9752-0410-ab17-bfafa751c56a>
2012-04-17 09:07:33 +0000

    - restructured data_node

--- trunk/src/cp/data_node.erl
+++ trunk/src/cp/data_node.erl
@@ -1,4 +1,4 @@
-%  @copyright 2007-2012 Zuse Institute Berlin
+%  @copyright 2012 Zuse Institute Berlin
 
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -13,83 +13,107 @@
 %   limitations under the License.
 
 %% @author Thorsten Schuett <schuett@zib.de>
+%% @author Florian Schintke <schintke@zib.de>
 %% @doc    data_node main file
 %% @end
 %% @version $Id$
 -module(data_node).
 -author('schuett@zib.de').
+-author('schintke@zib.de').
 -vsn('$Id').
 
 -include("scalaris.hrl").
 -behaviour(gen_component).
-
 -export([start_link/2, on/2, init/1]).
 
+%% handling state of a data node
+-export([new_state/0,
+         get_leases/1, set_leases/2,
+         get_db/1,
+         get_rlease_mgmt/1, set_rlease_mgmt/2]).
+
 -ifdef(with_export_type_support).
--export_type([message/0]).
+-export_type([msg/0, state/0]).
 -endif.
 
 % accepted messages of data_node processes
--type message() :: any().
+-type msg() ::
+        data_node_leases:msg()
+      | data_node_db:msg()
+      | {lookup_fin, ?RT:key(), comm:message()}.
 
-%===============================================================================
-%
-% on handler
-%
-%===============================================================================
+-record(state, {
+          %% my leases for working...
+          leases   :: data_node_leases:state(),
+          %% my database for user content
+          db       :: data_node_db:state(),
+          %% replicated lease storage and management of the lease_db
+          rlease_mgmt :: rlease_mgmt:state()
+         }).
+-type state() :: #state{}.
 
 %% @doc message handler
--spec on(message(), data_node_state:state()) -> data_node_state:state() | kill.
+-spec on(msg(), state()) -> state().
 
-on({read, SourcePid, SourceId, HashedKey}, State) ->
+%% messages concerning leases
+on({lease, _LeaseMsg} = Msg, State) ->
+    data_node_leases:on(Msg, State);
+
+%% messages concerining db access
+on({read, _SourcePid, _SourceId, _HashedKey} = Msg, State) ->
     % do not need to check lease
-    Value = data_node_state:db_read(HashedKey),
-    Msg = {read_reply, SourceId, HashedKey, Value},
-    comm:send(SourcePid, Msg),
-    State;
+    data_node_db:on(Msg, State);
 
-% messages concerning leases
-on({lease_message, LeaseMsg}, State) ->
-    data_node_lease:on(LeaseMsg, State);
-
+%% messages concerning routing
 on({lookup_fin, Key, Msg}, State) ->
-    case data_node_state:is_owner(State, Key) of
+    case data_node_leases:is_owner(State, Key) of
         true ->
             gen_component:post_op(State, Msg);
         false ->
+            io:format("I am not owner~n"),
             case pid_groups:get_my(router_node) of
-                failed ->
-                    ok; %% TODO: Delete this case, as it should not happen...
-                Pid ->
-                    comm:send_local(Pid, {lookup_aux, Key, Msg})
+                failed -> ok; %% TODO: Delete this case, should not happen.
+                Pid -> comm:send_local(Pid, {lookup_aux, Key, Msg})
             end,
             State
     end.
 
-%===============================================================================
-%
-% init and misc.
-%
-%===============================================================================
 
 %% @doc joins this node in the ring and calls the main loop
--spec init(Options::[tuple()])
-        -> dht_node_state:state() |
-           {'$gen_component', [{on_handler, Handler::gen_component:handler()}],
-            State::dht_node_join:join_state()}.
+-spec init(Options::[tuple()]) -> state().
 init(Options) ->
     true = is_first(Options),
-    data_node_join:join_as_first(Options).
+    State = new_state(),
+    data_node_join:join_as_first(State, Options).
 
 %% @doc spawns a scalaris node, called by the scalaris supervisor process
 -spec start_link(pid_groups:groupname(), [tuple()]) -> {ok, pid()}.
-start_link(DHTNodeGroup, Options) ->
-    gen_component:start_link(?MODULE, fun ?MODULE:on/2, Options,
-                             [{pid_groups_join_as, DHTNodeGroup, data_node},
-                              wait_for_init]).
+start_link(DataNodeGroup, Options) ->
+    gen_component:start_link(
+      ?MODULE, fun ?MODULE:on/2, Options,
+      [{pid_groups_join_as, DataNodeGroup, data_node}, wait_for_init]).
 
 %% @doc Checks whether this VM is marked as first, e.g. in a unit test, and
 %%      this is the first node in this VM.
 -spec is_first([tuple()]) -> boolean().
 is_first(Options) ->
     lists:member({first}, Options) andalso admin_first:is_first_vm().
+
+
+%% operations on the state
+-spec new_state() -> state().
+new_state() ->
+    #state{db  = data_node_db:new_state(),
+           leases = data_node_leases:new_state(),
+           rlease_mgmt = rlease_mgmt:new_state()}.
+
+-spec get_leases(state()) -> data_node_leases:state().
+get_leases(#state{leases=Leases}) -> Leases.
+-spec set_leases(state(), data_node_leases:state()) -> state().
+set_leases(State, LState) -> State#state{leases = LState}.
+-spec get_db(state()) -> data_node_db:state().
+get_db(#state{db=DB}) -> DB.
+-spec get_rlease_mgmt(state()) -> rlease_mgmt:state().
+get_rlease_mgmt(#state{rlease_mgmt=RLMState}) -> RLMState.
+-spec set_rlease_mgmt(state(), rlease_mgmt:state()) -> state().
+set_rlease_mgmt(State, RLMState) -> State#state{rlease_mgmt = RLMState}.
--- trunk/src/cp/data_node_join.erl
+++ trunk/src/cp/data_node_join.erl
@@ -1,4 +1,4 @@
-%  @copyright 2007-2012 Zuse Institute Berlin
+%  @copyright 2012 Zuse Institute Berlin
 
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -13,20 +13,25 @@
 %   limitations under the License.
 
 %% @author Thorsten Schuett <schuett@zib.de>
-%% @doc    data_node database file
+%% @author Florian Schintke <schintke@zib.de>
+%% @doc    data_node join protocol
 %% @end
 %% @version $Id$
 -module(data_node_join).
 -author('schuett@zib.de').
+-author('schintke@zib.de').
 -vsn('$Id').
-
 -include("scalaris.hrl").
 
--export([join_as_first/1
-         ]).
+-export([join_as_first/2]).
 
--spec join_as_first(Options::[tuple()]) -> data_node_state:state().
-join_as_first(Options) ->
-    State = data_node_state:new(),
-    {LeaseId, Lease} = data_node_lease:create_first_lease(),
-    data_node_state:store_lease(State, LeaseId, Lease).
+-spec join_as_first(data_node:state(), [tuple()]) -> data_node:state().
+join_as_first(State, _Options) ->
+    Lease = leases:new_for_all(),
+    LMState = data_node:get_rlease_mgmt(State),
+    LMState1 = rlease_mgmt:store_rlease(LMState, Lease, 0),
+    LMState2 = rlease_mgmt:store_rlease(LMState1, Lease, 1),
+    LMState3 = rlease_mgmt:store_rlease(LMState2, Lease, 2),
+    LMState4 = rlease_mgmt:store_rlease(LMState3, Lease, 3),
+    State1 = data_node:set_leases(State, [Lease]),
+    data_node:set_rlease_mgmt(State1, LMState4).
