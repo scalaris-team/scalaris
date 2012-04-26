6afa7f43d16735dd45995337659aa8dcc0329851
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-04-26 12:58:46 +0000

    - Wiki on Scalaris: extended the LinkedMultiHashMap concept to a generic MultiMap

--- trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/LinkedMultiHashMap.java
+++ trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/LinkedMultiHashMap.java
@@ -15,12 +15,9 @@
  */
 package de.zib.scalaris.examples.wikipedia;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * Provides a multi-map, i.e. a map associating multiple values with a single
@@ -33,13 +30,12 @@ import java.util.Set;
  * 
  * @author Nico Kruber, kruber@zib.de
  */
-public class LinkedMultiHashMap<K, V> implements Map<K, List<V>> {
-    final protected Map<K, List<V>> stats = new LinkedHashMap<K, List<V>>();
-
+public class LinkedMultiHashMap<K, V> extends MultiMap<LinkedHashMap<K, List<V>>, K, V> {
     /**
      * Constructs an empty map.
      */
     public LinkedMultiHashMap() {
+        super(LinkedHashMap.class);
     }
     
     /**
@@ -51,98 +47,6 @@ public class LinkedMultiHashMap<K, V> implements Map<K, List<V>> {
      * @see #putAll(Map)
      */
     public LinkedMultiHashMap(Map<? extends K, ? extends List<V>> other) {
-        putAll(other);
-    }
-    
-    @Override
-    public void clear() {
-        stats.clear();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return stats.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        for (Entry<? extends K, ? extends List<V>> stat : stats.entrySet()) {
-            if (stat.getValue().contains(value)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public Set<java.util.Map.Entry<K, List<V>>> entrySet() {
-        return stats.entrySet();
-    }
-
-    @Override
-    public List<V> get(Object key) {
-        return stats.get(key);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return stats.isEmpty();
-    }
-
-    @Override
-    public Set<K> keySet() {
-        return stats.keySet();
-    }
-
-    /**
-     * Convenience method for inserting a single value into the map.
-     * 
-     * @param key
-     *            the key to insert at
-     * @param value
-     *            the value to insert
-     * 
-     * @return the previous value associated with key, or null if there was no
-     *         mapping for key. (A null return can also indicate that the map
-     *         previously associated null with key, if the implementation
-     *         supports null values.)
-     */
-    public List<V> put(K key, V value) {
-        ArrayList<V> list = new ArrayList<V>();
-        list.add(value);
-        return put(key, list);
-    }
-
-    @Override
-    public List<V> put(K key, List<V> value) {
-        List<V> l = stats.get(key);
-        if (l == null) {
-            return stats.put(key, value);
-        } else {
-            l.addAll(value);
-            return stats.put(key, l);
-        }
-    }
-
-    @Override
-    public void putAll(Map<? extends K, ? extends List<V>> m) {
-        for (Entry<? extends K, ? extends List<V>> value : m.entrySet()) {
-            put(value.getKey(), value.getValue());
-        }
-    }
-
-    @Override
-    public List<V> remove(Object key) {
-        return stats.remove(key);
-    }
-
-    @Override
-    public int size() {
-        return stats.size();
-    }
-
-    @Override
-    public Collection<List<V>> values() {
-        return stats.values();
+        super(LinkedHashMap.class, other);
     }
 }
