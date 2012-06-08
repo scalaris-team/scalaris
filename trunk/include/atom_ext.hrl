501565f9a32f60461763dc27ee56cc9d8dc75f77
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-06-08 16:03:29 +0000

    - allow switching between atoms and integers for hot messages via DEBUG macro

--- trunk/include/atom_ext.hrl
+++ trunk/include/atom_ext.hrl
@@ -23,63 +23,69 @@
 
 %% TAKE EXTRA CARE THAT ALL VALUES ARE UNIQUE! %%
 
+-ifdef(DEBUG).
+-define(int_or_atom(Int, Atom), Atom).
+-else.
+-define(int_or_atom(Int, Atom), Int).
+-endif.
+
 %% lookup
 -define(lookup_aux_atom, lookup_aux).
--define(lookup_aux, 01).
+-define(lookup_aux, ?int_or_atom(01, ?lookup_aux_atom)).
 
 -define(lookup_fin_atom, lookup_fin).
--define(lookup_fin, 02).
+-define(lookup_fin, ?int_or_atom(02, ?lookup_fin_atom)).
 
 %% dht_node
 -define(get_key_with_id_reply_atom, get_key_with_id_reply).
--define(get_key_with_id_reply, 21).
+-define(get_key_with_id_reply, ?int_or_atom(21, ?get_key_with_id_reply_atom)).
 
 %% paxos
 -define(proposer_accept_atom, proposer_accept).
--define(proposer_accept, 41).
+-define(proposer_accept, ?int_or_atom(41, ?proposer_accept_atom)).
 
 -define(acceptor_accept_atom, acceptor_accept).
--define(acceptor_accept, 42).
+-define(acceptor_accept, ?int_or_atom(42, ?acceptor_accept_atom)).
 
 %% transactions
 -define(register_TP_atom, register_TP).
--define(register_TP, 61).
+-define(register_TP, ?int_or_atom(61, ?register_TP_atom)).
 
 -define(tx_tm_rtm_init_RTM_atom, tx_tm_rtm_init_RTM).
--define(tx_tm_rtm_init_RTM, 62).
+-define(tx_tm_rtm_init_RTM, ?int_or_atom(62, ?tx_tm_rtm_init_RTM_atom)).
 
 -define(tp_do_commit_abort_atom, tp_do_commit_abort).
--define(tp_do_commit_abort, 63).
+-define(tp_do_commit_abort, ?int_or_atom(63, ?tp_do_commit_abort_atom)).
 
 -define(tx_tm_rtm_delete_atom, tx_tm_rtm_delete).
--define(tx_tm_rtm_delete, 64).
+-define(tx_tm_rtm_delete, ?int_or_atom(64, ?tx_tm_rtm_delete_atom)).
 
 -define(tp_committed_atom, tp_committed).
--define(tp_committed, 65).
+-define(tp_committed, ?int_or_atom(65, ?tp_committed_atom)).
 
 -define(tx_state_atom, tx_state).
--define(tx_state, 66).
+-define(tx_state, ?int_or_atom(66, ?tx_state_atom)).
 
 -define(tx_id_atom, tx_id).
--define(tx_id, 67).
+-define(tx_id, ?int_or_atom(67, ?tx_id_atom)).
 
 -define(tx_item_id_atom, tx_item_id).
--define(tx_item_id, 68).
+-define(tx_item_id, ?int_or_atom(68, ?tx_item_id_atom)).
 
 -define(tx_item_state_atom, tx_item_state).
--define(tx_item_state, 69).
+-define(tx_item_state, ?int_or_atom(69, ?tx_item_state_atom)).
 
 -define(commit_client_id_atom, commit_client_id).
--define(commit_client_id, 70).
+-define(commit_client_id, ?int_or_atom(70, ?commit_client_id_atom)).
 
 -define(undecided_atom, undecided).
--define(undecided, 71).
+-define(undecided, ?int_or_atom(71, ?undecided_atom)).
 
 -define(prepared_atom, prepared).
--define(prepared, 72).
+-define(prepared, ?int_or_atom(72, ?prepared_atom)).
 
 -define(commit_atom, commit).
--define(commit, 73).
+-define(commit, ?int_or_atom(73, ?commit_atom)).
 
 -define(abort_atom, abort).
--define(abort, 74).
+-define(abort, ?int_or_atom(74, ?abort_atom)).
