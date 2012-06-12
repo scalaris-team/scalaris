72f5f12e97a0abb346c2782e5c899b8214ccdd62
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-06-12 15:17:48 +0000

    [branches/0.4] - Wiki on Scalaris: made LinkedMultiHashMap Java-1.7 compliant

--- branches/0.4/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/MyWikiModel.java
+++ branches/0.4/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/MyWikiModel.java
@@ -903,7 +903,7 @@ public class MyWikiModel extends WikiModel {
      *            the number of milliseconds it took to retrieve the page
      */
     public void addStat(String title, long value) {
-        stats.put(title, value);
+        stats.put1(title, value);
     }
 
     /**
