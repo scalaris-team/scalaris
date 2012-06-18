scalaris-team/scalaris

commit 36db50f07ed81e9fe70330ec3b8c77275e1a8ad4

- Wiki on Scalaris: allow logging of user requests (if a service_user parameter is given, this is also being logged)
git-svn-id: https://scalaris.googlecode.com/svn/trunk@3290 71766da9-9752-0410-ab17-bfafa751c56a

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/WikiServlet.java
@@ -28,6 +28,7 @@ import java.util.Arrays;
 import java.util.Calendar;
 import java.util.EnumMap;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -127,6 +128,10 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
      * Full list of normalised special page titles for {@link #existingPages}. 
      */
     protected final List<NormalisedTitle> specialPages;
+    
+    protected static LinkedList<Map<String, Object>>[] userReqLogs = null;
+    protected static int curReqLog = 0;
+    protected static int curReqLogStartTime = 0;
 
     /**
      * Creates the servlet. 
@@ -152,6 +157,7 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
      * Servlet initialisation: imports options from the servlet info, and
      * initialises it.
      */
+    @SuppressWarnings("unchecked")
     @Override
     public final void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -163,6 +169,14 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
         loadPlugins();
         startExistingPagesUpdate();
         existingPages.addAll(specialPages);
+        if (Options.getInstance().LOG_USER_REQS > 0) {
+            userReqLogs = new LinkedList[Options.getInstance().LOG_USER_REQS];
+            for (int i = 0; i < userReqLogs.length; ++i) {
+                userReqLogs[i] = new LinkedList<Map<String, Object>>();
+            }
+            // integer resolution should be enough
+            curReqLogStartTime = (int) (System.currentTimeMillis() / 1000);
+        }
     }
 
     /**
@@ -181,7 +195,8 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
                 config.getInitParameter("WIKI_SAVEPAGE_RETRY_DELAY"),
                 config.getInitParameter("WIKI_REBUILD_PAGES_CACHE"),
                 config.getInitParameter("WIKI_STORE_CONTRIBUTIONS"),
-                config.getInitParameter("WIKI_OPTIMISATIONS"));
+                config.getInitParameter("WIKI_OPTIMISATIONS"),
+                config.getInitParameter("LOG_USER_REQS"));
     }
     
     /**
@@ -1808,7 +1823,7 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
      * @param name
      *            the name of the parameter
      * 
-     * @return the notice parameter or ""
+     * @return the requested parameter or ""
      */
     public static String getParam(HttpServletRequest request, String name) {
         String parValue = request.getParameter(name);
@@ -1975,6 +1990,52 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
             } finally {
                 releaseConnection(null, connection);
             }
+        }
+    }
+    
+    /**
+     * Adds a user request to the user request log if enabled by setting
+     * {@link Options#LOG_USER_REQS} to a value larger than <tt>0</tt>.
+     * 
+     * @param timestamp
+     *            milliseconds since midnight, January 1, 1970
+     * @param serviceUser
+     *            service user name (may be null)
+     * @param servertime
+     *            time spend in the web server
+     */
+    public static void storeUserReq(long timestamp, String serviceUser, long servertime) {
+        if (userReqLogs.length > 0) {
+            int timestamp_s = (int) (timestamp / 1000);
+            // build log entry:
+            Map<String, Object> entry = new HashMap<String, Object>();
+            entry.put("timestamp", timestamp_s);
+            entry.put("serviceuser", serviceUser == null ? "" : serviceUser);
+            entry.put("servertime", servertime);
+            
+            // get the correct bucket:
+            LinkedList<Map<String, Object>> curReqLogList;
+            synchronized (userReqLogs) {
+                curReqLogList = userReqLogs[curReqLog];
+                int diffInMin = (timestamp_s - curReqLogStartTime) / 60;
+                if (diffInMin > 0) {
+                    if (diffInMin > userReqLogs.length) {
+                        for (LinkedList<Map<String, Object>> log : userReqLogs) {
+                            log.clear();
+                        }
+                        curReqLog = 0;
+                        curReqLogList = userReqLogs[curReqLog];
+                    } else  {
+                        while ((curReqLogStartTime + 60) < timestamp_s) {
+                            curReqLogList = userReqLogs[curReqLog];
+                            curReqLogList.clear();
+                            curReqLog = (++curReqLog) % userReqLogs.length;
+                        }
+                    }
+                    curReqLogStartTime += 60 * diffInMin;
+                }
+            }
+            curReqLogList.add(entry);
         }
     }
 }

contrib/wikipedia/scalaris-wiki/WEB-INF/web.xml
@@ -12,6 +12,11 @@
 			<param-value>localhost:8080</param-value>
 		</init-param>
 		<init-param>
+			<param-name>LOG_USER_REQS</param-name>
+			<!-- keep records of the last x minutes since the last request-->
+			<param-value>0</param-value>
+		</init-param>
+		<init-param>
 			<param-name>SERVERPATH</param-name>
 			<param-value>/scalaris-wiki/wiki</param-value>
 		</init-param>

contrib/wikipedia/scalaris-wiki/page.jsp
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
+<%@page import="de.zib.scalaris.examples.wikipedia.bliki.WikiServlet"%>
 <%@page import="de.zib.scalaris.examples.wikipedia.InvolvedKey"%>
 <%@page import="java.net.URL"%>
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
@@ -424,5 +425,6 @@ server: <%= renderTime %>
                 </pre></div>
         </div>
         <!-- /footer -->
+<% WikiServlet.storeUserReq(pageBean.getStartTime(), WikiServlet.getParam(request, "service_user"), renderTime); %>
 </body>
 </html>

contrib/wikipedia/scalaris-wiki/pageEdit.jsp
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
+<%@page import="de.zib.scalaris.examples.wikipedia.bliki.WikiServlet"%>
 <%@page import="de.zib.scalaris.examples.wikipedia.InvolvedKey"%>
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
  import="java.util.Calendar,java.util.Locale,java.text.DateFormat,java.text.SimpleDateFormat,java.util.TimeZone,java.util.Iterator,java.util.Map,java.util.List,org.apache.commons.lang.StringUtils,org.apache.commons.lang.StringEscapeUtils"%>
@@ -369,5 +370,6 @@ server: <%= renderTime %>
                 </pre></div>
         </div>
         <!-- /footer -->
+<% WikiServlet.storeUserReq(pageBean.getStartTime(), WikiServlet.getParam(request, "service_user"), renderTime); %>
 </body>
 </html>

contrib/wikipedia/scalaris-wiki/pageHistory.jsp
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
+<%@page import="de.zib.scalaris.examples.wikipedia.bliki.WikiServlet"%>
 <%@page import="de.zib.scalaris.examples.wikipedia.InvolvedKey"%>
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
  import="java.util.Calendar,java.util.Locale,java.text.DateFormat,java.text.SimpleDateFormat,java.util.TimeZone,java.util.Iterator,de.zib.scalaris.examples.wikipedia.data.Revision,de.zib.scalaris.examples.wikipedia.data.ShortRevision,org.apache.commons.lang.StringEscapeUtils,java.util.Map,java.util.List"%>
@@ -389,5 +390,6 @@ server: <%= renderTime %>
                 </pre></div>
         </div>
         <!-- /footer -->
+<% WikiServlet.storeUserReq(pageBean.getStartTime(), WikiServlet.getParam(request, "service_user"), renderTime); %>
 </body>
 </html>

contrib/wikipedia/scalaris-wiki/pageSpecial_pagelist.jsp
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8" ?>
+<%@page import="de.zib.scalaris.examples.wikipedia.bliki.WikiServlet"%>
 <%@page import="de.zib.scalaris.examples.wikipedia.InvolvedKey"%>
 <%@page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"
  import="java.util.Calendar,java.util.Locale,java.text.DateFormat,java.text.SimpleDateFormat,java.util.TimeZone,java.util.Iterator,de.zib.scalaris.examples.wikipedia.bliki.WikiPageListBean,java.util.Map,java.util.List,org.apache.commons.lang.StringEscapeUtils"%>
@@ -349,5 +350,6 @@ server: <%= renderTime %>
                 </pre></div>
         </div>
         <!-- /footer -->
+<% WikiServlet.storeUserReq(pageBean.getStartTime(), WikiServlet.getParam(request, "service_user"), renderTime); %>
 </body>
 </html>

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/Options.java
@@ -81,6 +81,12 @@ public class Options {
             ScalarisOpType.class);
     
     /**
+     * Store user requests in a log for the last x minutes before the last
+     * request.
+     */
+    public int LOG_USER_REQS = 0;
+    
+    /**
      * Creates a new default option object.
      */
     public Options() {
@@ -310,13 +316,16 @@ public class Options {
      *            {@link Options#WIKI_STORE_CONTRIBUTIONS}
      * @param OPTIMISATIONS
      *            {@link Options#OPTIMISATIONS}
+     * @param LOG_USER_REQS
+     *            {@link Options#LOG_USER_REQS}
      */
     public static void parseOptions(Options options, final String SERVERNAME, final String SERVERPATH,
             final String WIKI_USE_BACKLINKS,
             final String WIKI_SAVEPAGE_RETRIES,
             final String WIKI_SAVEPAGE_RETRY_DELAY,
             final String WIKI_REBUILD_PAGES_CACHE,
-            final String WIKI_STORE_CONTRIBUTIONS, final String OPTIMISATIONS) {
+            final String WIKI_STORE_CONTRIBUTIONS, final String OPTIMISATIONS,
+            final String LOG_USER_REQS) {
         if (SERVERNAME != null) {
             options.SERVERNAME = SERVERNAME;
         }
@@ -367,6 +376,9 @@ public class Options {
                     }
                 }
             }
+        }
+        if (LOG_USER_REQS != null) {
+            options.LOG_USER_REQS = Integer.parseInt(LOG_USER_REQS);
         }
     }
 }

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/data/xml/Main.java
@@ -289,7 +289,7 @@ public class Main {
         Options dbWriteOptions = new Options();
         if (args.length > i) {
             dbWriteOptionsStr = args[i];
-            Options.parseOptions(dbWriteOptions, null, null, null, null, null, null, null, dbWriteOptionsStr);
+            Options.parseOptions(dbWriteOptions, null, null, null, null, null, null, null, dbWriteOptionsStr, null);
         } else {
             System.err.println("need a new optimisation scheme for convert; arguments given: " + Arrays.toString(args));
             System.exit(-1);

commit 19c5402ff9389ec4c0817248e3bdd1567f87851e

- Wiki on Scalaris: fixed Exceptions in case LOG_USER_REQS is disabled (introduced in r3290)

contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/WikiServlet.java
@@ -2015,7 +2015,7 @@ public abstract class WikiServlet<Connection> extends HttpServlet implements
      *            time spend in the web server
      */
     public static void storeUserReq(long timestamp, String serviceUser, long servertime) {
-        if (userReqLogs.length > 0) {
+        if (userReqLogs != null && userReqLogs.length > 0) {
             int timestamp_s = (int) (timestamp / 1000);
             // build log entry:
             Map<String, Object> entry = new HashMap<String, Object>();