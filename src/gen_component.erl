de15b242fe8d5da94afae754b5060d5cf580afad
kruber@zib.de <kruber@zib.de>
2012-08-08 15:24:42 +0000

    gen_component:bp_set_cond/3: stronger requirement on the supplied function

--- src/gen_component.erl
+++ src/gen_component.erl
@@ -236,7 +236,7 @@ bp_set(Pid, MsgTag, BPName) ->
 bp_set_cond(Pid, {_Module, _Function, _Params = 2} = Cond, BPName) ->
     Pid ! {'$gen_component', bp, bp_set_cond, Cond, BPName},
     ok;
-bp_set_cond(Pid, Cond, BPName) when is_function(Cond) ->
+bp_set_cond(Pid, Cond, BPName) when is_function(Cond, 2) ->
     Pid ! {'$gen_component', bp, bp_set_cond, Cond, BPName},
     ok.
 
