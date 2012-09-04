da8604ee5e73a5593db868500d5df9fb9e918efd
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-09-04 15:26:32 +0000

    dht_node: moved lookup_fin handling to dht_node_lookup, too

--- trunk/src/dht_node_lookup.erl
+++ trunk/src/dht_node_lookup.erl
@@ -22,7 +22,8 @@
 
 -include("scalaris.hrl").
 
--export([lookup_aux/4, lookup_aux_failed/3, lookup_fin_failed/3]).
+-export([lookup_aux/4, lookup_fin/4,
+         lookup_aux_failed/3, lookup_fin_failed/3]).
 
 %% userdevguide-begin dht_node_lookup:routing
 %% @doc Find the node responsible for Key and send him the message Msg.
@@ -37,6 +38,56 @@ lookup_aux(State, Key, Hops, Msg) ->
         _ ->
             P = ?RT:next_hop(State, Key),
             comm:send(P, {?lookup_aux, Key, Hops + 1, Msg}, [{shepherd, self()}])
+    end.
+
+%% @doc Find the node responsible for Key and send him the message Msg.
+-spec lookup_fin(State::dht_node_state:state(), Key::intervals:key(),
+                 Hops::non_neg_integer(), Msg::comm:message()) -> dht_node_state:state().
+lookup_fin(State, Key, Hops, Msg) ->
+    MsgFwd = dht_node_state:get(State, msg_fwd),
+    FwdList = [P || {I, P} <- MsgFwd, intervals:in(Key, I)],
+    case FwdList of
+        []    ->
+            case dht_node_state:is_db_responsible(Key, State) of
+                true ->
+                    comm:send_local(dht_node_state:get(State, monitor_proc),
+                                    {lookup_hops, Hops}),
+                    gen_component:post_op(State, Msg);
+                false ->
+                    % it is possible that we received the message due to a
+                    % forward while sliding and before the other node removed
+                    % the forward -> do not warn then
+                    SlidePred = dht_node_state:get(State, slide_pred),
+                    SlideSucc = dht_node_state:get(State, slide_succ),
+                    Neighbors = dht_node_state:get(State, neighbors),
+                    case ((SlidePred =/= null andalso
+                               slide_op:get_sendORreceive(SlidePred) =:= 'send' andalso
+                               intervals:in(Key, slide_op:get_interval(SlidePred)))
+                              orelse
+                              (SlideSucc =/= null andalso
+                                   slide_op:get_sendORreceive(SlideSucc) =:= 'send' andalso
+                                   intervals:in(Key, slide_op:get_interval(SlideSucc)))
+                              orelse
+                              intervals:in(Key, nodelist:succ_range(Neighbors))) of
+                        true -> ok;
+                        false ->
+                            DBRange = dht_node_state:get(State, db_range),
+                            DBRange2 = [begin
+                                            case intervals:is_continuous(Interval) of
+                                                true -> {intervals:get_bounds(Interval), Id};
+                                                _    -> {Interval, Id}
+                                            end
+                                        end || {Interval, Id} <- DBRange],
+                            log:log(warn,
+                                    "[ ~.0p ] Routing is damaged!! Trying again...~n  myrange:~p~n  db_range:~p~n  msgfwd:~p~n  Key:~p",
+                                    [self(), intervals:get_bounds(nodelist:node_range(Neighbors)),
+                                     DBRange2, MsgFwd, Key])
+                    end,
+                    lookup_aux(State, Key, Hops, Msg),
+                    State
+            end;
+        [Pid] -> comm:send(Pid, {?lookup_fin, Key, Hops + 1, Msg}),
+                 State
     end.
 %% userdevguide-end dht_node_lookup:routing
 
