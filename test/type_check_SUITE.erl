cab815880b9795d65a22f4bfdb72b278c2e5cec0
kruber@zib.de <kruber@zib.de>
2012-09-10 12:44:52 +0000

    util: changed wait_for_table_to_disappear semantics: wait for a table owned by a specific process to disappear (most times it is used, the process is killed and then we wait for its tables to disappear - however, another process could have re-created the table!)

--- test/type_check_SUITE.erl
+++ test/type_check_SUITE.erl
@@ -430,7 +430,7 @@ tester_type_check_util(_Config) ->
              {wait_for, 1}, %% cannot create funs
              {wait_for, 2}, %% cannot create funs
              {wait_for_process_to_die, 1}, %% could wait forever
-             {wait_for_table_to_disappear, 1}, %% cannot create tids
+             {wait_for_table_to_disappear, 2}, %% cannot create tids
              {zipfoldl, 5} %% cannot create funs
            ],
            [ {dump_extract_from_list,2}, %% wrong spec
