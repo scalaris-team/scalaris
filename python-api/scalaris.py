8247d5b70234fc9303ac1c4e4ece50070a514bf9
kruber@zib.de <kruber@zib.de>
2012-10-19 15:23:01 +0000

    Python-API: removed shebang of scalaris.py (it is not meant to be executed directly)

--- python-api/scalaris.py
+++ python-api/scalaris.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-# Copyright 2011 Zuse Institute Berlin
+# Copyright 2011-2012 Zuse Institute Berlin
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
