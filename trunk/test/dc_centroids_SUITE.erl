a3b047d4a5d199f68e843be65d548cf1e56d1fcd
stiffy2@gmx.de <stiffy2@gmx.de@71766da9-9752-0410-ab17-bfafa751c56a>
2012-10-29 09:08:36 +0000

    unittest: added suite for dc_centroids

---
+++ trunk/test/dc_centroids_SUITE.erl
@@ -0,0 +1,50 @@
+%  @copyright 2012 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Magnus Mueller <mamuelle@informatik.hu-berlin.de>
+%% @doc    Test suite for the mathlib module.
+%% @end
+%% @version $Id$
+
+-module(dc_centroids_SUITE).
+-author('mamuelle@informatik.hu-berlin.de').
+-vsn('$Id$ ').
+
+-compile(export_all).
+
+-include("unittest.hrl").
+-include("scalaris.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
+all() -> [
+        distance
+    ].
+
+suite() ->
+    [
+        {timetrap, {seconds, 30}}
+    ].
+
+init_per_suite(Config) -> Config.
+end_per_suite(Config) -> Config.
+
+% Test dc_centroids:distance/2
+%
+% This is the euclidian distance between two centroids
+distance(_Config) ->
+    U = dc_centroids:new([0,0], 1.0),
+    V = dc_centroids:new([1,1], 1.0),
+    ?equals(dc_centroids:distance(U,V), math:sqrt(2)),
+    ok.
