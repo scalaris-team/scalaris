d621e27c1afa32376a39cd53aed55ed81b499720
stiffy2@gmx.de <stiffy2@gmx.de>
2012-12-20 13:30:15 +0000

    util: function to combine timing rrd slots
    - The function is used to accumulate the values from a number of rrd slots into one
    structure. The number of slots is determined by a time interval for which the statistics
    should be accumulated.
    - type_check_suite: ignore util:rrd_combine_timing_slots

--- test/type_check_SUITE.erl
+++ test/type_check_SUITE.erl
@@ -431,7 +431,9 @@ tester_type_check_util(_Config) ->
              {wait_for, 2}, %% cannot create funs
              {wait_for_process_to_die, 1}, %% could wait forever
              {wait_for_table_to_disappear, 2}, %% cannot create tids
-             {zipfoldl, 5} %% cannot create funs
+             {zipfoldl, 5}, %% cannot create funs
+             {rrd_combine_timing_slots,3}, %% values too big
+             {rrd_combine_timing_slots,4} %% values too big
            ],
            [ {dump_extract_from_list,2}, %% wrong spec
              {dumpXNoSort,2}, %% needs fun
