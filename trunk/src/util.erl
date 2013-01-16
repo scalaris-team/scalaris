76943b6f396ab34a942a3c7bb2a25d0c8ab3e84a
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-01-16 18:32:26 +0000

    add an 'ok' atom to atom_ext.hrl

--- trunk/src/util.erl
+++ trunk/src/util.erl
@@ -1404,7 +1404,8 @@ extint2atom(X) when is_integer(X) ->
         ?init_TP -> ?init_TP_atom;
         ?tp_do_commit_abort_fwd -> ?tp_do_commit_abort_fwd_atom;
         ?random_from_list -> ?random_from_list_atom;
-        ?sublist -> ?sublist_atom
+        ?sublist -> ?sublist_atom;
+        ?ok -> ?ok_atom
     end.
 
 -spec sets_map(Fun :: fun((A :: any()) -> B :: any()), Set :: set()) -> [any()].
