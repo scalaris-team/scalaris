669e12e9e38b6ba50f6565728d24f8157685ba55
kruber@zib.de <kruber@zib.de>
2013-01-22 19:31:58 +0000

    Wiki on Scalaris: adapted to r4300 (removed TimeoutException where it is not thrown)

--- contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/MyScalarisSingleOpExecutor.java
+++ contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/MyScalarisSingleOpExecutor.java
@@ -22,7 +22,6 @@ import com.ericsson.otp.erlang.OtpErlangException;
 import de.zib.scalaris.AbortException;
 import de.zib.scalaris.ConnectionException;
 import de.zib.scalaris.RequestList;
-import de.zib.scalaris.TimeoutException;
 import de.zib.scalaris.TransactionSingleOp;
 import de.zib.scalaris.TransactionSingleOp.ResultList;
 import de.zib.scalaris.UnknownException;
@@ -72,7 +71,7 @@ public class MyScalarisSingleOpExecutor extends ScalarisSingleOpExecutor {
      */
     @Override
     protected ResultList executeRequests(RequestList requests)
-            throws ConnectionException, TimeoutException, AbortException,
+            throws ConnectionException, AbortException,
             UnknownException {
         ScalarisDataHandler.addInvolvedKeys(involvedKeys, requests.getRequests());
         return super.executeRequests(requests);
