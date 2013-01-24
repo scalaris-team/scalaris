3b99cfc29dd4aaa92320ede6970879f262b18c1d
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-01-24 18:59:30 +0000

    Wiki on Scalaris: extend ScalarisReadRandomListEntryOp1 to be able to read from one bucket only or all of them
    This depends on the optimisation, e.g. optimisations with write caches read the value from the read-key plus one of the write caches. getRandomArticle() tries a single bucket first and if the fails, it reads all of them, also using ScalarisReadRandomListEntryOp1 for this part.
    If failNotFound is set and several buckets are read, the NotFoundException will only be thrown if all buckets were not found!

--- trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/ScalarisReadRandomListEntryOp1.java
+++ trunk/contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/ScalarisReadRandomListEntryOp1.java
@@ -2,6 +2,8 @@ package de.zib.scalaris.examples.wikipedia;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.TreeMap;
@@ -14,6 +16,7 @@ import de.zib.scalaris.NotFoundException;
 import de.zib.scalaris.RequestList;
 import de.zib.scalaris.ResultList;
 import de.zib.scalaris.UnknownException;
+import de.zib.scalaris.examples.wikipedia.Options.APPEND_INCREMENT_BUCKETS_WITH_WCACHE;
 import de.zib.scalaris.examples.wikipedia.Options.IBuckets;
 import de.zib.scalaris.examples.wikipedia.Options.IPartialRead;
 import de.zib.scalaris.examples.wikipedia.Options.Optimisation;
@@ -30,13 +33,15 @@ import de.zib.scalaris.operations.ReadRandomFromListOp;
  */
 public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
     final Collection<String> keys;
+    final HashMap<String, Integer> opsPerKey;
     final int buckets;
     final ErlangConverter<List<T>> listConv;
     final ErlangConverter<T> elemConv;
     final boolean failNotFound;
     T value = null;
     final Random random;
-    final boolean usePartialReads;
+    final private Optimisation optimisation;
+    final private boolean readOnlyOneBucket;
 
     /**
      * Creates a new (random) list entry read operation.
@@ -45,6 +50,9 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
      *            the keys under which the list is stored in Scalaris
      * @param optimisation
      *            the list optimisation to use
+     * @param readOnlyOneBucket
+     *            try to read fron a single bucket only (if supported by the
+     *            optimisation) - in contrast to reading values from all of them
      * @param elemConv
      *            converter to make an {@link ErlangValue} to a <tt>T</tt>
      * @param listConv
@@ -57,19 +65,21 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
      *            the random number generator to use
      */
     public ScalarisReadRandomListEntryOp1(final Collection<String> keys,
-            final Optimisation optimisation, ErlangConverter<T> elemConv, ErlangConverter<List<T>> listConv,
+            final Optimisation optimisation, boolean readOnlyOneBucket, ErlangConverter<T> elemConv, ErlangConverter<List<T>> listConv,
             boolean failNotFound, Random random) {
         this.keys = keys;
+        this.opsPerKey = new HashMap<String, Integer>(keys.size());
         if (optimisation instanceof IBuckets) {
             this.buckets = ((IBuckets) optimisation).getBuckets();
         } else {
             this.buckets = 1;
         }
-        this.usePartialReads = optimisation instanceof IPartialRead;
         this.listConv = listConv;
         this.elemConv = elemConv;
         this.failNotFound = failNotFound;
         this.random = random;
+        this.optimisation = optimisation;
+        this.readOnlyOneBucket = readOnlyOneBucket;
     }
 
     public int workPhases() {
@@ -97,10 +107,28 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
      */
     protected int prepareRead(final RequestList requests) {
         for (String key : keys) {
-            if (usePartialReads) {
-                requests.addOp(new ReadRandomFromListOp(key + ":" + random.nextInt(buckets)));
+            HashSet<String> bucketKeys = new HashSet<String>(readOnlyOneBucket ? 2 : buckets);
+            if (!readOnlyOneBucket) {
+                for (int i = 0; i < buckets; ++i) {
+                    bucketKeys.add(key + ":" + 0);
+                }
+            } else if (optimisation instanceof APPEND_INCREMENT_BUCKETS_WITH_WCACHE) {
+                // read the main key and a single write cache
+                bucketKeys.add(key + ":" + 0);
+                bucketKeys.add(key + ":" + (random.nextInt(buckets - 1) + 1));
             } else {
-                requests.addOp(new ReadOp(key + ":" + random.nextInt(buckets)));
+                bucketKeys.add(key + ":" + random.nextInt(buckets));
+            }
+            if (optimisation instanceof IPartialRead) {
+                opsPerKey.put(key, bucketKeys.size());
+                for (String bucketKey : bucketKeys) {
+                    requests.addOp(new ReadRandomFromListOp(bucketKey));
+                }
+            } else {
+                opsPerKey.put(key, bucketKeys.size());
+                for (String bucketKey : bucketKeys) {
+                    requests.addOp(new ReadOp(bucketKey));
+                }
             }
         }
         return 0;
@@ -117,7 +145,9 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
      */
     protected int checkRead(int firstOp, final ResultList results) throws OtpErlangException,
             UnknownException {
-        if (usePartialReads) {
+        int notFound = 0;
+        NotFoundException lastNotFound = null;
+        if (optimisation instanceof IPartialRead) {
             /*
              * each partition may contain a different number of keys
              * -> collect all partition's random values and calculate the final
@@ -130,18 +160,25 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
              */
             TreeMap<Integer, T> valueMap = new TreeMap<Integer, T>();
             Integer listLen = 0;
-            for (@SuppressWarnings("unused") String key : keys) {
-                try {
-                    ReadRandomFromListOp.Result res = ((ReadRandomFromListOp) results.get(firstOp++)).processResult();
-                    listLen += res.listLength;
-                    valueMap.put(listLen, elemConv.convert(res.randomElement));
-                } catch (NotFoundException e) {
-                    if (failNotFound) {
-                        throw e;
+            int numberOfOps = 0;
+            for (String key : keys) {
+                int opsForKey = this.opsPerKey.get(key);
+                numberOfOps += opsForKey;
+                for (int i = 0; i < opsForKey; ++i) {
+                    try {
+                        ReadRandomFromListOp.Result res = ((ReadRandomFromListOp) results.get(firstOp++)).processResult();
+                        listLen += res.listLength;
+                        valueMap.put(listLen, elemConv.convert(res.randomElement));
+                    } catch (NotFoundException e) {
+                        ++notFound;
+                        lastNotFound = e;
+                    } catch (EmptyListException e) {
+                        // this is ok - we simply ignore this partition
                     }
-                } catch (EmptyListException e) {
-                    // this is ok - we simply ignore this partition
                 }
+            }
+            if (failNotFound && notFound == numberOfOps) {
+                throw lastNotFound;
             }
             if (listLen != 0) {
                 /*
@@ -153,14 +190,21 @@ public class ScalarisReadRandomListEntryOp1<T> implements ScalarisOp {
             }
         } else {
             List<T> valueList = new ArrayList<T>();
-            for (@SuppressWarnings("unused") String key : keys) {
-                try {
-                    valueList.addAll(listConv.convert(results.processReadAt(firstOp++)));
-                } catch (NotFoundException e) {
-                    if (failNotFound) {
-                        throw e;
+            int numberOfOps = 0;
+            for (String key : keys) {
+                int opsForKey = this.opsPerKey.get(key);
+                numberOfOps += opsForKey;
+                for (int i = 0; i < opsForKey; ++i) {
+                    try {
+                        valueList.addAll(listConv.convert(results.processReadAt(firstOp++)));
+                    } catch (NotFoundException e) {
+                        ++notFound;
+                        lastNotFound = e;
                     }
                 }
+            }
+            if (failNotFound && notFound == numberOfOps) {
+                throw lastNotFound;
             }
             if (!valueList.isEmpty()) {
                 value = valueList.get(random.nextInt(valueList.size()));
