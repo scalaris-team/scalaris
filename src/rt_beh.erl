b116b209cb2909aa7a75e5c5f9cd0133d8e5739d
stiffy2@gmx.de <stiffy2@gmx.de>
2013-02-04 16:45:06 +0000

    rt_(g)frtchord: forwarding nodes should learn the issuing node

--- src/rt_beh.erl
+++ src/rt_beh.erl
@@ -66,7 +66,7 @@
             NewNeighbors::nodelist:neighborhood(), ReportToFD::boolean()) -> ok.
 
 -callback check_config() -> boolean().
--callback wrap_message(Msg::comm:message(), Hops::non_neg_integer()) -> comm:message().
+-callback wrap_message(Msg::comm:message(), State::dht_node_state:state(), Hops::non_neg_integer()) -> comm:message().
 -callback unwrap_message(Msg::comm:message(), State::dht_node_state:state()) ->
     comm:message().
 
@@ -110,7 +110,7 @@ behaviour_info(callbacks) ->
      {check, 4}, {check, 5},
      {check_config, 0},
      % wrap and unwrap lookup messages
-     {wrap_message, 2},
+     {wrap_message, 3},
      {unwrap_message, 2}
     ];
 behaviour_info(_Other) ->
