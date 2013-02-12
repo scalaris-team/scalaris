7fddb1b278055a4d6b70399eb56414fdb4e279c0
michels@zib.de <michels@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-02-12 15:01:44 +0000

    added missing copyright/license headers

--- trunk/src/cloud_local.erl
+++ trunk/src/cloud_local.erl
@@ -1,12 +1,26 @@
-%%% @author Maximilian Michels <max@pvs-pc03.zib.de>
-%%% @copyright (C) 2013, Maximilian Michels
-%%% @doc CLOUD LOCAL starts or stops local erlang vms based on alarms defined for the autoscale process.
-%%%      The module is used by autoscale if the following option has been set in scalaris.local.cfg:
-%%%        {as_cloud_module, cloud_local} 
-%%%      The following options can also be set:
-%%%        {cloud_local_min_vms, integer()}.
-%%%        {cloud_local_max_vms, integer()}.
-%%% @end
+#% @copyright 2007-2013 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Maximilian Michels <michels@zib.de>
+%% @copyright (C) 2013, Maximilian Michels
+%% @doc CLOUD LOCAL starts or stops local erlang vms based on alarms defined for the autoscale process.
+%%      The module is used by autoscale if the following option has been set in scalaris.local.cfg:
+%%        {as_cloud_module, cloud_local}
+%%      The following options can also be set:
+%%        {cloud_local_min_vms, integer()}.
+%%        {cloud_local_max_vms, integer()}.
+%% @end
 
 -module(cloud_local).
 -author('michels@zib.de').
