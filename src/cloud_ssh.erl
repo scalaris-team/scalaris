fc47d101bc1f4066e0cbdaba02533d7c231e1402
michels@zib.de <michels@zib.de>
2013-02-12 15:01:44 +0000

    added missing copyright/license headers

--- src/cloud_ssh.erl
+++ src/cloud_ssh.erl
@@ -1,13 +1,27 @@
-%%% @author Maximilian Michels <max@pvs-pc03.zib.de>
-%%% @copyright (C) 2013, Maximilian Michels
-%%% @doc Cloud SSH starts or stops erlang vms on ssh hosts based on alarms defined for the autoscale process.
-%%%      The module is used by autoscale if the following option has been set in scalaris.local.cfg:
-%%%        {as_cloud_module, cloud_ssh} 
-%%%      The following options can also be set:
-%%%        {cloud_ssh_hosts, ["host1", "host2", ..., "hostn"]}.
-%%%        {cloud_ssh_args, "arguments for ssh"}.
-%%%        {cloud_ssh_path, "path/to/scalaris/installation/on/host"}.
-%%% @end
+% @copyright 2007-2013 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Maximilian Michels <michels@zib.de>
+%% @copyright (C) 2013, Maximilian Michels
+%% @doc Cloud SSH starts or stops erlang vms on ssh hosts based on alarms defined for the autoscale process.
+%%      The module is used by autoscale if the following option has been set in scalaris.local.cfg:
+%%        {as_cloud_module, cloud_ssh}
+%%      The following options can also be set:
+%%        {cloud_ssh_hosts, ["host1", "host2", ..., "hostn"]}.
+%%        {cloud_ssh_args, "arguments for ssh"}.
+%%        {cloud_ssh_path, "path/to/scalaris/installation/on/host"}.
+%% @end
 
 -module(cloud_ssh).
 -author('michels@zib.de').
