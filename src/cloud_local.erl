f90f31dfcec3748292cafc403729e263b7eac8d7
michels@zib.de <michels@zib.de>
2013-02-12 14:07:11 +0000

    cloud_local implementation

--- src/cloud_local.erl
+++ src/cloud_local.erl
@@ -13,18 +13,25 @@
 
 -behavior(cloud_beh).
 
--include("../include/scalaris.hrl").
+-include("scalaris.hrl").
 
--export([init/0, get_number_of_vms/0, add_vms/1, remove_vms/1, find_free_port/0, find_free_port/1]).
+-export([init/0, get_number_of_vms/0, add_vms/1, remove_vms/1]).
+
+
+%%%%%%%%%%%%%%%%%%%%%
+%%%% Behavior methods
+%%%%%%%%%%%%%%%%%%%%%
 
 -spec init() -> ok.
 init() ->
-	case config:read("as_min_vms") andalso config:read("as_max_vms") of
+	case config:read(cloud_local_min_vms) of
 		failed ->
-			config:write("as_min_vms", 1),
-			config:write("as_max_vms", ?PLUS_INFINITY);
+			config:write(cloud_local_min_vms, 1),
+			config:write(cloud_local_max_vms, ?PLUS_INFINITY);
 		X -> X
-	end.
+	end,
+	ok.
+
 
 -spec get_number_of_vms() -> integer().
 get_number_of_vms() ->
@@ -32,8 +39,8 @@ get_number_of_vms() ->
 
 -spec add_vms(integer()) -> ok.
 add_vms(N) ->
-	BaseScalarisPort = 14195,%config:read(port),
-	BaseYawsPort = 8000,%config:read(yaws_port),
+	BaseScalarisPort = config:read(port),
+	BaseYawsPort = config:read(yaws_port),
 	{Mega, Secs, _} = now(),
 	Time = Mega * 1000000 + Secs,
 	SpawnFun = 
@@ -44,13 +51,34 @@ add_vms(N) ->
 				Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -e -detached -s -p ~p -y ~p -n ~s start", 
 									[Port, YawsPort, NodeName])),
 				io:format("Executing: ~p~n", [Cmd]),
+				NumberVMs = get_number_of_vms(),
 				os:cmd(Cmd),
-				wait_for(fun get_number_of_vms/0, get_number_of_vms() + 1),
+				wait_for(fun get_number_of_vms/0, NumberVMs + 1),
 				timer:sleep(200)
 		end,
-	[SpawnFun(X) || X <- lists:seq(1, N), get_number_of_vms() < config:read("as_max_vms")],		   
+	[SpawnFun(X) || X <- lists:seq(1, N), get_number_of_vms() < config:read(cloud_local_max_vms)],		   
 	ok.
 
+-spec remove_vms(integer()) -> ok.
+remove_vms(N) ->
+	AllVMs = lists:map(fun (El) -> erlang:element(1, El) end, erlang:element(2, erl_epmd:names())),
+	VMs = lists:filter(fun (El) -> El =/= "firstnode" end, AllVMs),
+	RemoveFun = 
+		fun(NodeName) ->						
+				Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -n ~s gstop", 
+												  [NodeName])),
+				NumberVMs = get_number_of_vms(),
+				io:format("Executing: ~p~n", [Cmd]),
+				os:cmd(Cmd),
+				wait_for(fun get_number_of_vms/0, NumberVMs - 1)
+		end,
+	[RemoveFun(NodeName) || NodeName <- lists:sublist(VMs, N), 
+							get_number_of_vms() > config:read(cloud_local_min_vms)],
+	ok.
+
+%%%%%%%%%%%%%%%%%%%
+%%%% Helper methods
+%%%%%%%%%%%%%%%%%%%
 wait_for(Fun, ExpectedValue) ->
 	case Fun() of
 		ExpectedValue -> ok;
@@ -58,15 +86,7 @@ wait_for(Fun, ExpectedValue) ->
 			wait_for(Fun, ExpectedValue)
 	end.
 
--spec remove_vms(integer()) -> ok.
-remove_vms(N) ->
-	
-	ok.
-
-find_free_port() ->
-	BasePort = config:read(port),
-	find_free_port(BasePort).
-
+-spec find_free_port(integer()) -> integer().
 find_free_port(Port) ->
 	case gen_tcp:listen(Port, []) of
 		{ok, Socket} -> gen_tcp:close(Socket), 
