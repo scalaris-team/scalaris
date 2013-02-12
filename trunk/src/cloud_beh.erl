22d372ad842a1dbf00dd9d86960c8f40ac2b6bb3
michels@zib.de <michels@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-02-12 14:07:21 +0000

    added init() method to cloud_beh.erl

--- trunk/src/cloud_beh.erl
+++ trunk/src/cloud_beh.erl
@@ -8,6 +8,7 @@
 -include("scalaris.hrl").
 -type state() :: term().
 
+-callback init() -> ok.
 -callback get_number_of_vms() -> non_neg_integer().
 -callback add_vms(Count::non_neg_integer()) -> state().
 -callback remove_vms(Count::non_neg_integer()) -> state().
@@ -16,6 +17,7 @@
 -spec behaviour_info(atom()) -> [{atom(), arity()}] | undefined.
 behaviour_info(callbacks) ->
     [
+	 {init, 0},
      {get_number_of_vms, 0},
      {add_vms, 1},
 	 {remove_vms, 1}
