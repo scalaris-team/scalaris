a97cc3d823e1873dd0f015551101d4ac0c015911
kruber@zib.de <kruber@zib.de>
2013-02-15 11:26:30 +0000

    autoscale: use spaces instead of tabs to indent!

--- src/autoscale.erl
+++ src/autoscale.erl
@@ -34,37 +34,37 @@
 % types
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -type(alarm_handler() ::
-    load_avg |
-	latency_avg |
-	random_churn).
+          load_avg |
+          latency_avg |
+          random_churn).
 
 -record(alarm, {
-		name             = ?required(alarm, name) :: atom(),
-		handler          = ?required(alarm, handler) :: alarm_handler(),
-		period_secs      = ?required(alarm, period_in_s) :: pos_integer(),
-		cooldown_secs    = 0 :: non_neg_integer(),
-		lower_limit      = ?MINUS_INFINITY :: number(),
-		upper_limit      = ?PLUS_INFINITY :: number(),
-		scale_down_by    = 1 :: non_neg_integer(),
-		scale_up_by      = 1 :: non_neg_integer(),
-		state			 = active :: active | inactive,
-		opt_fields       = [] :: [{atom(), any()}]
-	}).
+                name             = ?required(alarm, name) :: atom(),
+                handler          = ?required(alarm, handler) :: alarm_handler(),
+                period_secs      = ?required(alarm, period_in_s) :: pos_integer(),
+                cooldown_secs    = 0 :: non_neg_integer(),
+                lower_limit      = ?MINUS_INFINITY :: number(),
+                upper_limit      = ?PLUS_INFINITY :: number(),
+                scale_down_by    = 1 :: non_neg_integer(),
+                scale_up_by      = 1 :: non_neg_integer(),
+                state             = active :: active | inactive,
+                opt_fields       = [] :: [{atom(), any()}]
+               }).
 
 -type(state() ::
-	{IsLeader::boolean(), Alarms::dict()} |
-	unknown_event).
+          {IsLeader::boolean(), Alarms::dict()} |
+          unknown_event).
 
 -type(breach_state() ::
-		  breach_lower |
-		  breach_upper |
-		  ok).
+          breach_lower |
+          breach_upper |
+          ok).
 
 -type(message() ::
-	{get_state_response, MyRange::intervals:interval()} |
-	{check_alarm, Name::atom()} |
-	{toggle_alarm, Name::atom()} |
-	{deactivate_alarms, Name::atom()}).
+          {get_state_response, MyRange::intervals:interval()} |
+          {check_alarm, Name::atom()} |
+          {toggle_alarm, Name::atom()} |
+          {deactivate_alarms, Name::atom()}).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % startup
@@ -76,17 +76,17 @@ start_link(DHTNodeGroup) ->
 
 -spec init(null) -> state().
 init(null) ->
-	?CLOUD:init(),
-	% dict with alarm name as key and {alarm record, epoch} as value 
-	Alarms = dict:from_list(lists:map(fun(A) -> {A#alarm.name, {A, 0}} end, read_alarms_from_config())),
-	% check leadership and subscribe to direct neighborhood changes
-	check_leadership(),
-	rm_loop:subscribe(self(), autoscale,
-		fun rm_loop:subscribe_dneighbor_change_filter/3,
-		fun autoscale:check_leadership/4,
-		inf),
-	% initial state: not leader and alarms from config
-	{_IsLeader = false, Alarms}.
+    ?CLOUD:init(),
+    % dict with alarm name as key and {alarm record, epoch} as value 
+    Alarms = dict:from_list(lists:map(fun(A) -> {A#alarm.name, {A, 0}} end, read_alarms_from_config())),
+    % check leadership and subscribe to direct neighborhood changes
+    check_leadership(),
+    rm_loop:subscribe(self(), autoscale,
+                      fun rm_loop:subscribe_dneighbor_change_filter/3,
+                      fun autoscale:check_leadership/4,
+                      inf),
+    % initial state: not leader and alarms from config
+    {_IsLeader = false, Alarms}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % message handlers
@@ -94,179 +94,179 @@ init(null) ->
 %% leadership maintenance %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec on(message(), state()) -> state().
 on({get_state_response, MyRange}, {IsLeader, Alarms}) ->
-	%% @fix: dbrange vs myrange?
-	IsNewLeader = intervals:in(?RT:hash_key("0"), MyRange),
-	NewAlarms = 
-		case {IsLeader, IsNewLeader} of
-			{false, true} ->
-				dict:map(fun(Name, {Alarm, Epoch}) ->
-					case Alarm#alarm.state =:= active of
-						true ->
-							continue_alarm(Name, Alarm#alarm.period_secs, self(), Epoch+1);
-						false ->
-							ok
-						end,
-						{Alarm, Epoch+1}
-					end, Alarms);
-			{_, _}        -> Alarms
-		end,
-	{IsNewLeader, NewAlarms};
+    %% @fix: dbrange vs myrange?
+    IsNewLeader = intervals:in(?RT:hash_key("0"), MyRange),
+    NewAlarms = 
+        case {IsLeader, IsNewLeader} of
+            {false, true} ->
+                dict:map(fun(Name, {Alarm, Epoch}) ->
+                                 case Alarm#alarm.state =:= active of
+                                     true ->
+                                         continue_alarm(Name, Alarm#alarm.period_secs, self(), Epoch+1);
+                                     false ->
+                                         ok
+                                 end,
+                                 {Alarm, Epoch+1}
+                         end, Alarms);
+            {_, _}        -> Alarms
+        end,
+    {IsNewLeader, NewAlarms};
 
 %% alarm maintenance %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 on({check_alarm, Name, AlarmEpoch}, {IsLeader, Alarms}) -> 
-	{Alarm, Epoch} =
-		case dict:find(Name, Alarms) of
-			{ok, Value} -> Value;
-			error       -> {unknown_alarm, 0}
-		end,
-	case (Alarm =/= unknown_alarm) andalso (Alarm#alarm.state =:= active) andalso
-			 IsLeader andalso (Epoch =:= AlarmEpoch) of
-		true ->
-			LeaderPid = self(),
-			% spawn alarm handler and 
-			_ = spawn(fun() ->
-					case check_alarm(Alarm#alarm.handler, Alarm) of
-						unknown_alarm_handler ->
-							ok;
-						breach_lower ->
-							continue_alarm(Name, erlang:max(Alarm#alarm.period_secs, Alarm#alarm.cooldown_secs), LeaderPid, Epoch),
-							?CLOUD:remove_vms(Alarm#alarm.scale_down_by);
-						breach_upper ->
-							continue_alarm(Name, erlang:max(Alarm#alarm.period_secs, Alarm#alarm.cooldown_secs), LeaderPid, Epoch),
-							?CLOUD:add_vms(Alarm#alarm.scale_up_by);
-						ok ->
-							continue_alarm(Name, Alarm#alarm.period_secs, LeaderPid, Epoch)
-					end
-				  end),
+    {Alarm, Epoch} =
+        case dict:find(Name, Alarms) of
+            {ok, Value} -> Value;
+            error       -> {unknown_alarm, 0}
+        end,
+    case (Alarm =/= unknown_alarm) andalso (Alarm#alarm.state =:= active) andalso
+             IsLeader andalso (Epoch =:= AlarmEpoch) of
+        true ->
+            LeaderPid = self(),
+            % spawn alarm handler and 
+            _ = spawn(fun() ->
+                              case check_alarm(Alarm#alarm.handler, Alarm) of
+                                  unknown_alarm_handler ->
+                                      ok;
+                                  breach_lower ->
+                                      continue_alarm(Name, erlang:max(Alarm#alarm.period_secs, Alarm#alarm.cooldown_secs), LeaderPid, Epoch),
+                                      ?CLOUD:remove_vms(Alarm#alarm.scale_down_by);
+                                  breach_upper ->
+                                      continue_alarm(Name, erlang:max(Alarm#alarm.period_secs, Alarm#alarm.cooldown_secs), LeaderPid, Epoch),
+                                      ?CLOUD:add_vms(Alarm#alarm.scale_up_by);
+                                  ok ->
+                                      continue_alarm(Name, Alarm#alarm.period_secs, LeaderPid, Epoch)
+                              end
+                      end),
             ok;
-		false ->
-			ok
-	end,
-	{IsLeader, Alarms};
+        false ->
+            ok
+    end,
+    {IsLeader, Alarms};
 on({toggle_alarm, Name}, {_IsLeader, Alarms}) ->
-	NewAlarms = 
-		case dict:find(Name, Alarms) of
-			error                -> Alarms;
-			{ok, {Alarm, Epoch}} ->
-				case Alarm#alarm.state of
-					active ->
-						% update state in dict
-						update_alarm(Alarms, Name, [{state, inactive}]);
-					inactive ->
-						% update state and epoch in dict
-						AlarmsUpdated = update_alarm(Alarms, Name, [{state, active}, {epoch, Epoch+1}]),
-						continue_alarm(Name, Alarm#alarm.period_secs, self(), Epoch+1),
-						AlarmsUpdated
-				end
-		end,
-	{_IsLeader, NewAlarms};
+    NewAlarms = 
+        case dict:find(Name, Alarms) of
+            error                -> Alarms;
+            {ok, {Alarm, Epoch}} ->
+                case Alarm#alarm.state of
+                    active ->
+                        % update state in dict
+                        update_alarm(Alarms, Name, [{state, inactive}]);
+                    inactive ->
+                        % update state and epoch in dict
+                        AlarmsUpdated = update_alarm(Alarms, Name, [{state, active}, {epoch, Epoch+1}]),
+                        continue_alarm(Name, Alarm#alarm.period_secs, self(), Epoch+1),
+                        AlarmsUpdated
+                end
+        end,
+    {_IsLeader, NewAlarms};
 on({deactivate_alarms}, {_IsLeader, Alarms}) ->
-	AlarmNames = dict:fetch_keys(Alarms),
-	NewAlarms = lists:foldl(fun(Name, AlarmsAcc) -> update_alarm(AlarmsAcc, Name, {state, inactive}) end, Alarms, AlarmNames),
-	{_IsLeader, NewAlarms};
+    AlarmNames = dict:fetch_keys(Alarms),
+    NewAlarms = lists:foldl(fun(Name, AlarmsAcc) -> update_alarm(AlarmsAcc, Name, {state, inactive}) end, Alarms, AlarmNames),
+    {_IsLeader, NewAlarms};
 on(_, _) ->
-	unknown_event.
+    unknown_event.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % alarm handlers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec check_alarm(alarm_handler(), #alarm{}) -> breach_state() | unknown_alarm_handler.
 check_alarm(load_avg, Alarm) ->
-	AvgLoad = statistics:get_average_load(statistics:get_ring_details()),
-	get_breach_state(AvgLoad, Alarm);
+    AvgLoad = statistics:get_average_load(statistics:get_ring_details()),
+    get_breach_state(AvgLoad, Alarm);
 check_alarm(latency_avg, Alarm) ->
-	Monitor = pid_groups:find_a(monitor_perf),
-	{_CountD, _CountPerSD, AvgMsD, _MinMsD, _MaxMsD, _StddevMsD, _HistMsD} =
-		case statistics:getTimingMonitorStats(Monitor, [{api_tx, 'req_list'}], tuple) of
+    Monitor = pid_groups:find_a(monitor_perf),
+    {_CountD, _CountPerSD, AvgMsD, _MinMsD, _MaxMsD, _StddevMsD, _HistMsD} =
+        case statistics:getTimingMonitorStats(Monitor, [{api_tx, 'req_list'}], tuple) of
             []                           -> {[], [], [], [], [], [], []};
             [{api_tx, 'req_list', Data}] -> Data
         end,
-	History = get_opt_field(Alarm, history, 1),
-	[Hd|Tail] = lists:map(fun({_, V}) -> get_breach_state(V, Alarm) end, lists:sublist(AvgMsD, History)),
-	?IIF(lists:all(fun(X) -> X =:= Hd end, Tail), Hd, ok);
+    History = get_opt_field(Alarm, history, 1),
+    [Hd|Tail] = lists:map(fun({_, V}) -> get_breach_state(V, Alarm) end, lists:sublist(AvgMsD, History)),
+    ?IIF(lists:all(fun(X) -> X =:= Hd end, Tail), Hd, ok);
 check_alarm(random_churn, _Alarm) ->
-	case randoms:rand_uniform(1,4) of
-		1 -> breach_upper;
-		2 -> breach_lower;
-		3 -> ok
-	end;
+    case randoms:rand_uniform(1,4) of
+        1 -> breach_upper;
+        2 -> breach_lower;
+        3 -> ok
+    end;
 check_alarm(_, _) ->
-	unknown_alarm_handler.
+    unknown_alarm_handler.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % alarm helpers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec continue_alarm(atom(), pos_integer(), pid(), non_neg_integer()) -> ok.
 continue_alarm(Name, Secs, Pid, Epoch) ->
-	io:format("Alarm '~p' for ~p delayed by ~p seconds (Epoch ~p).~n", [Name, Pid, Secs, Epoch]),
-	msg_delay:send_local(Secs, Pid, {check_alarm, Name, Epoch}).
+    io:format("Alarm '~p' for ~p delayed by ~p seconds (Epoch ~p).~n", [Name, Pid, Secs, Epoch]),
+    msg_delay:send_local(Secs, Pid, {check_alarm, Name, Epoch}).
 
 update_alarm(Alarms, Name, {Field, NewValue}) ->
-	update_alarm(Alarms, Name, [{Field, NewValue}]);
+    update_alarm(Alarms, Name, [{Field, NewValue}]);
 update_alarm(Alarms, Name, [{Field, NewValue}|Tail]) ->
-	NewAlarms =
-		case Field =:= epoch of
-			true ->
-				% update epoch
-				dict:update(Name, fun({_Alarm, _Epoch}) -> {_Alarm, NewValue} end, Alarms);
-			false ->
-				% update record
-				FieldIndex =
-					case Field of
-						period_secs   -> 4;
-						cooldown_secs -> 5;
-						lower_limit   -> 6;
-						upper_limit   -> 7;
-						scale_down_by -> 8;
-						scale_up_by   -> 9;
-						state         -> 10;
-						_             -> -1
-					end,
-				dict:update(Name, fun({Alarm, Epoch}) -> {erlang:setelement(FieldIndex, Alarm, NewValue), Epoch} end, Alarms)
-		end,
-	update_alarm(NewAlarms, Name, Tail);
+    NewAlarms =
+        case Field =:= epoch of
+            true ->
+                % update epoch
+                dict:update(Name, fun({_Alarm, _Epoch}) -> {_Alarm, NewValue} end, Alarms);
+            false ->
+                % update record
+                FieldIndex =
+                    case Field of
+                        period_secs   -> 4;
+                        cooldown_secs -> 5;
+                        lower_limit   -> 6;
+                        upper_limit   -> 7;
+                        scale_down_by -> 8;
+                        scale_up_by   -> 9;
+                        state         -> 10;
+                        _             -> -1
+                    end,
+                dict:update(Name, fun({Alarm, Epoch}) -> {erlang:setelement(FieldIndex, Alarm, NewValue), Epoch} end, Alarms)
+        end,
+    update_alarm(NewAlarms, Name, Tail);
 update_alarm(Alarms, _Name, []) ->
-	Alarms.
+    Alarms.
 
 -spec get_opt_field(#alarm{}, atom(), any()) -> false | any().
 get_opt_field(Alarm, Field, Default) ->
-	case lists:keyfind(Field, 1, Alarm#alarm.opt_fields) of
-		false          -> Default;
-		{Field, Value} -> Value
-	end.
+    case lists:keyfind(Field, 1, Alarm#alarm.opt_fields) of
+        false          -> Default;
+        {Field, Value} -> Value
+    end.
 
 -spec get_breach_state(term(), #alarm{}) -> breach_state().
 get_breach_state(Value, Alarm) ->
-	?IIF(Value < Alarm#alarm.lower_limit, breach_lower, ?IIF(Value > Alarm#alarm.upper_limit, breach_upper, ok)).
+    ?IIF(Value < Alarm#alarm.lower_limit, breach_lower, ?IIF(Value > Alarm#alarm.upper_limit, breach_upper, ok)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % misc helpers
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec check_leadership() -> ok.
 check_leadership() ->
-	DhtNode = pid_groups:get_my(dht_node),
-	comm:send_local(DhtNode, {get_state, comm:this(), my_range}).
+    DhtNode = pid_groups:get_my(dht_node),
+    comm:send_local(DhtNode, {get_state, comm:this(), my_range}).
 
 -spec check_leadership(pid(), autoscale, nodelist:neighborhood(), nodelist:neighborhood()) -> ok.
 check_leadership(Pid, autoscale, _, _) ->
-	GrpName = pid_groups:group_of(Pid),
-	DhtNode = pid_groups:pid_of(GrpName, dht_node),
-	comm:send_local(DhtNode, {get_state, comm:make_global(Pid), my_range}).
+    GrpName = pid_groups:group_of(Pid),
+    DhtNode = pid_groups:pid_of(GrpName, dht_node),
+    comm:send_local(DhtNode, {get_state, comm:make_global(Pid), my_range}).
 
 -spec get_timestamp_secs() -> pos_integer().
 get_timestamp_secs() ->
-	get_timestamp_secs(erlang:now()).
+    get_timestamp_secs(erlang:now()).
 
 -spec get_timestamp_secs(erlang:timestamp()) -> pos_integer().
 get_timestamp_secs({MegaSecs, Secs, _}) ->
-	MegaSecs*1000000+Secs.
+    MegaSecs*1000000+Secs.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % config
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -spec read_alarms_from_config() -> [#alarm{}].
 read_alarms_from_config() ->
-	case Alarms = config:read(as_alarms) of
-		failed -> [];
-		_      -> Alarms
-	end.
+    case Alarms = config:read(as_alarms) of
+        failed -> [];
+        _      -> Alarms
+    end.
--- src/cloud_local.erl
+++ src/cloud_local.erl
@@ -40,72 +40,72 @@
 
 -spec init() -> ok.
 init() ->
-	case config:read(cloud_local_min_vms) of
-		failed ->
-			config:write(cloud_local_min_vms, 1),
-			config:write(cloud_local_max_vms, ?PLUS_INFINITY);
-		X -> X
-	end,
-	ok.
+    case config:read(cloud_local_min_vms) of
+        failed ->
+            config:write(cloud_local_min_vms, 1),
+            config:write(cloud_local_max_vms, ?PLUS_INFINITY);
+        X -> X
+    end,
+    ok.
 
 
 -spec get_number_of_vms() -> non_neg_integer().
 get_number_of_vms() ->
-	length(erlang:element(2, erl_epmd:names())).
+    length(erlang:element(2, erl_epmd:names())).
 
 -spec add_vms(integer()) -> ok.
 add_vms(N) ->
-	BaseScalarisPort = config:read(port),
-	BaseYawsPort = config:read(yaws_port),
-	{Mega, Secs, _} = now(),
-	Time = Mega * 1000000 + Secs,
-	SpawnFun = 
-		fun (X) -> 
-				Port = find_free_port(BaseScalarisPort),
-				YawsPort = find_free_port(BaseYawsPort),
-				NodeName = lists:flatten(io_lib:format("node~p_~p", [Time, X])),
-				Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -e -detached -s -p ~p -y ~p -n ~s start", 
-									[Port, YawsPort, NodeName])),
-				io:format("Executing: ~p~n", [Cmd]),
-				NumberVMs = get_number_of_vms(),
-				_ = os:cmd(Cmd),
-				wait_for(fun get_number_of_vms/0, NumberVMs + 1),
-				timer:sleep(200)
-		end,
-	_ = [SpawnFun(X) || X <- lists:seq(1, N), get_number_of_vms() < config:read(cloud_local_max_vms)],
-	ok.
+    BaseScalarisPort = config:read(port),
+    BaseYawsPort = config:read(yaws_port),
+    {Mega, Secs, _} = now(),
+    Time = Mega * 1000000 + Secs,
+    SpawnFun = 
+        fun (X) -> 
+                 Port = find_free_port(BaseScalarisPort),
+                 YawsPort = find_free_port(BaseYawsPort),
+                 NodeName = lists:flatten(io_lib:format("node~p_~p", [Time, X])),
+                 Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -e -detached -s -p ~p -y ~p -n ~s start", 
+                                                   [Port, YawsPort, NodeName])),
+                 io:format("Executing: ~p~n", [Cmd]),
+                 NumberVMs = get_number_of_vms(),
+                 _ = os:cmd(Cmd),
+                 wait_for(fun get_number_of_vms/0, NumberVMs + 1),
+                 timer:sleep(200)
+        end,
+    _ = [SpawnFun(X) || X <- lists:seq(1, N), get_number_of_vms() < config:read(cloud_local_max_vms)],
+    ok.
 
 -spec remove_vms(integer()) -> ok.
 remove_vms(N) ->
-	AllVMs = lists:map(fun (El) -> erlang:element(1, El) end, erlang:element(2, erl_epmd:names())),
-	VMs = lists:filter(fun (El) -> El =/= "firstnode" end, AllVMs),
-	RemoveFun = 
-		fun(NodeName) ->						
-				Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -n ~s gstop", 
-												  [NodeName])),
-				NumberVMs = get_number_of_vms(),
-				io:format("Executing: ~p~n", [Cmd]),
-				_ = os:cmd(Cmd),
-				wait_for(fun get_number_of_vms/0, NumberVMs - 1)
-		end,
-	_ = [RemoveFun(NodeName) || NodeName <- lists:sublist(VMs, N),
-							    get_number_of_vms() > config:read(cloud_local_min_vms)],
-	ok.
+    AllVMs = lists:map(fun (El) -> erlang:element(1, El) end, erlang:element(2, erl_epmd:names())),
+    VMs = lists:filter(fun (El) -> El =/= "firstnode" end, AllVMs),
+    RemoveFun = 
+        fun(NodeName) ->						
+                Cmd = lists:flatten(io_lib:format("./../bin/scalarisctl -n ~s gstop", 
+                                                  [NodeName])),
+                NumberVMs = get_number_of_vms(),
+                io:format("Executing: ~p~n", [Cmd]),
+                _ = os:cmd(Cmd),
+                wait_for(fun get_number_of_vms/0, NumberVMs - 1)
+        end,
+    _ = [RemoveFun(NodeName) || NodeName <- lists:sublist(VMs, N),
+                                get_number_of_vms() > config:read(cloud_local_min_vms)],
+    ok.
 
 %%%%%%%%%%%%%%%%%%%
 %%%% Helper methods
 %%%%%%%%%%%%%%%%%%%
 wait_for(Fun, ExpectedValue) ->
-	case Fun() of
-		ExpectedValue -> ok;
-		_ ->
-			wait_for(Fun, ExpectedValue)
-	end.
+    case Fun() of
+        ExpectedValue -> ok;
+        _ ->
+            wait_for(Fun, ExpectedValue)
+    end.
 
 -spec find_free_port(integer()) -> integer().
 find_free_port(Port) ->
-	case gen_tcp:listen(Port, []) of
-		{ok, Socket} -> gen_tcp:close(Socket), 
-						Port;
-		_ -> find_free_port(Port+1)
-	end.
+    case gen_tcp:listen(Port, []) of
+        {ok, Socket} -> gen_tcp:close(Socket), 
+                        Port;
+        _ -> find_free_port(Port+1)
+    end.
