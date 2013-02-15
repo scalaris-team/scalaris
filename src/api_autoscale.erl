7ec382de4da687eb26f552da92309ceed921f8a9
celebi@zib.de <celebi@zib.de>
2013-02-15 14:47:12 +0000

    replace send_to_group_member with macro ?send_to_group_member

--- src/api_autoscale.erl
+++ src/api_autoscale.erl
@@ -24,11 +24,11 @@
 -compile(export_all).
 
 toggle_alarm(Name) ->
-    api_dht_raw:unreliable_lookup(?RT:hash_key("0"), {send_to_group_member, autoscale,
+    api_dht_raw:unreliable_lookup(?RT:hash_key("0"), {?send_to_group_member, autoscale,
                                                       {toggle_alarm, Name}}).
 toggle_alarms(Names) ->
     lists:foreach(fun toggle_alarm/1, Names).
 
 deactivate_alarms () ->
-    api_dht_raw:unreliable_lookup(?RT:hash_key("0"), {send_to_group_member, autoscale,
+    api_dht_raw:unreliable_lookup(?RT:hash_key("0"), {?send_to_group_member, autoscale,
                                                       {deactivate_alarms}}).
