322f1ade5f5fc9c01bae1e9c4e194f713c526157
schintke <schintke@71766da9-9752-0410-ab17-bfafa751c56a>
2013-05-02 15:18:28 +0000

    tester function collector: more precise error message when no spec found.

--- trunk/test/tester_collect_function_info.erl
+++ trunk/test/tester_collect_function_info.erl
@@ -1,4 +1,4 @@
-%  @copyright 2010-2012 Zuse Institute Berlin
+%  @copyright 2010-2013 Zuse Institute Berlin
 
 %   Licensed under the Apache License, Version 2.0 (the "License");
 %   you may not use this file except in compliance with the License.
@@ -45,7 +45,7 @@ collect_fun_info(Module, Func, Arity, ParseState) ->
                   end,
     case tester_parse_state:lookup_type({'fun', Module, Func, Arity}, ParseState3) of
         {value, _} -> tester_parse_state:finalize(ParseState3);
-        none -> ?ct_fail("no function ~p:~p/~p found by tester~n", [Module, Func, Arity])
+        none -> ?ct_fail("no '-spec' definition for function ~p:~p/~p found by tester~n", [Module, Func, Arity])
     end.
 
 -spec collect_unknown_type_infos(tester_parse_state:state(), list()) ->
