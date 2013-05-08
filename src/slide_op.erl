adea97ed0b4073a0059a6bde332dddba54fc6079
kruber@zib.de <kruber@zib.de>
2013-05-08 16:31:25 +0000

    slide: implement async prepare_send_delta callback

--- src/slide_op.erl
+++ src/slide_op.erl
@@ -58,7 +58,6 @@
         null | % should only occur as an intermediate state, otherwise equal to "no slide op"
         wait_for_other | % a node initiated a slide but needs more info from its partner
         wait_for_node_update | % pred changing its id
-        wait_for_pred_update_data_ack | % wait for the local rm process to know about the changed pred's ID
         wait_for_data_ack | wait_for_delta_ack | % sending node
         wait_for_data | wait_for_delta | % receiving node
         wait_for_continue. % async (local) slide message to rm-specific implementation
