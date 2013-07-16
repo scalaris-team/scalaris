71e5dfb95616c3c0703815d3bc1f1c048352b871
kruber@zib.de <kruber@zib.de>
2013-07-16 18:06:31 +0000

    db: add copyright headers

--- src/db_dht.erl
+++ src/db_dht.erl
@@ -1,4 +1,28 @@
+% @copyright 2009-2013 Zuse Institute Berlin,
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Florian Schintke <schintke@zib.de>
+%% @author Nico Kruber <kruber@zib.de>
+%% @author Jan Fajerski <fajerski@zib.de>
+%% @doc    DB back-end for DHTs storing db_entry:entry() objects.
+%% @end
+%% @version $Id$
 -module(db_dht).
+-author('schintke@zib.de').
+-author('kruber@zib.de').
+-author('fajerski@zib.de').
+-vsn('$Id$').
 
 -include("scalaris.hrl").
 
