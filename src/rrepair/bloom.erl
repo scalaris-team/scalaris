ff3e60ade4dc17ef0945aec8fb5b26cd11b01012
kruber@zib.de <kruber@zib.de>
2013-08-07 17:59:50 +0000

    bloom: remove bloom_beh since there was only one module using it

--- src/rrepair/bloom.erl
+++ src/rrepair/bloom.erl
@@ -25,9 +25,14 @@
 
 -include("record_helpers.hrl").
 
--behaviour(bloom_beh).
-
 -include("scalaris.hrl").
+
+-export([new/2, new/3, new/4, add/2, is_element/2]).
+-export([equals/2, join/2, print/1]).
+
+-export([calc_HF_num/1, calc_HF_num/2, calc_HF_numEx/2,
+         calc_least_size/2,
+         calc_FPR/2, calc_FPR/3]).
 
 -export([get_property/2]). %for Tests
 
@@ -42,16 +47,38 @@
                 max_items     = undefined             :: non_neg_integer() | undefined, %extected number of items
                 items_count   = 0                     :: non_neg_integer() %number of inserted items
                }).
--type bloom_filter_t() :: #bloom{}.
+%-opaque bloom_filter() :: #bloom{}.
+-type bloom_filter() :: #bloom{}. %make opaque causes lots of dialyzer warnings
+-type key() :: binary() | integer() | float() | boolean() | atom() | tuple() | ?RT:key().
 
--include("bloom_beh.hrl").
+-ifdef(with_export_type_support).
+-export_type([bloom_filter/0, key/0]).
+-endif.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% API  
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% @doc Creates new bloom filter with default hash function set
+-spec new(non_neg_integer(), float()) -> bloom_filter().
+new(MaxItems, FPR) ->
+    Hfs = ?REP_HFS:new(calc_HF_numEx(MaxItems, FPR)),
+    new(MaxItems, FPR, Hfs).
+
+-spec new(non_neg_integer(), float() | integer(), ?REP_HFS:hfs()) -> bloom_filter().
+new(MaxItems, FPR, Hfs) when is_float(FPR) ->
+    Size = resize(calc_least_size(MaxItems, FPR), 8),
+    new_(Size, MaxItems, Hfs);
+new(MaxItems, BitPerItem, Hfs) ->
+    new_(resize(BitPerItem * MaxItems, 8), MaxItems, Hfs).
+
+-spec new(non_neg_integer(), float(), ?REP_HFS:hfs(), [key()]) -> bloom_filter().
+new(MaxItems, FPR, Hfs, Items) ->
+    BF = new(MaxItems, FPR, Hfs),
+    add(BF, Items).
+
 % @doc creates a new bloom filter
--spec new_(non_neg_integer(), non_neg_integer(), ?REP_HFS:hfs()) -> bloom_filter_t().
+-spec new_(non_neg_integer(), non_neg_integer(), ?REP_HFS:hfs()) -> bloom_filter().
 new_(BitSize, MaxItems, Hfs) when BitSize rem 8 =:= 0 ->
     #bloom{
            size = BitSize,
@@ -65,9 +92,9 @@ new_(BitSize, MaxItems, Hfs) ->
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% @doc adds a range of items to bloom filter
--spec add_list_(bloom_filter_t(), [key()]) -> bloom_filter_t().
-add_list_(Bloom, Items) ->
+%% @doc Adds one or more items to the bloom filter.
+-spec add(bloom_filter(), key() | [key()]) -> bloom_filter().
+add(Bloom, Items) when is_list(Items) ->
     #bloom{
            size = BFSize,
            hfs = Hfs,
@@ -78,7 +105,9 @@ add_list_(Bloom, Items) ->
     Bloom#bloom{
                 filter = F,
                 items_count = FilledCount + length(Items)
-               }.
+               };
+add(Bloom, Item) ->
+    add(Bloom, [Item]).
 
 % faster than lists:foldl
 p_add_list(Hfs, BFSize, Acc, [Item | Items]) ->
@@ -90,25 +119,24 @@ p_add_list(_Hfs, _BFSize, Acc, []) ->
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 % @doc returns true if the bloom filter contains item
--spec is_element_(bloom_filter_t(), key()) -> boolean().
-is_element_(#bloom{size = BFSize, hfs = Hfs, filter = Filter}, Item) -> 
+-spec is_element(bloom_filter(), key()) -> boolean().
+is_element(#bloom{size = BFSize, hfs = Hfs, filter = Filter}, Item) -> 
     Positions = ?REP_HFS:apply_val_rem(Hfs, Item, BFSize),
     check_Bits(Filter, Positions).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% @doc joins two bloom filter, returned bloom filter represents their union
--spec join_(bloom_filter(), bloom_filter()) -> bloom_filter().
-join_(#bloom{size = Size1, max_items = ExpItem1, items_count = Items1,
-             filter = F1, hfs = Hfs},
-      #bloom{size = Size2, max_items = ExpItem2, items_count = Items2,
-             filter = F2}) when Size1 =:= Size2 ->
-    <<F1Val : Size1>> = F1,
-    <<F2Val : Size2>> = F2,
+-spec join(bloom_filter(), bloom_filter()) -> bloom_filter().
+join(#bloom{size = Size, max_items = ExpItem1, items_count = Items1,
+            filter = F1, hfs = Hfs},
+     #bloom{size = Size, max_items = ExpItem2, items_count = Items2,
+            filter = F2}) ->
+    <<F1Val : Size>> = F1,
+    <<F2Val : Size>> = F2,
     NewFVal = F1Val bor F2Val,
-    #bloom{
-           size = Size1,
-           filter = <<NewFVal:Size1>>,
+    #bloom{size = Size,
+           filter = <<NewFVal:Size>>,
            max_items = erlang:max(ExpItem1, ExpItem2),
            hfs = Hfs,
            items_count = Items1 + Items2 %approximation            
@@ -116,19 +144,19 @@ join_(#bloom{size = Size1, max_items = ExpItem1, items_count = Items1,
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% @doc checks equality of two bloom filters
--spec equals_(bloom_filter(), bloom_filter()) -> boolean().
-equals_(#bloom{ size = Size1, items_count = Items1, filter = Filter1 },
-        #bloom{ size = Size2, items_count = Items2, filter = Filter2 }) ->
+%% @doc Checks whether two bloom filters are equal.
+-spec equals(bloom_filter(), bloom_filter()) -> boolean().
+equals(#bloom{ size = Size1, items_count = Items1, filter = Filter1 },
+       #bloom{ size = Size2, items_count = Items2, filter = Filter2 }) ->
     Size1 =:= Size2 andalso
         Items1 =:= Items2 andalso
         Filter1 =:= Filter2.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% @doc bloom filter debug information
--spec print_(bloom_filter_t()) -> [{atom(), any()}].
-print_(#bloom{ max_items = MaxItems,
+%% @doc Return bloom filter debug information.
+-spec print(bloom_filter()) -> [{atom(), any()}].
+print(#bloom{ max_items = MaxItems,
                size = Size,
                hfs = Hfs,
                items_count = NumItems } = Bloom) -> 
@@ -143,7 +171,7 @@ print_(#bloom{ max_items = MaxItems,
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
--spec get_property(bloom_filter_t(), atom()) -> not_found | term().
+-spec get_property(bloom_filter(), atom()) -> not_found | term().
 get_property(#bloom{ size = Size, hfs = Hfs, items_count = NumItems }, fpr) ->
     calc_FPR(Size, NumItems, ?REP_HFS:size(Hfs));
 get_property(Bloom, Property) ->
@@ -206,6 +234,64 @@ check_Bits(Filter, [Pos | Positions]) ->
 
 check_Bits(_, []) -> 
     true.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Common bloom filter calculations
+%
+%       Definitions:
+%       N=BF maximum element number
+%       M=BF bit size
+%       C=Compression Rate M/N
+%       E=FPR 0<=E<=1 (FPR=false-positive rate)
+%       k=Number of hash functions
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% @doc  Calculates optimal number of hash functions for
+%       an M-bit large BloomFilter with a maximum of N Elements.
+-spec calc_HF_num(pos_integer(), non_neg_integer()) -> pos_integer().
+calc_HF_num(M, N) ->
+    calc_HF_num(util:ceil(M / erlang:max(N, 1))).
+
+% @doc Calculates optimal number of hash functions for 
+%      a given compression rate of C.
+-spec calc_HF_num(pos_integer()) -> pos_integer().
+calc_HF_num(C) when C > 0 ->
+    K = C * math:log(2),
+    K_Min = util:floor(K),
+    K_Max = util:ceil(K),
+    A = calc_FPR(C, K_Max),
+    B = calc_FPR(C, K_Min),
+    case erlang:min(A, B) of
+        A -> K_Max;
+        B -> K_Min
+    end.
+
+% @doc Calculates opt. number of hash functions to
+%      code N elements into a bloom filter with false positive rate of FPR.
+-spec calc_HF_numEx(non_neg_integer(), float()) -> pos_integer().
+calc_HF_numEx(N, FPR) ->
+    M = calc_least_size(N, FPR),
+    calc_HF_num(M, N).
+
+% @doc  Calculates leasts bit size of a bloom filter
+%       with a bounded false-positive rate FPR up to N-Elements.
+-spec calc_least_size(non_neg_integer(), float()) -> pos_integer().
+calc_least_size(_, FPR) when FPR == 0 -> 1;
+calc_least_size(0, _) -> 1;
+calc_least_size(N, FPR) ->
+    util:ceil((N * util:log2(math:exp(1)) * util:log2(1 / FPR))).
+
+% @doc  Calculates FPR for an M-bit large bloom_filter with K Hashfuntions 
+%       and a maximum of N elements.
+%       FPR = (1-e^(-kn/m))^k
+%       M = number of BF-Bits
+-spec calc_FPR(pos_integer(), pos_integer(), pos_integer()) -> float().
+calc_FPR(M, N, K) -> 
+    math:pow(1 - math:exp((-K * N) / M), K).
+
+-spec calc_FPR(pos_integer(), pos_integer()) -> float().
+calc_FPR(C, K) ->
+    math:pow(1 - math:exp(-K / C), K).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% helper functions
