a2ea9029dfea9e173b7aa6880376d18392b9535f
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-08-14 13:24:27 +0000

    rr_resolve: add a TODO about resolve requests not originating from rr_recon covering more than a quadrant
    -> this is not supported at the moment!

--- trunk/src/rrepair/rr_resolve.erl
+++ trunk/src/rrepair/rr_resolve.erl
@@ -167,6 +167,7 @@ on({get_state_response, MyI}, State = #rr_resolve_state{ operation = Op,
                                                          dhtNodePid = DhtPid }) 
   when element(1, Op) =:= interval_upd;
        element(1, Op) =:= interval_upd_send ->
+    % TODO: the interval from Op may not be in a single quadrant!
     ISec = rr_recon:map_interval(MyI, element(2, Op)),
     NewState = State#rr_resolve_state{ my_range = MyI },
     case intervals:is_empty(ISec) of
