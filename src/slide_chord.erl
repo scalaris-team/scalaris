69630419b25e93768cf7aec4f3970a6f526a5170
kruber@zib.de <kruber@zib.de>
2013-09-09 13:27:47 +0000

    also optimise the rm_loop subscription parameter in modules using it

--- src/slide_chord.erl
+++ src/slide_chord.erl
@@ -99,7 +99,7 @@ change_my_id(State, SlideOp, ReplyPid) ->
         _ ->
             rm_loop:subscribe(
               ReplyPid, {move, slide_op:get_id(SlideOp2)},
-              fun(_OldN, NewN, _IsSlide) ->
+              fun(_OldN, NewN, _SlideType) ->
                       nodelist:nodeid(NewN) =:= TargetId
               % note: no need to check the id version
               end,
@@ -176,7 +176,7 @@ send_continue_msg_when_pred_ok(State, SlideOp, ReplyPid) ->
             OldPred = slide_op:get_node(SlideOp),
             rm_loop:subscribe(
               ReplyPid, {move, slide_op:get_id(SlideOp)},
-              fun(RMOldN, RMNewN, _IsSlide) ->
+              fun(RMOldN, RMNewN, _SlideType) ->
                       RMNewPred = nodelist:pred(RMNewN),
                       RMOldPred = nodelist:pred(RMOldN),
                       RMOldPred =/= RMNewPred orelse
