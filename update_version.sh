333e8664e3b4f22c453af861f0074caaf9b9ee98
michels@zib.de <michels@zib.de>
2013-09-09 15:22:30 +0000

    maven: update scalaris version number in pom.xml

--- update_version.sh
+++ update_version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Copyright 2007-2012 Zuse Institute Berlin
-# 
+#
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
 #    You may obtain a copy of the License at
-# 
+#
 #        http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 #    Unless required by applicable law or agreed to in writing, software
 #    distributed under the License is distributed on an "AS IS" BASIS,
 #    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -32,6 +32,8 @@ sed -e "s/Version: .*-.*/Version: ${VERSION}-1/g" \
     -i contrib/packages/*/*.dsc
 sed -e "0,/(.*-.*)/s//(${VERSION}-1)/" \
     -i contrib/packages/*/debian.changelog
+sed -e "0,/<version>.*<\/version>/s//<version>${VERSION}<\/version>/" \
+    -i java-api/pom.xml
 if [[ "$VERSION" == *svn* ]]; then
   RELEASE="unstable"
 else
