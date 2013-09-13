0d38bf48f602ee2c559292383dd1d055e00dbe59
kruber@zib.de <kruber@zib.de>
2013-09-13 15:50:55 +0000

    [branches/0.6] also optimise the rm_loop subscription parameter in modules using it

--- src/gossip.erl
+++ src/gossip.erl
@@ -717,8 +717,8 @@ is_leader(MyRange) ->
 %%      itself or its pred changed.
 -spec rm_my_range_changed(OldNeighbors::nodelist:neighborhood(),
                           NewNeighbors::nodelist:neighborhood(),
-                          IsSlide::rm_loop:slide()) -> boolean().
-rm_my_range_changed(OldNeighbors, NewNeighbors, _IsSlide) ->
+                          SlideType::rm_loop:slide()) -> boolean().
+rm_my_range_changed(OldNeighbors, NewNeighbors, _SlideType) ->
     nodelist:node(OldNeighbors) =/= nodelist:node(NewNeighbors) orelse
         nodelist:pred(OldNeighbors) =/= nodelist:pred(NewNeighbors).
 
