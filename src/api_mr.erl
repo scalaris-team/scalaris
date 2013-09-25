c55522fdd3ee2c09f2db2e55c5dba1a86318ead2
fajerski@zib.de <fajerski@zib.de>
2013-09-25 12:34:04 +0000

    mr: add api_mr module
    this provieds a synchronous (blocking) call to start a mr job. It returns the
    results of the job.

---
+++ src/api_mr.erl
@@ -0,0 +1,47 @@
+%  @copyright 2011-2013 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Jan Fajerski <fajerski@zib.de>
+%% @doc    API functions for the Map-Reduce system
+%% @end
+%% @version $Id$
+-module(api_mr).
+-author('fajerski@zib.de').
+-vsn('$Id$').
+
+-export([start_job/1]).
+
+-include("scalaris.hrl").
+
+-spec start_job([any()]) -> [any()].
+start_job(Job) ->
+    Id = randoms:getRandomString(),
+    comm:send_local(pid_groups:find_a(dht_node), 
+                    {mr, init, comm:this(), Id, Job}),
+    wait_for_results([], intervals:empty(), Id).
+
+-spec wait_for_results([any()], intervals:interval(), nonempty_string()) -> [any()].
+wait_for_results(Data, [all], _Id) ->
+    Data;
+wait_for_results(Data, Interval, Id) ->
+    {NewData, NewInterval} = receive
+        ?SCALARIS_RECV({mr_results, PartData, PartInterval},
+                       {PartData ++ Data, intervals:union(PartInterval, Interval)})
+    end,
+    case intervals:is_all(NewInterval) of
+        true ->
+            lists:sort(NewData);
+        _ ->
+            wait_for_results(NewData, NewInterval, Id)
+    end.
