046d1849bb92d8463c233737a085f1230494de6a
schuett <schuett@zib.de>
2013-11-14 14:56:35 +0000

    remove unused lease_gc

--- test/slide_leases_SUITE.erl
+++ test/slide_leases_SUITE.erl
@@ -78,8 +78,7 @@ init_per_testcase(TestCase, Config) ->
             unittest_helper:stop_ring(),
             {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
             unittest_helper:make_ring(1, [{config, [{log_path, PrivDir},
-                                                    {leases, true},
-                                                    {leases_gc, true}]}]),
+                                                    {leases, true}]}]),
             Config
     end.
 
