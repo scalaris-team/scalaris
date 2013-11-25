2d6536eb2eabc32a1d5ba04e8973fc39d848c788
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-11-25 15:16:12 +0000

    avoid changing the current working directory if not needed

--- trunk/test/tester_scheduler.erl
+++ trunk/test/tester_scheduler.erl
@@ -112,9 +112,8 @@ start_scheduling() ->
 instrument_module(Module) ->
     code:delete(Module),
     code:purge(Module),
-    Src = get_file_for_module(Module),
+    {Src, Options} = tester_helper:get_src_and_flags_for_module(Module),
     ct:pal("~p", [file:get_cwd()]),
-    Options = get_compile_flags_for_module(Module),
     MyOptions = [return_errors,
                  {d, with_ct},
                  {parse_transform, tester_scheduler_parse_transform},
@@ -257,53 +256,6 @@ pick_next_runner(Pids) ->
 % misc. helper functions
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--spec get_file_for_module(module()) -> string().
-get_file_for_module(Module) ->
-    % we have to be in $SCALARIS/ebin to find the beam file
-    {ok, CurCWD} = file:get_cwd(),
-    ok = fix_cwd_ebin(),
-    Res = beam_lib:chunks(Module, [compile_info]),
-    ok = file:set_cwd(CurCWD),
-    case Res of
-        {ok, {Module, [{compile_info, Options}]}} ->
-            {source, Filename} = lists:keyfind(source, 1, Options),
-            Filename;
-        X ->
-            ct:pal("~w ~p", [Module, X]),
-            ct:pal("~p", [file:get_cwd()]),
-            timer:sleep(1000),
-            ct:fail(unknown_module)
-    end.
-
--spec get_compile_flags_for_module(module()) -> list().
-get_compile_flags_for_module(Module) ->
-    % we have to be in $SCALARIS/ebin to find the beam file
-    {ok, CurCWD} = file:get_cwd(),
-    ok = fix_cwd_ebin(),
-    Res = beam_lib:chunks(Module, [compile_info]),
-    ok = file:set_cwd(CurCWD),
-    case Res of
-        {ok, {Module, [{compile_info, Options}]}} ->
-            {options, Opts} = lists:keyfind(options, 1, Options),
-            Opts;
-        X ->
-            ct:pal("~w ~w", [Module, X]),
-            timer:sleep(1000),
-            ct:fail(unknown_module),
-            []
-    end.
-
-% @doc set cwd to $SCALARIS/ebin
--spec fix_cwd_ebin() -> ok | {error, Reason::file:posix()}.
-fix_cwd_ebin() ->
-    case file:get_cwd() of
-        {ok, CurCWD} ->
-            case string:rstr(CurCWD, "/ebin") =/= (length(CurCWD) - 4 + 1) of
-                true -> file:set_cwd("../ebin");
-                _    -> ok
-            end;
-        Error -> Error
-    end.
 
 % @doc set cwd to $SCALARIS
 -spec fix_cwd_scalaris() -> ok | {error, Reason::file:posix()}.
