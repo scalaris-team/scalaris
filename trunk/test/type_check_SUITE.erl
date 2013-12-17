2d47a15abea4f1ec41ce0d26224db00c40a9da15
kruber@zib.de <kruber@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2013-12-17 16:36:24 +0000

    rrepair, recon: make calc_signature_size variable regarding the maximum hash length

--- trunk/test/type_check_SUITE.erl
+++ trunk/test/type_check_SUITE.erl
@@ -345,7 +345,7 @@ tester_type_check_rrepair(_Config) ->
              {build_recon_struct, 6}, %% DB items must be in interval
              {begin_sync, 3}, %% tries to send messages
              {shutdown, 2}, %% tries to send messages
-             {calc_signature_size, 2}, %% needs float > 0
+             {calc_signature_size, 3}, %% needs float > 0
              {check_node, 3}, %% needs merkle_tree/nodes with hashes
              {p_check_node, 7}, %% needs merkle_tree/nodes with hashes
              {process_tree_cmp_result, 5}, %% needs matching result and merkle nodes
