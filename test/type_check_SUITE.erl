3855476815765052610011030bbe92a9987f94a0
kruber@zib.de <kruber@zib.de>
2014-01-03 18:26:04 +0000

    rrepair, merkle: unify parameters of merkle_resolve_leaves_noninit/* and merkle_resolve_leaves_init/*

--- test/type_check_SUITE.erl
+++ test/type_check_SUITE.erl
@@ -358,8 +358,8 @@ tester_type_check_rrepair(_Config) ->
              {merkle_get_sync_leaves, 5}, %% needs merkle_tree with hashes
              {merkle_resolve_leaves_noninit, 4}, % needs only leaf nodes in node list
              {merkle_resolve_leaves_noninit, 9}, % needs only leaf nodes in node list
-             {merkle_resolve_leaves_init, 6}, % needs only leaf nodes in node list
-             {merkle_resolve_leaves_init, 11}, % needs only leaf nodes in node list
+             {merkle_resolve_leaves_init, 5}, % needs only leaf nodes in node list
+             {merkle_resolve_leaves_init, 10}, % needs only leaf nodes in node list
              {merkle_resolve_req_keys_noninit, 6}, % needs only leaf nodes in node list, same-length lists
              {resolve_leaves, 4}, %% tries to send messages
              {resolve_leaves, 6}, %% tries to send messages
