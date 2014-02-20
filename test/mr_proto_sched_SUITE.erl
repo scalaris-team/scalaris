832ae0ad5bda25132a351bef570c89504a56daf4
fajerski@zib.de <fajerski@zib.de>
2014-02-20 14:08:05 +0000

    mr, wpool: snapshot is made asynchronous by mr_master
    doing it in a wpool_worker synchonously ends up making a ?SCALARIS_RECV in a
    gen_component. This messes with proto_sched and could potentially break FIFO of
    message queues.

--- test/mr_proto_sched_SUITE.erl
+++ test/mr_proto_sched_SUITE.erl
@@ -23,23 +23,21 @@
 -compile([export_all]).
 
 -define(proto_sched(Action),
-        %% proto sched deadlocks sanity test for some reason
-        ok).
-        %% fun() ->
-        %%         case Action of
-        %%             start ->
-        %%                 ct:pal("Starting proto scheduler"),
-        %%                 proto_sched:start(),
-        %%                 proto_sched:start_deliver();
-        %%             stop ->
-        %%                 proto_sched:stop(),
-        %%                 case erlang:whereis(pid_groups) =:= undefined orelse pid_groups:find_a(proto_sched) =:= failed of
-        %%                     true -> ok;
-        %%                     false -> ct:pal("Proto scheduler stats: ~.2p", proto_sched:get_infos()),
-        %%                              proto_sched:cleanup()
-        %%                 end
-        %%         end
-        %% end()).
+        fun() ->
+                case Action of
+                    start ->
+                        ct:pal("Starting proto scheduler"),
+                        proto_sched:start(),
+                        proto_sched:start_deliver();
+                    stop ->
+                        proto_sched:stop(),
+                        case erlang:whereis(pid_groups) =:= undefined orelse pid_groups:find_a(proto_sched) =:= failed of
+                            true -> ok;
+                            false -> ct:pal("Proto scheduler stats: ~.2p", proto_sched:get_infos()),
+                                     proto_sched:cleanup()
+                        end
+                end
+        end()).
 
 -include("mr_SUITE.hrl").
 
@@ -49,18 +47,19 @@ all() ->
 suite() -> [ {timetrap, {seconds, 9}} ].
 
 test_join(_Config) ->
-    ?proto_sched(start),
     ct:pal("starting job that triggers breakpoint"),
     MrPid = spawn_link(fun() ->
-                    ct:pal("starting mr job"),
-                    Res = api_mr:start_job(get_wc_job_erl()),
-                    ct:pal("mr job finished"),
-                    check_results(Res)
-            end),
+                               ?proto_sched(start),
+                               ct:pal("starting mr job"),
+                               Res = api_mr:start_job(get_wc_job_erl()),
+                               ct:pal("mr job finished"),
+                               check_results(Res)
+                       end),
     ct:pal("adding node to provoke slide"),
     _AddPid = spawn_link(fun() ->
-                                api_vm:add_nodes(2)
-                        end),
+                                 ?proto_sched(start),
+                                 api_vm:add_nodes(2)
+                         end),
     unittest_helper:wait_for_stable_ring(),
     unittest_helper:check_ring_size_fully_joined(4),
     ct:pal("ring fully joined (4)"),
@@ -72,17 +71,18 @@ test_leave(_Config) ->
     {[AddedNode], _} = api_vm:add_nodes(1),
     unittest_helper:wait_for_stable_ring(),
     unittest_helper:check_ring_size_fully_joined(2),
-    ?proto_sched(start),
     MrPid = spawn_link(fun() ->
-                    ct:pal("starting mr job"),
-                    Res = api_mr:start_job(get_wc_job_erl()),
-                    ct:pal("mr job finished"),
-                    check_results(Res)
-            end),
+                               ?proto_sched(start),
+                               ct:pal("starting mr job"),
+                               Res = api_mr:start_job(get_wc_job_erl()),
+                               ct:pal("mr job finished"),
+                               check_results(Res)
+                       end),
     ct:pal("shutting down node ~p to provoke slide", [AddedNode]),
     _VMPid = spawn_link(fun() ->
-                               api_vm:shutdown_node(AddedNode)
-                       end),
+                                ?proto_sched(start),
+                                api_vm:shutdown_node(AddedNode)
+                        end),
     util:wait_for_process_to_die(MrPid),
     ok.
 
