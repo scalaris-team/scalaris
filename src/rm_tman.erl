d8ac5d677e8bac8a3554c777fe7603b01816bfcb
kruber@zib.de <kruber@zib.de>
2014-03-14 17:58:40 +0000

    rm_tman: fix type spec of cyclon messages

--- src/rm_tman.erl
+++ src/rm_tman.erl
@@ -36,7 +36,7 @@
 -type(custom_message() ::
     {rm, trigger} |
     {rm, trigger_action} |
-    {{cy_cache, Cache::[node:node_type()]}, rm} |
+    {rm, {cy_cache, Cache::[node:node_type()]}} |
     {rm, {get_node_details_response, NodeDetails::node_details:node_details()}} |
     {rm, buffer, OtherNeighbors::nodelist:neighborhood(), RequestPredsMinCount::non_neg_integer(), RequestSuccsMinCount::non_neg_integer()} |
     {rm, buffer_response, OtherNeighbors::nodelist:neighborhood()}).
