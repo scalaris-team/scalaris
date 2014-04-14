618129c90ae8877ffce555c60c961247d5e66d7d
michels@zib.de <michels@zib.de>
2014-04-14 16:27:20 +0000

    lb_active: some refactoring

--- src/lb_info.erl
+++ src/lb_info.erl
@@ -51,7 +51,6 @@
 new(NodeDetails) ->
     Items = node_details:get(NodeDetails, load),
     Load = case lb_active:get_load_metric() of
-               unknown -> lb_active:default_value();
                items -> Items;
                Metric -> Metric
            end,
