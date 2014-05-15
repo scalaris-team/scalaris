f418cca7b0d4a50a551883bb249a5acb3c8466ae
michels@zib.de <michels@zib.de@71766da9-9752-0410-ab17-bfafa751c56a>
2014-05-15 13:37:51 +0000

    lb_active: introduce confidence value for request balancing
    - compare number of requests with number of items
    - if not enough requests have been captured the confidence is low
    - request balancing is not performed when the confidene is low

--- trunk/src/lb_active.erl
+++ trunk/src/lb_active.erl
@@ -447,7 +447,8 @@ handle_dht_msg({lb_active, balance, HeavyNode, LightNode, LightNodeSucc, Options
                         items ->
                             {items, dht_node_state:get_split_key(DhtState, From, To, TargetLoadItems, Direction)};
                         requests ->
-                            case lb_stats:get_request_histogram_split_key(TargetLoadRequests, Direction, lb_info:get_time(HeavyNode)) of
+                            case lb_stats:get_request_histogram_split_key(TargetLoadRequests, Direction,
+                                                                          lb_info:get_time(HeavyNode), lb_info:get_items(HeavyNode)) of
                                 %% TODO fall back in a more clever way / abort lb request
                                 failed ->
                                     log:log(warn, "get_request_histogram failed. falling back to item balancing.~n", []),
