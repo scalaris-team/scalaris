9d269403fb9a9e406c8638a557581bf8aadba367
kruber@zib.de <kruber@zib.de>
2014-06-18 13:58:23 +0000

    client_types: exclude the non-characters U+FFFE and U+FFFF
    These are problematic with Erlang versions older than R15 and cannot be encoded to UTF8 there.

--- include/client_types.hrl
+++ include/client_types.hrl
@@ -24,10 +24,12 @@
 %% -type client_key() :: unicode:chardata().
 
 % invalid characters (from https://en.wikipedia.org/wiki/Unicode_plane):
-% * The High Surrogates (U+D800U+DBFF) and Low Surrogate (U+DC00U+DFFF) codes
-%   are reserved for encoding non-BMP characters in UTF-16 by using a pair of
-%   16-bit codes
--type unicode_char() :: 0..16#D7FF | 16#E000..16#10ffff.
+% * The High Surrogates (U+D800 - U+DBFF) and Low Surrogate (U+DC00 - U+DFFF)
+%   codes are reserved for encoding non-BMP characters in UTF-16 by using a
+%   pair of 16-bit codes
+% * The non-characters U+FFFE and U+FFFF can not be encoded into UTF8 by
+%   Erlang versions older than R15.
+-type unicode_char() :: 0..16#D7FF | 16#E000..16#FFFD | 16#10000..16#10ffff.
 -type client_key() :: [unicode_char()].
 -type client_value() :: any().
 -type client_version() :: non_neg_integer(). %%?DB:version().
