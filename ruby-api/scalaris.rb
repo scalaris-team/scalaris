df032d9ead871e90e3c9c5ac664c2f3786937c37
kruber@zib.de <kruber@zib.de>
2014-10-06 11:21:18 +0000

    Ruby-API: allow the scripts to run even if rubygems is not available
    The dependencies, e.g. rubygem-json, must be available though!

--- ruby-api/scalaris.rb
+++ ruby-api/scalaris.rb
@@ -13,9 +13,13 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-require 'rubygems'
-gem 'json', '>=1.4.1'
-require 'json'
+begin
+  require 'json'
+rescue LoadError => e
+  require 'rubygems'
+  gem 'json', '>=1.4.1'
+  require 'json'
+end
 require 'net/http'
 require 'base64'
 require 'open-uri'
