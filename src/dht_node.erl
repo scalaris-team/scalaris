83d50dcab0f40201b59aedf13420a8bb294cde89
kruber@zib.de <kruber@zib.de>
2014-12-08 17:59:31 +0000

    dht_node: fix util:is_unittest/0 debug call (regression of r7219)

--- src/dht_node.erl
+++ src/dht_node.erl
@@ -367,7 +367,7 @@ on({get_state, Pid, Which}, State) when is_atom(Which) ->
     comm:send(Pid, {get_state_response, dht_node_state:get(State, Which)}),
     State;
 on({set_state, Pid, F}, State) when is_function(F) ->
-    ?DBG_ASSERT(util:is_unit_test()), % may only be used in unit-tests
+    ?DBG_ASSERT(util:is_unittest()), % may only be used in unit-tests
     NewState = F(State),
     comm:send(Pid, {set_state_response, NewState}),
     NewState;
