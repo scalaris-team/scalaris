07b29a78ec1b3c71de84309ba355196a37612670
schintke <schintke@zib.de>
2015-01-22 14:11:12 +0000

    let python scripts find on their own where python is, instead of modifying checked out sourcefiles via ./configure

--- python-api/scalaris_bench.py
+++ python-api/scalaris_bench.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-# Copyright 2011 Zuse Institute Berlin
+#!/usr/bin/env python2
+# Copyright 2011-2015 Zuse Institute Berlin
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
