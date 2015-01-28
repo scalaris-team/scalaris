0ceb2f9bba0781be5781e2a8c1f371f921297e0e
schuett <schuett@71766da9-9752-0410-ab17-bfafa751c56a>
2015-01-28 13:02:18 +0000

    recover: implements recover with leases

---
+++ trunk/src/cp/lease_recover.erl
@@ -0,0 +1,66 @@
+% @copyright 2007-2014 Zuse Institute Berlin
+
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+
+%% @author Thorsten Schuett <schuett@zib.de>
+%% @doc    Recover leases.
+%% @end
+%% @version $$
+-module(lease_recover).
+-author('schuett@zib.de').
+-vsn('$Id$').
+
+-include("scalaris.hrl").
+-include("record_helpers.hrl").
+
+-export([recover/4]).
+
+-spec recover(prbr:state(), prbr:state(), prbr:state(), prbr:state()) -> lease_list:lease_list().
+recover(Leases1, Leases2, Leases3, Leases4) ->
+    %% Leases1 = filter(get_leases(leases_db1)),
+    %% Leases2 = filter(get_leases(leases_db2)),
+    %% Leases3 = filter(get_leases(leases_db3)),
+    %% Leases4 = filter(get_leases(leases_db4)),
+    ExtractLease = fun({_Id, Lease}) -> Lease end,
+    Candidates = lists:map(ExtractLease,
+                   filter(lists:append(prbr:tab2list(Leases4), 
+                                       lists:append(prbr:tab2list(Leases3), 
+                                                    lists:append(prbr:tab2list(Leases2), 
+                                                                 prbr:tab2list(Leases1)))))),
+    io:format("candidates ~p~n", [Candidates]),
+    case length(Candidates) of
+        1 -> % one potentially active lease: set active lease
+            [Lease] = Candidates,
+            lease_list:make_lease_list(Lease, [], []);
+        2 -> % could be an ongoing split or an ongoing merge: finish operation
+            ts = nyi, % ts: not yet implemented
+            lease_list:empty()
+    end.
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+% helper
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec filter([{?RT:key(), l_on_cseq:lease_t()}]) -> [{?RT:key(), l_on_cseq:lease_t()}].
+filter(Leases) ->
+    F = fun({Id, L}) ->
+                log:log("~w ~w~n", [Id, L]),
+                LeaseId = l_on_cseq:get_id(L),
+                Id =:= LeaseId andalso %% is first replica?
+                    l_on_cseq:is_live_aux_field(L) andalso
+                    l_on_cseq:has_timed_out(L)
+        end,
+    lists:filter(F, Leases).
+
---
+++ trunk/src/dht_node_join_recover.erl
@@ -0,0 +1,73 @@
+% @copyright 2007-2014 Zuse Institute Berlin
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Thorsten Schuett <schuett@zib.de>
+%% @doc Join ring on recover.
+%% @version $Id$
+-module(dht_node_join_recover).
+-author('schuett@zib.de').
+-vsn('$Id$').
+
+-include("scalaris.hrl").
+-include("record_helpers.hrl").
+
+-export([join/1]).
+
+-spec join(Options::[tuple()]) -> dht_node_state:state().
+join(Options) ->
+    io:format("dht_node_join:join ~p~n", [Options]),
+    % 1. get old lease databases
+    Lease_DB1 = get_db(Options, lease_db1),
+    Lease_DB2 = get_db(Options, lease_db2),
+    Lease_DB3 = get_db(Options, lease_db3),
+    Lease_DB4 = get_db(Options, lease_db4),
+    % 2. find old leases
+    LeaseList = lease_recover:recover(Lease_DB1, Lease_DB2, Lease_DB3, Lease_DB4),
+    % 3. create state with old mnesias
+    MyId = l_on_cseq:get_id(lease_list:get_active_lease(LeaseList)),
+    Me = node:new(comm:this(), MyId, 0), 
+    Neighbors = nodelist:new_neighborhood(Me), % needed for ?RT:empty/1
+    EmptyRT = ?RT:empty_ext(Neighbors), % only for rt_chord
+    RMState = rm_loop:init(Me, Me, Me, null),
+    PRBR_KV_DB = get_db(Options, prbr_kv_db),
+    TXID_DB1  = get_db(Options, txid_db1),
+    TXID_DB2  = get_db(Options, txid_db2),
+    TXID_DB3  = get_db(Options, txid_db3),
+    TXID_DB4  = get_db(Options, txid_db4),
+    State = dht_node_state:new_on_recover(EmptyRT, RMState,
+                               PRBR_KV_DB,
+                               TXID_DB1, TXID_DB2, TXID_DB3, TXID_DB4, 
+                               Lease_DB1, Lease_DB2, Lease_DB3, Lease_DB4,
+                               LeaseList
+                               ),
+    % 3. after the leases are known, we can start ring-maintenance and routing
+    % 4. now we can try to refresh the local leases
+    msg_delay:send_trigger(1, {l_on_cseq, renew_leases}),
+
+    % copied from dht_node_join
+    rt_loop:activate(Neighbors),
+    dc_clustering:activate(),
+    gossip:activate(Neighbors),
+    dht_node_reregister:activate(),
+    service_per_vm:register_dht_node(node:pidX(Me)),
+    State.
+
+get_db(Options, DBName) ->
+    case lists:keyfind(DBName, 1, Options) of
+        [] ->
+            log:log("error");
+        {DBName, DB, Subscriber} ->
+            % should be a call in db_prbr
+            {DB, Subscriber, {false, 0, 0}}
+    end.
