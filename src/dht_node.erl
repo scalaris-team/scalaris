5c94baa46f220d3f1526069ff8eeb49ecdd9e5ef
schuett <schuett@zib.de>
2015-01-28 13:02:18 +0000

    recover: implements recover with leases

--- src/dht_node.erl
+++ src/dht_node.erl
@@ -494,15 +494,19 @@ init(Options) ->
     rm_loop:init_first(),
     dht_node_move:send_trigger(),
 
-    case {is_first(Options), config:read(leases)} of
-        {true, true} ->
+    Recover = config:read(start_type) =:= recover,
+    FirstState = case {is_first(Options), config:read(leases), Recover} of
+        {_   , true, true} ->
+            % we are recovering
+            dht_node_join_recover:join(Options);
+        {true, true, false} ->
             msg_delay:send_trigger(1, {l_on_cseq, renew_leases}),
             Id = l_on_cseq:id(intervals:all()),
             TmpState = dht_node_join:join_as_first(Id, 0, Options),
             %% we have to inject the first lease by hand, as otherwise
             %% no routing will work.
             l_on_cseq:add_first_lease_to_db(Id, TmpState);
-        {false, true} ->
+        {false, true, false} ->
             msg_delay:send_trigger(1, {l_on_cseq, renew_leases}),
             % get my ID (if set, otherwise chose a random ID):
             Id = case lists:keyfind({dht_node, id}, 1, Options) of
@@ -510,7 +514,7 @@ init(Options) ->
                      _ -> ?RT:get_random_node_id()
                  end,
             dht_node_join:join_as_other(Id, 0, Options);
-        {IsFirst, _} ->
+        {IsFirst, _, _} ->
             % get my ID (if set, otherwise chose a random ID):
             Id = case lists:keyfind({dht_node, id}, 1, Options) of
                      {{dht_node, id}, IdX} -> IdX;
