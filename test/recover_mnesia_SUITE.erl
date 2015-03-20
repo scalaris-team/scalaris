325352c9130ce95ee2c5b78a97be8224fb2bbf25
tanracinet@gmail.com <tanracinet@gmail.com>
2015-03-20 11:45:39 +0000

    recover_SUITE: add test case with data stored in KV DBs

--- test/recover_mnesia_SUITE.erl
+++ test/recover_mnesia_SUITE.erl
@@ -30,7 +30,8 @@
 -define(EQ, ==).
 
 all() -> [
-  tester_ring_8].
+  tester_ring_8,
+  tester_with_data].
 
 suite() -> [ {timetrap, {seconds, 60}} ].
 
@@ -42,17 +43,26 @@ end_per_suite(Config) ->
 
 -ifdef(PRBR_MNESIA).
 init_per_testcase(_TestCase, Config) ->
-      {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
-      unittest_helper:make_ring(8, [{config, [{log_path, PrivDir},
-                                    {leases, true}]}]),
-      unittest_helper:check_ring_size_fully_joined(8),
-      Config.
+  % make sure ring is stopped, previous test may have run into timeout
+  unittest_helper:stop_ring(),
+  application:stop(mnesia),
+  file:delete("/home/tanguy/scalaris-dev/data/recover_mnesia_SUITE_ct@127.0.0.1/schema.DAT"),
+
+  {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
+  unittest_helper:make_ring(8, [{config, [{log_path, PrivDir},
+                                {leases, true}]}]),
+  unittest_helper:check_ring_size_fully_joined(8),
+  Config.
 -else.
 init_per_testcase(_TestCase, _Config) -> {skip, "db_mnesia not set -> skipping test SUITE"}.
 -endif.
 
 end_per_testcase(_TestCase, Config) ->
+  Tabs = lists:delete(schema, mnesia:system_info(tables)),
   unittest_helper:stop_ring(),
+  application:stop(mnesia),
+  [ok = file:delete("/home/tanguy/scalaris-dev/data/recover_mnesia_SUITE_ct@127.0.0.1/"++atom_to_list(X)++".DCD")||X<-Tabs],
+  ok = file:delete("/home/tanguy/scalaris-dev/data/recover_mnesia_SUITE_ct@127.0.0.1/schema.DAT"),
   Config.
 
 rw_suite_runs(N) ->
@@ -65,12 +75,29 @@ rw_suite_runs(N) ->
 tester_ring_8(Config) ->
   {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
   unittest_helper:stop_ring(),
-  ct:pal("ring stopped -> sleeping"),
+  ct:pal("ring stopped -> sleeping. Waiting for leases to expire"),
   timer:sleep(11000),
-  ct:pal("wake up"),
   unittest_helper:make_ring_recover( [{config, [{log_path, PrivDir},
                                       {leases, true},
                                       {start_type, recover}]}]),
   util:wait_for(fun admin:check_leases/0, 10000),
-  ct:pal("ring up to date"),
   true.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% test with_data/1 write data to KV DBs and ensure data integrity after recovery
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+tester_with_data(Config) ->
+  % writting data to KV
+  [kv_on_cseq:write(integer_to_list(X),X)||X<-lists:seq(1,100)],
+  {priv_dir, PrivDir} = lists:keyfind(priv_dir, 1, Config),
+  unittest_helper:stop_ring(),
+  ct:pal("ring stopped -> sleeping. Waiting for leases to expire"),
+  timer:sleep(11000),
+  unittest_helper:make_ring_recover( [{config, [{log_path, PrivDir},
+    {leases, true},
+    {start_type, recover}]}]),
+  util:wait_for(fun admin:check_leases/0, 10000),
+  % ring restored -> checking KV data integrity
+  [{ok, X} = kv_on_cseq:read(integer_to_list(X))||X<-lists:seq(1,100)],
+  true.
+
