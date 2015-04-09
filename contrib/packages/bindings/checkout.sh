b2e8c2edcfa5f2196ecabb279bb41aa6651d7348
Nico Kruber <kruber@zib.de>
2015-04-09 16:25:14 +0200

    packages: update checkout scripts and changelogs to git

--- contrib/packages/bindings/checkout.sh
+++ contrib/packages/bindings/checkout.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
 SCALARIS_VERSION="0.7.2+git"
-date=`date +"%Y%m%d"`
+date=`date +"%Y%m%d.%H%M"`
 name="scalaris-bindings" # folder base name (without version)
-url="http://scalaris.googlecode.com/svn/trunk/"
+url="https://github.com/scalaris-team/scalaris.git"
 deletefolder=0 # set to 1 to delete the folder the repository is checked out to
 
 #####
 
 folder="./${name}"
 
-if [ ! -d ${folder} ]; then
+if [ ! -d "${folder}" ]; then
   echo "checkout ${url} -> ${folder} ..."
-  svn checkout ${url} ${folder}
+  git clone "${url}" "${folder}"
   result=$?
 else
   echo "update ${url} -> ${folder} ..."
-  svn update ${folder}
+  cd "${folder}"
+  git pull
   result=$?
+  cd - >/dev/null
 fi
 
-if ! diff -q checkout.sh ${folder}/contrib/packages/bindings/checkout.sh > /dev/null ; then
+if ! diff -q checkout.sh "${folder}/contrib/packages/bindings/checkout.sh" > /dev/null ; then
   echo "checkout-script changed - re-run ./checkout.sh"
-  cp ${folder}/contrib/packages/bindings/checkout.sh ./ ; exit 1
+  cp "${folder}/contrib/packages/bindings/checkout.sh" ./ ; exit 1
 fi
 
 if [ ${result} -eq 0 ]; then
-  echo -n "get svn revision ..."
-  revision=`svn info ${folder} --xml | grep revision | cut -d '"' -f 2 | head -n 1`
+  echo -n "get git revision ..."
+  revision=`cd "${folder}" && git log --pretty=format:'%h' -n 1`
   result=$?
   echo " ${revision}"
-  # not safe in other languages than English:
-  # revision=`svn info ${name} | grep "Revision:" | cut -d ' ' -f 4`
-  pkg_version="${SCALARIS_VERSION}${revision}"
+  pkg_version="${SCALARIS_VERSION}${date}.${revision}"
 fi
 
 if [ ${result} -eq 0 ]; then
@@ -40,46 +40,46 @@ if [ ${result} -eq 0 ]; then
   targzfile="${tarfile}.gz"
   newfoldername="${folder}-${pkg_version}"
   echo "making ${targzfile} ..."
-  mv "${folder}" "${newfoldername}" && tar -cf ${tarfile} ${newfoldername} --exclude-vcs --exclude="${newfoldername}/src" --exclude="${newfoldername}/test" --exclude="${newfoldername}/include" --exclude="${newfoldername}/contrib" --exclude="${newfoldername}/user-dev-guide" --exclude="${newfoldername}/doc" --exclude="${newfoldername}/docroot" && tar -rf ${tarfile} ${newfoldername}/contrib/wikipedia/contrib/apache-tomcat ${tarfile} ${newfoldername}/contrib/init.d --exclude-vcs && gzip -f ${tarfile} && mv "${newfoldername}" "${folder}"
+  mv "${folder}" "${newfoldername}" && tar -cf "${tarfile}" "${newfoldername}" --exclude-vcs --exclude="${newfoldername}/src" --exclude="${newfoldername}/test" --exclude="${newfoldername}/include" --exclude="${newfoldername}/contrib" --exclude="${newfoldername}/user-dev-guide" --exclude="${newfoldername}/doc" --exclude="${newfoldername}/docroot" && tar -rf "${tarfile}" "${newfoldername}/contrib/wikipedia/contrib/apache-tomcat" "${newfoldername}/contrib/init.d" --exclude-vcs && gzip -f "${tarfile}" && mv "${newfoldername}" "${folder}"
   result=$?
 fi
 
 if [ ${result} -eq 0 ]; then
   echo "extracting .spec file ..."
-  sourcefolder=${folder}/contrib/packages/bindings
+  sourcefolder="${folder}/contrib/packages/bindings"
   sed -e "s/%define pkg_version .*/%define pkg_version ${pkg_version}/g" \
-      < ${sourcefolder}/scalaris-bindings.spec     > ./scalaris-bindings.spec && \
-  cp  ${sourcefolder}/scalaris-bindings.changes      ./scalaris-bindings.changes
+      < "${sourcefolder}/scalaris-bindings.spec"     > ./scalaris-bindings.spec && \
+  cp  "${sourcefolder}/scalaris-bindings.changes"      ./scalaris-bindings.changes
   result=$?
 fi
 
 if [ ${result} -eq 0 ]; then
   echo "extracting Debian package files ..."
-  sourcefolder=${folder}/contrib/packages/bindings
+  sourcefolder="${folder}/contrib/packages/bindings"
   sed -e "s/Version: .*-.*/Version: ${pkg_version}-1/g" \
       -e "s/scalaris-bindings\\.orig\\.tar\\.gz/scalaris-bindings-${pkg_version}\\.orig\\.tar\\.gz/g" \
       -e "s/scalaris-bindings\\.diff\\.tar\\.gz/scalaris-bindings-${pkg_version}\\.diff\\.tar\\.gz/g" \
-      < ${sourcefolder}/scalaris-bindings.dsc          > ./scalaris-bindings.dsc && \
+      < "${sourcefolder}/scalaris-bindings.dsc"          > ./scalaris-bindings.dsc && \
   sed -e "0,/(.*-.*)/s//(${pkg_version}-1)/" \
       -e "0,/ -- Nico Kruber <kruber@zib.de>  .*/s// -- Nico Kruber <kruber@zib.de>  `LANG=C date -R`/" \
-      < ${sourcefolder}/debian.changelog               > ./debian.changelog && \
-  cp  ${sourcefolder}/debian.compat                      ./debian.compat && \
-  cp  ${sourcefolder}/debian.control                     ./debian.control && \
-  cp  ${sourcefolder}/debian.rules                       ./debian.rules
+      < "${sourcefolder}/debian.changelog"               > ./debian.changelog && \
+  cp  "${sourcefolder}/debian.compat"                      ./debian.compat && \
+  cp  "${sourcefolder}/debian.control"                     ./debian.control && \
+  cp  "${sourcefolder}/debian.rules"                       ./debian.rules
   result=$?
 fi
 
 if [ ${result} -eq 0 ]; then
   echo "extracting ArchLinux package files ..."
-  sourcefolder=${folder}/contrib/packages/bindings
-  tarmd5sum=`md5sum ${targzfile} | cut -d' ' -f 1` && \
+  sourcefolder="${folder}/contrib/packages/bindings"
+  tarmd5sum=`md5sum "${targzfile}" | cut -d' ' -f 1` && \
   sed -e "s/pkgver=.*/pkgver=${pkg_version}/g" \
       -e "s/md5sums=('.*')/md5sums=('${tarmd5sum}')/g" \
-      < ${sourcefolder}/PKGBUILD                   > ./PKGBUILD
+      < "${sourcefolder}/PKGBUILD"                   > ./PKGBUILD
   result=$?
 fi
 
 if [ ${result} -eq 0 -a ${deletefolder} -eq 1 ]; then
   echo "removing ${folder} ..."
-  rm -rf ${folder}
+  rm -rf "${folder}"
 fi
--- contrib/packages/examples-wiki/checkout.sh
+++ contrib/packages/examples-wiki/checkout.sh
@@ -1,87 +1,74 @@
 #!/bin/bash
 
 SCALARIS_VERSION="0.7.2+git"
-date=`date +"%Y%m%d"`
+date=`date +"%Y%m%d.%H%M"`
 name="scalaris-examples-wiki" # folder base name (without version)
-url="http://scalaris.googlecode.com/svn/trunk/contrib/wikipedia"
-package_url="http://scalaris.googlecode.com/svn/trunk/contrib/packages/examples-wiki"
+url="https://github.com/scalaris-team/scalaris.git"
 deletefolder=0 # set to 1 to delete the folder the repository is checked out to
 
 #####
 
 folder="./${name}"
 
-if [ ! -d ${folder} ]; then
+if [ ! -d "${folder}" ]; then
   echo "checkout ${url} -> ${folder} ..."
-  svn checkout ${url} ${folder}
+  git clone "${url}" "${folder}"
   result=$?
 else
   echo "update ${url} -> ${folder} ..."
-  svn update ${folder}
+  cd "${folder}"
+  git pull
   result=$?
+  cd - >/dev/null
 fi
 
-package_folder="./packaging"
-
-if [ ! -d ${package_folder} ]; then
-  echo "checkout ${package_url} -> ${package_folder} ..."
-  svn checkout ${package_url} ${package_folder}
-  result=$?
-else
-  echo "update ${package_url} -> ${package_folder} ..."
-  svn update ${package_folder}
-  result=$?
-fi
-
-if ! diff -q checkout.sh ${package_folder}/checkout.sh > /dev/null ; then
+if ! diff -q checkout.sh "${folder}/contrib/packages/examples-wiki/checkout.sh" > /dev/null ; then
   echo "checkout-script changed - re-run ./checkout.sh"
-  cp ${package_folder}/checkout.sh ./ ; exit 1
+  cp "${folder}/contrib/packages/examples-wiki/checkout.sh" ./ ; exit 1
 fi
 
 if [ ${result} -eq 0 ]; then
-  echo -n "get svn revision ..."
-  revision=`svn info ${folder} --xml | grep revision | cut -d '"' -f 2 | head -n 1`
+  echo -n "get git revision ..."
+  revision=`cd "${folder}" && git log --pretty=format:'%h' -n 1`
   result=$?
   echo " ${revision}"
-  # not safe in other languages than English:
-  # revision=`svn info ${name} | grep "Revision:" | cut -d ' ' -f 4`
-  pkg_version="${SCALARIS_VERSION}${revision}"
+  pkg_version="${SCALARIS_VERSION}${date}.${revision}"
 fi
 
 if [ ${result} -eq 0 ]; then
   tarfile="${folder}-${pkg_version}.tar.gz"
   newfoldername="${folder}-${pkg_version}"
   echo "making ${tarfile} ..."
-  mv "${folder}" "${newfoldername}" && tar -czf ${tarfile} ${newfoldername} --exclude-vcs && mv "${newfoldername}" "${folder}"
+  mv "${folder}/contrib/wikipedia" "${newfoldername}" && tar -czf "${tarfile}" "${newfoldername}" --exclude-vcs && mv "${newfoldername}" "${folder}/contrib/wikipedia"
   result=$?
 fi
 
 if [ ${result} -eq 0 ]; then
   echo "extracting .spec file ..."
-  sourcefolder=${package_folder}
+  sourcefolder="${folder}/contrib/packages/examples-wiki"
   sed -e "s/%define pkg_version .*/%define pkg_version ${pkg_version}/g" \
-      < ${sourcefolder}/scalaris-examples-wiki.spec  > ./scalaris-examples-wiki.spec && \
-  cp  ${sourcefolder}/scalaris-examples-wiki.changes   ./scalaris-examples-wiki.changes && \
-  cp  ${sourcefolder}/scalaris-examples-wiki-rpmlintrc ./scalaris-examples-wiki-rpmlintrc
+      < "${sourcefolder}/scalaris-examples-wiki.spec"  > ./scalaris-examples-wiki.spec && \
+  cp  "${sourcefolder}/scalaris-examples-wiki.changes"   ./scalaris-examples-wiki.changes && \
+  cp  "${sourcefolder}/scalaris-examples-wiki-rpmlintrc" ./scalaris-examples-wiki-rpmlintrc
   result=$?
 fi
 
 if [ ${result} -eq 0 ]; then
   echo "extracting Debian package files ..."
-  sourcefolder=${package_folder}
+  sourcefolder="${folder}/contrib/packages/examples-wiki"
   sed -e "s/Version: .*-.*/Version: ${pkg_version}-1/g" \
       -e "s/scalaris-examples-wiki\\.orig\\.tar\\.gz/scalaris-examples-wiki-${pkg_version}\\.orig\\.tar\\.gz/g" \
       -e "s/scalaris-examples-wiki\\.diff\\.tar\\.gz/scalaris-examples-wiki-${pkg_version}\\.diff\\.tar\\.gz/g" \
       < ${sourcefolder}/scalaris-examples-wiki.dsc  > ./scalaris-examples-wiki.dsc && \
   sed -e "0,/(.*-.*)/s//(${pkg_version}-1)/" \
       -e "0,/ -- Nico Kruber <kruber@zib.de>  .*/s// -- Nico Kruber <kruber@zib.de>  `LANG=C date -R`/" \
-      < ${sourcefolder}/debian.changelog            > ./debian.changelog && \
-  cp  ${sourcefolder}/debian.compat                   ./debian.compat && \
-  cp  ${sourcefolder}/debian.control                  ./debian.control && \
-  cp  ${sourcefolder}/debian.rules                    ./debian.rules && \
-  cp  ${sourcefolder}/debian.scalaris-examples-wiki-tomcat5.conffiles \
+      < "${sourcefolder}/debian.changelog"            > ./debian.changelog && \
+  cp  "${sourcefolder}/debian.compat"                   ./debian.compat && \
+  cp  "${sourcefolder}/debian.control"                  ./debian.control && \
+  cp  "${sourcefolder}/debian.rules"                    ./debian.rules && \
+  cp  "${sourcefolder}/debian.scalaris-examples-wiki-tomcat5.conffiles" \
                                                       ./debian.scalaris-examples-wiki-tomcat5.conffiles && \
-  cp  ${sourcefolder}/debian.scalaris-examples-wiki-tomcat6.conffiles \
+  cp  "${sourcefolder}/debian.scalaris-examples-wiki-tomcat6.conffiles" \
                                                       ./debian.scalaris-examples-wiki-tomcat6.conffiles
 
   result=$?
@@ -89,5 +76,5 @@ fi
 
 if [ ${result} -eq 0 -a ${deletefolder} -eq 1 ]; then
   echo "removing ${folder} ..."
-  rm -rf ${folder}
+  rm -rf "${folder}"
 fi
