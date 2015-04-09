8d21d59313838aab08036033e3d0365b8db0ba2a
Nico Kruber <kruber@zib.de>
2015-04-09 14:53:52 +0200

    integrate update_version.sh into bootstrap.sh
    * running update_version.sh without bootstrap.sh created an incomplete version update due to configure using the new version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2007-2012 Zuse Institute Berlin
+# Copyright 2007-2015 Zuse Institute Berlin
 # 
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -13,7 +13,44 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-./update_version.sh
+## update version ##
+
+VERSION=`cat VERSION`
+VERSION_NOPLUS=`echo "${VERSION}" | tr + _`
+
+echo "Setting Scalaris version to ${VERSION}..."
+sed -e "s/-define(SCALARIS_VERSION, \".*\")\\./-define(SCALARIS_VERSION, \"${VERSION}\")./g" \
+    -i include/scalaris.hrl
+sed -e "s/AC_INIT(scalaris, .*, scalaris@googlegroups.com)/AC_INIT(scalaris, ${VERSION}, scalaris@googlegroups.com)/g" \
+    -i configure.ac
+sed -e "s/public static final String version = \".*\";/public static final String version = \"${VERSION}\";/g" \
+    -i contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/WikiServlet.java
+sed -e "s/version='.*',/version='${VERSION}',/g" \
+    -i python-api/setup.py
+sed -e "s/SCALARIS_VERSION=\".*\"/SCALARIS_VERSION=\"${VERSION}\"/g" \
+    -i contrib/packages/*/checkout.sh
+sed -e "s/%define pkg_version .*/%define pkg_version ${VERSION}/g" \
+    -i contrib/packages/*/*.spec
+sed -e "s/Version: .*-.*/Version: ${VERSION}-1/g" \
+    -i contrib/packages/*/*.dsc
+sed -e "0,/(.*-.*)/s//(${VERSION}-1)/" \
+    -i contrib/packages/*/debian.changelog
+sed -e "0,/<version>.*<\/version>/s//<version>${VERSION}<\/version>/" \
+    -i java-api/pom.xml
+sed -e "s/module scalaris .*;/module scalaris ${VERSION_NOPLUS};/g" \
+    -i contrib/systemd/scalaris.te
+if [[ "$VERSION" == *git* ]]; then
+  RELEASE="unstable"
+else
+  RELEASE="stable"
+fi
+sed -e "0,/u*n*stable;/s//${RELEASE};/" \
+    -i contrib/packages/*/debian.changelog
+echo "done"
+sed -e "s/pkgver=.*/pkgver=${VERSION}/g" \
+    -i contrib/packages/*/PKGBUILD
+
+##########
 
 echo "Creating configure script"
 aclocal
