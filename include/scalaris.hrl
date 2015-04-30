9c4c2e707f71a7e1d3d7a1263905b22477058737
Nico Kruber <kruber@zib.de>
2015-04-30 14:37:14 +0200

    trace_mpath: add a debug assert for receiving only infected messages when the receiving process is infected

--- include/scalaris.hrl
+++ include/scalaris.hrl
@@ -171,6 +171,7 @@
                end,
                Y;
             X ->
+               ?ASSERT2(not trace_mpath:infected(), 'noninfected_message_in_infected_process'),
                case gen_component:is_gen_component(self())
                    andalso ({current_function, {pid_groups, add, 3}}
                             =/=  process_info(self(), current_function)) of
