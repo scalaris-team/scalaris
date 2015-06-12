bf936b7f07895e98537dca7627fc3b5b50f90496
Thorsten Schuett <schuett@zib.de>
2015-06-12 14:15:39 +0200

    slurm: update sclaris benchmark

--- contrib/slurm/bench-script.sh
+++ contrib/slurm/bench-script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -l
+#!/bin/bash
 
 # -o: output log file: %j for the job ID, %N for the name of the first executing node
 # Change the path of the output logfile
@@ -7,11 +7,11 @@
 #SBATCH -N 2
 #SBATCH -p CSR
 #SBATCH -A csr
+#SBATCH --time=00:10:00
 #SBATCH --exclusive
 
+source /usr/share/modules/init/bash
 source $(pwd)/env.sh
-
-#$BINDIR/scalarisctl checkinstallation
 
 $(pwd)/scalaris-start.sh
 
@@ -21,12 +21,14 @@ $(pwd)/scalaris-start.sh
 #                                           #
 #############################################
 
-sleep 15
+METRICS="{value, {mean_troughput_overall, Mean}} = lists:keysearch(mean_troughput_overall, 1, Res), {value, {avg_latency_overall, Latency}} = lists:keysearch(avg_latency_overall, 1, Res)"
+LOGSTRING_INC="io:format('result data inc:~p:~p~n', [Mean, Latency])"
+LOGSTRING_QR="io:format('result data qr:~p:~p~n', [Mean, Latency])"
 
 echo "running bench:increment(10, 500)..."
-erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "rpc:call('first@`hostname -f`', bench, increment, [10, 500]), halt(0)."
+erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, increment, [10, 500]), $METRICS, $LOGSTRING_INC, halt(0)."
 echo "running bench:quorum_read(10, 5000)..."
-erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "rpc:call('first@`hostname -f`', bench, quorum_read, [10, 5000]), halt(0)."
+erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, quorum_read, [10, 5000]), $METRICS, $LOGSTRING_QR, halt(0)."
 
 #############################################
 #                                           #
--- contrib/slurm/bench.sh
+++ contrib/slurm/bench.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ERLANG_VERSIONS="R17B05 R14B04"
-SCALARIS_DIR=$HOME/scalaris
+
+mkdir -p $OUTPUTDIR
 
 for NNODES in "2" #  1 2 3 4 5 6 7 8 12 16 24 32
 do
@@ -11,7 +12,15 @@ do
         do
             for VERSION in "R17B05" # $ERLANG_VERSIONS
             do
-                sbatch --dependency=singleton --export=ERLANG_VERSION=$VERSION,SCALARIS_DIR=$SCALARIS_DIR,VMS_PER_NODE=$VMS_PER_NODE,DHT_NODES_PER_VM=$DHT_NODES_PER_VM --job-name scalaris-benchmark -N $NNODES bench-script.sh
+                export ERLANG_VERSION=$VERSION
+                export SCALARIS_DIR=$SCALARIS_DIR
+                export VMS_PER_NODE=$VMS_PER_NODE
+                export DHT_NODES_PER_VM=$DHT_NODES_PER_VM
+                for ITERATION in `seq 1 20`
+                do
+                    #sbatch --dependency=singleton --job-name scalaris-benchmark -N $NNODES bench-script.sh
+                    sbatch --dependency=singleton --job-name scalaris-benchmark -N $NNODES -o slurm-$ERLANG_VERSION-$NNODES-$VMS_PER_NODE-$DHT_NODES_PER_VM-%j.out bench-script.sh
+                done
             done
         done
     done
