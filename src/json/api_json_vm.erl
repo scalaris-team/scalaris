6bf8fb3b9af4ccd0f0d993e049f7066876476ca5
Thorsten Schuett <schuett@zib.de>
2015-08-10 15:23:14 +0200

    api_json_vm: pid_groups:groupname() <-> json

--- src/json/api_json_vm.erl
+++ src/json/api_json_vm.erl
@@ -75,39 +75,48 @@ number_of_nodes() ->
 
 -spec get_nodes() -> {struct, [{Key::atom(), Value::term()}]}.
 get_nodes() ->
-    {struct, [{status, "ok"}, {value, {array, api_vm:get_nodes()}}]}.
+    {struct, [{status, "ok"}, {value, {array, [pid_groups:group_to_string(Group) || Group <- api_vm:get_nodes()]}}]}.
 
 -spec add_nodes(Number::non_neg_integer()) -> {struct, [{Key::atom(), Value::term()}]}.
 add_nodes(Number) ->
     {Ok, Failed1} = api_vm:add_nodes(Number),
     Failed = [lists:flatten(io_lib:format("~p", [Reason])) || {error, Reason} <- Failed1],
-    {struct, [{status, "ok"}, {ok, {array, Ok}}, {failed, {array, Failed}}]}.
+    {struct, [{status, "ok"},
+              {ok, {array, [pid_groups:group_to_string(Group) || Group <- Ok]}},
+              {failed, {array, Failed}}]}.
 
 -spec shutdown_node(Name::pid_groups:groupname()) -> {struct, [{Key::atom(), Value::term()}]}.
 shutdown_node(Name) ->
-    {struct, [{status, erlang:atom_to_list(api_vm:shutdown_node(Name))}]}.
+    {struct, [{status, erlang:atom_to_list(api_vm:shutdown_node(pid_groups:string_to_group(Name)))}]}.
 
 -spec shutdown_nodes(Count::non_neg_integer()) -> {struct, [{Key::atom(), Value::term()}]}.
 shutdown_nodes(Count) ->
-    {struct, [{status, "ok"}, {ok, {array, api_vm:shutdown_nodes(Count)}}]}.
+    {struct, [{status, "ok"}, {ok, {array, [pid_groups:group_to_string(Group) || Group <- api_vm:shutdown_nodes(Count)]}}]}.
 
 -spec shutdown_nodes_by_name(Names::{array, [pid_groups:groupname()]}) -> {struct, [{Key::atom(), Value::term()}]}.
 shutdown_nodes_by_name({array, Names}) ->
-    {Ok, NotFound} = api_vm:shutdown_nodes_by_name(Names),
-    {struct, [{status, "ok"}, {ok, {array, Ok}}, {not_found, {array, NotFound}}]}.
+    {Ok, NotFound} = api_vm:shutdown_nodes_by_name(
+                       [pid_groups:string_to_group(Group) || Group <- Names]),
+    Ok2 = [pid_groups:group_to_string(Group) || Group <- Ok],
+    NotFound2 = [pid_groups:group_to_string(Group) || Group <- NotFound],
+    {struct, [{status, "ok"}, {ok, {array, Ok2}}, {not_found, {array, NotFound2}}]}.
 
 -spec kill_node(Name::pid_groups:groupname()) -> {struct, [{Key::atom(), Value::term()}]}.
 kill_node(Name) ->
-    {struct, [{status, erlang:atom_to_list(api_vm:kill_node(Name))}]}.
+    {struct, [{status, erlang:atom_to_list(api_vm:kill_node(pid_groups:string_to_group(Name)))}]}.
 
 -spec kill_nodes(Count::non_neg_integer()) -> {struct, [{Key::atom(), Value::term()}]}.
 kill_nodes(Count) ->
-    {struct, [{status, "ok"}, {ok, {array, api_vm:kill_nodes(Count)}}]}.
+    Ok = [pid_groups:group_to_string(Group) || Group <- api_vm:kill_nodes(Count)],
+    {struct, [{status, "ok"}, {ok, {array, Ok}}]}.
 
 -spec kill_nodes_by_name(Names::{array, [pid_groups:groupname()]}) -> {struct, [{Key::atom(), Value::term()}]}.
 kill_nodes_by_name({array, Names}) ->
-    {Ok, NotFound} = api_vm:kill_nodes_by_name(Names),
-    {struct, [{status, "ok"}, {ok, {array, Ok}}, {not_found, {array, NotFound}}]}.
+    {Ok, NotFound} = api_vm:kill_nodes_by_name(
+                       [pid_groups:string_to_group(Group) || Group <- Names]),
+    Ok2 = [pid_groups:group_to_string(Group) || Group <- Ok],
+    NotFound2 = [pid_groups:group_to_string(Group) || Group <- NotFound],
+    {struct, [{status, "ok"}, {ok, {array, Ok2}}, {not_found, {array, NotFound2}}]}.
 
 -spec get_other_vms(MaxVMs::pos_integer()) -> {struct, [{Key::atom(), Value::term()}]}.
 get_other_vms(MaxVMs) ->
