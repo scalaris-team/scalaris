7ef299b61bb0d05314474423c27026d1c8943589
jensvfischer <jensvfischer@gmail.com>
2015-08-17 13:34:21 +0200

    contrib/slurm: trap SIGTERM (i.e. trap canceled jobs)

--- contrib/slurm/env.sh
+++ contrib/slurm/env.sh
@@ -8,3 +8,22 @@ export BINDIR=$SCALARIS_DIR/bin
 export BEAMDIR=$SCALARIS_DIR/ebin
 
 export SHUFFLE_NODE_IDS=1
+
+function cleanup() {
+    NODES=$(scontrol show hostnames)
+    NODELIST=$(echo $NODES | sed 's/ /,/g')
+    echo "Nodelist of the cancelled job: $NODELIST"
+    echo -e "Use:
+    srun -p CSR -A csr --nodelist=\"$NODELIST\" cleanup.sh"
+    echo "to clean up the nodes manually"
+
+    # comment in for automatic cleanup
+    # for NODE in $NODELIST; do
+        # sbatch --job-name cleanup -p CSR -A csr --nodelist="$NODE" -o cleanup-%j.out cleanup.sh
+    # done
+    exit 1
+}
+
+trap cleanup SIGTERM
+
+
