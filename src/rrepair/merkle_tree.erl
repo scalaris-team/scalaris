scalaris-team/scalaris

commit 29664d575f44258f94f81cb74fffe4bf6ec5f83d

rrepair, merkle: hashes of empty leaves on both nodes should match (regression of 90c7197ad9)

src/rrepair/rr_recon.erl
@@ -1513,9 +1513,9 @@ merkle_compress_hashlist([N1 | TL], Bin, SigSizeI, SigSizeL) ->
     H1 = merkle_tree:get_hash(N1),
     case merkle_tree:is_leaf(N1) of
         true ->
-            Bin2 = case merkle_tree:get_item_count(N1) of
-                       0 -> <<Bin/bitstring, 1:1, 0:1>>;
-                       _ -> <<Bin/bitstring, 1:1, 1:1, H1:SigSizeL>>
+            Bin2 = case merkle_tree:is_empty(N1) of
+                       true  -> <<Bin/bitstring, 1:1, 0:1>>;
+                       false -> <<Bin/bitstring, 1:1, 1:1, H1:SigSizeL>>
                    end,
             merkle_compress_hashlist(TL, Bin2, SigSizeI, SigSizeL);
         false ->
@@ -1578,14 +1578,17 @@ check_node([{Hash, IsLeafHash} | TK], [Node | TN], SigSizeI, SigSizeL,
            MyMaxItemsCount, OtherMaxItemsCount, Params, Stats, FlagsAcc,
            RestTreeAcc, MerkleSyncAccSend, MerkleSyncAccRcv, MerkleSyncIN, AccCmp, AccSkip) ->
     IsLeafNode = merkle_tree:is_leaf(Node),
-    NodeHash0 = merkle_tree:get_hash(Node),
-    if IsLeafHash ->
-           <<NodeHash:SigSizeL/integer-unit:1>> = <<NodeHash0:SigSizeL>>,
-           ok;
-       true ->
-           <<NodeHash:SigSizeI/integer-unit:1>> = <<NodeHash0:SigSizeI>>,
-           ok
-    end,
+    EmptyNode = merkle_tree:is_empty(Node),
+    NodeHash =
+        if IsLeafNode andalso EmptyNode ->
+               none; % to match with the hash from merkle_decompress_hashlist/3
+           IsLeafNode ->
+               <<X:SigSizeL/integer-unit:1>> = <<(merkle_tree:get_hash(Node)):SigSizeL>>,
+               X;
+           true ->
+               <<X:SigSizeI/integer-unit:1>> = <<(merkle_tree:get_hash(Node)):SigSizeI>>,
+               X
+        end,
     if Hash =:= NodeHash andalso IsLeafHash =:= IsLeafNode ->
            Skipped = merkle_tree:size(Node) - 1,
            check_node(TK, TN, SigSizeI, SigSizeL,

commit 8a42d0ac7e5b18ea80723f87278336b92fea79c5

rrepair, merkle: extend merkle_tree:is_empty/1 to merkle nodes
this also fixes a regression of 29664d575f which caused empty leaves to be transferred instead of being skipped

src/rrepair/merkle_tree.erl
@@ -125,9 +125,11 @@ get_root(_) -> undefined.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% @doc Checks whether the merkle tree has any children or elements.
--spec is_empty(merkle_tree()) -> boolean().
-is_empty({merkle_tree, _, {_H, _ICnt = 0, _Bkt = [], _I}}) -> true;
-is_empty(_) -> false.
+-spec is_empty(merkle_tree() | mt_node()) -> boolean().
+is_empty({merkle_tree, _, Root}) -> is_empty(Root);
+is_empty({_H, _ICnt = 0, _Bkt = [], _I}) -> true;
+is_empty({_H, _Cnt, _ICnt, _I, _CL}) -> false;
+is_empty({_H, _ICnt, _Bkt, _I}) -> false. % inner node (must have children!)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 

src/rrepair/rr_recon.erl
@@ -933,7 +933,7 @@ begin_sync(MySyncStruct, _OtherSyncStruct,
                           "Inner/Leaf/Items: ~p, EmptyLeaves: ~B, Items/Leaf: ~g",
                           [length(MySyncStruct), merkle_tree:size_detail(MySyncStruct),
                            length([ok || L <- merkle_tree:get_leaves(MySyncStruct),
-                                         merkle_tree:get_item_count(L) =:= 0]),
+                                         merkle_tree:is_empty(L)]),
                            lists:sum([merkle_tree:get_item_count(M) || M <- MySyncStruct]) /
                                lists:sum([merkle_tree:get_leaf_count(M) || M <- MySyncStruct])]),
             P1ETotal2 = calc_n_subparts_p1e(2, P1ETotal),
@@ -1003,7 +1003,7 @@ begin_sync(MySyncStruct, _OtherSyncStruct,
                           "Inner/Leaf/Items: ~p, EmptyLeaves: ~B, Items/Leaf: ~g",
                           [length(SyncStruct), merkle_tree:size_detail(SyncStruct),
                            length([ok || L <- merkle_tree:get_leaves(SyncStruct),
-                                         merkle_tree:get_item_count(L) =:= 0]),
+                                         merkle_tree:is_empty(L)]),
                            lists:sum([merkle_tree:get_item_count(M) || M <- SyncStruct]) /
                                lists:sum([merkle_tree:get_leaf_count(M) || M <- SyncStruct])]),
             

test/rrepair_SUITE.erl
@@ -243,9 +243,9 @@ prop_merkle_compress_hashlist(Nodes0, SigSizeI, SigSizeL) ->
                      H0 = merkle_tree:get_hash(N),
                      case merkle_tree:is_leaf(N) of
                          true ->
-                             case merkle_tree:get_item_count(N) of
-                                 0 -> H = none;
-                                 _ -> <<H:SigSizeL/integer-unit:1>> = <<H0:SigSizeL>>
+                             case merkle_tree:is_empty(N) of
+                                 true  -> H = none;
+                                 false -> <<H:SigSizeL/integer-unit:1>> = <<H0:SigSizeL>>
                              end;
                          false ->
                              <<H:SigSizeI/integer-unit:1>> = <<H0:SigSizeI>>