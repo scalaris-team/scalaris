b08818d97cfe57faae5ed6e807149e2754c71170
Nico Kruber <kruber@zib.de>
2015-09-16 16:18:39 +0200

    make: fix benchmarks not working on different ports (regression of 3fe13f2619)

--- bin/setup-ring-for-benchmarks.sh
+++ bin/setup-ring-for-benchmarks.sh
@@ -30,7 +30,7 @@ usage(){
 start_ring(){
     KEYS=`./bin/scalarisctl -t nostart -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
 
-    SCALARIS_ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}]"
+    export SCALARIS_ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}]"
     idx=0
     for key in $KEYS; do
         let idx+=1
