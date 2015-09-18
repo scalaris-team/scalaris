fa774ecd278b727def0e4206dc86cff8bf2114af
Nico Kruber <kruber@zib.de>
2015-09-18 16:33:31 +0200

    also use ${NODEPREFIX} for the command that gets evenly spaced keys

--- bin/setup-ring-for-benchmarks.sh
+++ bin/setup-ring-for-benchmarks.sh
@@ -28,7 +28,7 @@ usage(){
 }
 
 start_ring(){
-    KEYS=`./bin/scalarisctl -t nostart -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
+    KEYS=`./bin/scalarisctl -t nostart -n "${NODEPREFIX}" -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
 
     export SCALARIS_ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}]"
     idx=0
