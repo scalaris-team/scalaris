ac81bcd390e08ed9e6b9bf8b5d920ac8bcb501df
jensvfischer <jensvfischer@gmail.com>
2015-09-22 16:15:28 +0200

    contrib/slurm: make bench-scripts.sh more verbose

--- contrib/slurm/bench-script.sh
+++ contrib/slurm/bench-script.sh
@@ -20,15 +20,28 @@ $(pwd)/scalaris-start.sh
 #     Place your commands between here      #
 #                                           #
 #############################################
+HEAD=$(git rev-parse --short HEAD)
+JOBID=$SLURM_JOB_ID
+NO_OF_NODES=$SLURM_JOB_NUM_NODES
+
+echo "HEAD; JOBID; NO_OF_NODES; VMS_PER_NODE; PID; NO_OF_CH"
+echo "Git-Head: $HEAD"
+echo "Job-Id: $JOBID"
+echo "Number of DHT Nodes: $(($NO_OF_NODES*$VMS_PER_NODE)) (Nodes: $NO_OF_NODES; VMs per Node: $VMS_PER_NODE)"
 
 METRICS="{value, {mean_troughput_overall, Mean}} = lists:keysearch(mean_troughput_overall, 1, Res), {value, {avg_latency_overall, Latency}} = lists:keysearch(avg_latency_overall, 1, Res)"
 LOGSTRING_INC="io:format('result data inc:~p:~p~n', [Mean, Latency])"
 LOGSTRING_QR="io:format('result data qr:~p:~p~n', [Mean, Latency])"
 
-echo "running bench:increment(10, 500)..."
-erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, increment, [10, 500]), $METRICS, $LOGSTRING_INC, halt(0)."
-echo "running bench:quorum_read(10, 5000)..."
-erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, quorum_read, [10, 5000]), $METRICS, $LOGSTRING_QR, halt(0)."
+THREADS=1024
+ITERATIONS=4
+echo "running bench:increment($THREADS, $ITERATIONS)..."
+erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, increment, [$THREADS, $ITERATIONS]), $METRICS, $LOGSTRING_INC, halt(0)."
+
+THREADS=2048
+ITERATIONS=10
+echo "running bench:increment($THREADS, $ITERATIONS)..."
+erl -setcookie "chocolate chip cookie" -name bench_ -noinput -eval "{ok, Res} = rpc:call('first@`hostname -f`', bench, quorum_read, [$THREADS, $ITERATIONS]), $METRICS, $LOGSTRING_QR, halt(0)."
 
 #############################################
 #                                           #
