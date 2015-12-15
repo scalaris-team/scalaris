0debbf39d95fc2173baa65c4a9acdfaa53de63d5
jensvfischer <jensvfischer@gmail.com>
2015-12-15 11:37:04 +0100

    contrib/slurm: remove debug output from sync_scalaris_to_local_dir

--- contrib/slurm/util/sync_scalaris_to_local_dir.sh
+++ contrib/slurm/util/sync_scalaris_to_local_dir.sh
@@ -2,9 +2,6 @@
 
 # SCALARIS_SRC and SCALARIS_DIR inherited from environment
 
-echo $SCALARIS_SRC
-echo $SCALARIS_DIR
-
 echo "syncing scalaris to $SCALARIS_DIR on $(hostname -f)"
 [[ -d $SCALARIS_DIR ]] || mkdir -p $SCALARIS_DIR
 rsync -ayhxq --executability --delete-after $SCALARIS_SRC/ $SCALARIS_DIR/
