b2b11d0c55609aee1dba396a36f0e693e9514369
jensvfischer <jensvfischer@gmail.com>
2015-12-15 11:30:45 +0100

    contrib/slurm: minor improvements for crashwatcher.sh

--- contrib/slurm/util/crashwatcher.sh
+++ contrib/slurm/util/crashwatcher.sh
@@ -16,7 +16,7 @@
 JOBID=$1
 NODES=$2
 
-WD="HOME/bbench"
+WD="$HOME/bbench"
 
 [[ -z $JOBID ]] && { echo "no jobid, exiting..."; exit 1; }
 echo "JOBID: $JOBID"
@@ -29,9 +29,9 @@ watch() {
 }
 
 kill_bbench(){
-    # kill all remaining processes with the SLURM_ID of the current job
-    ssh bzcfisch@buildbot2.zib.de bash -c "'pkill -f $JOBID'"
-    pkill -f $JOBID
+    # kill all remaining basho_bench processes
+    ssh bzcfisch@buildbot2.zib.de bash -c "'pkill -f basho_bench'"
+    pkill -f basho_bench
 }
 
 wait_for_crash() {
@@ -48,17 +48,21 @@ wait_for_crash() {
     echo ": ok (${timer}s)"
 }
 
-wait_for_crash
-
-# echo "$(date +%y.%m.%d-%H:%M:%S): sleeping for 10 minutes"
-# sleep 10m
-
-echo "sending SIGUSR1"
-
-srun --jobid=$JOBID -p CUMU -A csr -N $NODES bash <<'EOF'
+send_sigusr1() {
+    echo "sending SIGUSR1"
+    srun --jobid=$JOBID -p CUMU -A csr -N $NODES bash <<'EOF'
 echo -n "$(hostname). "
 ps -e -o user,pid,start_time,comm | grep beam | awk '{print $2}' | xargs -r kill -SIGUSR1
 echo "ret: $?"
 EOF
+}
 
+wait_for_crash
 kill_bbench
+
+# sleeptime="10m"
+# echo "$(date +%y.%m.%d-%H:%M:%S): sleeping for $sleeptime"
+# sleep $sleeptime
+
+send_sigusr1
+
