e06b84f93af4c9af59c5e223f624ef708a7d58c6
Nico Kruber <kruber@zib.de>
2016-08-10 16:47:51 +0200

    RT: improve some type specs

--- src/rt_frt.erl
+++ src/rt_frt.erl
@@ -284,7 +284,7 @@ n() -> 16#100000000000000000000000000000000.
 
 %% @doc Gets the number of keys in the interval (Begin, End]. In the special
 %%      case of Begin==End, the whole key range as specified by n/0 is returned.
--spec get_range(Begin::key(), End::key() | ?PLUS_INFINITY_TYPE) -> number().
+-spec get_range(Begin::key(), End::key() | ?PLUS_INFINITY_TYPE) -> non_neg_integer().
 get_range(Begin, End) -> rt_chord:get_range(Begin, End).
 
 %% @doc Gets the key that splits the interval (Begin, End] so that the first
