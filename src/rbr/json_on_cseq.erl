8e6eb03a744609ba1bfd199a4c5333dd71c19af1
Jan Skrzypczak <skrzypczak@zib.de>
2017-11-06 09:05:50 +0100

    json_on_cseq: JSON storage using RFC 6902
    First version providing basic replicated storage of JSON objects.
    In addition to simple read/write operations, partial updates on stored objects
    are supported as described in RFC 6902.

---
+++ src/rbr/json_on_cseq.erl
@@ -0,0 +1,204 @@
+% @copyright 2012-2017 Zuse Institute Berlin,
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Jan Skrzypczak  <skrzypczak@zib.de>
+%% @doc    Replicated storage of JSON objects based on rbrcseq. Implements
+%%         a superset of RFC 6902 (JSON Patch) which allows  partial updates on
+%%         the stored objects. This is based on dotto:
+%%              https://github.com/marianoguerra/dotto
+%%
+%%         In its current state, erlang maps are used to represent JSON objects.
+%% @end
+%% @version $Id$
+-module(json_on_cseq).
+-author('skrzypczak@zib.de').
+-vsn('$Id:$ ').
+
+-define(NON_EXIST_VAL, no_value_yet).
+
+-include("scalaris.hrl").
+-include("client_types.hrl").
+
+
+%% PUBLIC API
+-export([read/1]).
+-export([write/2]).
+-export([patch/2]).
+
+%% READ FILTER
+-export([rf_empty/1]).
+-export([rf_val/1]).
+
+%% CONTENT CHECK
+-export([cc_noop/3]).
+
+%% WRITE FILTER
+-export([wf_val/3]).
+-export([wf_patch/3]).
+
+-type json() :: map().
+
+-type patch() :: [patch_cmd()].
+-type patch_cmd() :: {patch_cmd_type(), path()} |
+                     {patch_cmd_type(), path(), any()}.
+
+-type patch_cmd_type() :: add | remove | replace | move | copy | test.
+-type path() :: [any()].
+
+%% TODO: Checking at write/read of object is really a json?
+%% @doc Read full JSON object stored at given key.
+-spec read(client_key()) -> {ok, client_value()} | {fail, not_found}.
+read(Key) ->
+    rbrcseq:qread(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
+                  fun ?MODULE:rf_val/1),
+    trace_mpath:thread_yield(),
+    receive
+        ?SCALARIS_RECV({qread_done, _ReqId, _NextFastWriteRound, _OldWriteRound, Value},
+                       case Value of
+                           ?NON_EXIST_VAL -> {fail, not_found};
+                           _ -> {ok, Value}
+                           end
+                      )
+    after 1000 ->
+        log:log("read hangs ~p~n", [erlang:process_info(self(), messages)]),
+        receive
+            ?SCALARIS_RECV({qread_done, _ReqId, _NextFastWriteRound, _OldWriteRound, Value},
+                            case Value of
+                                ?NON_EXIST_VAL -> {fail, not_found};
+                                _ -> {ok, Value}
+                            end
+                          )
+        end
+    end.
+
+%% @doc Write full JSON object to a given key.
+-spec write(client_key(), json()) -> ok | {fail, reason}.
+write(Key, Value) ->
+    rbrcseq:qwrite(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
+                    fun ?MODULE:rf_empty/1,
+                    fun ?MODULE:cc_noop/3,
+                    fun ?MODULE:wf_val/3, Value),
+    trace_mpath:thread_yield(),
+    receive
+        ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, _WriteRet}, ok);
+        ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                        begin
+                            log:log("Write failed on key ~p: ~p~n", [Key, Reason]),
+                            {fail, Reason}
+                        end)
+    after 1000 ->
+        log:log("~p write hangs at key ~p, ~p~n",
+                [self(), Key, erlang:process_info(self(), messages)]),
+        receive
+            ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, Value, _WriteRet},
+                            begin
+                                log:log("~p write was only slow at key ~p~n", [self(), Key]),
+                                ok
+                            end);
+            ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                            begin
+                                log:log("~p Write failed: ~p~n", [self(), Reason]),
+                                {fail, Reason}
+                            end)
+            end
+    end.
+
+
+%% %%%%%%%%%%%%%%%%
+%% JSON PATCH SPECIFIC API
+%% %%%%%%%%%%%%%%%%
+
+%% @doc Apply a JSON patch to the JSON object at a given key. A patch consists
+%% of a list of patch commands, which are applied sequentially to the JSON
+%% object. If all operations are performed successfully, then the resulting
+%% JSON object replaces the currently stored value. If *any* command fails,
+%% the stored object will not be modified.
+%% Returns:
+%%      ok                  - The patch was successfully applied
+%%      {error, ErrorList}  - At least one patch command failed.
+%%      {fail, Reason}      - The write was denied by scalaris.
+-spec patch(client_key(), patch() | patch_cmd()) ->
+    ok | {fail, any()} | {error, [any()]}.
+patch(Key, PatchCommand) when not is_list(PatchCommand) ->
+    patch(Key, [PatchCommand]);
+patch(Key, Patch) ->
+    rbrcseq:qwrite(kv_db, self(), ?RT:hash_key(Key), ?MODULE,
+                    fun ?MODULE:rf_empty/1,
+                    fun ?MODULE:cc_noop/3,
+                    fun ?MODULE:wf_patch/3, Patch),
+    trace_mpath:thread_yield(),
+    receive
+        ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, Result}, Result);
+        ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                        begin
+                            log:log("Write failed on key ~p: ~p~n", [Key, Reason]),
+                            {fail, Reason}
+                        end)
+    after 1000 ->
+        log:log("~p write hangs at key ~p, ~p~n",
+                [self(), Key, erlang:process_info(self(), messages)]),
+        receive
+            ?SCALARIS_RECV({qwrite_done, _ReqId, _NextFastWriteRound, _Value, Result},
+                            begin
+                                log:log("~p write was only slow at key ~p~n", [self(), Key]),
+                                Result
+                            end);
+            ?SCALARIS_RECV({qwrite_deny, _ReqId, _NextFastWriteRound, _Value, Reason},
+                            begin
+                                log:log("~p Write failed: ~p~n", [self(), Reason]),
+                                {fail, Reason}
+                            end)
+            end
+    end.
+
+
+
+%% %%%%%%%%%%%%%%%%
+%% READ FILTER
+%% %%%%%%%%%%%%%%%%
+%% @doc RedFilter that always returns an empty value
+-spec rf_empty(any()) -> none.
+rf_empty(_Any) -> none.
+
+%% @doc ReadFilter returning the full JSON object
+-spec rf_val(json() | prbr_bottom) -> client_value() | ?NON_EXIST_VAL.
+rf_val(prbr_bottom) -> ?NON_EXIST_VAL;
+rf_val(X)          -> X.
+
+
+%% %%%%%%%%%%%%%%%%
+%% CONTENT CHECK
+%% %%%%%%%%%%%%%%%%
+%% @doc Empty ContentCheck that always passes. And gives no update information
+%% (none) to the WriteFilter
+-spec cc_noop(any(), prbr:write_filter(), any()) -> {true, none}.
+cc_noop(_ReadVal, _WriteFilter, _ValToWrite) -> {true, none}.
+
+%% %%%%%%%%%%%%%%%%
+%% WRITE FILTER
+%% %%%%%%%%%%%%%%%%
+%% @doc WriteFilter that replaces the current value with the client provided one.
+-spec wf_val(json() | prbr_bottom, any(), json()) -> {json(), ok}.
+wf_val(_OldVal, _UpdateInfo, ValToWrite) -> {ValToWrite, ok}.
+
+%% @doc WriteFilter that applies a list of patch commands to a JSON object.
+%% A non-existing JSON object is treated as an empty object (#{}).
+-spec wf_patch(json() | prbr_bottom, any(), patch()) ->  {json(), ok | {error, [any()]}}.
+wf_patch(prbr_bottom, UI, Patch) -> wf_patch(#{}, UI, Patch);
+wf_patch(Json, _UI, Patch) ->
+    case dotto:apply(Patch, Json) of
+        {ok, ResultJson} -> {ResultJson, ok};
+        {error, _, Errors} -> {Json, {error, Errors}}
+    end.
+
