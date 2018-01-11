73f6c97460c18d201ce8c574975d76f4fc409486
Thorsten Schuett <schuett@zib.de>
2018-01-11 13:39:24 +0100

    improve error message

--- bootstrap.sh
+++ bootstrap.sh
@@ -30,43 +30,43 @@ else
 fi
 
 
-echo "Setting Scalaris version to ${VERSION}..."
-sed -e "s/-define(SCALARIS_VERSION, \".*\")\\./-define(SCALARIS_VERSION, \"${VERSION}\")./g" \
-    -i include/scalaris.hrl
-sed -e "s/AC_INIT(scalaris, .*, scalaris@googlegroups.com)/AC_INIT(scalaris, ${VERSION}, scalaris@googlegroups.com)/g" \
-    -i configure.ac
-sed -e "s/public static final String version = \".*\";/public static final String version = \"${VERSION}\";/g" \
-    -i contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/WikiServlet.java
-sed -e "s/version='.*',/version='${VERSION}',/g" \
-    -i python-api/setup.py
-sed -e "s/SCALARIS_VERSION=\".*\"/SCALARIS_VERSION=\"${VERSION}\"/g" \
-    -i contrib/packages/*/checkout.sh
-sed -e "s/%define pkg_version .*/%define pkg_version ${VERSION}/g" \
-    -i contrib/packages/*/*.spec
-sed -e "s/Version: .*-.*/Version: ${VERSION}-1/g" \
-    -i contrib/packages/*/*.dsc
-sed -e "0,/(.*-.*)/s//(${VERSION}-1)/" \
-    -i contrib/packages/*/debian.changelog
-sed -e "0,/<version>.*<\/version>/s//<version>${VERSION_MAVEN}<\/version>/" \
-    -i java-api/pom.xml contrib/datanucleus/scalaris-datanucleus-store/pom.xml
-sed -e "s/module scalaris .*;/module scalaris ${VERSION_NOPLUS};/g" \
-    -i contrib/systemd/scalaris.te
-if [[ "$VERSION" == *git* ]]; then
-  RELEASE="unstable"
-else
-  RELEASE="stable"
-fi
-sed -e "0,/u*n*stable;/s//${RELEASE};/" \
-    -i contrib/packages/*/debian.changelog
-echo "done"
-sed -e "s/pkgver=.*/pkgver=${VERSION}/g" \
-    -i contrib/packages/*/PKGBUILD
+## echo "Setting Scalaris version to ${VERSION}..."
+## sed -e "s/-define(SCALARIS_VERSION, \".*\")\\./-define(SCALARIS_VERSION, \"${VERSION}\")./g" \
+##     -i include/scalaris.hrl
+## sed -e "s/AC_INIT(scalaris, .*, scalaris@googlegroups.com)/AC_INIT(scalaris, ${VERSION}, scalaris@googlegroups.com)/g" \
+##     -i configure.ac
+## sed -e "s/public static final String version = \".*\";/public static final String version = \"${VERSION}\";/g" \
+##     -i contrib/wikipedia/src/de/zib/scalaris/examples/wikipedia/bliki/WikiServlet.java
+## sed -e "s/version='.*',/version='${VERSION}',/g" \
+##     -i python-api/setup.py
+## sed -e "s/SCALARIS_VERSION=\".*\"/SCALARIS_VERSION=\"${VERSION}\"/g" \
+##     -i contrib/packages/*/checkout.sh
+## sed -e "s/%define pkg_version .*/%define pkg_version ${VERSION}/g" \
+##     -i contrib/packages/*/*.spec
+## sed -e "s/Version: .*-.*/Version: ${VERSION}-1/g" \
+##     -i contrib/packages/*/*.dsc
+## sed -e "0,/(.*-.*)/s//(${VERSION}-1)/" \
+##     -i contrib/packages/*/debian.changelog
+## sed -e "0,/<version>.*<\/version>/s//<version>${VERSION_MAVEN}<\/version>/" \
+##     -i java-api/pom.xml contrib/datanucleus/scalaris-datanucleus-store/pom.xml
+## sed -e "s/module scalaris .*;/module scalaris ${VERSION_NOPLUS};/g" \
+##     -i contrib/systemd/scalaris.te
+## if [[ "$VERSION" == *git* ]]; then
+##   RELEASE="unstable"
+## else
+##   RELEASE="stable"
+## fi
+## sed -e "0,/u*n*stable;/s//${RELEASE};/" \
+##     -i contrib/packages/*/debian.changelog
+## echo "done"
+## sed -e "s/pkgver=.*/pkgver=${VERSION}/g" \
+##     -i contrib/packages/*/PKGBUILD
 
 ##########
 
 echo "Creating configure script"
 autoreconf --verbose --force --install # -Wall
-echo "            ^ automake is supposed to fail, this is ok"
+echo "            ^ automake is supposed to fail, this is ok, no need to worry"
 echo ""
 echo "A ./configure file should be created."
 echo "Please proceed with calling './configure'"
