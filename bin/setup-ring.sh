d31fc32dc29b4fae937d3f511b4c2344c47f35ff
Florian Schintke <schintke@zib.de>
2018-06-04 19:58:03 +0200

    setup-ring.sh: use custom ports already to get the keys

--- bin/setup-ring.sh
+++ bin/setup-ring.sh
@@ -34,7 +34,7 @@ usage(){
 }
 
 start_ring(){
-    KEYS=`./bin/scalarisctl -t nostart -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
+    KEYS=`./bin/scalarisctl -t nostart -p $SCALARIS_UNITTEST_PORT -y $SCALARIS_UNITTEST_YAWS_PORT -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
 
     export SCALARIS_ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}] -scalaris verbatim $SESSIONKEY"
     idx=0
