1b5eedb371a44061867709a1c075cee36cd6df8a
Thorsten Schuett <schuett@zib.de>
2018-06-06 15:26:11 +0200

    setup-ring: switch to client start mode
    client does not start a yaws server. it prevents collisions with other processes

--- bin/setup-ring.sh
+++ bin/setup-ring.sh
@@ -34,7 +34,7 @@ usage(){
 }
 
 start_ring(){
-    KEYS=`./bin/scalarisctl -t nostart -p $SCALARIS_UNITTEST_PORT -y $SCALARIS_UNITTEST_YAWS_PORT -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
+    KEYS=`./bin/scalarisctl -t client -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
 
     export SCALARIS_ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}] -scalaris verbatim $SESSIONKEY"
     idx=0
@@ -60,7 +60,7 @@ start_ring(){
     let TESTPORT=$SCALARIS_UNITTEST_PORT+$RINGSIZE
     let YAWSPORT=$SCALARIS_UNITTEST_YAWS_PORT+$RINGSIZE
 
-    ./bin/scalarisctl -t nostart  -p $TESTPORT -y $YAWSPORT -e "-noinput -eval \"jsonclient:wait_for_ring_size($RINGSIZE, 10000, {127,0,0,1}, $SCALARIS_UNITTEST_YAWS_PORT), halt(0).\"" start
+    ./bin/scalarisctl -t client -e "-noinput -eval \"jsonclient:wait_for_ring_size($RINGSIZE, 10000, {127,0,0,1}, $SCALARIS_UNITTEST_YAWS_PORT), halt(0).\"" start
 }
 
 stop_ring(){
