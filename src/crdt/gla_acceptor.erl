3e5e3eb0c8cdca64bfc77326f68f2a54c3b36c9b
Jan Skrzypczak <skrzypczak@zib.de>
2018-06-29 17:22:01 +0200

    crdt: (WIP) implementation of generalized lattice agreement
    Implementation of base protocol is semi-complete. Howerver, notifying
    a client once an update is learnerd is not implemented yet (thus,
    clients wait forever right now).

---
+++ src/crdt/gla_acceptor.erl
@@ -0,0 +1,169 @@
+% @copyright 2012-2018 Zuse Institute Berlin,
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Jan Skrzypczak
+%% @doc    Acceptor implementation from "Generalized Lattice Agreement"
+%%  TODO:
+%%      - right now proposerlist is lazily extended
+%% @end
+-module(gla_acceptor).
+-author('skrzypczak@zib.de').
+
+%-define(TRACE(X,Y), ct:pal(X,Y)).
+-define(TRACE(X,Y), ok).
+-include("scalaris.hrl").
+
+-define(PDB, db_prbr).
+
+%%% functions for module where embedded into
+-export([on/2, init/1, close/1, close_and_delete/1]).
+-export([check_config/0]).
+-export([new/2]).
+-export([set_entry/2]).
+-export([get_entry/2]).
+-export([entry_key/1]).
+-export([entry_val/2]).
+-export([entry_set_val/2]).
+
+-export_type([state/0]).
+-export_type([entry/0]).
+
+-type state() :: {?PDB:db(), [comm:mypid()]}.
+
+%% so there are no longer any read denies, all reads succeed.
+-type entry() :: {
+                   any(), %% key
+                   any() %% value
+                 }.
+
+%% Messages to expect from this module
+-spec msg_get_val_reply(comm:mypid(), ?RT:key(), crdt:crdt()) -> ok.
+msg_get_val_reply(Client, Key, UpdatedVal) ->
+    comm:send(Client, {proposal_val_reply, Key, UpdatedVal}).
+
+-spec msg_ack_reply(comm:mypid(), ?RT:key(), pr:pr(), crdt:crdt()) -> ok.
+msg_ack_reply(Client, Key, ProposalNumber, ProposalValue) ->
+    comm:send(Client, {ack_reply, Key, ProposalNumber, ProposalValue}).
+
+-spec msg_learner_ack_reply(comm:mypid(), ?RT:key(), crdt:crdt_module(), pr:pr(), crdt:crdt(), any()) -> ok.
+msg_learner_ack_reply(Client, Key, DataType, ProposalNumber, ProposalValue, ProposerId) ->
+    comm:send(Client, {learner_ack_reply, Key, DataType, ProposalNumber, ProposalValue, ProposerId}).
+
+-spec msg_nack_reply(comm:mypid(), ?RT:key(), pr:pr(), crdt:crdt()) -> ok.
+msg_nack_reply(Client, Key, ProposalNumber, ProposalValue) ->
+    comm:send(Client, {nack_reply, Key, ProposalNumber, ProposalValue}).
+
+
+%% initialize: return initial state.
+-spec init(atom() | tuple()) -> state().
+init(DBName) -> {?PDB:new(DBName), []}.
+
+%% @doc Closes the given DB (it may be recoverable using open/1 depending on
+%%      the DB back-end).
+-spec close(state()) -> true.
+close(_State={TableName, _}) -> ?PDB:close(TableName).
+
+%% @doc Closes the given DB and deletes all contents (this DB can thus not be
+%%      re-opened using open/1).
+-spec close_and_delete(state()) -> true.
+close_and_delete(_State={TableName, _}) -> ?PDB:close_and_delete(TableName).
+
+-spec on(tuple(), state()) -> state().
+on({gla_acceptor, get_proposal_value, _Cons, Proposer, Key, DataType, UpdateFun}, State={TableName, _ProposerList}) ->
+    ?TRACE("gla_acceptor:update: ~p ~p ", [Key, Proposer]),
+    Entry = get_entry(Key, TableName),
+
+    Keys = lists:sort(replication:get_keys(Key)),
+    Tmp = lists:dropwhile(fun(E) -> E =/= Key end, Keys),
+    ThisReplicaId = length(Keys) - length(Tmp) + 1,
+
+    CVal = entry_val(Entry, DataType),
+    NewCVal = DataType:apply_update(UpdateFun, ThisReplicaId, CVal),
+    NewEntry = entry_set_val(Entry, NewCVal),
+    _ = set_entry(NewEntry, TableName),
+
+    msg_get_val_reply(Proposer, Key, NewCVal),
+    trace_mpath:log_info(self(), {acceptor_update,
+                                  key, Key,
+                                  old_value, CVal,
+                                  new_value, NewCVal}),
+    State;
+
+
+on({gla_acceptor, propose, _Cons, Proposer, Key, ProposalNumber, ProposedValue, DataType},
+   _State={TableName, ProposerList}) ->
+    ?TRACE("crdt_acceptor:propose: ~p ~p ~n ~p ~p", [Key, Proposer, ProposalNumber, ProposedValue]),
+    NewProposerList = case lists:member(Proposer, ProposerList) of
+                          true -> ProposerList;
+                          false -> [Proposer | ProposerList]
+                      end,
+
+    Entry = get_entry(Key, TableName),
+    AcceptedValue = entry_val(Entry, DataType),
+
+    case DataType:lteq(AcceptedValue, ProposedValue) of
+        true ->
+            NewEntry = entry_set_val(Entry, ProposedValue),
+            _ = set_entry(NewEntry, TableName),
+
+            _ = [msg_learner_ack_reply(Learner, Key, DataType, ProposalNumber, ProposedValue, Proposer)
+                 || Learner <- NewProposerList], %% each proposer is also a learner
+            msg_ack_reply(Proposer, Key, ProposalNumber, ProposedValue);
+        false ->
+            NewEntry = entry_set_val(Entry, DataType:merge(AcceptedValue, ProposedValue)),
+            _ = set_entry(NewEntry, TableName),
+            msg_nack_reply(Proposer, Key, ProposalNumber, AcceptedValue)
+    end,
+
+    {TableName, NewProposerList}.
+
+-spec get_entry(any(), ?PDB:db()) -> entry().
+get_entry(Id, TableName) ->
+    case ?PDB:get(TableName, Id) of
+        {}    -> new(Id);
+        Entry -> Entry
+    end.
+
+-spec set_entry(entry(), ?PDB:db()) -> ?PDB:db().
+set_entry(NewEntry, TableName) ->
+    _ = ?PDB:set(TableName, NewEntry),
+    TableName.
+
+
+-spec new(any()) -> entry().
+new(Key) ->
+    new(Key, gla_bottom).
+
+-spec new(any(), any()) -> entry().
+new(Key, Val) ->
+    {Key, _Value = Val}.
+
+-spec entry_key(entry()) -> any().
+entry_key(Entry) -> element(1, Entry).
+-spec entry_val(entry()) -> crdt:crdt() | gla_bottom.
+entry_val(Entry) -> element(2, Entry).
+-spec entry_val(entry(), crdt:crdt_module()) -> crdt:crdt().
+entry_val(Entry, DataType) ->
+    case entry_val(Entry) of
+        gla_bottom -> DataType:new();
+        Any -> Any
+    end.
+-spec entry_set_val(entry(), any()) -> entry().
+entry_set_val(Entry, Value) -> setelement(2, Entry, Value).
+
+
+%% @doc Checks whether config parameters exist and are valid.
+-spec check_config() -> boolean().
+check_config() -> true.
+
---
+++ src/crdt/gla_proposer.erl
@@ -0,0 +1,446 @@
+% @copyright 2012-2018 Zuse Institute Berlin,
+
+%   Licensed under the Apache License, Version 2.0 (the "License");
+%   you may not use this file except in compliance with the License.
+%   You may obtain a copy of the License at
+%
+%       http://www.apache.org/licenses/LICENSE-2.0
+%
+%   Unless required by applicable law or agreed to in writing, software
+%   distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
+%% @author Jan Skrzypczak <skrzypczak@zib.de>
+%% @doc    Proposer + Learner implementation from "Generalized Lattice Agreement"
+%% @end
+-module(gla_proposer).
+-author('skrzypczak.de').
+
+-define(PDB, pdb).
+%-define(TRACE(X,Y), ct:pal(X,Y)).
+-define(TRACE(X,Y), ok).
+
+-define(ROUTING_DISABLED, config:read(routing_disabled)).
+
+-include("scalaris.hrl").
+
+-behaviour(gen_component).
+
+-export([write/5]).
+-export([write_eventual/5]).
+-export([read/5]).
+-export([read_eventual/5]).
+
+-export([start_link/3]).
+-export([init/1, on/2]).
+
+-type state() :: { ?PDB:tableid(),
+                   dht_node_state:db_selector(),
+                   non_neg_integer(), %% period this process is in
+                   [any()] %% list of known proposers
+                 }.
+
+%% TODO: how to get list of all proposers?
+
+-type entry() :: {?RT:key(), %% key
+                  crdt:crdt_module(), %% data type
+                  [any()], %% clients waiting for reply TODO use this or keep close to paper
+                  active | passive,
+                  non_neg_integer(), %% ack count
+                  non_neg_integer(), %% nack count
+                  non_neg_integer(), %% active proposal number
+                  crdt:crdt(), %% proposed value
+                  crdt:crdt(), %% buffered value
+                  crdt:crdt() %% output value
+                 }.
+
+-type learner_entry() :: {{learner, ?RT:key()},
+                           crdt:crdt_module(),
+                           crdt:crdt(),
+                           [{any(), non_neg_integer()}]}.
+
+-include("gen_component.hrl").
+
+
+-spec start_link(pid_groups:groupname(), pid_groups:pidname(), dht_node_state:db_selector())
+                -> {ok, pid()}.
+start_link(DHTNodeGroup, Name, DBSelector) ->
+    gen_component:start_link(?MODULE, fun ?MODULE:on/2, DBSelector,
+                             [{pid_groups_join_as, DHTNodeGroup, Name}]).
+
+-spec init(dht_node_state:db_selector()) -> state().
+init(DBSelector) ->
+    {?PDB:new(?MODULE, [set]), DBSelector, 0, []}.
+
+
+%%%% API
+
+-spec write(pid_groups:pidname(), comm:erl_local_pid(), ?RT:key(), crdt:crdt_module(), crdt:update_fun()) -> ok.
+write(CSeqPidName, Client, Key, DataType, UpdateFun) ->
+    start_request(CSeqPidName, {req_start, {write, strong, Client, Key, DataType, UpdateFun}}).
+
+-spec read(pid_groups:pidname(), comm:erl_local_pid(), ?RT:key(), crdt:crdt_module(), crdt:query_fun()) -> ok.
+read(CSeqPidName, Client, Key, DataType, QueryFun) ->
+    start_request(CSeqPidName, {req_start, {read, strong, Client, Key, DataType, QueryFun}}).
+
+%% eventual operations are not really supported -> make stronger ops
+-spec write_eventual(pid_groups:pidname(), comm:erl_local_pid(), ?RT:key(), crdt:crdt_module(), crdt:update_fun()) -> ok.
+write_eventual(CSeqPidName, Client, Key, DataType, UpdateFun) ->
+    write(CSeqPidName, Client, Key, DataType, UpdateFun).
+
+-spec read_eventual(pid_groups:pidname(), comm:erl_local_pid(), ?RT:key(), crdt:crdt_module(), crdt:query_fun()) -> ok.
+read_eventual(CSeqPidName, Client, Key, DataType, QueryFun) ->
+    read(CSeqPidName, Client, Key, DataType, QueryFun).
+
+
+-spec start_request(pid_groups:pidname(), comm:message()) -> ok.
+start_request(CSeqPidName, Msg) ->
+    Pid = pid_groups:find_a(CSeqPidName),
+    trace_mpath:log_info(self(), {start_request, request, Msg}),
+    comm:send_local(Pid, Msg).
+
+
+-spec on(comm:message(), state()) -> state().
+
+
+on({req_start, {read, strong, Client, Key, DataType, QueryFun}}, State) ->
+    This = comm:reply_as(comm:this(), 6, {write_done, Client, Key, DataType, QueryFun, '_'}),
+    gen_component:post_op({req_start, {write, strong, This, Key, DataType, fun crdt:update_noop/2}}, State);
+
+on({write_done, Client, Key, DataType, QueryFun, _Msg}, State) ->
+    This = comm:reply_as(comm:this(), 5, {apply_query, Client, DataType, QueryFun, '_'}),
+    gen_component:post_op({get_learnt_value, Key, This}, State);
+
+on({apply_query, Client, DataType, QueryFun, {ok, LearntValue}}, State) ->
+    inform_client(read_done, Client, DataType:apply_query(QueryFun, LearntValue)),
+    State;
+
+%%%%% strong consistent write
+
+%% Start request... first retrieve value to propose as a crdt only supplies an update fun
+on({req_start, {write, strong, Client, Key, DataType, UpdateFun}}, State) ->
+    NewState =
+        case get_entry(Key, tablename(State)) of
+            undefined ->
+                NewEntry = new_entry(Key, DataType),
+                save_entry(NewEntry, tablename(State)),
+                add_proposer(State, comm:this());
+            _ -> State
+        end,
+
+    Msg = {gla_acceptor, get_proposal_value, '_', comm:this(), key, DataType, UpdateFun},
+    send_to_local_replica(Key, Msg),
+
+    NewState;
+
+%% ReceiveValue
+on({proposal_val_reply, Key, ProposalValue}, State) ->
+    ProposerList = proposers(State),
+    [comm:send(Proposer, {internal_receive, Key, ProposalValue})
+     || Proposer <- ProposerList],
+
+    State;
+
+%% Internal Receive
+on({internal_receive, Key, PropVal}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    DataType = entry_datatype(Entry),
+    NewBufVal = DataType:merge(entry_bufval(Entry), PropVal),
+    NewEntry = entry_set_bufval(Entry, NewBufVal),
+    _ = save_entry(NewEntry, tablename(State)),
+
+    gen_component:post_op({propose, Key, _ForeceProposal=true}, State);
+
+%% Propose
+on({propose, Key, ForceProposal}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    DataType = entry_datatype(Entry),
+    PropVal = entry_propval(Entry),
+    BufVal = entry_bufval(Entry),
+    NewPropVal = DataType:merge(PropVal, BufVal),
+    case entry_status(Entry) =:= passive
+        andalso (ForceProposal orelse DataType:lt(PropVal, NewPropVal)) of
+        true ->
+            NewPropNum = entry_propnum(Entry) + 1,
+            NewEntry1 = entry_set_ackcount(Entry, 0),
+            NewEntry2 = entry_set_nackcount(NewEntry1, 0),
+            NewEntry3 = entry_set_propnum(NewEntry2, NewPropNum),
+            NewEntry4 = entry_set_propval(NewEntry3, NewPropVal),
+            NewEntry5 = entry_set_bufval(NewEntry4, DataType:new()),
+            NewEntry6 = entry_set_status(NewEntry5, active),
+            _ = save_entry(NewEntry6, tablename(State)),
+            Msg =  {gla_acceptor, propose, '_', comm:this(), key, NewPropNum, NewPropVal, DataType},
+            send_to_all_replicas(Key, Msg),
+
+            State;
+        false ->
+            State
+    end;
+
+%% Proposer Ack
+on({ack_reply, Key, ProposalNumber, _ProposalValue}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    case entry_propnum(Entry) =:= ProposalNumber of
+        true ->
+            NewAckCount = entry_ackcount(Entry) + 1,
+            NewEntry = entry_set_ackcount(Entry, NewAckCount),
+            _ = save_entry(NewEntry, tablename(State)),
+            gen_component:post_op({refine, Key}, State);
+        false -> State
+    end;
+
+%% Proposer Nack
+on({nack_reply, Key, ProposalNumber, ProposalValue}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    DataType = entry_datatype(Entry),
+    case entry_propnum(Entry) =:= ProposalNumber of
+        true ->
+            NewAckCount = entry_nackcount(Entry) + 1,
+            NewProposalValue = DataType:merge(ProposalValue, entry_propval(Entry)),
+            NewEntry = entry_set_nackcount(Entry, NewAckCount),
+            NewEntry2 = entry_set_propval(NewEntry, NewProposalValue),
+            _ = save_entry(NewEntry2, tablename(State)),
+            gen_component:post_op({refine, Key}, State);
+        false -> State
+    end;
+
+%% Proposer Refine
+on({refine, Key}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    case entry_status(Entry) =:= active andalso
+         entry_nackcount(Entry) > 0 andalso
+         replication:quorum_accepted(entry_nackcount(Entry) + entry_ackcount(Entry)) of
+        true ->
+            NewPropNum = entry_propnum(Entry) + 1,
+            DataType = entry_datatype(Entry),
+            PropVal = entry_propval(Entry),
+            NewEntry1 = entry_set_ackcount(Entry, 0),
+            NewEntry2 = entry_set_nackcount(NewEntry1, 0),
+            NewEntry3 = entry_set_propnum(NewEntry2, NewPropNum),
+            _ = save_entry(NewEntry3, tablename(State)),
+            Msg =  {gla_acceptor, propose, '_', comm:this(), key, NewPropNum, PropVal, DataType},
+            send_to_all_replicas(Key, Msg),
+            State;
+        false ->
+            gen_component:post_op({decide, Key}, State)
+    end;
+
+%% Proposer Decide
+on({decide, Key}, State) ->
+    Entry = get_entry(Key, tablename(State)),
+    case entry_status(Entry) =:= active andalso
+         replication:quorum_accepted(entry_ackcount(Entry)) of
+        true ->
+            PropVal = entry_propval(Entry),
+            NewEntry = entry_set_outval(Entry, PropVal),
+            NewEntry2 = entry_set_status(NewEntry, passive),
+            _ = save_entry(NewEntry2, tablename(State)),
+            gen_component:post_op({propose, Key, _ForceProposal=false}, State);
+        false ->
+            State
+    end;
+
+%% Learner ack
+on({learner_ack_reply, Key, DataType, ProposalNumber, ProposalValue, ProposerId}, State) ->
+    Entry = case get_entry({learner, Key}, tablename(State)) of
+                undefined -> new_learner_entry(Key, DataType);
+                E -> E
+            end,
+    NewEntry = learner_add_vote(Entry, ProposerId, ProposalNumber),
+    VoteCount = learner_votes(NewEntry, ProposerId, ProposalNumber),
+    Learnt = learner_learnt(NewEntry),
+    NewEntry2 =
+        case replication:quorum_accepted(VoteCount) andalso
+            DataType:lteq(Learnt, ProposalValue) of
+            true ->
+                %% TODO what clients do we notify here?
+                ct:pal("learner ~p learnt value ~p", [comm:this(), ProposalValue]),
+                learner_set_learnt(NewEntry, ProposalValue);
+            false ->
+                NewEntry
+        end,
+
+    _ = save_entry(NewEntry2, tablename(State)),
+
+    %% lazily complete lists of active proposers
+    add_proposer(State, ProposerId);
+
+on({get_learnt_value, Key, Client}, State) ->
+    Ret = case get_entry({learner, Key}, tablename(State)) of
+            undefined -> {fail, not_found};
+            Entry -> {ok, learner_learnt(Entry)}
+        end,
+
+    comm:send(Client, Ret),
+    State;
+
+%% Internal to get local acceptor process
+on({local_range_req, Key, Message, {get_state_response, LocalRange}}, State) ->
+    Keys = replication:get_keys(Key),
+
+    LocalKeys = lists:filter(fun(K) -> intervals:in(K, LocalRange) end, Keys),
+
+    K = case LocalKeys of
+        [] ->
+            ?TRACE("cannot send locally ~p ~p ", [Keys, LocalRange]),
+            %% the local dht node is not responsible for any replca... route to
+            %% random replica
+            Idx = randoms:rand_uniform(1, length(Keys)+1),
+            lists:nth(Idx, Keys);
+        [H|_] ->
+            %% use replica managed by local dht node
+            H
+        end,
+
+    Dest = pid_groups:find_a(routing_table),
+    LookupEnvelope = dht_node_lookup:envelope(3, setelement(5, Message, K)),
+    comm:send_local(Dest, {?lookup_aux, K, 0, LookupEnvelope}),
+    State.
+
+%%%%%% internal helper
+
+-spec send_to_local_replica(?RT:key(), tuple()) -> ok.
+send_to_local_replica(Key, Message) ->
+    %% assert element(3, message) =:= '_'
+    %% assert element(5, message) =:= key
+    send_to_local_replica(Key, Message, not ?ROUTING_DISABLED).
+
+-spec send_to_local_replica(?RT:key(), tuple(), boolean()) -> ok.
+send_to_local_replica(Key, Message, _Routing=true) ->
+    LocalDhtNode = pid_groups:get_my(dht_node),
+    This = comm:reply_as(comm:this(), 4, {local_range_req, Key, Message, '_'}),
+    comm:send_local(LocalDhtNode, {get_state, This, my_range});
+
+send_to_local_replica(Key, Message, _Routing=false) ->
+    LocalDhtNode = pid_groups:get_my(dht_node),
+    FilledMsg = setelement(5, setelement(3, Message, true), Key),
+    comm:send_local(LocalDhtNode, FilledMsg).
+
+-spec send_to_all_replicas(?RT:key(), tuple()) -> ok.
+send_to_all_replicas(Key, Message) ->
+    %% assert element(3, message) =:= '_'
+    %% assert element(6, message) =:= key
+    send_to_all_replicas(Key, Message, not ?ROUTING_DISABLED).
+
+-spec send_to_all_replicas(?RT:key(), tuple(), boolean()) -> ok.
+send_to_all_replicas(Key, Message, _Routing=true) ->
+    Dest = pid_groups:find_a(routing_table),
+
+    _ = [begin
+            LookupEnvelope = dht_node_lookup:envelope(3, setelement(5, Message, K)),
+            comm:send_local(Dest, {?lookup_aux, K, 0, LookupEnvelope})
+         end
+        || K <- replication:get_keys(Key)],
+    ok;
+
+send_to_all_replicas(Key, Message, _Routing=false) ->
+    DhtNodes = pid_groups:find_all(dht_node),
+    FilledMsg = setelement(5, setelement(3, Message, true), Key), % cons and key
+    _ = [begin
+            Dest = comm:make_global(Node),
+            comm:send(Dest, FilledMsg)
+         end|| Node <- DhtNodes].
+
+-spec entry_key(entry())            -> any().
+entry_key(Entry)                    -> element(1, Entry).
+-spec entry_datatype(entry())       -> crdt:crdt_module().
+entry_datatype(Entry)               -> element(2, Entry).
+
+-spec entry_status(entry())         -> passive | active.
+entry_status(Entry)                 -> element(4, Entry).
+-spec entry_ackcount(entry())       -> non_neg_integer().
+entry_ackcount(Entry)               -> element(5, Entry).
+-spec entry_nackcount(entry())      -> non_neg_integer().
+entry_nackcount(Entry)              -> element(6, Entry).
+-spec entry_propnum(entry())        -> non_neg_integer().
+entry_propnum(Entry)                -> element(7, Entry).
+-spec entry_propval(entry())        -> crdt:crdt().
+entry_propval(Entry)                -> element(8, Entry).
+-spec entry_bufval(entry())        -> crdt:crdt().
+entry_bufval(Entry)                -> element(9, Entry).
+-spec entry_outval(entry())        -> crdt:crdt().
+entry_outval(Entry)                -> element(10, Entry).
+
+%% TODO specs
+entry_set_status(Entry, Status) -> setelement(4, Entry, Status).
+entry_set_ackcount(Entry, X)    -> setelement(5, Entry, X).
+entry_set_nackcount(Entry, X)   -> setelement(6, Entry, X).
+entry_set_propnum(Entry, X)     -> setelement(7, Entry, X).
+entry_set_propval(Entry, X)     -> setelement(8, Entry, X).
+entry_set_bufval(Entry, X)      -> setelement(9, Entry, X).
+entry_set_outval(Entry, X)      -> setelement(10, Entry, X).
+
+-spec learner_key(entry())      -> any().
+learner_key(Entry)              -> element(1, Entry).
+-spec learner_datatype(entry()) -> crdt:crdt_module().
+learner_datatype(Entry)         -> element(2, Entry).
+-spec learner_learnt(entry())   -> crdt:crdt().
+learner_learnt(Entry)           -> element(3, Entry).
+
+-spec learner_set_learnt(learner_entry(), crdt:crdt()) -> learner_entry().
+learner_set_learnt(Entry, Value) -> setelement(3, Entry, Value).
+
+-spec learner_votes(learner_entry(), any(), non_neg_integer()) -> non_neg_integer().
+learner_votes(Entry, Proposer, ProposalNumber) ->
+    Votes = element(4, Entry),
+    Key = {Proposer, ProposalNumber},
+    maps:get(Key, Votes).
+
+-spec learner_add_vote(learner_entry(), any(), non_neg_integer()) -> learner_entry().
+learner_add_vote(Entry, Proposer, ProposalNumber) ->
+    Votes = element(4, Entry),
+    Key = {Proposer, ProposalNumber},
+    NewVotes = maps:update_with(Key, fun(E) -> E + 1 end, 1, Votes),
+    setelement(4, Entry, NewVotes).
+
+
+-spec new_entry(?RT:key(), crdt:crdt_module()) -> entry().
+new_entry(Key, DataType) ->
+    {lowest_key(Key), DataType, [], passive, 0, 0, 0, DataType:new(), DataType:new(), DataType:new()}.
+
+-spec new_learner_entry(?RT:key(), crdt:crdt_module()) -> entry().
+new_learner_entry(Key, DataType) ->
+    {{learner, lowest_key(Key)}, DataType, DataType:new(), #{}}.
+
+
+-spec get_entry(?RT:key(), ?PDB:tableid()) -> entry() | undefined.
+get_entry({learner, Key}, TableName) ->
+    ?PDB:get({learner, lowest_key(Key)}, TableName);
+get_entry(Key, TableName) ->
+    ?PDB:get(lowest_key(Key), TableName).
+
+-spec save_entry(entry(), ?PDB:tableid()) -> ok.
+save_entry(NewEntry, TableName) ->
+    ?PDB:set(NewEntry, TableName).
+
+-spec lowest_key(?RT:key()) -> ?RT:key().
+lowest_key(Key) ->
+    hd(lists:sort(replication:get_keys(Key))).
+
+-spec tablename(state()) -> ?PDB:tableid().
+tablename(State) -> element(1, State).
+
+-spec proposers(state()) -> [any()].
+proposers(State) -> element(4, State).
+
+-spec add_proposer(state(), any()) -> state().
+add_proposer(State, Proposer) ->
+    Known =  element(4, State),
+    case lists:member(Proposer, Known) of
+        true -> State;
+        false -> setelement(4, State, [Proposer | Known])
+    end.
+
+
+inform_client(write_done, Client) ->
+    case is_tuple(Client) of
+        true ->
+            comm:send(Client, {write_done});
+        false ->
+            comm:send_local(Client, {write_done})
+    end.
+
+inform_client(read_done, Client, Value) ->
+    comm:send_local(Client, {read_done, Value}).
