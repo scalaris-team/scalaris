030840e75c39b4270d77640def2476afeb801f10
Florian Schintke <schintke@zib.de>
2019-01-08 10:00:39 +0100

    setup-ring.sh: support recover of a ring

--- bin/setup-ring.sh
+++ bin/setup-ring.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2018 Zuse Institute Berlin
+# Copyright 2018, 2019 Zuse Institute Berlin
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -34,6 +34,7 @@ usage(){
     echo "    start       - start a ring"
     echo "    stop        - stop a ring"
     echo "    kill        - kill a node"
+    echo "    recover     - recover a ring"
     exit $1
 }
 
@@ -41,6 +42,7 @@ start_ring(){
     KEYS=`./bin/scalarisctl -t client -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
 
     ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}] -scalaris verbatim $SESSIONKEY"
+    ADDITIONAL_PARAMETERS_BAK="$ADDITIONAL_PARAMETERS"
     idx=0
     for key in $KEYS; do
         let idx+=1
@@ -67,11 +69,53 @@ start_ring(){
         ./bin/scalarisctl -d -k $key -p $TESTPORT -y $YAWSPORT -t $STARTTYPE start $WITHSSL
 
     done
+    export SCALARIS_ADDITIONAL_PARAMETERS="$ADDITIONAL_PARAMETERS_BAK"
 
     let TESTPORT=$SCALARIS_UNITTEST_PORT+$RINGSIZE
     let YAWSPORT=$SCALARIS_UNITTEST_YAWS_PORT+$RINGSIZE
 
     ./bin/jsonclient -p $SCALARIS_UNITTEST_YAWS_PORT -r $RINGSIZE waitforringsize
+
+    # $SSL
+}
+
+recover_ring(){
+    KEYS=`./bin/scalarisctl -t client -e "-noinput -eval \"io:format('\n%~p', [api_rt:escaped_list_of_keys(api_rt:get_evenly_spaced_keys($RINGSIZE))]), halt(0)\"" start | grep % | cut -c 3- | rev | cut -c 2- | rev`
+
+    ADDITIONAL_PARAMETERS="-scalaris mgmt_server {{127,0,0,1},$SCALARIS_UNITTEST_PORT,mgmt_server} -scalaris known_hosts [{{127,0,0,1},$SCALARIS_UNITTEST_PORT,service_per_vm}] -scalaris verbatim $SESSIONKEY"
+    ADDITIONAL_PARAMETERS_BAK="$ADDITIONAL_PARAMETERS"
+    idx=0
+    for key in $KEYS; do
+        let idx+=1
+
+        STARTTYPE="recover"
+        if [ "x$idx" == x1 ]
+        then
+            TESTPORT=$SCALARIS_UNITTEST_PORT
+            YAWSPORT=$SCALARIS_UNITTEST_YAWS_PORT
+        else
+            let TESTPORT=$SCALARIS_UNITTEST_PORT+$idx-1
+            let YAWSPORT=$SCALARIS_UNITTEST_YAWS_PORT+$idx-1
+        fi
+
+        if [ -n $VMPREFIX ]
+        then
+            export SCALARIS_ADDITIONAL_PARAMETERS="$ADDITIONAL_PARAMETERS -scalaris vmname $VMPREFIX$idx"
+        else
+            export SCALARIS_ADDITIONAL_PARAMETERS="$ADDITIONAL_PARAMETERS"
+        fi
+
+        # to recover, we have to be located in the bin directory
+        (cd bin ; ./scalarisctl -d -k $key -p $TESTPORT -y $YAWSPORT -t $STARTTYPE start $WITHSSL)
+
+    done
+    export SCALARIS_ADDITIONAL_PARAMETERS="$ADDITIONAL_PARAMETERS_BAK"
+
+    let TESTPORT=$SCALARIS_UNITTEST_PORT+$RINGSIZE
+    let YAWSPORT=$SCALARIS_UNITTEST_YAWS_PORT+$RINGSIZE
+
+    ## TODO: also recover mgmt_server, so this check can be done
+    #./bin/jsonclient -p $SCALARIS_UNITTEST_YAWS_PORT -r $RINGSIZE waitforringsize
 
     # $SSL
 }
@@ -118,6 +162,9 @@ until [ -z "$1" ]; do
         kill)
             cmd="$1"
             shift;;
+        recover)
+            cmd="$1"
+            shift;;
         *)
             echo "Unknown parameter: $1."
             shift
@@ -138,6 +185,8 @@ case $cmd in
         stop_ring;;
     kill)
         kill_node;;
+    recover)
+        recover_ring;;
     *)
         echo "Unknown command: $cmd."
         usage 1;;
