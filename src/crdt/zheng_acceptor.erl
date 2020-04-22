04747da41a79ea5ca54a4de5e8d78a1df3f0222f
Jan Skrzypczak <skrzypczak@zib.de>
2020-04-22 14:06:04 +0200

    crdt: zhen: smaller messages
    Do not include query commands in to-learn lattice.
    Instead, replace all queries with a noop command.
    Once the noop command is learned at the current replica,
    apply the queries and notify clients of the result.

--- src/crdt/zheng_acceptor.erl
+++ src/crdt/zheng_acceptor.erl
@@ -49,7 +49,7 @@
 -define(get(X, State), get_field(?i(X), State)).
 -define(set(X, NewV, State), set_field(?i(X), State, NewV)).
 -define(union(X, ListOfElements, State), union_buffer(?i(X), ListOfElements, State)).
--define(add(X, Client, Fun, State), add_to_buffer(?i(X), Client, Fun, State)).
+-define(add(X, Cmd, State), add_to_buffer(?i(X), Cmd, State)).
 
 -define(rinc(X, State), inc_field(?r(X), State)).
 -define(rget(X, State), get_field(?r(X), State)).
@@ -69,6 +69,9 @@
 
 -record(pstate,
   {
+    cid = 0 :: non_neg_integer(), %% used to give command unique identifier
+
+    %% core protocol variables 
     seq = 0 :: non_neg_integer(),
     max_seq = -1 :: integer(),
 
@@ -78,25 +81,36 @@
     accept_val = new_buffer() :: buffer(),
     active = false :: boolean(),
 
+    %%% management of pending cmds to notify clients
+    to_notify_pending = [] :: [notify_cmd()], %% list before they are included in accept_val
+    wait_for_learned = dict:new() :: dict:dict(cmd_id(), [notify_cmd()]),
+
+    %% CRDT managment
     crdt_type = null :: null | crdt:crdt_module(),
     cmd_set = new_buffer() :: buffer(),
     crdt = null :: null | crdt:crdt(),
+
+    %% loop variables for agreement loop
+    loop_val = new_buffer :: buffer(),
+    loop_iter = 0 :: non_neg_integer(),
     replies = #replies{} :: #replies{} | done,
 
-    loop_val = null :: buffer(),
-    loop_iter = null :: non_neg_integer(),
-
+    %% for waiting in prop functions
     prop_wait_list = [] :: [tuple()]
   }).
 
-%% More sensible management of query commands... these do not have to be sent
+%% TODO More sensible management of query commands... these do not have to be sent
 %% to remotely as they are noops for updates?
+-type client() :: {any(), any()}. %% reqid and pid
+-type notify_cmd() :: {client(), query_cmd() | notify_ok}.
 
--type buffer() :: set:sets(buffer_element()).
--type buffer_element() ::
-  {comm:mypid(),
-    {qfun, crdt:query_fun() | [crdt:query_fun()]}
-    | [crdt:update_fun()]}.
+-type buffer() :: set:sets(cmd()).
+-type cmd() :: {cmd_id(), query_cmd() | update_cmd() | noop}.
+
+-type cmd_id() :: any().
+-type query_cmd() :: {qfun, crdt:query_fun() | [crdt:query_fun()]}.
+-type update_cmd() :: crdt:update_fun() | [crdt:update_fun()].
+
 
 %% initialize: return initial state.
 -spec init(atom() | tuple()) -> state().
@@ -114,18 +128,35 @@ close(State) -> ?PDB:close(State).
 close_and_delete(State) -> ?PDB:close_and_delete(State).
 
 -spec on(tuple(), state()) -> state().
+%% Procedure ReceiveValue
 on({zheng_acceptor, new_read, _Cons, Proposer, Key, ReqId, DataType, QueryFuns}, State) ->
-    comm:send_local(self(),
-      {zheng_acceptor, new_write, _Cons, Proposer, Key, ReqId, DataType, {qfun, QueryFuns}}),
+    ?TRACE("~p NEW QUERY COMMAND ~p ~p~n~p", [Key, Proposer, ReqId, QueryFuns]),
+    PState = get_pstate(Key, State),
+
+    {Cid, NP1} = get_new_id(Key, PState),
+    QueryCmd = new_query_cmd(Cid, QueryFuns),
+    Client = new_client(ReqId, Proposer),
+
+    NP2 = add_to_notify(Client, QueryCmd, NP1),
+    NP3 = ?add(buff_val, QueryCmd, NP2),
+    save_pstate(Key, NP3, State),
+
+    comm:send_local(self(), {zheng_acceptor, agree, Key, DataType}),
     State;
 
 
-%% Procedure ReceiveValue
 on({zheng_acceptor, new_write, _Cons, Proposer, Key, ReqId, DataType, UpdateFuns}, State) ->
-    ?TRACE("NEW WRITE ~p ~p", [Proposer, ReqId]),
+    ?TRACE("~p NEW WRITE COMMAND ~p ~p~n~p", [Key, Proposer, ReqId, UpdateFuns]),
     PState = get_pstate(Key, State),
-    NewPState = ?add(buff_val, {ReqId, Proposer}, UpdateFuns, PState),
-    save_pstate(Key, NewPState, State),
+
+    {Cid, NP1} = get_new_id(Key, PState),
+    UpdateCmd = new_update_cmd(Cid, UpdateFuns),
+    Client = new_client(ReqId, Proposer),
+
+    NP2 = add_to_notify(Client, UpdateCmd, NP1),
+    NP3 = ?add(buff_val, UpdateCmd, NP2),
+    save_pstate(Key, NP3, State),
+
     comm:send_local(self(), {zheng_acceptor, agree, Key, DataType}),
     State;
 
@@ -133,25 +164,39 @@ on({zheng_acceptor, new_write, _Cons, Proposer, Key, ReqId, DataType, UpdateFuns
 %% The following on-handlers implement the Agree Procedure in the paper
 on({zheng_acceptor, agree, Key, DataType}, State) ->
   PState = get_pstate(Key, State),
-  NP1 =
-    case ?get(crdt, PState) =:= null of
-        true ->
-          T = ?set(crdt, DataType:new(), PState),
-          ?set(crdt_type, DataType, T);
-        false ->
-          PState
-    end,
+  NP1 = init_pstate_if_not_already(PState, DataType),
   Buffered = ?get(buff_val, NP1),
-  case ?get(active, NP1) =:= false andalso
-    (not sets:is_empty(Buffered)  orelse ?get(max_seq, NP1) >= ?get(seq, NP1)) of
-    true ->
-      ?TRACE("START AGREE ~p ~p", [Key, {?get(active, NP1), Buffered, ?get(max_seq, NP1), ?get(seq, NP1)}]),
-      NP2 = ?set(active, true, NP1),
-      NP3 = ?union(accept_val, Buffered, NP2),
-      NP4 = ?set(buff_val, new_buffer(), NP3),
 
-      NewReplies = new_replies(),
-      NP5 = ?set(replies, NewReplies, NP4),
+  case ?get(active, NP1) =:= false andalso
+    (not is_empty(Buffered) orelse ?get(max_seq, NP1) >= ?get(seq, NP1)) of
+    true ->
+
+      %% As per otimization of the full paper, queries do not have be included
+      %% into the set (should not filter anything if notify buffer below is empty)
+      UpdateCmds = remove_query_commans(Buffered),
+      
+      %% If we included commands from clients in this buffer that needs to be
+      %% notified of the result as soon as the commands are learend, add a noop
+      %% with an ID. If this noop is learned, notify all clients.
+      ToNotifyList = ?get(to_notify_pending, NP1),
+      NP2 =
+        case ToNotifyList =:= [] of
+          true ->
+            ?union(accept_val, UpdateCmds, NP1);
+          false ->
+            {Cid, T1} = get_new_id(Key, NP1),
+            NoopCmd = new_noop(Cid),
+            T2 = ?union(accept_val, sets:add_element(NoopCmd, UpdateCmds), T1),
+            T3 = ?set(to_notify_pending, [], T2),
+            set_to_notify_after_cmd_learned(ToNotifyList, NoopCmd, T3)
+        end,
+
+      ?TRACE("START AGREE ~p~n~p", [Key, {?get(active, NP1), ?get(accept_val, NP2),
+        ?get(max_seq, NP1), ?get(seq, NP1)}]),
+
+      NP3 = ?set(active, true, NP2),
+      NP4 = ?set(buff_val, new_buffer(), NP3),
+      NP5 = ?set(replies, new_replies(), NP4),
       save_pstate(Key, NP5, State),
       comm:send_local(self(), {zheng_acceptor, agree_loop, Key, _Iteration=1}),
       State;
@@ -197,12 +242,9 @@ on({zheng_acceptor, agree_loop_collect, Key,
       NR1 = ?rinc(count, Replies),
       NR2 = 
         case ReplyType of
-          accept ->
-            ?rinc(accept_count, NR1);
-          reject ->
-            ?radd(reject_val, ReplyVal, NR1);
-          decide ->
-            ?radd(decide_val, ReplyVal, NR1)
+          accept -> ?rinc(accept_count, NR1);
+          reject -> ?radd(reject_val, ReplyVal, NR1);
+          decide -> ?radd(decide_val, ReplyVal, NR1)
         end,
       NP1 = ?set(replies, NR2, PState),
       ?TRACE("~p LOOP COLLECT~n~p", [Key, {CurSeqNum, CurIter, NR2}]),
@@ -245,7 +287,7 @@ on({zheng_acceptor, agree_loop_collect, Key,
 
 %%% prop function
 on({zheng_acceptor, prop, Key, SrcKey, DataType, Client, Val, Iteration, SeqNum, _Cons}, State) ->
-  ?TRACE("~p prop: receievd msg from ~p ", [Key, {SrcKey, Iteration, SeqNum}]),
+  ?TRACE("~p prop: receievd msg from~n~p ", [Key, {SrcKey, Iteration, SeqNum}]),
   PState = get_pstate(Key, State),
   Seq = ?get(seq, PState),
 
@@ -302,6 +344,9 @@ on({zheng_acceptor, prop_wait, Key}, State) ->
   save_pstate(Key, NP2, State),
   State.
 
+%%%%%%%%%%%%%%%%%%%%%
+%% Functions that execute part of the core protocol logic
+%%%%%%%%%%%%%%%%%%%%%
 eval_prop_wait(Key, SrcKey, Client, Val, Iteration, CurrentSeqNum, WaitForSeqNum, State) ->
   ?TRACE("~p prop: waiting ~p ~p", [Key,CurrentSeqNum, WaitForSeqNum]),
   case CurrentSeqNum < WaitForSeqNum of
@@ -328,16 +373,13 @@ eval_prop_wait(Key, SrcKey, Client, Val, Iteration, CurrentSeqNum, WaitForSeqNum
       false
   end.
 
-send_prop_reply(Client, Key, Msg) ->
-  comm:send(Client, {zheng_acceptor, agree_loop_collect, Key, Msg}).
-
 accept_loop_end(Key, LearnedVal, PState, State) -> 
   SeqNum = ?get(seq, PState),
   LearnedDict = ?get(learned_val, PState),
   AcceptedVal = ?get(accept_val, PState),
-
   NewLD = dict:store(SeqNum, LearnedVal, LearnedDict),
 
+  %% truncate acceted val ... line delta2 in paper
   NewAV =
     case dict:find(SeqNum - 1, NewLD) of
       {ok, Val} -> subtract(AcceptedVal, Val);
@@ -359,53 +401,52 @@ accept_loop_end(Key, LearnedVal, PState, State) ->
   NewLearned = subtract(LearnedVal, CrdtAppliedSet),
   LearnedAsList = sets:to_list(NewLearned),
   NCrdt = lists:foldl(
-                fun({_Client, Fun}, Acc) ->
-                  case Fun of
-                    {qfun, _} -> Acc;
-                    Fun -> apply_updates(DataType, Fun, ReplicaId, Acc)
-                  end
+                fun(Cmd, Acc) ->
+                  apply_updates(DataType, Cmd, ReplicaId, Acc)
                 end, Crdt, LearnedAsList),
   NP5 = ?set(crdt, NCrdt, NP4),
   NP6 = ?set(cmd_set, union(NewLearned, CrdtAppliedSet), NP5),
 
   %% notify all clients of learned commands and apply queries
-  [
-    begin
-      ?TRACE("~p Responding to request ~p", [Key, R]),
-      case Fun of
-        {qfun, F} ->
-          Result = apply_queries(DataType, F, NCrdt),
-          comm:send(Client, {read_result, ReqId, Result});
-        _ ->
-          comm:send(Client, {write_result, ReqId, ok})
-      end
-    end
-  || R = {{ReqId, Client}, Fun} <- LearnedAsList],
-
+  NP7 = notify_clients(Key, DataType, NCrdt, LearnedAsList, NP6),
 
   comm:send_local(self(), {zheng_acceptor, agree, Key, DataType}),
-  save_pstate(Key, NP6, State),
+  save_pstate(Key, NP7, State),
+  ?TRACE("~p RESPOND DONE seq number ~p~n CRDT VALUE ~p", [Key, ?get(seq, NP7) - 1, NCrdt]),
 
   %% must be executed immediately, so that sequence number is not missed
   %% at prop_wait, but we cannot use gen_component:post_op(?)
   on({zheng_acceptor, prop_wait, Key}, State), 
-  ?TRACE("~p RESPOND DONE seq number ~p", [Key, ?get(seq, NP6) - 1]),
-  NP6.
+  get_pstate(Key, State). %% retrieve state again as changes by prop_wait
 
-apply_updates(DataType, Funs, ReplicaId, Crdt) when is_list(Funs) ->
+
+%%%%%%%%%%%%%%%%%%%%%
+%% Application of learned functions on the CRDT
+%%%%%%%%%%%%%%%%%%%%%
+apply_updates(DataType, Cmd, ReplicaId, Crdt) ->
+  case is_noop(Cmd) orelse is_query(Cmd) of
+    true -> Crdt;
+    false -> apply_updates_helper(DataType, get_cmd_fun(Cmd), ReplicaId, Crdt)
+  end.
+
+apply_updates_helper(DataType, Funs, ReplicaId, Crdt) when is_list(Funs) ->
   lists:foldl(
     fun(UpdateFun, Acc) ->
-      DataType:apply_update(UpdateFun, ReplicaId, Acc)
+      apply_updates_helper(DataType, UpdateFun, ReplicaId, Acc)
     end, Crdt, lists:flatten(Funs));
-apply_updates(DataType, Fun, ReplicaId, Crdt) ->
+apply_updates_helper(DataType, Fun, ReplicaId, Crdt) ->
   DataType:apply_update(Fun, ReplicaId, Crdt).
 
--spec apply_queries(crdt:crdt_module(), [crdt:query_fun()] | crdt:query_fun(), crdt:crdt()) -> any() | [any()].
+-spec apply_queries(crdt:crdt_module(), [crdt:query_fun()] | crdt:query_fun(),
+    crdt:crdt()) -> any() | [any()].
 apply_queries(DataType, QueryFun, Crdt) when is_function(QueryFun) ->
     DataType:apply_query(QueryFun, Crdt);
 apply_queries(DataType, QueryFuns, Crdt) ->
    [apply_queries(DataType, Fun, Crdt) || Fun <- QueryFuns].
 
+%%%%%%%%%%%%%%%%%%%%%
+%% Helpers for sending messages
+%%%%%%%%%%%%%%%%%%%%%
 send_to_all_replicas(Key, Message, ConsLookupField, KeyFieldReplace) ->
   Dest = pid_groups:find_a(routing_table),
   [
@@ -417,14 +458,59 @@ send_to_all_replicas(Key, Message, ConsLookupField, KeyFieldReplace) ->
     end
   || K <- replication:get_keys(Key) ].
 
-get_replica_id(_Key) ->
-  % DOES NOT MATTER AS CRDT VALUES ARE NEVER MERGED
-  1.
-  %Keys = lists:sort(replication:get_keys(Key)),
-  %Tmp = lists:dropwhile(fun(E) -> E =/= Key end, Keys),
-  %length(Keys) - length(Tmp) + 1.
+send_prop_reply(Client, Key, Msg) ->
+  comm:send(Client, {zheng_acceptor, agree_loop_collect, Key, Msg}).
 
-%%%%%%%%%%% State management 
+get_replica_id(Key) ->
+  Keys = lists:sort(replication:get_keys(Key)),
+  Tmp = lists:dropwhile(fun(E) -> E =/= Key end, Keys),
+  length(Keys) - length(Tmp) + 1.
+
+notify_clients(_Key, DataType, Crdt, LearnedCmds, PState) ->
+  ListenDict = ?get(wait_for_learned, PState),
+  ?TRACE("~p Notify_clients ~p ~n~p", [_Key, LearnedCmds, ListenDict]),
+  NewListenDict =
+    lists:foldl(
+      fun(Cmd, DictAcc) ->
+        case dict:take(get_cmd_id(Cmd), DictAcc) of
+          error ->
+            DictAcc;
+          {ToNotify, NewDict} ->
+            notify_clients_helper(_Key, DataType, ToNotify, Crdt),
+            NewDict
+        end
+      end, ListenDict, LearnedCmds),
+
+  ?set(wait_for_learned, NewListenDict, PState).
+
+notify_clients_helper(_Key, DataType, ToNotifyList, Crdt) ->
+  [begin
+    ?TRACE("~p Responding to request ~p", [_Key, _R]),
+    case Fun of
+      {qfun, F} ->
+        Result = apply_queries(DataType, F, Crdt),
+        comm:send(Client, {read_result, ReqId, Result});
+      _ ->
+        comm:send(Client, {write_result, ReqId, ok})
+    end
+  end || _R = {{ReqId, Client}, Fun} <- ToNotifyList],
+  ok.
+
+%%%%%%%%%%%%%%%%%%%%%
+%% PState management
+%%%%%%%%%%%%%%%%%%%%%
+%% Initialize the CRDT and CRDT type if it is not already.
+%% This saves of from including the CRDT module in the majority of messages.
+-spec init_pstate_if_not_already(#pstate{}, crdt:crdt_module()) -> #pstate{}.
+init_pstate_if_not_already(PState, DataType) ->
+  case ?get(crdt, PState) =:= null of
+    true ->
+      T = ?set(crdt, DataType:new(), PState),
+      ?set(crdt_type, DataType, T);
+    false ->
+      PState
+  end.
+
 -spec get_pstate(any(), state()) -> #pstate{}.
 get_pstate(Key, State) ->
   case ?PDB:get(tablename(State), Key) of
@@ -438,20 +524,30 @@ get_pstate(Key, State) ->
 save_pstate(Key, PState, State) ->
   ?PDB:set(tablename(State), {Key, PState}).
 
+-spec get_new_id(any(), #pstate{}) -> {cmd_id(), #pstate{}}.
+get_new_id(Key, PState) ->
+  Cid = ?get(cid, PState) + 1,
+  NP1 = ?set(cid, Cid, PState),
+  Rid = get_replica_id(Key),
+  {{Rid, Cid}, NP1}.
+
+-spec new_replies() -> #replies{}.
+new_replies() -> #replies{}.
+
+%% generic record access
 -spec union_buffer(non_neg_integer(), buffer(), tuple()) -> tuple().
 union_buffer(BufIdx, BufferToMerge, PState) ->
   NewBuf = union(BufferToMerge, element(BufIdx, PState)),
   setelement(BufIdx, PState, NewBuf).
 
--spec add_to_list(non_neg_integer(), buffer(), tuple()) -> tuple().
-add_to_list(BufIdx, NewBuffer, PState) ->
-  NewBuf = [NewBuffer | element(BufIdx, PState)],
+-spec add_to_list(non_neg_integer(), any(), tuple()) -> tuple().
+add_to_list(BufIdx, NewListEle, PState) ->
+  NewBuf = [NewListEle | element(BufIdx, PState)],
   setelement(BufIdx, PState, NewBuf).
 
--spec add_to_buffer(non_neg_integer(), {any(), comm:mypid()}, crdt:update_fun(), tuple()) -> tuple().
-add_to_buffer(BufIdx, Client, Fun, PState) ->
-  BufEle = {Client, Fun},
-  NewBuf = add(BufEle, element(BufIdx, PState)),
+-spec add_to_buffer(non_neg_integer(), cmd(), tuple()) -> tuple().
+add_to_buffer(BufIdx, Cmd, PState) ->
+  NewBuf = add(Cmd, element(BufIdx, PState)),
   setelement(BufIdx, PState, NewBuf).
 
 -spec get_field(non_neg_integer(), tuple()) -> any().
@@ -467,11 +563,63 @@ inc_field(FieldIdx, PState) ->
 set_field(FieldIdx, PState, NewValue) ->
   setelement(FieldIdx, PState, NewValue).
 
--spec new_replies() -> #replies{}.
-new_replies() -> #replies{}.
+%% more specialized acess
+add_to_notify(Client, Cmd, PState) ->
+  NotifyCmd = to_notify_cmd(Client, Cmd),
+  ToNotifyList = ?get(to_notify_pending, PState),
+  ?set(to_notify_pending, [NotifyCmd | ToNotifyList], PState).
 
-%% Buffer management
--spec add(buffer_element(), buffer()) -> buffer().
+set_to_notify_after_cmd_learned(NotifyList, WaitForCmd, PState) ->
+  ListenDict = ?get(wait_for_learned, PState),
+  CmdId = get_cmd_id(WaitForCmd),
+  ?set(wait_for_learned, dict:store(CmdId, NotifyList, ListenDict), PState).
+
+%%%%%%%%%%%%%%%%%%%%%
+%% Command and Command set managment
+%%%%%%%%%%%%%%%%%%%%%
+-spec new_client(any(), any()) -> client().
+new_client(ReqId, Client) -> {ReqId, Client}.
+
+-spec new_update_cmd(cmd_id(), crdt:update_fun()) -> cmd().
+new_update_cmd(Id, Fun) -> {Id, Fun}.
+
+-spec new_query_cmd(cmd_id(), crdt:query_fun()) -> cmd().
+new_query_cmd(Id, Fun) -> {Id, {qfun, Fun}}.
+
+-spec new_noop(cmd_id()) -> cmd().
+new_noop(Id) -> {Id, noop}.
+
+%% Notification commands are used only for client notification
+%% We do not need to store the update function as the resutl is always ok.
+%% Query functions are applied upon learning. We do not need old ID because
+%% they are never merged with other buffers.
+-spec to_notify_cmd(client(), cmd()) -> notify_cmd().
+to_notify_cmd(Client, Cmd = {_ID, Fun}) ->
+  case is_query(Cmd) of
+    true ->
+      {Client, Fun};
+    false ->
+      {Client, notify_ok}
+  end.
+
+-spec get_cmd_id(cmd()) -> cmd_id().
+get_cmd_id({Id, _}) -> Id.
+
+-spec get_cmd_fun(cmd()) -> any().
+get_cmd_fun({_Id, Fun}) -> Fun.
+
+-spec is_noop(cmd()) -> boolean().
+is_noop({_Id, noop}) -> true;
+is_noop(_) -> false.
+
+-spec is_query(cmd()) -> boolean().
+is_query({_Id, {qfun, _Fun}}) -> true;
+is_query(_) -> false.
+
+-spec is_update(cmd()) -> boolean().
+is_update(Cmd) -> not is_query(Cmd).
+
+-spec add(cmd(), buffer()) -> buffer().
 add(Element, Buff) -> sets:add_element(Element, Buff).
 
 -spec subtract(buffer(), buffer()) -> buffer().
@@ -479,6 +627,9 @@ subtract(A, B) -> sets:subtract(A, B).
 
 -spec is_subset(buffer(), buffer()) -> boolean().
 is_subset(A, B) -> sets:is_subset(A, B).
+
+-spec is_empty(buffer()) -> boolean().
+is_empty(A) -> sets:is_empty(A).
 
 -spec union([buffer()]) -> buffer().
 union(ListOfBuffers) -> sets:union(ListOfBuffers).
@@ -489,11 +640,15 @@ union(A, B) -> sets:union(A, B).
 -spec new_buffer() -> buffer().
 new_buffer() -> sets:new().
 
-%%%%%%%%%
+-spec remove_query_commans(buffer()) -> buffer().
+remove_query_commans(Buffer) -> sets:filter(fun is_update/1, Buffer).
+
+%%%%%%%%%%%%%%%%%%%%%
+%% Access of module State
+%%%%%%%%%%%%%%%%%%%%%
 
 -spec tablename(state()) -> ?PDB:db().
 tablename(State) -> element(1, State).
-
 
 -spec get_load(state()) -> non_neg_integer().
 get_load(State) -> ?PDB:get_load(State).
@@ -518,11 +673,12 @@ tab2list_raw(State) ->
 -spec check_config() -> boolean().
 check_config() -> true.
 
-
-%% DEBUGGING
+%%%%%%%%%%%%%%%%%%%%%
+%% Application of learned functions on the CRDT
+%%%%%%%%%%%%%%%%%%%%%
 print_pstate(Key, PState) ->
   print_pstate("", Key, PState).
-print_pstate(Msg, Key, PState) ->
+print_pstate(_Msg, _Key, _PState) ->
   ?TRACE(
     "Msg: ~p~n"
     "Key:~p~n~n" ++
@@ -537,17 +693,17 @@ print_pstate(Msg, Key, PState) ->
     "crdt:~p~n" ++
     "replies:~p~n"
     ,
-    [Msg, Key,
-    ?get(seq, PState),
-    ?get(max_seq, PState),
-    ?get(buff_val, PState),
-    ?get(learned_val, PState),
-    ?get(accept_val, PState),
-    ?get(active, PState),
+    [_Msg, _Key,
+    ?get(seq, _PState),
+    ?get(max_seq, _PState),
+    ?get(buff_val, _PState),
+    ?get(learned_val, _PState),
+    ?get(accept_val, _PState),
+    ?get(active, _PState),
 
-    ?get(cmd_set, PState),
-    ?get(crdt_type, PState),
-    ?get(crdt, PState),
-    ?get(replies, PState)
+    ?get(cmd_set, _PState),
+    ?get(crdt_type, _PState),
+    ?get(crdt, _PState),
+    ?get(replies, _PState)
     ]).
 
